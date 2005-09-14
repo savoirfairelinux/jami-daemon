6884423742f80bdc9b698fa56fe6daa12db51a61
yanmorin <yanmorin>
2005-09-14 19:38:52 +0000

    protocol contains functions that be use when the grammar will be ok

---
+++ src/gui/server/protocol.tab.h
@@ -0,0 +1,78 @@
+/* A Bison parser, made by GNU Bison 1.875c.  */
+
+/* Skeleton parser for Yacc-like parsing with Bison,
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+/* As a special exception, when this file is copied by Bison into a
+   Bison output file, you may use that output file without restriction.
+   This special exception was added by the Free Software Foundation
+   in version 1.24 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     AROBASE = 258,
+     SIP_URL_PREFIX = 259,
+     CMD_CALL = 260,
+     CMD_CONFIG = 261,
+     CMD_HANGUP = 262,
+     CMD_HOLD = 263,
+     CMD_TRANSFER = 264,
+     CMD_QUIT = 265,
+     TOKEN_VOICEMAIL = 266,
+     TOKEN_RECEPTION = 267,
+     TOKEN_ALL = 268,
+     TOKEN_GET = 269,
+     TOKEN_SET = 270,
+     NUMBER = 271,
+     USERNAME = 272
+   };
+#endif
+#define AROBASE 258
+#define SIP_URL_PREFIX 259
+#define CMD_CALL 260
+#define CMD_CONFIG 261
+#define CMD_HANGUP 262
+#define CMD_HOLD 263
+#define CMD_TRANSFER 264
+#define CMD_QUIT 265
+#define TOKEN_VOICEMAIL 266
+#define TOKEN_RECEPTION 267
+#define TOKEN_ALL 268
+#define TOKEN_GET 269
+#define TOKEN_SET 270
+#define NUMBER 271
+#define USERNAME 272
+
+
+
+
+#if ! defined (YYSTYPE) && ! defined (YYSTYPE_IS_DECLARED)
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+
+
+
+
