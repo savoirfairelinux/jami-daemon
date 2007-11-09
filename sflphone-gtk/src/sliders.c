3944ac8be586bcd879f878cf99690839767bd906
Pierre-Luc Beaudoin <pierre-luc.beaudoin@savoirfairelinux.com>
2007-11-09 16:23:42 -0500

    Volume icons now represent the current value

--- sflphone-gtk/src/sliders.c
+++ sflphone-gtk/src/sliders.c
@@ -21,56 +21,80 @@
 #include <dbus.h>
 #include <string.h>
 
-gdouble speakerValue;
-gdouble micValue;
+gdouble     value[2];
+GtkWidget * slider[2];
+GtkWidget * button[2];
 
-GtkWidget * speakerSlider;
-GtkWidget * micSlider;
-GtkWidget * micMuteButton;
-GtkWidget * speakerMuteButton;
+// icons
+GtkWidget * images[2][4];
+enum device_t {
+  SPEAKER = 0,
+  MIKE,
+  DEVICE_COUNT
+} ;
 
-guint speakerToggledConnId; //The button toggled signal connection ID
-guint micToggledConnId;     //The button toggled signal connection ID
+enum volume_t {
+  MUTED = 0,
+  VOL25,
+  VOL50,
+  VOL75
+} ;
 
-guint speakerMovedConnId; //The slider_moved signal connection ID
-guint micMovedConnId;     //The slider_moved signal connection ID
+guint toggledConnId[2]; // The button toggled signal connection ID
+guint movedConnId[2];   // The slider_moved signal connection ID
 
-void slider_moved(GtkRange *range,
-                    gchar * device){
+void 
+update_icons (int dev)
+{
+  float val = gtk_range_get_value(GTK_RANGE(slider[dev]));
+  if(button[dev])
+  {
+    int icon = MUTED;
+    if(val == 0)
+      icon = MUTED;
+    else if( val < 0.33)
+      icon = VOL25;
+    else if( val < 0.66)
+      icon = VOL50;
+    else if( val <= 1)
+      icon = VOL75;
+    gtk_button_set_image(GTK_BUTTON(button[dev]), GTK_WIDGET(images[dev][icon]));
+  }
+}
+
+void 
+slider_moved(GtkRange* range, gchar* device)
+{
   gdouble value = gtk_range_get_value(range);
-  g_print("Volume changed for %s: %f\n ",device, value);
+  g_print("Volume changed for %s: %f\n ", device, value);
   dbus_set_volume(device, value);
+  if(strcmp(device, "speaker") == 0)
+    update_icons(SPEAKER);
+  else
+    update_icons(MIKE);
 }
 
 static void 
 mute_cb( GtkWidget *widget, gchar*  device )
 {
+  int dev;
+  if(strcmp(device, "speaker") == 0)
+    dev = SPEAKER;
+  else
+    dev = MIKE;
+    
   if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget))) 
   { // Save value
     g_print("Save");
-    if(strcmp(device, "speaker") == 0)
-    {
-      speakerValue = gtk_range_get_value(GTK_RANGE(speakerSlider));
-    }
-    else if (strcmp(device, "mic")== 0)
-    {
-      micValue = gtk_range_get_value(GTK_RANGE(micSlider));
-    }
+    value[dev] = gtk_range_get_value(GTK_RANGE(slider[dev]));
     dbus_set_volume(device, 0);
   }
   else 
   { //Restore value
     g_print("Restore");
-    if(strcmp(device, "speaker") == 0)
-    {
-      dbus_set_volume(device, speakerValue);
-    }
-    else if (strcmp(device, "mic")== 0)
-    {
-      dbus_set_volume(device, micValue);
-    }
+    dbus_set_volume(device, value[dev]);
   }
-  
+  update_icons (dev);
 }
 
 /** This function updates the sliders without sending the value to the server.
@@ -80,103 +104,74 @@ mute_cb( GtkWidget *widget, gchar*  device )
   * @param value The value to set [0, 1.0]
   */
 void 
-set_slider(const gchar * device, gdouble value)
+set_slider(const gchar * device, gdouble newval)
 {
-  GtkWidget * slider;
-  GtkWidget * mute;
-  guint * movedConnId;
-  guint * toggledConnId;
+  int dev;
   if(strcmp(device, "speaker") == 0)
-  {
-    slider = speakerSlider;
-    mute   = speakerMuteButton;
-    movedConnId = &speakerMovedConnId;
-    toggledConnId = &speakerToggledConnId;
-  }
-  else if (strcmp(device, "mic")== 0)
-  {
-    slider = micSlider;
-    mute   = micMuteButton;
-    movedConnId = &micMovedConnId;
-    toggledConnId = &micToggledConnId;
-  }
-  gtk_signal_handler_block(GTK_OBJECT(slider),*movedConnId);
-  gtk_range_set_value(GTK_RANGE(slider), value);
-  gtk_signal_handler_unblock(slider, *movedConnId);
+    dev = SPEAKER;
+  else
+    dev = MIKE;
+    
+  gtk_signal_handler_block(GTK_OBJECT(slider[dev]), movedConnId[dev]);
+  gtk_range_set_value(GTK_RANGE(slider[dev]), newval);
+  gtk_signal_handler_unblock(slider[dev], movedConnId[dev]);
   
-  gtk_signal_handler_block(GTK_OBJECT(mute),*toggledConnId);
-  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(mute), (value == 0 ? TRUE: FALSE));
-  gtk_signal_handler_unblock(mute, *toggledConnId);
+  gtk_signal_handler_block(GTK_OBJECT(button[dev]),toggledConnId[dev]);
+  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(button[dev]), (newval == 0 ? TRUE: FALSE));
+  gtk_signal_handler_unblock(button[dev], toggledConnId[dev]);
+  
+  update_icons (dev);
 }
 
 /** Generates the speaker slider and mute button */
 GtkWidget * 
 create_slider(const gchar * device)
 { 
+  // Increment the references count for the images
+  // When the image is removed from a button, if the ref count = 0, then it is destroyed
+  // which we don't want ;)
+    
   GtkWidget * ret;
-  GtkWidget * slider;
-  GtkWidget * button;
-  GtkWidget * image;
-  //GtkIconTheme * icon_theme;
-  //GdkPixbuf * pixbuf = NULL;
-  //GError * error = NULL;
-  guint * movedConnId = NULL;
-  guint * toggledConnId = NULL;
+  int dev;
   
   if(strcmp(device, "speaker") == 0)
   {
-    movedConnId = &speakerMovedConnId;
-    toggledConnId = &speakerToggledConnId;
-    image = gtk_image_new_from_file( ICONS_DIR "/speaker.svg");
-    //icon_theme = gtk_icon_theme_get_default ();
-    //pixbuf = gtk_icon_theme_load_icon (icon_theme,
-    //                               "audio-volume-muted", /* icon name */
-    //                               16, /* size */
-    //                               0,  /* flags */
-    //                               &error);
-    //image = gtk_image_new_from_pixbuf(pixbuf);
+    dev = SPEAKER;
+    images[SPEAKER][MUTED] = gtk_image_new_from_file( ICONS_DIR "/speaker.svg");
+    images[SPEAKER][VOL25] = gtk_image_new_from_file( ICONS_DIR "/speaker_25.svg");
+    images[SPEAKER][VOL50] = gtk_image_new_from_file( ICONS_DIR "/speaker_50.svg");
+    images[SPEAKER][VOL75] = gtk_image_new_from_file( ICONS_DIR "/speaker_75.svg");
+	  g_object_ref(images[SPEAKER][MUTED]);
+	  g_object_ref(images[SPEAKER][VOL25]);
+	  g_object_ref(images[SPEAKER][VOL50]);
+	  g_object_ref(images[SPEAKER][VOL75]);
   }
   else if (strcmp(device, "mic") == 0)
   {
-    movedConnId = &micMovedConnId;
-    toggledConnId = &micToggledConnId;
-    image = gtk_image_new_from_file( ICONS_DIR "/mic.svg");
+    dev = MIKE;
+    images[MIKE][MUTED] = gtk_image_new_from_file( ICONS_DIR "/mic.svg");
+    images[MIKE][VOL25] = gtk_image_new_from_file( ICONS_DIR "/mic_25.svg");
+    images[MIKE][VOL50] = gtk_image_new_from_file( ICONS_DIR "/mic_50.svg");
+    images[MIKE][VOL75] = gtk_image_new_from_file( ICONS_DIR "/mic_75.svg");
+	  g_object_ref(images[MIKE][MUTED]);
+	  g_object_ref(images[MIKE][VOL25]);
+	  g_object_ref(images[MIKE][VOL50]);
+	  g_object_ref(images[MIKE][VOL75]);
   }
-  
   
   ret = gtk_hbox_new ( FALSE /*homogeneous*/, 5 /*spacing*/);
   
-  button = gtk_toggle_button_new();
-  gtk_box_pack_start (GTK_BOX (ret), button, FALSE /*expand*/, TRUE /*fill*/, 0 /*padding*/);
-  *toggledConnId = g_signal_connect (G_OBJECT (button), "toggled",
+  button[dev] = gtk_toggle_button_new();
+  gtk_box_pack_start (GTK_BOX (ret), button[dev], FALSE /*expand*/, TRUE /*fill*/, 0 /*padding*/);
+  toggledConnId[dev] = g_signal_connect (G_OBJECT (button[dev]), "toggled",
                     G_CALLBACK (mute_cb), (gpointer)device);
   
-  if (image)
-  {
-    gtk_button_set_image(GTK_BUTTON(button), image);
-  }
-  else
-  {
-    g_warning ("Couldn't load icon");
-  }
-  
-  slider = gtk_hscale_new_with_range(0, 1, 0.05);
-  gtk_scale_set_draw_value(GTK_SCALE(slider), FALSE);
+  slider[dev] = gtk_hscale_new_with_range(0, 1, 0.05);
+  gtk_scale_set_draw_value(GTK_SCALE(slider[dev]), FALSE);
   //gtk_range_set_update_policy(GTK_RANGE(slider), GTK_UPDATE_DELAYED);
-  *movedConnId = g_signal_connect (G_OBJECT (slider), "value_changed",
+  movedConnId[dev] = g_signal_connect (G_OBJECT (slider[dev]), "value_changed",
                     G_CALLBACK (slider_moved), (gpointer)device);
-  gtk_box_pack_start (GTK_BOX (ret), slider, TRUE /*expand*/, TRUE /*fill*/, 0 /*padding*/);
-  
-  if(strcmp(device, "speaker") == 0)
-  {
-    speakerSlider = slider;
-    speakerMuteButton = button;
-  }
-  else if (strcmp(device, "mic") == 0)
-  {
-    micSlider = slider;
-    micMuteButton = button;
-  }
+  gtk_box_pack_start (GTK_BOX (ret), slider[dev], TRUE /*expand*/, TRUE /*fill*/, 0 /*padding*/);
   
   set_slider(device, dbus_get_volume(device));
   
