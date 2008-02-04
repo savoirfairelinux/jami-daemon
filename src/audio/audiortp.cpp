6e415c5aa964e651d0c141e06a9921861012d65f
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2008-02-04 11:17:51 -0500

    Shared libraries moved from /usr/lib to /usr/lib/sflphone/codecs.
    Codecs dynamic libraries are created at the compilation time if they don't exist and copied in /usr/lib/sflphone/codecs at the installation

--- src/audio/audiortp.cpp
+++ src/audio/audiortp.cpp
@@ -259,16 +259,16 @@ AudioRtpRTX::loadCodec(int payload)
 
 	switch(payload){
 	  case 0:
-            handle_codec = dlopen("codec_ulaw.so", RTLD_LAZY);
+            handle_codec = dlopen("libcodec_ulaw.so", RTLD_LAZY);
 	    break;
 	  case 3:
-	    handle_codec = dlopen("codec_gsm.so", RTLD_LAZY);
+	    handle_codec = dlopen("libcodec_gsm.so", RTLD_LAZY);
 	    break;
 	  case 8:
-	    handle_codec = dlopen("codec_alaw.so", RTLD_LAZY);
+	    handle_codec = dlopen("libcodec_alaw.so", RTLD_LAZY);
 	    break;
 	  case 97:
-            handle_codec = dlopen("codec_ilbc.so", RTLD_LAZY);
+            handle_codec = dlopen("libcodec_ilbc.so", RTLD_LAZY);
 	    break;
 	}
 
