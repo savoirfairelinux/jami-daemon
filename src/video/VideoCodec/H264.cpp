7d68984c989392b917ea5d399ec94b6167593a83
sfl <sfl@l4815-04.(none)>
2008-03-03 08:11:58 -0500

    Copie Squelette

---
+++ src/video/VideoCodec/H264.cpp
@@ -0,0 +1,20 @@
+
+
+#include "H264.h"
+
+H264::H264(){}
+H264::~H264(){}
+
+int H264::videoEncode(int width, int height, uint8_t* buf, unsigned int size){
+
+
+return 0;
+}
+int H264::videoDecode(uint8_t *in_buf, int width, int height, uint8_t* out_buf  ){
+
+return 0;
+
+}
+
+
+
---
+++ src/video/VideoRtp.cpp
@@ -0,0 +1,11 @@
+
+
+#include "VideoRtp.h"
+#include "VideoRtpRTX.h"
+
+
+VideoRtp::VideoRtp(){}
+VideoRtp::~VideoRtp(){}
+int VideoRtp::createNewVideoSession(bool conf){return 1;}
+int VideoRtp::closeVideoSession(bool conf){return 1;}
+
