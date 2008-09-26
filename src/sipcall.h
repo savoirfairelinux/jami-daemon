88cb2f191c93c8691797576fda5ed780780869fb
Yun Liu <yun@yun.(none)>
2008-09-26 10:03:44 -0400

    Modify the header file

--- src/sipcall.h
+++ src/sipcall.h
@@ -25,6 +25,8 @@
 #include "audio/codecDescriptor.h"
 #include <eXosip2/eXosip.h>
 
+#include "sipmanager.h"
+/*
 struct pjsip_rx_data;
 struct pjmedia_sdp_session;
 struct pjmedia_sdp_media;
@@ -32,7 +34,7 @@ struct pjmedia_sdp_neg;
 struct pj_pool_t;
 struct pjsip_inv_session;
 struct pjsip_evsub;
-
+*/
 class AudioCodec;
 
 /**
