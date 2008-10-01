bd3bc1a56bafa981fdc568c35f51e8f12e05f56f
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2008-10-01 14:25:55 -0400

    Add an autogen script on client and server side
    Just have to run it and then make

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
+aclocal -I m4
+libtoolize --force
+autoheader
+autoconf -f
+automake -a
+./configure --prefix=/usr --with-debug
