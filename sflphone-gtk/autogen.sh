fd0980796c572fb673d848b79727b15f40cf8bbb
Yun Liu <yun@yun.(none)>
2008-11-26 17:10:57 -0500

    Add checking m4 dir in autogen.sh

--- sflphone-gtk/autogen.sh
+++ sflphone-gtk/autogen.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
+if [ ! -d "m4" ]; then
+	mkdir m4
+fi
+
 aclocal -I m4
 libtoolize --force
 autoheader
