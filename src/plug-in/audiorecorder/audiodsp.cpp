0166930d3387393091802ca2b56336d003ca1679
Alexandre Savard <alexandre.savard@savoirfairelinux.net>
2009-02-11 10:11:24 -0500

    Recording: default home folder at startup

---
+++ src/plug-in/audiorecorder/audiodsp.cpp
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (C) 2008 Savoir-Faire Linux inc.
+ *  Author: Alexandre Savard <alexandre.savard@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "audiodsp.h"
+
+AudioDSP::AudioDSP()
+{
+
+  bufPointer_ = 0;
+  bufferLength_ = 1024;
+  circBuffer_ = new float[bufferLength_];
+
+}
+
+
+AudioDSP::~AudioDSP()
+{
+  
+  delete[] circBuffer_;
+
+}
+
+
+float AudioDSP::getRMS(int data)
+{
+  printf("AudioDSP::getRMS() : bufPointer_ %i \n ", bufPointer_);
+  circBuffer_[bufPointer_++] = (float)data;
+  
+  if(bufPointer_ >= bufferLength_)
+    bufPointer_ = 0;
+
+  return computeRMS(); 
+} 
+
+
+float AudioDSP::computeRMS()
+{
+
+  rms = 0.0;
+
+  
+  for(int i = 0; i < bufferLength_; i++){
+    printf("AudioDSP::computeRMS() : i_ %i \n ", i);
+    rms += (float)(circBuffer_[i]*circBuffer_[i]);
+  }
+  rms = sqrt(rms / (float)bufferLength_);
+
+  printf("AudioDSP::computeRMS() : RMS VALUE: %f \n", rms);
+  return rms;
+ 
+}
--- src/user_cfg.h
+++ src/user_cfg.h
@@ -24,7 +24,7 @@
 
 #include <stdlib.h>
 
-#define HOMEDIR	(getenv ("HOME"))		      /** Home directory */
+#define HOMEDIR	(getenv ("HOME"))	              /** Home directory */
 
 #define DIR_SEPARATOR_CH '/'			      /** Directory separator string */
 #define DIR_SEPARATOR_STR "/"			      /** Directory separator char */
@@ -92,7 +92,8 @@
 #define DFT_EXPIRE_VALUE	"180"			/** Default expire value for registration */
 #define DFT_AUDIO_MANAGER	"1"			/** Default audio manager */
 #define DFT_SIP_PORT            "5060"
-#define DFT_STUN_ENABLE     "0"
+#define DFT_STUN_ENABLE         "0"
+#define DFT_RECORD_PATH         HOMEDIR
 
 #ifdef USE_ZEROCONF
 #define CONFIG_ZEROCONF_DEFAULT_STR "1"			/** Default Zero configuration networking module value */
