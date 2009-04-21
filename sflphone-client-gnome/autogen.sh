d7586e47421a7460721bc819b3995ded3ba0438a
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2009-04-21 12:15:26 -0400

    [#1212] Update the manpages generation - edit preinst maintainer script

--- sflphone-client-gnome/autogen.sh
+++ sflphone-client-gnome/autogen.sh
@@ -1,15 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-# could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
-if [ ! -d "m4" ]; then
-	mkdir m4
-fi
+function autocmd()
+{
+    echo "Running ${1}..."
+        $* || {
+            echo "Error running ${1}"
+                exit 1
+        }
+}
 
-aclocal -I m4
-libtoolize --force
-autoheader
-autoconf -f
-automake -a
+autocmd libtoolize --force --copy
+autocmd aclocal
+autocmd autoheader
+autocmd autoconf -f
+autocmd automake --add-missing --force-missing --copy -Wall
+
 ./configure $@
 
 
+#!/bin/sh
+
+# could be replaced with autoconf -v -f (verbose, force rebuild of ltmain, .in files, etc.)
+#if [ ! -d "m4" ]; then
+#	mkdir m4
+#fi
+#
+#aclocal -I m4
+#libtoolize --force
+#autoheader
+#autoconf -f
+#automake -a
+#./configure $@
+#
+#
