ad911b2add287804cdf869f9363ba4da13bb772a
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2009-05-20 09:42:40 -0400

    [#1403] Remove all actions on notifications; code refactoring

--- sflphone-client-gnome/src/sflnotify.c
+++ sflphone-client-gnome/src/sflnotify.c
@@ -19,17 +19,50 @@
 
 #include <sflnotify.h>
 
-static NotifyNotification *notification;
+GnomeNotification *_gnome_notification;
 
-    void
-notify_incoming_call( call_t* c  )
+void create_new_gnome_notification (gchar *title, gchar *body, NotifyUrgency urgency, gint timeout, GnomeNotification **notif)
 {
+    GnomeNotification *_notif;
+
     if( dbus_get_notify()){
 
-        GdkPixbuf *pixbuf;
+        _notif = g_new0 (GnomeNotification, 1);
+
+        notify_init ("SFLphone");
+
+        // Set struct fields
+        _notif->notification = notify_notification_new (title, body, NULL, NULL);
+        //_notif->icon = gdk_pixbuf_new_from_file_at_size (LOGO, 120, 120, NULL);
+        _notif->icon = gdk_pixbuf_new_from_file (LOGO, NULL);
+#if GTK_CHECK_VERSION(2,10,0)
+        notify_notification_attach_to_status_icon (_notif->notification , get_status_icon() );
+#endif
+
+        notify_notification_set_urgency (_notif->notification, urgency);
+        
+        if (_notif->icon != NULL)
+            notify_notification_set_icon_from_pixbuf (_notif->notification, _notif->icon);
+        else
+            ERROR ("notify(), cannot load notification icon");
+
+        notify_notification_set_timeout (_notif->notification, timeout);
+
+        if (!notify_notification_show (_notif->notification, NULL)) {
+            ERROR("notify(), failed to send notification");
+        }
+
+        *notif = _notif;
+    }
+}
+
+
+    void
+notify_incoming_call (call_t* c)
+{
+
         gchar* callerid;
         gchar* title;
-        notify_init("sflphone");
 
         if (g_strcasecmp (c->accountID,"") == 0) {
             title = g_markup_printf_escaped ("IP-to-IP call");
@@ -41,102 +74,39 @@ notify_incoming_call( call_t* c  )
         }
         callerid = g_markup_printf_escaped(_("<i>From:</i> %s") , c->from);
 
-        pixbuf = gdk_pixbuf_new_from_file(ICONS_DIR "/ring.svg", NULL);
-
-        notification = notify_notification_new( title,
-                callerid,
-                NULL,
-                NULL);
-        notify_notification_set_urgency( notification , NOTIFY_URGENCY_CRITICAL );
-        notify_notification_set_icon_from_pixbuf (notification, pixbuf);
-#if GTK_CHECK_VERSION(2,10,0)
-        notify_notification_attach_to_status_icon( notification , get_status_icon() );
-#endif
-        notify_notification_set_timeout( notification , (( g_strcasecmp(__TIMEOUT_MODE, "default") == 0 )? __TIMEOUT_TIME : NOTIFY_EXPIRES_NEVER ));
-        g_object_set_data( G_OBJECT( notification ) , "call" , c );
-        notify_notification_add_action( notification , "accept" , _("Accept") , (NotifyActionCallback) answer_call_cb , NULL,  NULL );
-        notify_notification_add_action( notification , "refuse" , _("Refuse") , (NotifyActionCallback) refuse_call_cb , NULL , NULL );
-        notify_notification_add_action( notification , "ignore" , _("Ignore") , (NotifyActionCallback) ignore_call_cb , NULL , NULL );
-
-        if (!notify_notification_show (notification, NULL)) {
-            ERROR("notify(), failed to send notification");
-        }
-    }
-}
-    void
-answer_call_cb( NotifyNotification *notification, gpointer data  UNUSED )
-{
-    call_t* c = (call_t*)g_object_get_data( G_OBJECT( notification ) , "call" );
-    c->history_state = INCOMING;
-    calltree_update_call( history , c );
-    dbus_accept(c);
-#if GTK_CHECK_VERSION(2,10,0)
-    if( __POPUP_WINDOW )
-        status_icon_unminimize();
-#endif
-    g_object_unref( notification );
+        create_new_gnome_notification (title,
+                                        callerid, 
+                                        NOTIFY_URGENCY_CRITICAL, 
+                                        (g_strcasecmp(__TIMEOUT_MODE, "default") == 0 )? __TIMEOUT_TIME : NOTIFY_EXPIRES_NEVER,
+                                        &_gnome_notification); 
 }
 
     void
-refuse_call_cb( NotifyNotification *notification, gpointer data  UNUSED)
+notify_voice_mails (guint count, account_t* acc)
 {
-    call_t* c = (call_t*)g_object_get_data( G_OBJECT( notification ) , "call" );
-    dbus_refuse(c);
-    g_object_unref( notification );
-}
-
-    void
-ignore_call_cb( NotifyNotification *notification, gpointer data  UNUSED)
-{
-    g_object_unref( notification );
-}
-
-    void
-notify_voice_mails( guint count , account_t* acc )
-{
-
-    if( dbus_get_mail_notify()) {
         // the account is different from NULL
-        GdkPixbuf *pixbuf;
         gchar* title;
         gchar* body;
-        notify_init("sflphone");
 
         title = g_markup_printf_escaped(_("%s account: %s") ,
                 (gchar*)g_hash_table_lookup(acc->properties , ACCOUNT_TYPE) ,
                 (gchar*) g_hash_table_lookup(acc->properties , ACCOUNT_ALIAS) ) ;
         body = g_markup_printf_escaped(_("%d voice mails"), count);
 
-        pixbuf = gdk_pixbuf_new_from_file(LOGO, NULL);
-
-        notification = notify_notification_new( title,
-                body,
-                NULL,
-                NULL);
-        notify_notification_set_urgency( notification , NOTIFY_URGENCY_LOW );
-        notify_notification_set_icon_from_pixbuf (notification, pixbuf);
-#if GTK_CHECK_VERSION(2,10,0)
-        notify_notification_attach_to_status_icon( notification , get_status_icon() );
-#endif
-        notify_notification_set_timeout( notification , NOTIFY_EXPIRES_DEFAULT );
-        notify_notification_add_action( notification , "ignore" , _("Ignore") , (NotifyActionCallback) ignore_call_cb , NULL , NULL );
-
-        if (!notify_notification_show (notification, NULL)) {
-            ERROR("notify(), failed to send notification");
-        }
-    }
+        create_new_gnome_notification (title,
+                                        body, 
+                                        NOTIFY_URGENCY_LOW, 
+                                        NOTIFY_EXPIRES_DEFAULT,
+                                        &_gnome_notification); 
 }
 
     void
-notify_current_account( account_t* acc )
+notify_current_account (account_t* acc)
 {
 
-    if( dbus_get_notify()){
         // the account is different from NULL
-        GdkPixbuf *pixbuf;
         gchar* title;
         gchar* body="";
-        notify_init("sflphone");
 
         body = g_markup_printf_escaped(_("Calling with %s account <i>%s</i>") ,
                 (gchar*)g_hash_table_lookup( acc->properties , ACCOUNT_TYPE) ,
@@ -144,109 +114,70 @@ notify_current_account( account_t* acc )
 
         title = g_markup_printf_escaped(_("Current account"));
 
-        pixbuf = gdk_pixbuf_new_from_file(LOGO, NULL);
-
-        notification = notify_notification_new( title,
-                body,
-                NULL,
-                NULL);
-        notify_notification_set_urgency( notification , NOTIFY_URGENCY_NORMAL );
-        notify_notification_set_icon_from_pixbuf (notification, pixbuf);
-#if GTK_CHECK_VERSION(2,10,0)
-        notify_notification_attach_to_status_icon( notification , get_status_icon() );
-#endif
-        notify_notification_set_timeout( notification , NOTIFY_EXPIRES_DEFAULT );
-        notify_notification_add_action( notification , "ignore" , _("Ignore") , (NotifyActionCallback) ignore_call_cb , NULL , NULL );
-
-        if (!notify_notification_show (notification, NULL)) {
-            ERROR("notify(), failed to send notification");
-        }
-    }
+        create_new_gnome_notification (title,
+                                        body, 
+                                        NOTIFY_URGENCY_NORMAL, 
+                                        NOTIFY_EXPIRES_DEFAULT,
+                                        &_gnome_notification); 
 }
+
     void
-notify_no_accounts(  )
+notify_no_accounts ()
 {
-    GdkPixbuf *pixbuf;
     gchar* title;
     gchar* body="";
-    notify_init("sflphone");
 
     body = g_markup_printf_escaped(_("You haven't setup any accounts"));
-
     title = g_markup_printf_escaped(_("Error"));
 
-    pixbuf = gdk_pixbuf_new_from_file(LOGO, NULL);
-
-    notification = notify_notification_new( title,
-            body,
-            NULL,
-            NULL);
-    notify_notification_set_urgency( notification , NOTIFY_URGENCY_CRITICAL );
-    notify_notification_set_icon_from_pixbuf (notification, pixbuf);
-#if GTK_CHECK_VERSION(2,10,0)
-    notify_notification_attach_to_status_icon( notification , get_status_icon() );
-#endif
-    notify_notification_set_timeout( notification , NOTIFY_EXPIRES_DEFAULT );
-    //notify_notification_add_action( notification , "setup" , _("Setup Accounts") , (NotifyActionCallback) setup_accounts_cb , NULL , NULL );
-
-    if (!notify_notification_show (notification, NULL)) {
-        ERROR("notify(), failed to send notification");
-    }
+    create_new_gnome_notification (title,
+                                    body, 
+                                    NOTIFY_URGENCY_CRITICAL, 
+                                    NOTIFY_EXPIRES_DEFAULT,
+                                    &_gnome_notification); 
 }
 
-    void
-setup_accounts_cb( NotifyNotification *notification, gpointer data  UNUSED)
-{
-    //gtk_widget_set_parent( show_accounts_window() , GTK_WIDGET(get_main_window()));
-    show_accounts_window();
-    g_object_unref( notification );
-}
 
     void
-notify_no_registered_accounts(  )
+notify_no_registered_accounts ()
 {
-    GdkPixbuf *pixbuf;
     gchar* title;
     gchar* body="";
-    notify_init("sflphone");
-
 
     body = g_markup_printf_escaped(_("You have no registered accounts"));
-
-
     title = g_markup_printf_escaped(_("Error"));
 
-    pixbuf = gdk_pixbuf_new_from_file(LOGO, NULL);
-
-    notification = notify_notification_new( title,
-            body,
-            NULL,
-            NULL);
-    notify_notification_set_urgency( notification , NOTIFY_URGENCY_CRITICAL );
-    notify_notification_set_icon_from_pixbuf (notification, pixbuf);
-#if GTK_CHECK_VERSION(2,10,0)
-    notify_notification_attach_to_status_icon( notification , get_status_icon() );
-#endif
-    notify_notification_set_timeout( notification , NOTIFY_EXPIRES_DEFAULT );
-    //notify_notification_add_action( notification , "setup" , _("Setup Accounts") , (NotifyActionCallback) setup_accounts_cb , NULL , NULL );
-
-  if (!notify_notification_show (notification, NULL)) {
-    ERROR("notify(), failed to send notification");
-  }
-
+    create_new_gnome_notification (title,
+                                    body, 
+                                    NOTIFY_URGENCY_CRITICAL, 
+                                    NOTIFY_EXPIRES_DEFAULT,
+                                    &_gnome_notification); 
 }
 
     void
 stop_notification( void )
 {
-    if( notification != NULL )
+    if( _gnome_notification->notification != NULL )
     {
-        if(notify_notification_show( notification , NULL))
+        if(notify_notification_show (_gnome_notification->notification, NULL))
         {
-            notify_notification_close( notification , NULL);
-            g_object_unref( notification );
-            notification = NULL;
+            notify_notification_close (_gnome_notification->notification, NULL);
+            g_object_unref(_gnome_notification->notification );
+            _gnome_notification->notification = NULL;
         }
-        notification = NULL;
+        _gnome_notification->notification = NULL;
     }
+    //free_notification (_gnome_notification);
+}
+
+/**
+ * Freeing a notification instance
+ */
+void free_notification (GnomeNotification *g)
+{
+  g_free(g->title);
+  g_free(g->body);
+  g_free(g->notification);
+  g_free(g->icon);
+  g_free(g);
 }
