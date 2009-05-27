ec861396e542ca1b3250024d947eac79f87f371c
Julien Bonjean <julien@bonjean.info>
2009-05-27 10:57:21 -0400

    Updated autogen.sh for pjsip

--- sflphone-common/libs/pjproject-1.0.1/autogen.sh
+++ sflphone-common/libs/pjproject-1.0.1/autogen.sh
@@ -7,14 +7,17 @@
 # Author: Julien Bonjean (julien@bonjean.info) 
 #
 # Creation Date: 2009-05-26
-# Last Modified: 2009-05-26 11:55:12 -0400
+# Last Modified: 2009-05-27 10:56:43 -0400
 #####################################################
 
-# it's only a workaround to generate config.guess and config.sub
-# that are currently static
-# this will generate errors but we don't care
-aclocal --force >/dev/null 2>&1
-automake --add-missing --force-missing --copy >/dev/null 2>&1
+if [ -e /usr/share/misc/config.guess ]; then
+	rm -f config.sub config.guess
+        ln -s /usr/share/misc/config.sub .
+        ln -s /usr/share/misc/config.guess .	
+else
+	echo Running autoreconf...
+	autoreconf --force --install
+fi
 
 # now we launch configure
 ./configure $@
