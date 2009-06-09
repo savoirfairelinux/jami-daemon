c132248cd37c0e0f211cf690ba1d723e5feec336
Alexandre Savard <alexandre.savard@savoirfairelinux.net>
2009-06-09 16:49:37 -0400

    [#936] Try to decode using 1280 samples

--- sflphone-common/src/audio/codecs/speexcodec_ub.cpp
+++ sflphone-common/src/audio/codecs/speexcodec_ub.cpp
@@ -129,7 +129,7 @@ class Speex : public AudioCodec{
             
             speex_preprocess_run(_preprocess_state, src);
 #endif 
-
+	    printf("Codec::codecEncode() nbBytes: %i \n",size);
             speex_encode_int(_speex_enc_state, src, &_speex_enc_bits);
             int nbBytes = speex_bits_write(&_speex_enc_bits, (char*)dst, size);
             printf("Codec::codecEncode() nbBytes: %i \n",nbBytes);
