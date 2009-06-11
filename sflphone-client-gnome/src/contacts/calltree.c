51f55f26096394daf885f1eeee6d8626f5442622
Alexandre Savard <alexandre.savard@savoirfairelinux.net>
2009-06-11 09:59:28 -0400

    [#1620] Get back caltree as it was during gtkcritical area

--- sflphone-client-gnome/src/contacts/calltree.c
+++ sflphone-client-gnome/src/contacts/calltree.c
@@ -547,10 +547,16 @@ void calltree_add_call (calltab_t* tab, callable_obj_t * c)
         g_object_unref(G_OBJECT(pixbuf));
 
 
+    // history_reinit (tab);
+
     sel = gtk_tree_view_get_selection(GTK_TREE_VIEW(tab->view));
     gtk_tree_selection_select_iter(GTK_TREE_SELECTION(sel), &iter);
 
+    // history_reinit (tab);
+
     // gtk_tree_view_set_model(GTK_TREE_VIEW(tab->view), GTK_TREE_MODEL(tab->store));
+
+    // gtk_tree_view_set_model (GTK_TREE_VIEW (tab->view), GTK_TREE_MODEL (history_filter));
 
     // gtk_tree_selection_select_iter(gtk_tree_view_get_selection(GTK_TREE_VIEW(tab->view)), &iter);
 
