c3200637ed8f2d8feaec75d7d726e3e8dbe57fff
Jérémy Quentin <jquentin@jquentin-laptop-kub2.(none)>
2009-07-17 15:57:45 -0400

    Spanish translation finished + po README files updated + echo's in copy-in-clients

--- lang/copy-in-gnome.sh
+++ lang/copy-in-gnome.sh
@@ -2,4 +2,5 @@ for fichier in `find  .  -name sflphone.po `
 do
 locale=`echo $fichier | cut -d / -f2`
 cp $fichier ../sflphone-client-gnome/po/$locale/sflphone-client-gnome.po
+echo "$fichier	copied to	../sflphone-client-gnome/po/$locale/sflphone-client-gnome.po"
 done
--- lang/copy-in-kde.sh
+++ lang/copy-in-kde.sh
@@ -2,4 +2,5 @@ for fichier in `find  .  -name sflphone.po `
 do
 locale=`echo $fichier | cut -d / -f2`
 cp $fichier ../sflphone-client-kde/po/$locale/sflphone-client-kde.po
+echo "$fichier	copied to	../sflphone-client-kde/po/$locale/sflphone-client-kde.po"
 done
