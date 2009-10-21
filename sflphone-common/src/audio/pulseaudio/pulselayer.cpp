0728e6856b31fbd83380b303d75fe8dfe5a73e19
Alexandre Savard <alexandre.savard@savoirfairelinux.net>
2009-10-21 13:33:07 -0400

    [#2333] Comment pa_stream_trigger in pulseaudio underrun

--- sflphone-common/src/audio/pulseaudio/pulselayer.cpp
+++ sflphone-common/src/audio/pulseaudio/pulselayer.cpp
@@ -57,7 +57,8 @@ static void stream_moved_callback(pa_stream *s UNUSED, void *userdata UNUSED)
 static void playback_underflow_callback (pa_stream* s,  void* userdata UNUSED)
 {
     _debug ("PulseLayer::Buffer Underflow\n");
-    pa_stream_trigger (s, NULL, NULL);
+    // pa_stream_trigger (s, NULL, NULL);
+
 }
 
 
