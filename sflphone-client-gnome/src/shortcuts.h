cd33051c4fb485534fdcf393f13c4793e4e9566a
Julien Bonjean <julien@bonjean.info>
2010-04-30 12:09:28 -0400

    [#3335] Cleaning & refactoring

--- sflphone-client-gnome/src/shortcuts.h
+++ sflphone-client-gnome/src/shortcuts.h
@@ -23,20 +23,20 @@
 typedef struct
 {
   gchar *action;
+  guint key;
   GdkModifierType mask;
-  guint value;
   void
   (*callback) (void);
 } Accelerator;
 
 static void
-grab_key (int key_code, GdkModifierType mask, GdkWindow *root);
+grab_key (guint key, GdkModifierType mask, const GdkWindow *root);
 
 static void
-ungrab_key (int key_code, GdkModifierType mask, GdkWindow *root);
+ungrab_key (guint key, GdkModifierType mask, const GdkWindow *root);
 
 static GdkFilterReturn
-filter_keys (GdkXEvent *xevent, GdkEvent *event, gpointer data);
+filter_keys (const GdkXEvent *xevent, const GdkEvent *event, gpointer data);
 
 static void
 remove_bindings ();
@@ -57,7 +57,7 @@ static void
 toggle_hold_callback ();
 
 static void
-initialize_binding (const gchar* action, const guint code, const GdkModifierType mask);
+initialize_binding (const gchar* action, guint key, GdkModifierType mask);
 
 static void
 initialize_shortcuts_keys ();
@@ -66,7 +66,7 @@ static void*
 get_action_callback (const gchar* action);
 
 static void
-update_bindings_data (const guint index, const guint code, GdkModifierType mask);
+update_bindings_data (guint index, guint key, GdkModifierType mask);
 
 static void
 update_shortcuts_map (const gchar* action, guint value, GdkModifierType mask);
@@ -79,7 +79,7 @@ void
 shortcuts_initialize_bindings ();
 
 void
-shortcuts_update_bindings (const guint index, const guint code, GdkModifierType mask);
+shortcuts_update_bindings (guint index, guint key, GdkModifierType mask);
 
 void
 shortcuts_destroy_bindings ();
