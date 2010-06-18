8a5c8534b0fe47cee6246845e8691e8066d7f847
Alexandre Savard <msavard@msavard-desktop.(none)>
2010-06-18 11:04:16 -0400

    [#3558] Prefix all md5 related function by iax in LibIAX
    To prevent linking conflict

--- sflphone-common/libs/iax2/md5.h
+++ sflphone-common/libs/iax2/md5.h
@@ -8,20 +8,20 @@ typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
 #endif
 
-struct MD5Context {
+struct IAX_MD5Context {
 	uint32_t buf[4];
 	uint32_t bits[2];
 	uint8_t in[64];
 };
 
-void MD5Init(struct MD5Context *context);
-void MD5Update(struct MD5Context *context, uint8_t const *buf, unsigned int len);
-void MD5Final(uint8_t digest[16], struct MD5Context *context);
-void MD5Transform(uint32_t buf[4], uint32_t const in[16]);
+void IAX_MD5Init(struct IAX_MD5Context *context);
+void IAX_MD5Update(struct IAX_MD5Context *context, uint8_t const *buf, unsigned int len);
+void IAX_MD5Final(uint8_t digest[16], struct IAX_MD5Context *context);
+void IAX_MD5Transform(uint32_t buf[4], uint32_t const in[16]);
 
 /*
  * This is needed to make RSAREF happy on some MS-DOS compilers.
  */
-typedef struct MD5Context MD5_CTX;
+typedef struct IAX_MD5Context MD5_CTX;
 
 #endif /* !MD5_H */
