73770a13adc9d76f3cf8a7498fe200dda96afcdf
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2010-08-17 12:01:08 -0400

    [#3621] Make links clickable in javascript

--- sflphone-client-gnome/webkit/im/im.js
+++ sflphone-client-gnome/webkit/im/im.js
@@ -1,6 +1,23 @@
+function linkify(text){
+    if (text) {
+        text = text.replace(
+            /((https?\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,
+            function(url){
+                var full_url = url;
+                if (!full_url.match('^https?:\/\/')) {
+                    full_url = 'http://' + full_url;
+                }
+                return '<a href="' + full_url + '">' + url + '</a>';
+            }
+        );
+    }
+    return text;
+}
+
+
 function add_message (message)
 {
 	var msgBody = document.getElementById ('messages');
-	msgBody.innerHTML = '<div class="message">' + message + '</div>' + msgBody.innerHTML;
+	msgBody.innerHTML = '<div class="message">' + linkify (message) + '</div>' + msgBody.innerHTML;
 }
 
