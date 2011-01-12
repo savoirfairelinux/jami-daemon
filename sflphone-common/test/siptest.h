4e9d6d80b702b91195b1669517bd78a43a0d200e
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2011-01-12 12:00:48 -0500

    [#4885] Deactivate siptest that require more than one sipp instance

--- sflphone-common/test/siptest.h
+++ sflphone-common/test/siptest.h
@@ -54,11 +54,11 @@ class SIPTest : public CppUnit::TestCase {
      */
     CPPUNIT_TEST_SUITE( SIPTest );
     CPPUNIT_TEST ( testSimpleOutgoingIpCall );
-    CPPUNIT_TEST ( testSimpleIncomingIpCall );
-    CPPUNIT_TEST ( testTwoOutgoingIpCall );
+    // CPPUNIT_TEST ( testSimpleIncomingIpCall );
+    // CPPUNIT_TEST ( testTwoOutgoingIpCall );
     // CPPUNIT_TEST ( testTwoIncomingIpCall );
-    CPPUNIT_TEST ( testHoldIpCall);
-    CPPUNIT_TEST ( testIncomingIpCallSdp );
+    // CPPUNIT_TEST ( testHoldIpCall);
+    // CPPUNIT_TEST ( testIncomingIpCallSdp );
     CPPUNIT_TEST_SUITE_END();
 
     public:
