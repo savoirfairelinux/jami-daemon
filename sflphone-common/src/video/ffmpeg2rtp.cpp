b6f8c1d5d58dc3d29a1a987652f77c46d630b180
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2011-07-06 10:57:01 -0400

    * #6358: fix make check

--- sflphone-common/src/video/ffmpeg2rtp.cpp
+++ sflphone-common/src/video/ffmpeg2rtp.cpp
@@ -14,35 +14,36 @@ void attach_signal_handlers() { signal(SIGINT, signal_handler); }
 int main(int argc, char *argv[])
 {
     using std::string;
+    using std::map;
     attach_signal_handlers();
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] <<  " <filename> (OPTIONAL) <codec> <bitrate> <destination>" << std::endl;
         return 1;
     }
 
-    string input(argv[1]);
-    string codec("mpeg4");
-    string bitrate("1000000");
-    string destination("rtp://127.0.0.1:5000");
+    map<string, string> args;
+    args["input"] =  argv[1];
+    args["codec"] = "mpeg4";
+    args["bitrate"] = "1000000";
+    args["destination"] = "rtp://127.0.0.1:5000";
 
     switch (argc)
     {
         case 5:
-            destination = argv[4];
+            args["destination"] = argv[4];
             /* fallthrough */
         case 4:
-            bitrate = argv[3];
+            args["bitrate"] = argv[3];
             /* fallthrough */
         case 3:
-            codec = argv[2];
+            args["codec"] = argv[2];
             /* fallthrough */
         case 2:
         default:
             break;
     }
 
-    sfl_video::VideoRtpSession session(input, codec,
-            atoi(bitrate.c_str()), destination);
+    sfl_video::VideoRtpSession session(args);
     session.test();
     while (not interrupted)
         sleep(1);
