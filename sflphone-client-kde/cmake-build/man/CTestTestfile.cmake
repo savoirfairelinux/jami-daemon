a01d4e80adf5fe49b21b97ffe33a759bd04fe3c8
Rafaël Carré <rafael.carre@savoirfairelinux.com>
2011-07-08 10:05:44 -0400

    Remove sflphone-client-kde
    It's obsolete and gets in my path when i want sflphone-cl to autocomplete

--- sflphone-client-kde/cmake-build/man/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emmanuel/sflphone/sflphone-client-kde/man
-# Build directory: /home/emmanuel/sflphone/sflphone-client-kde/cmake-build/man
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- sflphone-client-kde/cmake-build/po/ru/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emmanuel/sflphone/sflphone-client-kde/po/ru
-# Build directory: /home/emmanuel/sflphone/sflphone-client-kde/cmake-build/po/ru
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
