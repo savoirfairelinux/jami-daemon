e789603eebf24f55c71ac3238f493554113bcc29
Rafaël Carré <rafael.carre@savoirfairelinux.com>
2011-09-19 13:48:32 -0400

    ManagerImpl::registerAccounts(): simplify

--- daemon/src/managerimpl_registration.cpp
+++ daemon/src/managerimpl_registration.cpp
@@ -49,14 +49,14 @@
 void
 ManagerImpl::registerAccounts()
 {
-    AccountMap::iterator iter;
+    for (AccountMap::iterator iter = _accountMap.begin(); iter != _accountMap.end(); ++iter) {
+    	Account *a = iter->second;
+        if (!a)
+			continue;
 
-    for (iter = _accountMap.begin(); iter != _accountMap.end(); ++iter) {
-        if (iter->second) {
-            iter->second->loadConfig();
-            if (iter->second->isEnabled())
-                iter->second->registerVoIPLink();
-        }
+        a->loadConfig();
+		if (a->isEnabled())
+			a->registerVoIPLink();
     }
 }
 
