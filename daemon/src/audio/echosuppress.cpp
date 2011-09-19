cb9ea90649ded86e974db50397d5eb9d9acbf5f2
Rafaël Carré <rafael.carre@savoirfairelinux.com>
2011-09-19 13:34:08 -0400

    * #5884: don't mess with pjsip threads in echo suppressor
    Seems to fix all SIP problems on Manu's desktop
    
    Also destroy resources allocated

--- daemon/src/audio/echosuppress.cpp
+++ daemon/src/audio/echosuppress.cpp
@@ -17,21 +17,13 @@
 
 EchoSuppress::EchoSuppress(pj_pool_t *pool)
 {
-    pj_thread_desc aPJThreadDesc;
-    pj_thread_t *pjThread;
-
-    if (pj_thread_register("EchoCanceller", aPJThreadDesc, &pjThread) != PJ_SUCCESS)
-    	_error("EchoCancel: Error: Could not register new thread");
-
-    if (!pj_thread_is_registered())
-    	_warn("EchoCancel: Thread not registered...");
-
     if (pjmedia_echo_create(pool, 8000, SAMPLES_PER_FRAME, 250, 0, PJMEDIA_ECHO_SIMPLE, &echoState) != PJ_SUCCESS)
     	throw std::runtime_error("EchoCancel: Error: Could not create echo canceller");
 }
 
 EchoSuppress::~EchoSuppress()
 {
+	pjmedia_echo_destroy(echoState);
 }
 
 void EchoSuppress::putData (SFLDataFormat *inputData, int samples)
