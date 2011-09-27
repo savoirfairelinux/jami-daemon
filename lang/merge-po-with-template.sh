243a22392a087924b37f39a2ce759258b24da84e
Rafaël Carré <rafael.carre@savoirfairelinux.com>
2011-09-27 16:23:59 -0400

    merge language scripts

--- lang/merge-po-with-template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Merge sflphone template into the existing po files
-
-for file in `find  .  -name *.po`
-do
-    msgmerge --update $file sflphone.pot
-done
--- lang/update.sh
+++ lang/update.sh
@@ -4,3 +4,9 @@
 
 xgettext --from-code=utf-8 --language=C -k_ -kN_ -kc_:1c,2  -kn_:1,2    -ktr2i18n -ktr2i18n:2c,1 -kki18nc:1c,2 -kki18n -ki18n -ki18nc:1c,2 -o sflphone.pot `find ../gnome/src -name \*.c`
 
+# Merge template into the existing po files
+
+for file in *.po
+do
+    msgmerge --update $file sflphone.pot
+done
