2f15047cd1119f74999c33c0d7cb1c85119c8d52
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2011-10-12 13:40:41 -0400

    * #7150: remove unused pluginmanager

--- daemon/test/run_tests.sh
+++ daemon/test/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CODECS_PATH="../src/audio/codecs" FAKE_PLUGIN_DIR="../src/plug-in/test/" FAKE_PLUGIN_NAME="../src/plug-in/test/libplugintest.so" exec ./test --xml
+CODECS_PATH="../src/audio/codecs" exec ./test --xml
