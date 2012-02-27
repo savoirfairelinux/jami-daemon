5bb15ef876c33aec8aea409b94bd197cee5d22b6
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2012-02-27 11:59:41 -0500

    #8763 Add static code analysis during Hudson build

--- hudson-sflphone-script.sh
+++ hudson-sflphone-script.sh
@@ -4,10 +4,26 @@
 #
 # Author: Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
 
+XML_RESULTS="cppunitresults.xml"
+TEST=0
+BUILD=
+CODE_ANALYSIS=0
+
+function run_code_analysis {
+	# Check if cppcheck is installed on the system
+	if [ `which cppcheck &>/dev/null ; echo $?` -ne 1 ] ; then
+		cppcheck . --enable=all --xml 2> cppcheck-report.xml
+	fi
+}
+
 function build_daemon {
 
 	# Compile the daemon
 	pushd daemon
+	# Run static analysis code tool
+	if [ $CODE_ANALYSIS == 1 ]; then
+		run_code_analysis
+	fi
 	make distclean
 	./autogen.sh
 	# Compile pjproject first
@@ -18,12 +34,15 @@ function build_daemon {
 	popd
 	./configure --prefix=/usr
 	make clean
+	# Compile src code
 	make -j
+	# Generate documentation
 	make doc
+	# Compile unit tests
 	make check
 	popd
 
-	if [ $1 == 1 ]; then
+	if [ $TEST == 1 ]; then
 		# Run the unit tests for the daemon
 		pushd daemon/test
 		# Remove the previous XML test file
@@ -63,11 +82,8 @@ fi
 
 
 git clean -f -d -x
-XML_RESULTS="cppunitresults.xml"
-TEST=0
-BUILD=
 
-while getopts ":b: t" opt; do
+while getopts ":b: t a" opt; do
 	case $opt in
 		b)
 			echo "-b was triggered. Parameter: $OPTARG" >&2
@@ -76,6 +92,10 @@ while getopts ":b: t" opt; do
 		t)
 			echo "-t was triggered. Tests will be run" >&2
 			TEST=1
+			;;
+		a)
+			echo "-a was triggered. Static code analysis will be run" >&2
+			CODE_ANALYSIS=1
 			;;
 		\?)
 			echo "Invalid option: -$OPTARG" >&2
@@ -89,7 +109,7 @@ while getopts ":b: t" opt; do
 done
 
 # Call appropriate build function, with parameters if needed
-build_$BUILD $TEST
+build_$BUILD
 
 # SUCCESS
 exit 0
