144d6e23120369c8fc9dc8099f037a6504f62cfb
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2012-04-12 15:16:46 -0400

    * #9782: sdes_negotiator: fix iterator usage and set dangling pointers to 0

--- daemon/src/sip/sdes_negotiator.cpp
+++ daemon/src/sip/sdes_negotiator.cpp
@@ -178,7 +178,7 @@ bool SdesNegotiator::negotiate()
     std::vector<CryptoAttribute *> cryptoAttributeVector = parse();
     std::vector<CryptoAttribute *>::iterator iter_offer = cryptoAttributeVector.begin();
 
-    std::vector<CryptoSuiteDefinition>::iterator iter_local = localCapabilities_.begin();
+    std::vector<CryptoSuiteDefinition>::const_iterator iter_local = localCapabilities_.begin();
 
     bool negotiationSuccess = false;
 
@@ -197,10 +197,11 @@ bool SdesNegotiator::negotiate()
                     authTagLength_ = cryptoSuite_.substr(cryptoSuite_.size()-2, 2);
                 }
 
-                iter_local++;
+                ++iter_local;
             }
-            delete(*iter_offer);
-            iter_offer++;
+            delete *iter_offer;
+            *iter_offer = 0;
+            ++iter_offer;
         }
 
     } catch (const ParseError& exception) {
