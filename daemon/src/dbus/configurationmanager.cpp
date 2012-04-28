f8a9a02773e27aceb64286178d6512277a3e6542
Tristan Matthews <le.businessman@gmail.com>
2012-04-28 18:25:31 -0400

    * #9832: dbus: fix logging

--- daemon/src/dbus/configurationmanager.cpp
+++ daemon/src/dbus/configurationmanager.cpp
@@ -56,7 +56,7 @@ std::map<std::string, std::string> ConfigurationManager::getIp2IpDetails()
     SIPAccount *sipaccount = Manager::instance().getIP2IPAccount();
 
     if (!sipaccount) {
-        ERROR("ConfigurationManager: could not find IP2IP account");
+        ERROR("Could not find IP2IP account");
         return ip2ipAccountDetails;
     } else
         return sipaccount->getIp2IpDetails();
@@ -116,7 +116,7 @@ void ConfigurationManager::setTlsSettings(const std::map<std::string, std::strin
     SIPAccount * sipaccount = Manager::instance().getIP2IPAccount();
 
     if (!sipaccount) {
-        DEBUG("ConfigurationManager: Error: No valid account in set TLS settings");
+        DEBUG("No valid account in set TLS settings");
         return;
     }
 
@@ -264,7 +264,7 @@ int32_t ConfigurationManager::getAudioDeviceIndex(const std::string& name)
 
 std::string ConfigurationManager::getCurrentAudioOutputPlugin()
 {
-    DEBUG("ConfigurationManager: Get audio plugin %s", Manager::instance().getCurrentAudioOutputPlugin().c_str());
+    DEBUG("Get audio plugin %s", Manager::instance().getCurrentAudioOutputPlugin().c_str());
 
     return Manager::instance().getCurrentAudioOutputPlugin();
 }
