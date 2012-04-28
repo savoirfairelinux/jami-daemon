c7c05100cd819ec610d60fcfc2fe449c16255c95
Tristan Matthews <le.businessman@gmail.com>
2012-04-28 18:23:52 -0400

    * #9832: config: cleanup logging

--- daemon/src/config/config.cpp
+++ daemon/src/config/config.cpp
@@ -197,7 +197,7 @@ void ConfigTree::setConfigTreeItem(const std::string& section,
 bool
 ConfigTree::populateFromFile(const std::string& fileName)
 {
-    DEBUG("ConfigTree: Populate from file %s", fileName.c_str());
+    DEBUG("Populate from file %s", fileName.c_str());
 
     if (fileName.empty())
         return false;
