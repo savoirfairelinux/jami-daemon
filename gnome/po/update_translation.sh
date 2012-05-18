a96cb8cfc79d3f810e05866ffc6c62e7a8bff074
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-05-18 15:36:58 -0400

    #11210: update translations

--- gnome/po/update_translation.sh
+++ gnome/po/update_translation.sh
@@ -2,7 +2,7 @@
 #
 #  Copyright (C) 2004-2012 Savoir-Faire Linux Inc.
 #
-#  Author: Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
+#  Author: Alexandre Savard <alexandre.savard@savoirfairelinux.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
