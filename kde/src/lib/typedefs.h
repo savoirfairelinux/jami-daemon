2ad692ea182aa3b549d00c696cda2601c323cdd1
Emmanuel Lepage <emmanuel.lepage@savoirfairelinux.com>
2012-06-14 09:52:54 -0400

    [ #12438 ] Improve backward compatibility to compile on GCC 4.4

--- kde/src/lib/typedefs.h
+++ kde/src/lib/typedefs.h
@@ -45,5 +45,11 @@ typedef QMap<QString, int> MapStringInt;
    #define LIB_IMPORT
 #endif
 
+#if __GNUC__ < 4 || \
+              (__GNUC__ == 5 && (__GNUC_MINOR__ < 5 || \
+                                 (__GNUC_MINOR__ == 5)))
+#define nullptr 0
+#endif
+
 
 #endif
