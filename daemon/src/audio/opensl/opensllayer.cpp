d086c28cdf9edb0dcf5eaa34fac66f3e90e9a929
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-08-06 14:27:45 -0400

    #14371: Add basic opensl layer structure in source repository

---
+++ daemon/src/audio/opensl/opensllayer.cpp
@@ -0,0 +1,566 @@
+/*
+ *  Copyright (C) 2004, 2005, 2006, 2008, 2009, 2010, 2011 Savoir-Faire Linux Inc.
+ *  Author: Alexandre Savard <alexandre.savard@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+#include "pinknoise.h"
+#include "opensllayer.h"
+
+#define NB_AUDIO_BUFFER 10
+#define SAMPLE_PER_FRAME 160
+
+#define WITH_STATIC_BUFFER 1
+
+static long sawPtr = 0;
+static void generateSawTooth(short *buffer, int length) {
+    assert(NULL != buffer);
+    assert(length > 0);
+
+    unsigned int i;
+    for (i = 0; i < length; ++i, ++sawPtr) {
+        buffer[i] = 32768 - ((sawPtr % 100) * 660);
+    }
+}
+
+class OpenSLThread : public ost::Thread {
+    public:
+        OpenSLThread(OpenSLLayer *opensl);
+
+        ~OpenSLThread() { ost::Thread::terminate(); }
+
+        void initAudioLayer();
+
+        virtual void run();
+
+    private:
+        NON_COPYABLE(OpenSLThread);
+        OpenSLLayer* opensl_;
+};
+
+OpenSLThread::OpenSLThread(OpenSLLayer *opensl)
+    : ost::Thread(), opensl_(opensl)
+{
+}
+
+void OpenSLThread::initAudioLayer(void)
+{
+    opensl_->initAudioEngine();
+    opensl_->initAudioPlayback();
+}
+
+/**
+ * Reimplementation of run()
+ */
+void OpenSLThread::run()
+{
+    initAudioLayer();
+
+    opensl_->startAudioPlayback();
+    opensl_->startAudioCapture();
+
+    opensl_->isStarted_ = true;
+    while (opensl_->isStarted_) {
+        opensl_->audioCallback();
+        ost::Thread::sleep(20 /* ms */);
+    }
+
+    opensl_->shutdownAudioEngine();
+}
+
+// Constructor
+OpenSLLayer::OpenSLLayer()
+    : indexIn_(0)
+    , indexOut_(0)
+    , indexRing_(0)
+    , audioThread_(NULL)
+    , isStarted_(false)
+    , engineObject(NULL)
+    , engineInterface(NULL)
+    , outputMixer(NULL)
+    , playerObject(NULL)
+    , playerInterface(NULL)
+    , playbackBufferQueue(NULL)
+    , audioBufferList()
+    , emptyBufferList()
+{
+    initBufferList();
+}
+
+// Destructor
+OpenSLLayer::~OpenSLLayer()
+{
+    isStarted_ = false;
+    delete audioThread_;
+
+    cleanupBufferList();
+}
+
+void
+OpenSLLayer::initBufferList()
+{
+    for(int i = 0; i < NB_AUDIO_BUFFER; i++) {
+        AudioBuffer *audiobuffer = new AudioBuffer(SAMPLE_PER_FRAME);
+        emptyBufferList.push_back(audiobuffer);
+    }
+
+}
+
+void
+OpenSLLayer::cleanupBufferList()
+{
+    BufferList::iterator iter;
+    for(iter = audioBufferList.begin(); iter != audioBufferList.end(); iter++) {
+        delete *iter;
+    }
+    audioBufferList.clear();
+
+    for(iter = emptyBufferList.begin(); iter != emptyBufferList.end(); iter++) {
+        delete *iter;
+    }
+    emptyBufferList.clear();
+}
+
+void
+OpenSLLayer::startStream()
+{
+    if (audioThread_ == NULL) {
+        audioThread_ = new OpenSLThread(this);
+        audioThread_->start();
+    }
+}
+
+void
+OpenSLLayer::stopStream()
+{
+    isStarted_ = false;
+
+    delete audioThread_;
+    audioThread_ = NULL;
+}
+
+void
+OpenSLLayer::initAudioEngine()
+{
+    SLresult result;
+
+    printf("Create Audio Engine\n");
+    result = slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Realize Audio Engine\n");
+    result = (*engineObject)->Realize(engineObject, SL_BOOLEAN_FALSE);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Create Audio Engine Interface\n");
+    result = (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &engineInterface);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Create Output Mixer\n");
+    const SLInterfaceID ids[1] = {SL_IID_ENVIRONMENTALREVERB};
+    const SLboolean req[1] = {SL_BOOLEAN_FALSE};
+    result = (*engineInterface)->CreateOutputMix(engineInterface, &outputMixer, 1, ids, req);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Realize Output Mixer\n");
+    result = (*outputMixer)->Realize(outputMixer, SL_BOOLEAN_FALSE);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Audio Engine Initialization Done\n");
+}
+
+void
+OpenSLLayer::shutdownAudioEngine()
+{
+    // destroy buffer queue audio player object, and invalidate all associated interfaces
+    printf("Shutdown audio player\n");
+    if (playerObject != NULL) {
+        (*playerObject)->Destroy(playerObject);
+        playerObject = NULL;
+        playerInterface = NULL;
+        playbackBufferQueue = NULL;
+    }
+
+    // destroy output mix object, and invalidate all associated interfaces
+    printf("Shutdown audio mixer\n");
+    if (outputMixer != NULL) {
+        (*outputMixer)->Destroy(outputMixer);
+        outputMixer= NULL;
+    }
+
+    // destroy engine object, and invalidate all associated interfaces
+    printf("Shutdown audio engine\n");
+    if (engineObject != NULL) {
+        (*engineObject)->Destroy(engineObject);
+        engineObject = NULL;
+        engineInterface = NULL;
+    }
+}
+
+void
+OpenSLLayer::initAudioPlayback()
+{
+    assert(NULL != engineObject);
+    assert(NULL != engineInterface);
+    assert(NULL != outputMixer);
+
+    SLresult result;
+
+    // Initialize the location of the buffer queue
+    printf("Create playback queue\n");
+    SLDataLocator_AndroidSimpleBufferQueue bufferLocation = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
+
+    // Initnialize the audio format for this queue
+    printf("Setting audio format\n");
+    SLDataFormat_PCM audioFormat = {SL_DATAFORMAT_PCM, 1,
+                                    SL_SAMPLINGRATE_8,
+                                    SL_PCMSAMPLEFORMAT_FIXED_16,
+                                    SL_PCMSAMPLEFORMAT_FIXED_16,
+                                    SL_SPEAKER_FRONT_CENTER,
+                                    SL_BYTEORDER_LITTLEENDIAN};
+
+    // Create the audio source
+    printf("Set Audio Sources\n");
+    SLDataSource audioSource = {&bufferLocation, &audioFormat};
+
+    // Cofiguration fo the audio sink as an output mixer
+    printf("Set output mixer location\n");
+    SLDataLocator_OutputMix mixerLocation = {SL_DATALOCATOR_OUTPUTMIX, outputMixer};
+    SLDataSink audioSink = {&mixerLocation, NULL};
+
+#if 1
+    const SLInterfaceID ids[3] = {SL_IID_BUFFERQUEUE, SL_IID_EFFECTSEND,
+            /*SL_IID_MUTESOLO,*/ SL_IID_VOLUME};
+    const SLboolean req[3] = {SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE,
+            /*SL_BOOLEAN_TRUE,*/ SL_BOOLEAN_TRUE};
+#else
+    const SLInterfaceID ids[1] = {SL_IID_ANDROIDSIMPLEBUFFERQUEUE};
+    const SLboolean req[1] = {SL_BOOLEAN_TRUE};
+#endif
+
+
+    // create audio player
+    printf("Create audio player\n");
+    result = (*engineInterface)->CreateAudioPlayer(engineInterface, &playerObject, &audioSource, &audioSink, 3, ids, req);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Realize audio player\n");
+    result = (*playerObject)->Realize(playerObject, SL_BOOLEAN_FALSE);
+    assert(SL_RESULT_SUCCESS == result);
+
+    // create audio interface
+    printf("Create audio player interface\n");
+    result = (*playerObject)->GetInterface(playerObject, SL_IID_PLAY, &playerInterface);
+    assert(SL_RESULT_SUCCESS == result);
+
+    // create the buffer queue interface
+    printf("Create buffer queue interface\n");
+    result = (*playerObject)->GetInterface(playerObject, SL_IID_BUFFERQUEUE, &playbackBufferQueue);
+    assert(SL_RESULT_SUCCESS == result);
+
+    // register the buffer queue on the buffer object
+    printf("Register audio callback\n");
+    result = (*playbackBufferQueue)->RegisterCallback(playbackBufferQueue, audioPlaybackCallback, this);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Audio Playback Initialization Done\n");
+}
+
+void
+OpenSLLayer::initAudioCapture()
+{
+    SLresult result;
+
+    // configure audio source
+    printf("Configure audio source\n");
+    SLDataLocator_IODevice deviceLocator = {SL_DATALOCATOR_IODEVICE, SL_IODEVICE_AUDIOINPUT,
+            SL_DEFAULTDEVICEID_AUDIOINPUT, NULL};
+    SLDataSource audioSource = {&deviceLocator, NULL};
+
+    // configure audio sink
+    printf("Configure audio sink\n");
+    SLDataLocator_AndroidSimpleBufferQueue bufferLocator = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
+    SLDataFormat_PCM audioFormat = {SL_DATAFORMAT_PCM, 1, SL_SAMPLINGRATE_16,
+        SL_PCMSAMPLEFORMAT_FIXED_16, SL_PCMSAMPLEFORMAT_FIXED_16,
+        SL_SPEAKER_FRONT_CENTER, SL_BYTEORDER_LITTLEENDIAN};
+    SLDataSink audioSink = {&bufferLocator, &audioFormat};
+
+    // create audio recorder
+    // (requires the RECORD_AUDIO permission)
+    printf("Create audio recorder\n");
+    const SLInterfaceID id[1] = {SL_IID_ANDROIDSIMPLEBUFFERQUEUE};
+    const SLboolean req[1] = {SL_BOOLEAN_TRUE};
+    result = (*engineInterface)->CreateAudioRecorder(engineInterface, &recorderObject, &audioSource,
+            &audioSink, 1, id, req);
+    if (SL_RESULT_SUCCESS != result) {
+        printf("Error: could not create audio recorder");
+        return;
+    }
+
+    // realize the audio recorder
+    printf("Realize the audio recorder\n");
+    result = (*recorderObject)->Realize(recorderObject, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != result) {
+        printf("Error: could not realize audio recorder");
+        return;
+    }
+
+    // get the record interface
+    printf("Create the record interface\n");
+    result = (*recorderObject)->GetInterface(recorderObject, SL_IID_RECORD, &recorderInterface);
+    assert(SL_RESULT_SUCCESS == result);
+
+    // get the buffer queue interface
+    printf("Create the buffer queue interface\n");
+    result = (*recorderObject)->GetInterface(recorderObject, SL_IID_ANDROIDSIMPLEBUFFERQUEUE,
+            &recorderBufferQueue);
+    assert(SL_RESULT_SUCCESS == result);
+
+    // register callback on the buffer queue
+    printf("Register the audio capture callback\n");
+    result = (*recorderBufferQueue)->RegisterCallback(recorderBufferQueue, audioCaptureCallback, this);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Audio capture initialized\n");
+}
+
+
+
+void
+OpenSLLayer::startAudioPlayback()
+{
+    AudioBuffer buffer(SAMPLE_PER_FRAME);
+    int nbInitialBuf = 2;
+
+    assert(NULL != playbackBufferQueue);
+
+    printf("Start audio playback\n");
+
+    SLresult result;
+
+    for(int i = 0; i < nbInitialBuf; i++) {
+        printf("generate sawtooth\n");
+        // generate_pink_noise((uint8_t*)buffer, 0, bufferSize);
+        generateSawTooth(&(*buffer.begin()), buffer.size());
+        result = (*playbackBufferQueue)->Enqueue(playbackBufferQueue, &(*buffer.begin()), buffer.size());
+        if (SL_RESULT_SUCCESS != result) {
+            printf("Error could not enqueue initial buffers\n");
+        }
+    }
+
+    result = (*playerInterface)->SetPlayState(playerInterface, SL_PLAYSTATE_PLAYING);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Audio playback started\n");
+}
+
+void
+OpenSLLayer::startAudioCapture()
+{
+    int nbInitialBuf = 2;
+
+    printf("Start audio capture\n");
+
+    SLresult result;
+    // result = (*recorderInterface)->SetRecordState(recorderInterface, SL_RECORDSTATE_RECORDING);
+    // assert(SL_RESULT_SUCCESS == result);
+
+    // in case already recording, stop recording and clear buffer queue
+    result = (*recorderInterface)->SetRecordState(recorderInterface, SL_RECORDSTATE_STOPPED);
+    assert(SL_RESULT_SUCCESS == result);
+    result = (*recorderBufferQueue)->Clear(recorderBufferQueue);
+    assert(SL_RESULT_SUCCESS == result);
+
+    // enqueue an empty buffer to be filled by the recorder
+    // (for streaming recording, we enqueue at least 2 empty buffers to start things off)
+    AudioBuffer *buffer = emptyBufferList.front();
+    emptyBufferList.pop_front();
+    result = (*recorderBufferQueue)->Enqueue(recorderBufferQueue, &(*buffer->begin()), buffer->size() * sizeof(short));
+    // the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
+    // which for this code example would indicate a programming error
+    assert(SL_RESULT_SUCCESS == result);
+
+    // start recording
+    result = (*recorderInterface)->SetRecordState(recorderInterface, SL_RECORDSTATE_RECORDING);
+    assert(SL_RESULT_SUCCESS == result);
+
+    printf("Audio capture started\n");
+}
+
+void
+OpenSLLayer::stopAudioPlayback()
+{
+    printf("Stop audio playback\n");
+
+    SLresult result;
+    result = (*playerInterface)->SetPlayState(playerInterface, SL_PLAYSTATE_STOPPED);
+    assert(SL_RESULT_SUCCESS == result);
+}
+
+void
+OpenSLLayer::stopAudioCapture()
+{
+    printf("Stop audio capture\n");
+
+    SLresult result;
+    result = (*recorderInterface)->SetRecordState(recorderInterface, SL_RECORDSTATE_STOPPED);
+    assert(SL_RESULT_SUCCESS == result);
+}
+
+std::vector<std::string>
+OpenSLLayer::getCaptureDeviceList() const
+{
+    std::vector<std::string> captureDeviceList;
+
+    return captureDeviceList;
+}
+
+std::vector<std::string>
+OpenSLLayer::getPlaybackDeviceList() const
+{
+    std::vector<std::string> playbackDeviceList;
+
+    return playbackDeviceList;
+}
+
+bool OpenSLLayer::audioCallback()
+{
+}
+
+void OpenSLLayer::audioPlaybackCallback(SLAndroidSimpleBufferQueueItf queue, void *context)
+{
+#if WITH_STATIC_BUFFER
+    AudioBuffer staticbuffer(SAMPLE_PER_FRAME);
+    AudioBuffer *buffer = &staticbuffer;
+#endif
+
+    // assert(queue == playbackBufferQueue);
+    assert(NULL != queue);
+    assert(NULL != context);
+
+    SLresult result;
+
+    // std::vector<SFLDataFormat> out(maxSamples, 0);
+    // SFLDataFormat * const out_ptr = &(*out.begin());
+    // generate_pink_noise((uint8_t*)out_ptr, 0, out.size());
+    OpenSLLayer *opensl = (OpenSLLayer *)context;
+
+    BufferList &audioBufferList = opensl->getAudioBufferList();
+    BufferList &emptyBufferList = opensl->getEmptyBufferList();
+
+#if WITH_STATIC_BUFFER != 1
+    if(audioBufferList.empty()) {
+        printf("Nothing to play!!!\n");
+        return;
+    }
+
+    AudioBuffer *buffer = audioBufferList.back();
+    if(NULL == buffer) {
+        printf("Error could not retreive audio from audio buffer list\n");
+        return;
+    }
+
+    audioBufferList.pop_back();
+#endif
+    // generateSawTooth(&(*buffer->begin()), buffer->size());
+    // generate_pink_noise((uint8_t*)buffer, 0, bufferSize);
+
+    result = (*queue)->Enqueue(queue, &(*buffer->begin()), buffer->size());
+    if (SL_RESULT_SUCCESS != result) {
+        printf("Error could not enqueue buffers in playback callback\n");
+    }
+
+#if WITH_STATIC_BUFFER != 1
+    emptyBufferList.push_front(buffer);
+#endif
+}
+
+void OpenSLLayer::audioCaptureCallback(SLAndroidSimpleBufferQueueItf queue, void *context)
+{
+#if WITH_STATIC_BUFFER
+    AudioBuffer staticbuffer(SAMPLE_PER_FRAME);
+    AudioBuffer *buffer = &staticbuffer;
+#endif
+
+    assert(NULL != queue);
+    assert(NULL != context);
+
+    OpenSLLayer *opensl = (OpenSLLayer *)context;
+
+    BufferList &audioBufferList = opensl->getAudioBufferList();
+    BufferList &emptyBufferList = opensl->getEmptyBufferList();
+
+    if(emptyBufferList.empty()) {
+        printf("No buffer available to store recording!!!!\n");
+        return;
+    }
+
+    SLRecordItf recorderInterface = opensl->getRecorderInterface();
+    SLAndroidSimpleBufferQueueItf recorderBufferQueue = opensl->getRecorderBufferQueue();
+
+    SLresult result;
+
+#if WITH_STATIC_BUFFER != 1
+    AudioBuffer *buffer = emptyBufferList.back();
+    if(NULL == buffer) {
+        printf("Error could not get buffer from emptyBufferList in audio playback\n");
+        return;
+    }
+
+    emptyBufferList.pop_back();
+#endif
+
+    // enqueue an empty buffer to be filled by the recorder
+    // (for streaming recording, we enqueue at least 2 empty buffers to start things off)
+    result = (*recorderBufferQueue)->Enqueue(recorderBufferQueue, &(*buffer->begin()), buffer->size() * sizeof(short));
+    // the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
+    // which for this code example would indicate a programming error
+    assert(SL_RESULT_SUCCESS == result);
+
+#if WITH_STATIC_BUFFER != 1
+    audioBufferList.push_front(buffer);
+#endif
+}
+
+void OpenSLLayer::updatePreference(AudioPreference &preference, int index, PCMType type)
+{
+#ifdef OUTSIDE_TESTING
+    switch (type) {
+        case SFL_PCM_PLAYBACK:
+            break;
+        case AudioLayer::SFL_PCM_CAPTURE:
+            break;
+        case AudioLayer::SFL_PCM_RINGTONE:
+            break;
+        default:
+            break;
+    }
+#endif
+}
---
+++ daemon/src/audio/opensl/opensllayer.h
@@ -0,0 +1,255 @@
+/*
+ *  Copyright (C) 2004, 2005, 2006, 2008, 2009, 2010, 2011 Savoir-Faire Linux Inc.
+ *  Author: Alexandre Savard <alexandre.savard@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+
+#ifndef _OPENSL_LAYER_H
+#define _OPENSL_LAYER_H
+
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+// #define OUTSIDE_TESTING 1
+#include <vector>
+#include <list>
+
+#include "cc++/thread.h"
+
+enum PCMType {
+    SFL_PCM_BOTH = 0x0021,          /** To open both playback and capture devices */
+    SFL_PCM_PLAYBACK = 0x0022,      /** To open playback device only */
+    SFL_PCM_CAPTURE = 0x0023,       /** To open capture device only */
+    SFL_PCM_RINGTONE = 0x0024       /** To open the ringtone device only */
+};
+
+class AudioPreference;
+
+#include "noncopyable.h"
+
+class OpenSLThread;
+
+typedef std::vector<short> AudioBuffer;
+typedef std::list<AudioBuffer *> BufferList;
+
+/**
+ * @file  OpenSLLayer.h
+ * @brief Main sound class for android. Manages the data transfers between the application and the hardware.
+ */
+
+class OpenSLLayer {// : public AudioLayer {
+    public:
+        /**
+         * Constructor
+         */
+        OpenSLLayer();
+
+        /**
+         * Destructor
+         */
+        ~OpenSLLayer();
+
+        void initBufferList();
+
+        void cleanupBufferList();
+
+        /**
+         * Start the capture stream and prepare the playback stream.
+         * The playback starts accordingly to its threshold
+         */
+        virtual void startStream();
+
+        /**
+         * Stop the playback and capture streams.
+         * Drops the pending frames and put the capture and playback handles to PREPARED state
+         */
+        virtual void stopStream();
+
+        /**
+         * Scan the sound card available for capture on the system
+         * @return std::vector<std::string> The vector containing the string description of the card
+         */
+        virtual std::vector<std::string> getCaptureDeviceList() const;
+
+        /**
+         * Scan the sound card available for capture on the system
+         * @return std::vector<std::string> The vector containing the string description of the card
+         */
+        virtual std::vector<std::string> getPlaybackDeviceList() const;
+
+        /**
+         * OpenSL standard object interface
+         */
+        SLObjectItf getEngineObject(void) { return engineObject; }
+
+        /**
+         * OpenSL sound engine interface
+         */
+        SLEngineItf getEngineInterface(void) { return engineInterface; }
+
+        /**
+         * Output mix interface
+         */
+        SLObjectItf getOutputMixer(void) { return outputMixer; }
+
+        SLObjectItf getPlayerObject(void) { return playerObject; }
+
+        SLObjectItf getRecorderObject(void) { return recorderObject; }
+
+        /**
+         *
+         */
+        SLPlayItf getPlayerInterface(void) { return playerInterface; }
+
+        SLRecordItf getRecorderInterface(void) { return recorderInterface; }
+
+        /**
+         * OpenSL playback buffer
+         */
+        SLAndroidSimpleBufferQueueItf getPLaybackBufferQueue(void) { return playbackBufferQueue; }
+
+        SLAndroidSimpleBufferQueueItf getRecorderBufferQueue(void) { return recorderBufferQueue; }
+
+        BufferList &getAudioBufferList(void) { return audioBufferList; }
+        BufferList &getEmptyBufferList(void) { return emptyBufferList; }
+
+        void initAudioEngine();
+
+        void shutdownAudioEngine();
+
+        void initAudioPlayback();
+
+        void initAudioCapture();
+
+        void startAudioPlayback();
+
+        void startAudioCapture();
+
+        void stopAudioPlayback();
+
+        void stopAudioCapture();
+
+        /**
+         * Main audio callback method, all data transfer between application and hardware is preformed
+         * inside this method.
+         */
+        bool audioCallback();
+
+        static void audioPlaybackCallback(SLAndroidSimpleBufferQueueItf bq, void *context);
+        static void audioCaptureCallback(SLAndroidSimpleBufferQueueItf bq, void *context);
+
+        /**
+         * Get the index of the audio card for capture
+         * @return int The index of the card used for capture
+         *                     0 for the first available card on the system, 1 ...
+         */
+        virtual int getIndexCapture() const {
+            return indexIn_;
+        }
+
+        /**
+         * Get the index of the audio card for playback
+         * @return int The index of the card used for playback
+         *                     0 for the first available card on the system, 1 ...
+         */
+        virtual int getIndexPlayback() const {
+            return indexOut_;
+        }
+
+        /**
+         * Get the index of the audio card for ringtone (could be differnet from playback)
+         * @return int The index of the card used for ringtone
+         *                 0 for the first available card on the system, 1 ...
+         */
+        virtual int getIndexRingtone() const {
+            return indexRing_;
+        }
+
+    private:
+        friend class OpenSLThread;
+
+        /**
+         * Number of audio cards on which capture stream has been opened
+         */
+        int indexIn_;
+
+        /**
+         * Number of audio cards on which playback stream has been opened
+         */
+        int indexOut_;
+
+        /**
+         * Number of audio cards on which ringtone stream has been opened
+         */
+        int indexRing_;
+
+        NON_COPYABLE(OpenSLLayer);
+
+        virtual void updatePreference(AudioPreference &pref, int index, PCMType type);
+
+        OpenSLThread *audioThread_;
+
+        bool isStarted_;
+
+        /**
+         * OpenSL standard object interface
+         */
+        SLObjectItf engineObject;
+
+        /**
+         * OpenSL sound engine interface
+         */
+        SLEngineItf engineInterface;
+
+        /**
+         * Output mix interface
+         */
+        SLObjectItf outputMixer;
+
+        SLObjectItf playerObject;
+
+        SLObjectItf recorderObject;
+
+        /**
+         *
+         */
+        SLPlayItf playerInterface;
+
+        SLRecordItf recorderInterface;
+
+        /**
+         * OpenSL playback buffer
+         */
+        SLAndroidSimpleBufferQueueItf playbackBufferQueue;
+
+        SLAndroidSimpleBufferQueueItf recorderBufferQueue;
+
+        BufferList audioBufferList;
+        BufferList emptyBufferList;
+
+};
+
+#endif // _OPENSL_LAYER_H_
