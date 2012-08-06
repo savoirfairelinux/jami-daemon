14cca1c6d7799f75c5e6cdc06af102cec9b67a30
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2012-08-06 17:32:54 -0400

    * #11278: alsa: increase sleep between calls to open

--- daemon/src/audio/alsa/alsalayer.cpp
+++ daemon/src/audio/alsa/alsalayer.cpp
@@ -160,7 +160,7 @@ bool AlsaLayer::openDevice(snd_pcm_t **pcm, const std::string &dev, snd_pcm_stre
 
     // Retry if busy, since dmix plugin may not have released the device yet
     for (int tries = 0; tries < MAX_RETRIES and err == -EBUSY; ++tries) {
-        const struct timespec req = {0, 10000000};
+        const struct timespec req = {0, 100000000L};
         nanosleep(&req, 0);
         err = snd_pcm_open(pcm, dev.c_str(), stream, 0);
     }
