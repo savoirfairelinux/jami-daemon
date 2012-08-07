012f6ba3684e542752cde11f0cd0975ec6a6a205
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-08-07 17:25:30 -0400

    #14371: Add libsound android mk file

---
+++ daemon/src/audio/sound/Android.mk
@@ -0,0 +1,47 @@
+LOCAL_PATH:= $(call my-dir)
+
+# FIXME
+MY_PREFIX=/sdcard
+MY_DATADIR=
+MY_PJPROJECT="pjproject/android"
+MY_PJDIR=
+MY_COMMONCPP=commoncpp2-1.8.1-android
+MY_CCRTP=ccrtp-1.8.0-android
+MY_LIBSAMPLE=libsamplerate-0.1.8
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := audiofile.cpp \
+		tone.cpp \
+		tonelist.cpp \
+		dtmf.cpp \
+		dtmfgenerator.cpp
+
+# FIXME
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/.. \
+					$(LOCAL_PATH)/../.. \
+					$(LOCAL_PATH)/../sip \
+					$(LOCAL_PATH)/../config \
+					$(LOCAL_PATH)/../history \
+					$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
+					$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
+					$(APP_PROJECT_PATH)/jni/$(MY_LIBSAMPLE)/src \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/speex/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/build/speex \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjsip/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib-util/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjmedia/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjnath/include \
+
+#LOCAL_CPP_EXTENSION := .cpp .h
+
+LOCAL_MODULE := libsound
+LOCAL_CPPFLAGS += $(NETWORKMANAGER) \
+				  -DCCPP_PREFIX \
+				  -DPREFIX=\"$(MY_PREFIX)\" \
+				  -DPROGSHAREDIR=\"${MY_DATADIR}/sflphone\" \
+				  -DHAVE_CONFIG_H \
+				  -std=gnu++0x -frtti -fpermissive -fexceptions
+
+include $(BUILD_STATIC_LIBRARY)
