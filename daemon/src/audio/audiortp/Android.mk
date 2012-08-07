805f10874e87d8d891f691cec346082c8f3bff8a
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-08-07 19:28:27 -0400

    #14371: Add aduiortp mk file

---
+++ daemon/src/audio/audiortp/Android.mk
@@ -0,0 +1,49 @@
+LOCAL_PATH:= $(call my-dir)
+
+# FIXME
+MY_PREFIX=/sdcard
+MY_DATADIR=
+MY_PJPROJECT="pjproject/android"
+MY_PJDIR=
+MY_COMMONCPP=commoncpp2-1.8.1-android
+MY_CCRTP=ccrtp-1.8.0-android
+MY_LIBSAMPLE=libsamplerate-0.1.8
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := audio_rtp_session.cpp \
+		audio_symmetric_rtp_session.cpp \
+		audio_rtp_record_handler.cpp \
+		audio_rtp_factory.cpp \
+		audio_srtp_session.cpp
+
+# FIXME
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/.. \
+					$(LOCAL_PATH)/../.. \
+					$(LOCAL_PATH)/../sip \
+					$(LOCAL_PATH)/../config \
+					$(LOCAL_PATH)/../history \
+					$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
+					$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
+					$(APP_PROJECT_PATH)/jni/$(MY_LIBSAMPLE)/src \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/speex/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/build/speex \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjsip/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib-util/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjmedia/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjnath/include \
+
+#LOCAL_CPP_EXTENSION := .cpp .h
+
+LOCAL_MODULE := librtp
+LOCAL_CPPFLAGS += $(NETWORKMANAGER) \
+				  -DCCPP_PREFIX \
+				  -DPREFIX=\"$(MY_PREFIX)\" \
+				  -DPROGSHAREDIR=\"${MY_DATADIR}/sflphone\" \
+				  -DHAVE_CONFIG_H \
+				  -std=gnu++0x -frtti -fpermissive
+
+LOCAL_SHARED_LIBRARIES += libccrtp1 libccgnu2
+
+include $(BUILD_STATIC_LIBRARY)
