528c85e7acc022f339dd70ae2a573018542cafe4
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-08-07 20:31:56 -0400

    #14371: Add speex and dbus-c++ path to compile rtp layer

--- daemon/src/audio/audiortp/Android.mk
+++ daemon/src/audio/audiortp/Android.mk
@@ -3,11 +3,13 @@ LOCAL_PATH:= $(call my-dir)
 # FIXME
 MY_PREFIX=/sdcard
 MY_DATADIR=
-MY_PJPROJECT="pjproject/android"
+MY_PJPROJECT=pjproject-android/android
 MY_PJDIR=
 MY_COMMONCPP=commoncpp2-1.8.1-android
 MY_CCRTP=ccrtp-1.8.0-android
 MY_LIBSAMPLE=libsamplerate-0.1.8
+MY_DBUS=libdbus-c++-0.9.0-android
+MY_SPEEX=speex
 
 include $(CLEAR_VARS)
 
@@ -26,6 +28,7 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/.. \
 					$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
 					$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
 					$(APP_PROJECT_PATH)/jni/$(MY_LIBSAMPLE)/src \
+					$(APP_PROJECT_PATH)/jni/$(MY_SPEEX)/include \
 					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/speex/include \
 					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/build/speex \
 					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjsip/include \
@@ -33,6 +36,8 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/.. \
 					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib-util/include \
 					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjmedia/include \
 					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjnath/include \
+					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjsip-apps/src/3rdparty_media_sample \
+					$(APP_PROJECT_PATH)/jni/$(MY_DBUS)/include
 
 #LOCAL_CPP_EXTENSION := .cpp .h
 
