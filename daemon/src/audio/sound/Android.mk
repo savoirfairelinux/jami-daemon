0e2fd96304ec111d0e2852fe8b03a9751d2036be
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-08-08 16:51:53 -0400

    #14470: Refactor sound static library Android.mk

--- daemon/src/audio/sound/Android.mk
+++ daemon/src/audio/sound/Android.mk
@@ -1,10 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
 # FIXME
-MY_PREFIX=/sdcard
-MY_DATADIR=
-MY_PJPROJECT="pjproject/android"
-MY_PJDIR=
 MY_COMMONCPP=commoncpp2-1.8.1-android
 MY_CCRTP=ccrtp-1.8.0-android
 MY_LIBSAMPLE=libsamplerate-0.1.8
@@ -20,23 +16,12 @@ LOCAL_SRC_FILES := audiofile.cpp \
 # FIXME
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/.. \
 					$(LOCAL_PATH)/../.. \
-					$(LOCAL_PATH)/../sip \
-					$(LOCAL_PATH)/../config \
-					$(LOCAL_PATH)/../history \
 					$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
 					$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
 					$(APP_PROJECT_PATH)/jni/$(MY_LIBSAMPLE)/src \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/speex/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/build/speex \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjsip/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib-util/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjmedia/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjnath/include \
-
-#LOCAL_CPP_EXTENSION := .cpp .h
 
 LOCAL_MODULE := libsound
+
 LOCAL_CPPFLAGS += $(NETWORKMANAGER) \
 				  -DCCPP_PREFIX \
 				  -DPREFIX=\"$(MY_PREFIX)\" \
