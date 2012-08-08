9c87fa04d61dbf99c898a9bfdd83da24d4b89d53
Alexandre Savard <alexandre.savard@savoirfairelinux.com>
2012-08-08 18:17:54 -0400

    #14470: Refactor libaudio build for android

--- daemon/src/audio/Android.mk
+++ daemon/src/audio/Android.mk
@@ -4,12 +4,7 @@ include $(call all-subdir-makefiles)
 LOCAL_PATH := $(MYAUDIO_LOCAL_PATH)
 
 # FIXME
-MY_PREFIX=/sdcard
-MY_DATADIR=
-MY_PJPROJECT="pjproject/android"
-MY_PJDIR=
 MY_COMMONCPP=commoncpp2-1.8.1-android
-MY_CCRTP=ccrtp-1.8.0-android
 MY_LIBSAMPLE=libsamplerate-0.1.8
 MY_DBUS=libdbus-c++-0.9.0-android
 
@@ -29,30 +24,18 @@ LOCAL_SRC_FILES := audioloop.cpp \
 		dcblocker.cpp
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/.. \
-					$(LOCAL_PATH)/../.. \
-					$(LOCAL_PATH)/../sip \
-					$(LOCAL_PATH)/../config \
-					$(LOCAL_PATH)/../history \
-					$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
-					$(APP_PROJECT_PATH)/jni/$(MY_CCRTP)/src \
-					$(APP_PROJECT_PATH)/jni/$(MY_LIBSAMPLE)/src \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/speex/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/third_party/build/speex \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjsip/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjlib-util/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjmedia/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_PJPROJECT)/pjnath/include \
-					$(APP_PROJECT_PATH)/jni/$(MY_DBUS)/include \
+			$(LOCAL_PATH)/../.. \
+			$(APP_PROJECT_PATH)/jni/$(MY_COMMONCPP)/inc \
+			$(APP_PROJECT_PATH)/jni/$(MY_DBUS)/include \
+			$(APP_PROJECT_PATH)/jni/$(MY_LIBSAMPLE)/src
 
 LOCAL_MODULE := libaudio
+
 LOCAL_CPPFLAGS += $(NETWORKMANAGER) \
 				  -DCCPP_PREFIX \
 				  -DPREFIX=\"$(MY_PREFIX)\" \
 				  -DPROGSHAREDIR=\"${MY_DATADIR}/sflphone\" \
 				  -DHAVE_CONFIG_H \
 				  -std=gnu++0x -frtti -fpermissive -fexceptions
-
-#LOCAL_SHARED_LIBRARIES += libOpenSLES
 
 include $(BUILD_STATIC_LIBRARY)
