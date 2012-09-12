8277f42b7646b6219c5eaa5e244e8611299662d1
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2012-09-12 11:37:43 -0400

    * #15493: fix FSF address for gnome/tests

--- gnome/tests/check_dbus.c
+++ gnome/tests/check_dbus.c
@@ -14,7 +14,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  *
  *  If you modify this program, or any covered work, by linking or
  *  combining it with the OpenSSL project's OpenSSL library (or a
