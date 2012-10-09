aa6071f19600a2cecbe93f2865146a521fde330d
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2012-10-09 11:37:41 -0400

    * #16378: pjsip: don't build unused sound module

--- daemon/libs/compile_pjsip.sh
+++ daemon/libs/compile_pjsip.sh
@@ -2,4 +2,4 @@
 
 # TODO: autotools should be doing this
 cd "`dirname $BASH_SOURCE`"/pjproject
-CFLAGS=-fPIC ./configure && make dep && make -j1 && echo "pjsip successfully compiled"
+CFLAGS="-fPIC" ./configure --disable-sound && make dep && make -j1 && echo "pjsip successfully compiled"
