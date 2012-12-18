708c2de133be931915c253f50bc1669cf70bfafe
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2012-12-17 22:25:58 -0500

    * #18603: libs: disable video when building pjsip

--- daemon/libs/compile_pjsip.sh
+++ daemon/libs/compile_pjsip.sh
@@ -2,4 +2,4 @@
 
 # TODO: autotools should be doing this
 cd "`dirname $BASH_SOURCE`"/pjproject-2.0.1
-CFLAGS="-fPIC" ./configure --disable-sound && make dep && make -j1 && echo "pjsip successfully compiled"
+CFLAGS="-fPIC" ./configure --disable-sound --disable-video && make dep && make -j1 && echo "pjsip successfully compiled"
