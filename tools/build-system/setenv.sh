4efc9e07dde9684f34c318bb6008756c3e4a1e05
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2013-01-03 11:28:02 -0500

    [#18940] Do not build packages for Natty anymore @1h

--- tools/build-system/setenv.sh
+++ tools/build-system/setenv.sh
@@ -22,5 +22,5 @@ export REFERENCE_REPOSITORY="${ROOT_DIR}/sflphone-source-repository"
 
 export WORKING_DIR="${ROOT_DIR}/sflphone-build-repository/tools/build-system"
 export LAUNCHPAD_DIR="${WORKING_DIR}/launchpad"
-LAUNCHPAD_DISTRIBUTIONS=("natty" "oneiric" "precise" "quantal")
+LAUNCHPAD_DISTRIBUTIONS=("oneiric" "precise" "quantal")
 export LAUNCHPAD_DISTRIBUTIONS
