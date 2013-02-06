e51dcaeaba5b2f5952e819375f035e2cc40b581f
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2013-02-06 11:29:59 -0500

    gnome: fix make check

--- gnome/autogen.sh
+++ gnome/autogen.sh
@@ -22,7 +22,7 @@ fi
 GNOMEDOC=`which yelp-build`
 if test -z $GNOMEDOC; then
         echo "*** The tools to build the documentation are not found,"
-        echo "    please intall the yelp-tools package ***"
+        echo "    please install the yelp-tools package ***"
         exit 1
 fi
 
