b725100dc85b9028b45d64329bd3cf0fab2ad3a0
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2013-02-20 17:33:02 -0500

    * #20661: opus: use system opus if available

--- daemon/src/audio/codecs/opus.cpp
+++ daemon/src/audio/codecs/opus.cpp
@@ -30,51 +30,14 @@
 #include "opus.h"
 #include <stdexcept>
 #include <iostream>
-#include <dlfcn.h>
-
-//BEGIN FUNCTION TYPE
-#define OPUS_TYPE_ENCODER_CREATE   (OpusEncoder* (*)(int32_t,int,int,int*))
-#define OPUS_TYPE_ENCODE          (int32_t (*)(OpusEncoder*,const int16_t*,int,unsigned char*,int32_t))
-#define OPUS_TYPE_ENCODER_DESTROY  (void (*)(OpusEncoder*))
-
-#define OPUS_TYPE_DECODER_CREATE   (OpusDecoder* (*)(int32_t,int,int*))
-#define OPUS_TYPE_DECODE          (int (*)(OpusDecoder*,const unsigned char*,int32_t,int16_t*,int,int))
-#define OPUS_TYPE_DECODER_DESTROY  (void (*)(OpusDecoder*))
-
-//END FUNCTION TYPE
 
 static const int Opus_PAYLOAD_TYPE = 104; // dynamic payload type, out of range of video (96-99)
 
-Opus::OpusEncoder*(*Opus::opus_encoder_create              )(int32_t Fs, int channels, int application, int *error ) = 0;
-int32_t           (*Opus::opus_encode                      )(Opus::OpusEncoder *st, const int16_t *pcm, int frame_size, unsigned char *data, int32_t max_data_bytes ) = 0;
-void              (*Opus::opus_encoder_destroy             )(Opus::OpusEncoder *st) = 0;
-Opus::OpusDecoder*(*Opus::opus_decoder_create              )(int32_t Fs, int channels, int *error ) = 0;
-int               (*Opus::opus_decode                      )(Opus::OpusDecoder *st, const unsigned char *data, int32_t len, int16_t *pcm, int frame_size, int decode_fec ) = 0;
-void              (*Opus::opus_decoder_destroy             )(Opus::OpusDecoder *st) = 0;
-
 Opus::Opus() : sfl::AudioCodec(Opus_PAYLOAD_TYPE, "OPUS", CLOCK_RATE, FRAME_SIZE, CHANNELS),
-    handler_(0),
     encoder_(0),
     decoder_(0)
 {
    hasDynamicPayload_ = true;
-
-   handler_ = dlopen("libopus.so.0", RTLD_NOW);
-   if (!handler_)
-       throw std::runtime_error("opus: did not open shared lib");
-
-   opus_encoder_create  = OPUS_TYPE_ENCODER_CREATE dlsym(handler_, "opus_encoder_create");
-   loadError(dlerror());
-   opus_encode          = OPUS_TYPE_ENCODE dlsym(handler_, "opus_encode");
-   loadError(dlerror());
-   opus_encoder_destroy = OPUS_TYPE_ENCODER_DESTROY dlsym(handler_, "opus_encoder_destroy");
-   loadError(dlerror());
-   opus_decoder_create  = OPUS_TYPE_DECODER_CREATE dlsym(handler_, "opus_decoder_create");
-   loadError(dlerror());
-   opus_decode          = OPUS_TYPE_DECODE dlsym(handler_, "opus_decode");
-   loadError(dlerror());
-   opus_decoder_destroy = OPUS_TYPE_DECODER_DESTROY dlsym(handler_, "opus_decoder_destroy");
-   loadError(dlerror());
 
    int err = 0;
    encoder_ = opus_encoder_create(CLOCK_RATE, CHANNELS, OPUS_APPLICATION_VOIP, &err);
@@ -92,8 +55,6 @@ Opus::~Opus()
         opus_encoder_destroy(encoder_);
     if (decoder_)
         opus_decoder_destroy(decoder_);
-    if (handler_)
-        dlclose(handler_);
 }
 
 int Opus::decode(short *dst, unsigned char *buf, size_t buffer_size)
@@ -104,12 +65,6 @@ int Opus::decode(short *dst, unsigned char *buf, size_t buffer_size)
 int Opus::encode(unsigned char *dst, short *src, size_t buffer_size)
 {
    return opus_encode(encoder_, src, FRAME_SIZE, dst, buffer_size * 2);
-}
-
-void Opus::loadError(const char *error)
-{
-   if (error != NULL)
-      throw std::runtime_error("opus failed to load");
 }
 
 // cppcheck-suppress unusedFunction
--- daemon/src/audio/codecs/opus.h
+++ daemon/src/audio/codecs/opus.h
@@ -34,8 +34,7 @@
 
 #include "audiocodec.h"
 
-#define MAX_ENCODER_BUFFER 480
-#define OPUS_APPLICATION_VOIP 2048
+#include <opus/opus.h>
 
 class Opus : public sfl::AudioCodec {
 public:
@@ -45,116 +44,13 @@ private:
    virtual int decode(short *dst, unsigned char *buf, size_t buffer_size);
    virtual int encode(unsigned char *dst, short *src, size_t buffer_size);
 
-   typedef struct {
-      int32_t  nChannelsAPI;
-      int32_t  nChannelsInternal;
-      int32_t  API_sampleRate;
-      int32_t  internalSampleRate;
-      int      payloadSize_ms;
-      int      prevPitchLag;
-   } silk_DecControlStruct;
-
-   struct silk_EncControlStruct {
-      int32_t  nChannelsAPI;
-      int32_t  nChannelsInternal;
-      int32_t  API_sampleRate;
-      int32_t  maxInternalSampleRate;
-      int32_t  minInternalSampleRate;
-      int32_t  desiredInternalSampleRate;
-      int      payloadSize_ms;
-      int32_t  bitRate;
-      int      packetLossPercentage;
-      int      complexity;
-      int      useInBandFEC;
-      int      useDTX;
-      int      useCBR;
-      int      maxBits;
-      int      toMono;
-      int      opusCanSwitch;
-      int32_t  internalSampleRate;
-      int      allowBandwidthSwitch;
-      int      inWBmodeWithoutVariableLP;
-      int      stereoWidth_Q14;
-      int      switchReady;
-   };
-   typedef struct silk_EncControlStruct silk_EncControlStruct;
-
-   struct OpusEncoder {
-      int          celt_enc_offset;
-      int          silk_enc_offset;
-      silk_EncControlStruct silk_mode;
-      int          application;
-      int          channels;
-      int          delay_compensation;
-      int          force_channels;
-      int          signal_type;
-      int          user_bandwidth;
-      int          max_bandwidth;
-      int          user_forced_mode;
-      int          voice_ratio;
-      int32_t      Fs;
-      int          use_vbr;
-      int          vbr_constraint;
-      int32_t      bitrate_bps;
-      int32_t      user_bitrate_bps;
-      int          encoder_buffer;
-
-      #define OPUS_ENCODER_RESET_START stream_channels
-      int          stream_channels;
-      int16_t      hybrid_stereo_width_Q14;
-      int32_t      variable_HP_smth2_Q15;
-      int32_t      hp_mem[4];
-      int          mode;
-      int          prev_mode;
-      int          prev_channels;
-      int          prev_framesize;
-      int          bandwidth;
-      int          silk_bw_switch;
-      /* Sampling rate (at the API level) */
-      int          first;
-      int16_t      delay_buffer[MAX_ENCODER_BUFFER*2];
-
-      uint32_t  rangeFinal;
-   };
-
-   struct OpusDecoder {
-      int          celt_dec_offset;
-      int          silk_dec_offset;
-      int          channels;
-      int32_t      Fs;          /** Sampling rate (at the API level) */
-      silk_DecControlStruct DecControl;
-      int          decode_gain;
-
-      /* Everything beyond this point gets cleared on a reset */
-   #define OPUS_DECODER_RESET_START stream_channels
-      int          stream_channels;
-      int          bandwidth;
-      int          mode;
-      int          prev_mode;
-      int          frame_size;
-      int          prev_redundancy;
-      int32_t      rangeFinal;
-   };
-
    NON_COPYABLE(Opus);
    //Attributes
-   void * handler_;
    OpusEncoder *encoder_;
    OpusDecoder *decoder_;
    static const int FRAME_SIZE = 160;
    static const int CLOCK_RATE = 16000;
    static const int CHANNELS   = 1;
-
-   //Helpers
-   static void loadError(const char *error);
-
-   //Extern functions
-   static OpusEncoder*      (*opus_encoder_create              )(int32_t Fs, int channels, int application, int *error);
-   static int32_t           (*opus_encode                      )(OpusEncoder *st, const int16_t *pcm, int frame_size, unsigned char *data, int32_t max_data_bytes);
-   static void              (*opus_encoder_destroy             )(OpusEncoder *st);
-   static OpusDecoder*      (*opus_decoder_create              )(int32_t Fs, int channels, int *error);
-   static int               (*opus_decode                      )(OpusDecoder *st, const unsigned char *data, int32_t len, int16_t *pcm, int frame_size, int decode_fec);
-   static void              (*opus_decoder_destroy             )(OpusDecoder *st);
 };
 
 #endif
