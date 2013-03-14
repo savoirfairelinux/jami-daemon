6e2fc70ed14a714eff6376ab4f619efd614f16c8
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2013-03-14 16:08:32 -0400

    * #21625: daemon: fix various warnings

--- daemon/src/audio/alsa/alsalayer.cpp
+++ daemon/src/audio/alsa/alsalayer.cpp
@@ -57,8 +57,8 @@ class AlsaThread {
         static void *runCallback(void *context);
 
         NON_COPYABLE(AlsaThread);
-        AlsaLayer* alsa_;
         pthread_t thread_;
+        AlsaLayer* alsa_;
         bool running_;
 };
 
--- daemon/src/fileutils.cpp
+++ daemon/src/fileutils.cpp
@@ -144,7 +144,9 @@ create_pidfile()
     }
 
     snprintf(buf, sizeof(buf), "%ld\n", (long) getpid());
-    if (write(f.fd, buf, strlen(buf)) != strlen(buf)) {
+
+    const int buf_strlen = strlen(buf);
+    if (write(f.fd, buf, buf_strlen) != buf_strlen) {
         ERROR("Problem writing to PID file '%s'", f.name.c_str());
         close(f.fd);
         f.fd = -1;
@@ -187,8 +189,8 @@ get_home_dir()
         struct passwd pwbuf, *pw;
         if (getpwuid_r(getuid(), &pwbuf, buf, sizeof(buf), &pw) == 0 and pw != NULL)
             return pw->pw_dir;
-    } else {
-        return "";
     }
+
+    return "";
 }
 }
