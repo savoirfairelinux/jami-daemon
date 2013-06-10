7b62728dc1614bd2cf157577b0bd86607c760bc1
Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
2013-06-10 17:31:31 -0400

    [#25528] Update doc with new version number, and jenkins script

--- daemon/src/audio/codecs/audiocodec.h
+++ daemon/src/audio/codecs/audiocodec.h
@@ -38,7 +38,7 @@
 
 /* bump when codec binary interface changes */
 #define AUDIO_CODEC_ENTRY create_1_2_2
-#define AUDIO_CODEC_ENTRY_SYMBOL "create_1_2_2"
+#define AUDIO_CODEC_ENTRY_SYMBOL "create_1_2_3"
 
 // We assume all decoders will be fed 20ms of audio or less
 // And we'll resample them to 44.1kHz or less
