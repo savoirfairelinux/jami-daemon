475c218316fa855e5fcf5bb5d9db0968843f8f93
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2013-07-17 15:23:07 -0400

    * #27352: daemon: dbus is now optional

--- daemon/configure-android.sh
+++ daemon/configure-android.sh
@@ -1,1 +1,1 @@
-./configure --without-alsa --without-pulse --without-tls --without-iax2  --without-zrtp --without-sdes --without-speexdsp --without-speex
+./configure --without-dbus --without-alsa --without-pulse --without-tls --without-iax2  --without-zrtp --without-sdes --without-speexdsp --without-speex
