542b83a1cbfbc30fed510c3b4fbb1b704299597b
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2013-07-30 14:41:33 -0400

    * #27693: audio: fix variable names

--- daemon/src/audio/audiorecorder.cpp
+++ daemon/src/audio/audiorecorder.cpp
@@ -87,11 +87,11 @@ void AudioRecorder::run()
     AudioBuffer buffer(BUFFER_LENGTH);
 
     while (running_) {
-        const size_t availableBytes = mbuffer_->availableForGet(recorderId_);
-        buffer.resize(std::min(availableBytes, BUFFER_LENGTH));
+        const size_t availableSamples = mbuffer_->availableForGet(recorderId_);
+        buffer.resize(std::min(availableSamples, BUFFER_LENGTH));
         mbuffer_->getData(buffer, recorderId_);
 
-        if (availableBytes > 0)
+        if (availableSamples > 0)
             arecord_->recData(buffer);
 
         usleep(20000); // 20 ms
