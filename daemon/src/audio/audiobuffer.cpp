cc6835028be6c738a74b7c7baf95eb70b506428c
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2013-08-07 16:24:23 -0400

    Revert "* #27819: audiobuffer: iterate more efficiently"
    This reverts commit ce1d39f9fe02d8fbe2caacc5e550165ada8b16dd.

--- daemon/src/audio/audiobuffer.cpp
+++ daemon/src/audio/audiobuffer.cpp
@@ -144,9 +144,9 @@ void AudioBuffer::deinterleave(const SFLAudioSample* in, size_t sample_num, unsi
     setChannelNum(channel_num);
     resize(sample_num);
 
-    for (unsigned i = 0; i < samples_.size(); ++i) {
-        for (unsigned j = 0; j < samples_[i].size(); ++j)
-            samples_[i][j] = *in++;
+    for (unsigned i = 0; i < samples_[0].size(); i++) {
+        for (unsigned j = 0; j < samples_.size(); j++)
+            samples_[j][i] = *in++;
     }
 }
 
