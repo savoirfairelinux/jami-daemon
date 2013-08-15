32114fbe1b311c69c38376537d144fb56733ef0e
Patrick Keroulas <patrick.keroulas@savoirfairelinux.com>
2013-08-15 11:29:45 -0400

    * #28473: sip: added missing presence files

---
+++ daemon/src/sip/sippresence.cpp
@@ -0,0 +1,204 @@
+/*
+ *  Copyright (C) 2004-2013 Savoir-Faire Linux Inc.
+ *
+ *  Author: Patrick Keroulas  <patrick.keroulas@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+
+
+#include "logger.h"
+#include "manager.h"
+#include "client/client.h"
+#include "client/callmanager.h"
+#include "sipaccount.h"
+#include "sipbuddy.h"
+#include "sippublish.h"
+#include "sippresence.h"
+#include "presence_subscription.h"
+#include "sipvoiplink.h"
+
+/**
+ * Generic function.
+ * Fill the header.
+ */
+void pres_process_msg_data(pjsip_tx_data *tdata, const pres_msg_data *msg_data){
+    if (tdata->msg->type == PJSIP_REQUEST_MSG) {
+        const pj_str_t STR_USER_AGENT = pj_str("User-Agent");
+        pjsip_hdr *h;
+        pj_str_t ua = pj_str("SFLPhone");
+        h = (pjsip_hdr*) pjsip_generic_string_hdr_create(tdata->pool, &STR_USER_AGENT, &ua);
+        pjsip_msg_add_hdr(tdata->msg, h);
+    }
+
+    if(msg_data == NULL)
+        return;
+
+    const pjsip_hdr *hdr;
+    hdr = msg_data->hdr_list.next;
+    while (hdr && hdr != &msg_data->hdr_list) {
+        pjsip_hdr *new_hdr;
+        new_hdr = (pjsip_hdr*) pjsip_hdr_clone(tdata->pool, hdr);
+        DEBUG("adding header", new_hdr->name.ptr);
+        pjsip_msg_add_hdr(tdata->msg, new_hdr);
+        hdr = hdr->next;
+    }
+
+    if (msg_data->content_type.slen && msg_data->msg_body.slen) {
+        pjsip_msg_body *body;
+        pj_str_t type = pj_str("application");
+        pj_str_t subtype = pj_str("pidf+xml");
+        body = pjsip_msg_body_create(tdata->pool, &type, &subtype, &msg_data->msg_body);
+        tdata->msg->body = body;
+    }
+}
+
+
+SIPPresence::SIPPresence(SIPAccount *acc)
+    : acc_(acc)
+    , pres_status_data()
+    , online_status()
+    , rpid({PJRPID_ELEMENT_TYPE_PERSON,
+            pj_str("20"),
+            PJRPID_ACTIVITY_BUSY,
+            pj_str("")})
+    , publish_sess()
+    , publish_state()
+    , publish_enabled(true)
+    , serverSubscriptions_ ()
+    , buddies_ ()
+{
+    // init default status
+    updateStatus("open","Available");
+}
+
+
+SIPPresence::~SIPPresence(){}
+
+SIPAccount * SIPPresence::getAccount(){
+    return acc_;
+}
+
+pjsip_pres_status * SIPPresence::getStatus(){
+    return &pres_status_data;
+}
+
+int SIPPresence::getModId(){
+    return  ((SIPVoIPLink*) (acc_->getVoIPLink()))->getModId();
+}
+
+void SIPPresence::updateStatus(const std::string &status, const std::string &note){
+    pjrpid_element rpid = {PJRPID_ELEMENT_TYPE_PERSON,
+            pj_str("20"),
+            PJRPID_ACTIVITY_UNKNOWN,
+            pj_str(strdup(note.c_str()))};
+
+    /* fill activity if user not available. */
+    if(note=="away")
+        rpid.activity = PJRPID_ACTIVITY_AWAY;
+    else if (note=="busy")
+        rpid.activity = PJRPID_ACTIVITY_BUSY;
+    else
+        DEBUG("Presence : no activity");
+
+    pj_bzero(&pres_status_data, sizeof(pres_status_data));
+    pres_status_data.info_cnt = 1;
+    pres_status_data.info[0].basic_open = (status == "open")? true: false;
+    pres_status_data.info[0].id = pj_str("0"); /* todo: tuplie_id*/
+    pj_memcpy(&pres_status_data.info[0].rpid, &rpid,sizeof(pjrpid_element));
+    /* "contact" field is optionnal */
+}
+
+void SIPPresence::sendPresence(const std::string &status, const std::string &note){
+    updateStatus(status,note);
+    if (acc_->isIP2IP())
+        notifyServerSubscription();
+    else
+        pres_publish(this);
+}
+
+
+void SIPPresence::reportBuddy(const std::string& buddySipUri, pjsip_pres_status * status){
+    /* Update our info. See pjsua_buddy_get_info() for additionnal ideas*/
+    const std::string basic(status->info[0].basic_open ? "open" : "closed");
+    const std::string note(status->info[0].rpid.note.ptr,status->info[0].rpid.note.slen);
+    DEBUG(" Received presenceStateChange for %s status=%s note=%s",buddySipUri.c_str(),basic.c_str(),note.c_str());
+    /* report status to client signal */
+    Manager::instance().getClient()->getCallManager()->newPresenceNotification(buddySipUri, basic, note);
+}
+
+void SIPPresence::subscribeBuddy(const std::string& buddySipUri){
+    std::list< SIPBuddy *>::iterator buddyIt;
+    for (buddyIt = buddies_.begin(); buddyIt != buddies_.end(); buddyIt++)
+        if((*buddyIt)->getURI()==buddySipUri){
+            DEBUG("-Buddy:%s exist in the list. Replace it.",buddySipUri.c_str());
+            delete *buddyIt;
+            removeBuddy(*buddyIt);
+            break;
+        }
+    SIPBuddy *b = new SIPBuddy(buddySipUri, acc_);
+    if(!(b->subscribe())){
+        WARN("Failed to add buddy.");
+        delete b;
+    }
+}
+
+void SIPPresence::unsubscribeBuddy(const std::string& buddySipUri){
+    std::list< SIPBuddy *>::iterator buddyIt;
+    for (buddyIt = buddies_.begin(); buddyIt != buddies_.end(); buddyIt++)
+        if((*buddyIt)->getURI()==buddySipUri){
+            DEBUG("-Found buddy:%s in the buddy list.",buddySipUri.c_str());
+            delete *buddyIt;
+            removeBuddy(*buddyIt);
+            return;
+        }
+}
+
+void SIPPresence::addBuddy(SIPBuddy *b){
+    DEBUG("-New buddy subscription added in the buddy list.");
+    buddies_.push_back(b);
+}
+void SIPPresence::removeBuddy(SIPBuddy *b){
+    DEBUG("-Buddy subscription removed from the buddy list.");
+    buddies_.remove(b);
+}
+
+void SIPPresence::addServerSubscription(PresenceSubscription *s) {
+    DEBUG("-PresenceServer subscription added.");
+    serverSubscriptions_.push_back(s);
+}
+
+void SIPPresence::removeServerSubscription(PresenceSubscription *s) {
+    serverSubscriptions_.remove(s);
+    DEBUG("-PresenceServer removed");
+}
+
+void SIPPresence::notifyServerSubscription() {
+    std::list< PresenceSubscription *>::iterator serverIt;
+    DEBUG("-Iterating through PresenceServers:");
+    for (serverIt = serverSubscriptions_.begin(); serverIt != serverSubscriptions_.end(); serverIt++)
+        (*serverIt)->notify();
+}
