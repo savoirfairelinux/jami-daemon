c513172a88b931c3ccd76afcffa434290555f52e
Patrick Keroulas <patrick.keroulas@savoirfairelinux.com>
2013-10-15 14:51:48 -0400

    * #31636: Presence, adapt the gnome client to the new support/enable * fields.

--- gnome/src/presence.c
+++ gnome/src/presence.c
@@ -359,7 +359,8 @@ presence_buddy_subscribe(buddy_t * buddy, gboolean flag)
     if(acc)
     {
         if((acc->state == (ACCOUNT_STATE_REGISTERED)) &&
-                account_lookup(acc, CONFIG_PRESENCE_SUBSCRIBE_ENABLED) &&
+                account_lookup(acc, CONFIG_PRESENCE_ENABLED) &&
+                account_lookup(acc, CONFIG_PRESENCE_SUBSCRIBE_SUPPORTED) &&
                 (flag != buddy->subscribed))
             dbus_presence_subscribe(buddy->acc, buddy->uri, flag);
     }
