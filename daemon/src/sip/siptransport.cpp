df8a85a0321a365f0360e76c31599a07c0098c3c
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2014-01-10 18:12:43 -0500

    STUN: don't request addresses twice
    Accidentally added in commit 3a1abc5654bb2a81f4cd60dd1dc4839e11362682

--- daemon/src/sip/siptransport.cpp
+++ daemon/src/sip/siptransport.cpp
@@ -323,12 +323,6 @@ SipTransport::getSTUNAddresses(const SIPAccount &account,
 
     std::vector<pj_sockaddr_in> result(socketDescriptors.size());
 
-    if (pjstun_get_mapped_addr(&cp_->factory, socketDescriptors.size(), &socketDescriptors[0],
-                &serverName, port, &serverName, port, &result[0]) != PJ_SUCCESS) {
-        ERROR("STUN server \"%.*s\" failed", serverName.slen, serverName.ptr);
-        throw std::runtime_error("Can't contact STUN server");
-    }
-
     pj_status_t ret;
     if ((ret = pjstun_get_mapped_addr(&cp_->factory, socketDescriptors.size(), &socketDescriptors[0],
                     &serverName, port, &serverName, port, &result[0])) != PJ_SUCCESS) {
