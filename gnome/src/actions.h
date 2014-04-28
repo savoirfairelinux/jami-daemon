40601885066dc6bd7f119d6be1a1a32416b8209f
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2014-04-28 19:29:37 -0400

    gnome: rework the switch source functions
    - Refactor the logic inside the getter functions for the input source
      functions.
    - Use the glib everywhere.
    - Remove some memory leaks.
    - Add a sflphone_toggle_camera() function to switch between the
      active_device and the none.
    
    Change-Id: Iae83c5b4922722c8914ba9339c6d53b8e9ef53d8
    Refs: #45417

--- gnome/src/actions.h
+++ gnome/src/actions.h
@@ -261,8 +261,14 @@ void sflphone_srtp_sdes_off(callable_obj_t * c, SFLPhoneClient *client);
 
 void sflphone_fill_conference_list(SFLPhoneClient *client);
 
-char *sflphone_get_display(void);
+gchar * sflphone_get_display(void);
+
+gchar * sflphone_get_active_video(void);
+
+gchar * sflphone_get_video_none(void);
 
 void sflphone_toggle_screenshare(void);
+
+void sflphone_toggle_camera(void);
 
 #endif
