a7d1ff4ca91b12d46c5141bbb90e33e11207a7ca
Sebastien Bourdelin <sebastien.bourdelin@savoirfairelinux.com>
2014-04-30 13:28:44 -0400

    gnome: video_widget
    This commit introduces the new video_widget.
    
    - The video_renderer has been reworked to take a Clutter Texture as
      parameter and render the video inside.
    - The video_callback has been rewritten and now only handles the dbus
      callback as it should be.
    - The new class video_widget handles all the stuff related to the video
      window, the clutter scene is a part of it.
    - The client dbus callback started_decoding_video_cb and
      stopped_decoding_video_cb passed the SFLclient as parameter in its
      userdate pointer.
    
    Refs #46425
    
    Change-Id: I20e8872c395278ae9332e100bf9ec339d6d75ddf

---
+++ gnome/src/video/video_widget.h
@@ -0,0 +1,74 @@
+/*
+ *  Copyright (C) 2004-2013 Savoir-Faire Linux Inc.
+ *  Author: Sebastien Bourdelin <sebastien.bourdelin@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+
+#ifndef VIDEO_WIDGET_H__
+#define VIDEO_WIDGET_H__
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#define VIDEO_WIDGET_TYPE              (video_widget_get_type())
+#define VIDEO_WIDGET(obj)              (G_TYPE_CHECK_INSTANCE_CAST((obj), VIDEO_WIDGET_TYPE, VideoWidget))
+#define VIDEO_WIDGET_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST((klass), VIDEO_WIDGET_TYPE, VideoWidgetClass))
+#define IS_VIDEO_WIDGET(obj)           (G_TYPE_CHECK_INSTANCE_TYPE((obj), VIDEO_WIDGET_TYPE))
+#define IS_VIDEO_WIDGET_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE((klass), VIDEO_WIDGET_TYPE))
+
+#define VIDEO_WIDGET_WIDTH             400
+#define VIDEO_WIDGET_HEIGHT            350
+#define VIDEO_LOCAL_WIDTH              100
+#define VIDEO_LOCAL_HEIGHT             125
+
+typedef struct _VideoWidgetPrivate VideoWidgetPrivate;
+typedef struct _VideoWidgetClass VideoWidgetClass;
+typedef struct _VideoWidget VideoWidget;
+
+typedef enum {
+    VIDEO_AREA_REMOTE,
+    VIDEO_AREA_LOCAL,
+    VIDEO_AREA_LAST
+} VIDEO_AREA_ID;
+
+struct _VideoWidgetClass {
+    GtkWindowClass parent_class;
+};
+
+struct _VideoWidget {
+    GtkWindow parent;
+    /* Private */
+    VideoWidgetPrivate *priv;
+};
+
+/* Public interface */
+GType video_widget_get_type(void) G_GNUC_CONST;
+GtkWidget *video_widget_new(/* gchar *, gchar *, gint, gint */);
+void video_widget_camera_start(GtkWidget *, VIDEO_AREA_ID, gchar *, gchar *, guint, guint);
+void video_widget_camera_stop(GtkWidget *self, VIDEO_AREA_ID);
+
+#endif // __VIDEO_WIDGET_H__
