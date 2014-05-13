bde42a8c2617c4b0295365e20f8df985ad0792d5
Vivien Didelot <vivien.didelot@savoirfairelinux.com>
2014-05-13 14:42:26 -0400

    daemon: (client) add getSettingsFor() stub
    The stub for VideoManager::getSettingsFor() was missing.
    
    Refs #46837
    Change-Id: Ifb6dceb8d5f4aa762b721b415e02134ac65a0284

--- daemon/src/client/videomanager_stub.cpp
+++ daemon/src/client/videomanager_stub.cpp
@@ -52,6 +52,12 @@ void VideoManager::stoppedDecoding(const std::string &id, const std::string)
 {
 }
 
+std::map<std::string, std::string>
+VideoManager::getSettingsFor(const std::string& name)
+{
+    return std::map<std::string, std::string>();
+}
+
 std::map<std::string, std::string> VideoManager::getSettings()
 {
     return std::map<std::string, std::string>();
