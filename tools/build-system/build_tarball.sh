9a9efdf0ed042eb0e939f60b78eb83639d8fb2af
Vittorio Giovara <vittorio.giovara@savoirfairelinux.com>
2014-06-06 13:08:12 -0400

    rip out the embedded pjproject sources
    Refs: #46617
    Change-Id: Ib86e58f8834e26a3730b5d6e49d99cb2928d5d5e

--- tools/build-system/build_tarball.sh
+++ tools/build-system/build_tarball.sh
@@ -50,47 +50,6 @@ pushd daemon
 ./autogen.sh
 popd
 
-pushd daemon/libs/pjproject-2.2.1
-find -name os-auto.mak -type f -exec rm {} \;
-
-# Remove pre-built binaries
-rm -f pjsip/bin/pjsip-test-x86_64-unknown-linux-gnu
-rm -f pjsip-apps/bin/pjsua-x86_64-unknown-linux-gnu
-rm -f pjlib/bin/pjlib-test-x86_64-unknown-linux-gnu
-rm -f pjnath/bin/pjnath-test-x86_64-unknown-linux-gnu
-rm -f pjnath/bin/pjturn-client-x86_64-unknown-linux-gnu
-rm -f pjlib-util/bin/pjlib-util-test-x86_64-unknown-linux-gnu
-rm -f pjnath/bin/pjturn-srv-x86_64-unknown-linux-gnu
-rm -f pjmedia/bin/pjmedia-test-x86_64-unknown-linux-gnu
-
-rm -f third_party/portaudio/src/hostapi/asio/Pa_ASIO.pdf
-rm -f third_party/portaudio/src/hostapi/asio/Callback_adaptation_.pdf
-
-# Put actual files in the tarball
-rm -f config.guess config.sub
-cp /usr/share/misc/config.guess .
-cp /usr/share/misc/config.sub .
-
-rm -f config.log config.status build.mak
-rm -f pjlib/include/pj/compat/m_auto.h
-rm -f pjlib/include/pj/compat/os_auto.h
-rm -f pjmedia/include/pjmedia-codec/config_auto.h
-rm -f pjmedia/include/pjmedia/config_auto.h
-rm -f pjsip/include/pjsip/sip_autoconf.h
-
-# Remove non-GPL compatible code
-sed -e 's/ milenage / /' third_party/build/Makefile > third_party/build/Makefile.new
-mv third_party/build/Makefile.new third_party/build/Makefile
-sed -e 's/ -lmilenage-$(TARGET_NAME) / /' build.mak.in > build.mak.in.new
-mv build.mak.in.new build.mak.in
-sed -e 's/ $(PJ_DIR)\/third_party\/lib\/libmilenage-$(LIB_SUFFIX) / /' build.mak.in > build.mak.in.new
-mv build.mak.in.new build.mak.in
-rm -rf third_party/build/milenage/
-rm -rf third_party/milenage/
-
-aclocal --force
-popd
-
 pushd gnome
 ./autogen.sh
 popd
