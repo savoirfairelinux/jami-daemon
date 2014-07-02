f1835ce541a10213a1ebbd097c7fff5c8f5319e4
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2014-07-02 13:21:08 -0400

    ulaw: cleanup names
    Refs #49181
    
    Change-Id: I30f2f30090638790e1f705109f2a4635501d2c3b

--- daemon/src/audio/codecs/ulaw.cpp
+++ daemon/src/audio/codecs/ulaw.cpp
@@ -48,16 +48,20 @@ class Ulaw : public sfl::AudioCodec {
             return new Ulaw;
         }
 
-        int decode(SFLAudioSample *dst, unsigned char *src, size_t buf_size) {
-            for (unsigned char* end = src + buf_size; src < end; ++src, ++dst)
-                *dst = ULawDecode(*src);
+        int decode(SFLAudioSample *pcm, unsigned char *data, size_t len)
+        {
+            for (unsigned char *end = data + len; data < end;
+                 ++data, ++pcm)
+                *pcm = ULawDecode(*data);
 
-            return buf_size;
+            return len;
         }
 
-        int encode(unsigned char *dst, SFLAudioSample *src, size_t /* buf_size */) {
-            for (unsigned char * end = dst + frameSize_; dst < end; ++src, ++dst)
-                *dst = ULawEncode(*src);
+        int encode(unsigned char *data, SFLAudioSample *pcm, size_t /* max_data_bytes */)
+        {
+            for (unsigned char *end = data + frameSize_; data < end;
+                 ++data, ++pcm)
+                *data = ULawEncode(*pcm);
 
             return frameSize_;
         }
