4cc2c1405899b76a0c7fe5028e116a3cec1a6b03
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2014-07-14 11:41:52 -0400

    android: disable video by default
    Refs #50857
    
    Change-Id: Id9476426aa2ad03d586dbd7bcf75cd9029a4b6e8

--- daemon/configure-android.sh
+++ daemon/configure-android.sh
@@ -1,1 +1,1 @@
-./configure --without-dbus --without-alsa --without-pulse --with-iax2 --without-tls
+./configure --disable-video --without-dbus --without-alsa --without-pulse --with-iax2 --without-tls
