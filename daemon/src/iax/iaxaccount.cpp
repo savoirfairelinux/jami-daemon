ec8cde5f9ba3a0cd3b6f339feded187896cfd51a
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2014-08-07 19:53:04 -0400

    iax: code cleanup and more iax API lock
    Refs #52750
    
    Change-Id: I8b9d4669c5bc45d0cb8fe82f4a9daafbafb9c6b9

--- daemon/src/iax/iaxaccount.cpp
+++ daemon/src/iax/iaxaccount.cpp
@@ -4,7 +4,7 @@
  *  Author: Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
  *  Author: Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
  *  Author: Alexandre Savard <alexandre.savard@savoirfairelinux.com>
- *  Author : Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
+ *  Author: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -49,7 +49,7 @@
 constexpr const char * const IAXAccount::ACCOUNT_TYPE;
 
 IAXAccount::IAXAccount(const std::string& accountID)
-    : Account(accountID), password_(), link_(std::make_shared<IAXVoIPLink>(*this))
+    : Account(accountID), link_(std::make_shared<IAXVoIPLink>(*this))
 {}
 
 std::shared_ptr<VoIPLink> IAXAccount::getVoIPLink()
