fb78d551d7c100be145c23ea7691439cd6f4f717
Philippe Groarke <philippe.groarke@savoirfairelinux.com>
2014-09-08 13:36:31 -0400

    windows: Dependencies build, still hit-and-miss.
    More fixes to contrib for Windows port.
    
    Change-Id: I0fbc007d9f0c6d27bdacfc98a506e5701812540b

--- daemon/autogen.sh
+++ daemon/autogen.sh
@@ -6,11 +6,13 @@ if test -z $AUTORECONF; then
         exit 1
 fi
 
-PKGCONFIG=`which pkg-config`
-if test -z $PKGCONFIG; then
-        echo "*** No pkg-config found, please install it ***"
-        # warn without exiting, since pkg-config is only needed
-        # by configure, not autogen.sh
+if test -z PKGCONFIG; then #Test if empty so we don't override mingw pkg-config directory.
+    PKGCONFIG=`which pkg-config`
+    if test -z $PKGCONFIG; then
+            echo "*** No pkg-config found, please install it ***"
+            # warn without exiting, since pkg-config is only needed
+            # by configure, not autogen.sh
+    fi
 fi
 
 LIBTOOLIZE=`which libtoolize || which glibtoolize`
