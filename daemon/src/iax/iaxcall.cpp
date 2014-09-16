eadae28f9fbb59f17a4caa933c08b06aa8ad6564
Philippe Groarke <philippe.groarke@savoirfairelinux.com>
2014-09-16 16:36:39 -0400

    windows: Fix includes.
    Change-Id: Iebe32b042320615bb515b946692289ad47feec9b

--- daemon/src/iax/iaxcall.cpp
+++ daemon/src/iax/iaxcall.cpp
@@ -31,7 +31,6 @@
  */
 
 #include <cstring>
-#include <sys/socket.h>
 #include <iax/iax-client.h>
 #include <iax/frame.h>
 
@@ -42,6 +41,11 @@
 #include "iaxvoiplink.h"
 #include "audio/ringbufferpool.h"
 #include "audio/ringbuffer.h"
+
+#ifdef _WIN32
+#else
+	#include <sys/socket.h>
+#endif
 
 #if HAVE_INSTANT_MESSAGING
 #include "im/instant_messaging.h"
