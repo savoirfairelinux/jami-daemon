2a60a4bba6c5c2904acb52877607f0de7832702d
Simon Piette <simon.piette@savoirfairelinux.com>
2014-09-17 15:10:53 -0400

    build-system: get-kde.sh as a separate script
    Refs #55333
    
    Change-Id: I75ca3188301649388576dea8dc9ec561f9d168f6

--- tools/build-system/setenv.sh
+++ tools/build-system/setenv.sh
@@ -4,15 +4,15 @@
 # home directory
 export ROOT_DIR=${HOME}
 
+# In case the script is executed manually, replace the variables set by Jenkins
+export WORKSPACE=${WORKSPACE:=.}
+
 # gpg passphrase file
 export GPG_FILE="${WORKSPACE}/.gpg-sflphone"
 
 export EDITOR="echo"
 
 export REFERENCE_REPOSITORY="${WORKSPACE}"
-
-# In case the script is executed manually, replace the variables set by Jenkins
-WORKSPACE=${WORKSPACE:=.}
 
 export WORKING_DIR="${WORKSPACE}/tools/build-system"
 export LAUNCHPAD_DIR="${WORKING_DIR}/launchpad"
