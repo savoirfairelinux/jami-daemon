22087a560b249f3b19fee1bbd9631b35c0b15baf
Simon Piette <simon.piette@savoirfairelinux.com>
2014-09-18 15:44:02 -0400

    build-system: retry to get kde.org script and conf
    Refs #55333
    
    Change-Id: Iee0559e30f20fd552ea7c14589d018bd605431b7

--- tools/build-system/get-kde.sh
+++ tools/build-system/get-kde.sh
@@ -6,8 +6,31 @@
 set -o errexit
 source $(dirname $0)/setenv.sh
 cd "$WORKSPACE"
-curl -O https://projects.kde.org/projects/playground/network/sflphone-kde/repository/revisions/master/raw/data/config.ini
-curl -O https://projects.kde.org/projects/kde/kdesdk/kde-dev-scripts/repository/revisions/master/raw/createtarball/create_tarball.rb
+baseurl='https://projects.kde.org/projects'
+config_uri='/playground/network/sflphone-kde/repository/revisions/master/raw/data/config.ini'
+createtarball_uri='/kde/kdesdk/kde-dev-scripts/repository/revisions/master/raw/createtarball/create_tarball.rb'
+
+set -x
+
+# timeout in seconds
+let -i timeout=300
+let -i timestamp=$(date +%s)
+while ! curl --fail --remote-name ${baseurl}${config_uri}
+do
+    if [ $(date +%s) -gt $(( $timestamp + $timeout)) ]; then
+        break
+    fi
+    sleep 15
+done
+let -i timestamp=$(date +%s)
+while ! curl --fail --remote-name ${baseurl}${createtarball_uri}
+do
+    if [ $(date +%s) -gt $(( $timestamp + $timeout)) ]; then
+        break
+    fi
+    sleep 15
+done
+
 ruby create_tarball.rb --noaccount --application sflphone-kde
 rm -rf kde
 rm -rf sflphone-kde-*.tar.*
