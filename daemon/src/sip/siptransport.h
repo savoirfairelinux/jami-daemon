fda8dd47e7326c12670f28b947c743155ebdf276
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2014-09-24 14:57:49 -0400

    transport refactoring
    SIP transport refactor.
    SIPTransport now represents a SIP transport.
    The old SIPTransport class becomes SIPTransportBroker.
    Makes every sip call have its own transport, to allow IP2IP calls using TLS.
    
    Refs #53057
    Change-Id: I6ae8e1a4c681c6f4f5887772f5b852bd440df13f

--- daemon/src/sip/siptransport.h
+++ daemon/src/sip/siptransport.h
@@ -40,10 +40,6 @@
 #include "logger.h"
 
 #include <pjsip.h>
-#include <pjsip_ua.h>
-#include <pjlib.h>
-#include <pjlib-util.h>
-#include <pjnath.h>
 #include <pjnath/stun_config.h>
 
 #include <functional>
@@ -59,118 +55,191 @@
 
 class SIPAccountBase;
 
+struct SipTransportDescr
+{
+    SipTransportDescr() {}
+    SipTransportDescr(pjsip_transport_type_e t)
+     : type(t), listenerPort(pjsip_transport_get_default_port_for_type(t)) {}
+    SipTransportDescr(pjsip_transport_type_e t, pj_uint16_t port, std::string i)
+     : type(t), listenerPort(port), interface(i) {}
 
-class SipTransport {
-    public:
-        SipTransport(pjsip_endpoint *endpt, pj_caching_pool& cp, pj_pool_t& pool);
-        ~SipTransport();
+    static inline pjsip_transport_type_e actualType(pjsip_transport_type_e t) {
+        return (t == PJSIP_TRANSPORT_START_OTHER) ? PJSIP_TRANSPORT_UDP : t;
+    }
 
-        /**
-         * General Sip transport creation method according to the
-         * transport type specified in account settings
-         * @param account The account for which a transport must be created.
-         */
-        void createSipTransport(SIPAccountBase &account);
+    inline bool operator==(SipTransportDescr const& o) const {
+        return actualType(type) == actualType(o.type)
+         && listenerPort == o.listenerPort
+         && interface == o.interface;
+    }
 
-        /**
-         * Initialize the transport selector
-         * @param transport     A transport associated with an account
-         * @return          	A transport selector structure
-         */
-        static inline pjsip_tpselector getTransportSelector(pjsip_transport *transport) {
-            pjsip_tpselector tp = {PJSIP_TPSELECTOR_TRANSPORT, {transport}};
-            return tp;
-        }
+    inline bool operator<(SipTransportDescr const& o) const {
+        return actualType(type) < actualType(o.type)
+         || listenerPort < o.listenerPort
+         || std::hash<std::string>()(interface) < std::hash<std::string>()(o.interface);
+    }
 
-        /**
-         * This function returns a list of STUN mapped sockets for
-         * a given set of socket file descriptors */
-        std::vector<pj_sockaddr> getSTUNAddresses(const SIPAccountBase &account, std::vector<long> &socks) const;
+    std::string toString() const;
 
-        /**
-         * Get the correct address to use (ie advertised) from
-         * a uri. The corresponding transport that should be used
-         * with that uri will be discovered.
-         *
-         * @param uri The uri from which we want to discover the address to use
-         * @param transport The transport to use to discover the address
-         */
-        void findLocalAddressFromTransport(pjsip_transport *transport, pjsip_transport_type_e transportType, const std::string &host, std::string &address, pj_uint16_t &port) const;
-
-        void findLocalAddressFromSTUN(pjsip_transport *transport, pj_str_t *stunServerName,
-                int stunPort, std::string &address, pj_uint16_t &port) const;
-
-        /**
-         * Go through the transport list and remove unused ones.
-         */
-        void cleanupTransports();
-
-        /**
-         * Call released_cb(success) when transport tp is destroyed, making the
-         * socket available for a new similar transport.
-         * success is true if the transport is actually released.
-         * TODO: make this call non-blocking.
-         */
-        void waitForReleased(pjsip_transport* tp, std::function<void(bool)> released_cb);
-
-    private:
-        NON_COPYABLE(SipTransport);
-
-#if HAVE_TLS
-        /**
-         * Create a connection oriented TLS transport and register to the specified remote address.
-         * First, initialize the TLS listener sole instance. This means that, for the momment, only one TLS transport
-         * is allowed to be created in the application. Any subsequent account attempting to
-         * register a new using this transport even if new settings are specified.
-         * @param the account that is creating the TLS transport
-         */
-        pjsip_transport *
-        createTlsTransport(SIPAccountBase &account);
-
-        /**
-         * Create The default TLS listener which is global to the application. This means that
-         * only one TLS connection can be established for the momment.
-         * @param the SIPAccount for which we are creating the TLS listener
-         * @param IP protocol version to use, can be pj_AF_INET() or pj_AF_INET6()
-         * @return a pointer to the new listener
-         */
-        pjsip_tpfactory *
-        createTlsListener(SIPAccountBase &account, pj_uint16_t family = pj_AF_UNSPEC());
-#endif
-
-        /**
-        * Create SIP UDP transport from account's setting
-        * @param account The account for which a transport must be created.
-        * @param IP protocol version to use, can be pj_AF_INET() or pj_AF_INET6()
-        * @return a pointer to the new transport
-        */
-        pjsip_transport *createUdpTransport(const std::string &interface,
-                                            pj_uint16_t port, pj_uint16_t family = pj_AF_UNSPEC());
-
-        /**
-         * Go through the transport list and remove unused ones.
-         * Returns a list of LOCKED transports that have to be processed and unlocked.
-         */
-        std::vector<pjsip_transport*> _cleanupTransports();
-
-        static void tp_state_callback(pjsip_transport *, pjsip_transport_state, const pjsip_transport_state_info *);
-
-        void transportStateChanged(pjsip_transport* tp, pjsip_transport_state state, const pjsip_transport_state_info* info);
-
-        /**
-         * UDP Transports are stored in this map in order to retreive them in case
-         * several accounts would share the same port number.
-         */
-        std::map<std::string, pjsip_transport*> transportMap_;
-        std::mutex transportMapMutex_;
-        std::condition_variable transportDestroyedCv_;
-
-        pj_caching_pool& cp_;
-        pj_pool_t& pool_;
-
-        pjsip_endpoint *endpt_;
+    pjsip_transport_type_e type {PJSIP_TRANSPORT_UNSPECIFIED};
+    pj_uint16_t listenerPort {DEFAULT_SIP_PORT};
+    std::string interface {"default"};
 };
 
-void sip_strerror(pj_status_t code);
+struct SipTransport;
+
+struct TlsListener
+{
+    TlsListener() {}
+    TlsListener(pjsip_tpfactory* f) : listener(f) {}
+    virtual ~TlsListener() {
+        DEBUG("Destroying listener");
+        listener->destroy(listener);
+    }
+    pjsip_tpfactory* get() {
+        return listener;
+    }
+private:
+    NON_COPYABLE(TlsListener);
+    pjsip_tpfactory* listener {nullptr};
+};
+
+typedef std::function<void(pjsip_transport_state, const pjsip_transport_state_info*)> SipTransportStateCallback;
+
+struct SipTransport
+{
+    SipTransport() {}
+    SipTransport(pjsip_transport*, const std::shared_ptr<TlsListener>& l = {});
+
+    virtual ~SipTransport();
+
+    void stateCallback(pjsip_transport_state state, const pjsip_transport_state_info *info) {
+        std::vector<SipTransportStateCallback> cbs {};
+        {
+            std::lock_guard<std::mutex> lock(stateListenersMutex_);
+            cbs.reserve(stateListeners.size());
+            for (auto& l : stateListeners)
+                cbs.push_back(l.second);
+        }
+        for (auto& cb : cbs)
+            cb(state, info);
+    }
+
+    pjsip_transport* get() {
+        return transport;
+    }
+
+    void addStateListener(uintptr_t lid, SipTransportStateCallback cb) {
+        std::lock_guard<std::mutex> lock(stateListenersMutex_);
+        stateListeners[lid] = cb;
+    }
+
+    bool removeStateListener(uintptr_t lid) {
+        std::lock_guard<std::mutex> lock(stateListenersMutex_);
+        auto it = stateListeners.find(lid);
+        if (it != stateListeners.end()) {
+            stateListeners.erase(it);
+            return true;
+        }
+        return false;
+    }
+
+    static bool isAlive(const std::shared_ptr<SipTransport>&, pjsip_transport_state state);
+
+private:
+    NON_COPYABLE(SipTransport);
+    pjsip_transport* transport {nullptr};
+    std::shared_ptr<TlsListener> tlsListener {};
+    std::map<uintptr_t, SipTransportStateCallback> stateListeners {};
+    std::mutex stateListenersMutex_ {};
+};
+
+/**
+ * Manages the transports and receive callbacks from PJSIP
+ */
+class SipTransportBroker {
+public:
+    SipTransportBroker(pjsip_endpoint *endpt, pj_caching_pool& cp, pj_pool_t& pool);
+    ~SipTransportBroker();
+
+    std::shared_ptr<SipTransport> getUpdTransport(const SipTransportDescr&);
+
+#if HAVE_TLS
+    std::shared_ptr<TlsListener> getTlsListener(const SipTransportDescr&, const pjsip_tls_setting*);
+
+    std::shared_ptr<SipTransport> getTlsTransport(const std::shared_ptr<TlsListener>&, const std::string& remoteSipUri);
+#endif
+
+    std::shared_ptr<SipTransport> findTransport(pjsip_transport*);
+
+    /**
+     * Initialize the transport selector
+     * @param transport     A transport associated with an account
+     * @return          	A transport selector structure
+     */
+    static inline pjsip_tpselector getTransportSelector(pjsip_transport *transport) {
+        pjsip_tpselector tp = {PJSIP_TPSELECTOR_TRANSPORT, {transport}};
+        return tp;
+    }
+
+    /**
+     * This function returns a list of STUN mapped sockets for
+     * a given set of socket file descriptors */
+    std::vector<pj_sockaddr> getSTUNAddresses(const SIPAccountBase &account, std::vector<long> &socks) const;
+
+    /**
+     * Get the correct address to use (ie advertised) from
+     * a uri. The corresponding transport that should be used
+     * with that uri will be discovered.
+     *
+     * @param uri The uri from which we want to discover the address to use
+     * @param transport The transport to use to discover the address
+     */
+    void findLocalAddressFromTransport(pjsip_transport *transport, pjsip_transport_type_e transportType, const std::string &host, std::string &address, pj_uint16_t &port) const;
+
+    void findLocalAddressFromSTUN(pjsip_transport *transport, pj_str_t *stunServerName,
+            int stunPort, std::string &address, pj_uint16_t &port) const;
+
+    /**
+     * Call released_cb(success) when transport tp is destroyed, making the
+     * socket available for a new similar transport.
+     * success is true if the transport is actually released.
+     * TODO: make this call non-blocking.
+     */
+    void waitForReleased(const SipTransportDescr& tp, std::function<void(bool)> released_cb);
+
+private:
+    NON_COPYABLE(SipTransportBroker);
+
+    /**
+    * Create SIP UDP transport from account's setting
+    * @param account The account for which a transport must be created.
+    * @param IP protocol version to use, can be pj_AF_INET() or pj_AF_INET6()
+    * @return a pointer to the new transport
+    */
+    std::shared_ptr<SipTransport> createUdpTransport(const SipTransportDescr&);
+
+    static void tp_state_callback(pjsip_transport*, pjsip_transport_state, const pjsip_transport_state_info*);
+
+    void transportStateChanged(pjsip_transport*, pjsip_transport_state, const pjsip_transport_state_info*);
+
+    /**
+     * List of transports so we can bubble the events up.
+     */
+    std::map<pjsip_transport*, std::weak_ptr<SipTransport>> transports_ {};
+
+    /**
+     * Transports are stored in this map in order to retreive them in case
+     * several accounts would share the same port number.
+     */
+    std::map<SipTransportDescr, pjsip_transport*> udpTransports_ {};
+
+    std::mutex transportMapMutex_ {};
+    std::condition_variable transportDestroyedCv_ {};
+
+    pj_caching_pool& cp_;
+    pj_pool_t& pool_;
+    pjsip_endpoint *endpt_;
+};
 
 #endif // SIPTRANSPORT_H_
