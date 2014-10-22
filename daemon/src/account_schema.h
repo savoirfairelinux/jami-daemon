551058028bf3d10eec2903c1d16586cd2b228a8d
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2014-10-22 11:12:13 -0400

    dht: add port preference
    Allow to configure the DHT listener port
    
    Refs #56312
    
    Change-Id: I600448a0b84e3d97e845019eda4e46f169651a2e

--- daemon/src/account_schema.h
+++ daemon/src/account_schema.h
@@ -100,6 +100,9 @@ static const char *const CONFIG_TLS_VERIFY_CLIENT               = "TLS.verifyCli
 static const char *const CONFIG_TLS_REQUIRE_CLIENT_CERTIFICATE  = "TLS.requireClientCertificate";
 static const char *const CONFIG_TLS_NEGOTIATION_TIMEOUT_SEC     = "TLS.negotiationTimeoutSec";
 
+// DHT specific parameters
+static const char *const CONFIG_DHT_PORT                        = "DHT.port";
+
 // Volatile parameters
 static const char *const CONFIG_ACCOUNT_REGISTRATION_STATUS     = "Account.registrationStatus";
 static const char *const CONFIG_ACCOUNT_REGISTRATION_STATE_CODE = "Account.registrationCode";
