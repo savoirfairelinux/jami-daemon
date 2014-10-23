ed9788e0b9dee725c3f60197790437f7c1712c44
Philippe Groarke <philippe.groarke@savoirfairelinux.com>
2014-10-23 15:21:49 -0400

    OSX: Audio capture prelims.
    Change-Id: Iaf9837fe3a0616aed8578b8aefd3a39bb198a856

--- daemon/bin/osxmain.cpp
+++ daemon/bin/osxmain.cpp
@@ -151,10 +151,21 @@ static int osxTests()
     sflph_call_play_dtmf("1");
     sleep(1);
 
-    for (auto x : sflph_config_get_audio_output_device_list())
-        std::cout << x << std::endl;
 
-   // sflph_fini();
+    sflph_call_place("IP2IP", "patate", "127.0.0.1");
+
+
+    // Debug info
+    for (auto x : sflph_config_get_account_details("IP2IP"))
+    std::cout << x.first << "    " << x.second << std::endl;
+
+    std::cout << std::endl << "Detected output devices: ";
+    for (auto x : sflph_config_get_audio_output_device_list())
+        std::cout << "    " << x << std::endl;
+
+    std::cout << "Recorde path: " << sflph_config_get_record_path() << std::endl;
+
+    sflph_fini();
 
 }
 static int run()
