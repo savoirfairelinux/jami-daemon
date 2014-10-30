87abc4917e7f87eeded45c9c78e8803cfe8388ba
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2014-10-30 02:03:28 -0400

    daemon: remove getRingtoneList()
    Refs #59255
    Refs #65652
    
    Change-Id: I8aff174c317948e47316d636458f19d265456808

--- daemon/src/fileutils.cpp
+++ daemon/src/fileutils.cpp
@@ -92,13 +92,6 @@ const char *get_program_dir()
     return program_dir;
 }
 
-// FIXME: This should use our real DATADIR
-std::string
-get_ringtone_dir()
-{
-    return std::string(get_program_dir()) + "/../../share/sflphone/ringtones/";
-}
-
 /* Lock a file region */
 static int
 lockReg(int fd, int cmd, int type, int whence, int start, off_t len)
