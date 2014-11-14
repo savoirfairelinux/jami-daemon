8febc69740f2c3edb4e14958e089e320c017b3f5
Tristan Matthews <tristan.matthews@savoirfairelinux.com>
2014-11-14 11:37:50 -0500

    configure: enable C++11 with extensions by default
    This allows for M_PI to be used if building with mingw.
    
    Refs #60455
    
    Change-Id: Ie92a5db2f4221e608d4beef35f5df6b6d46ae862

--- daemon/bin/winmain.cpp
+++ daemon/bin/winmain.cpp
@@ -1,8 +1,10 @@
 #include <speex/speex.h>
+#include <cstdio>
+#include <cmath>
 
 int main()
 {
     const SpeexMode *mode = speex_lib_get_mode(SPEEX_MODEID_NB);
-    printf("Hello %s\n", mode->modeName);
+    printf("Hello %s %f\n", mode->modeName, M_PI);
     return 0;
 }
