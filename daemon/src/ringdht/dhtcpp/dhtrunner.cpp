1e95e3df029b893fcf78bbe66e5bdc5198ecefad
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2014-11-18 16:35:19 -0500

    dht: cleanup
    Change-Id: Ibb4bd26f2f256f971c25634910ae7179d017665b
    Refs: #59541, #59540

--- daemon/src/ringdht/dhtcpp/dhtrunner.cpp
+++ daemon/src/ringdht/dhtcpp/dhtrunner.cpp
@@ -108,7 +108,7 @@ DhtRunner::loop_()
         for (auto& put : dht_eputs) {
             auto& id = std::get<0>(put);
             auto& val = std::get<2>(put);
-            std::cout << "Processing encrypted put from " << id << " to " << std::get<1>(put) << " -> " << val << std::endl;
+            std::cout << "Processing encrypted put at " << id << " for " << std::get<1>(put) << " -> " << val << std::endl;
             dht->putEncrypted(id, std::get<1>(put), std::move(val), std::get<3>(put));
         }
         dht_eputs.clear();
