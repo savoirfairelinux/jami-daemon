00eb5e5c205fc210a02910f2735394e447da6851
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2014-12-03 15:38:59 -0500

    Add a pjsip SIP transport type for ICE
    Refs #61426
    
    Change-Id: Ic81c9b282c26d1246c1e449a5b767cd84d5fb71f

--- daemon/src/ice_transport.h
+++ daemon/src/ice_transport.h
@@ -51,6 +51,9 @@ namespace sfl {
 class IceTransport;
 
 using IceTransportCompleteCb = std::function<void(IceTransport&, bool)>;
+
+using IceRecvCb = std::function<ssize_t(unsigned char* buf, size_t len)>;
+
 using IceCandidate = pj_ice_sess_cand;
 
 class IceTransport {
@@ -84,9 +87,15 @@ class IceTransport {
         IpAddr getRemote(unsigned comp_id) const;
 
         ssize_t recv(int comp_id, unsigned char* buf, size_t len);
+        void setOnRecv(unsigned comp_id, IceRecvCb cb);
+
         ssize_t send(int comp_id, const unsigned char* buf, size_t len);
         ssize_t getNextPacketSize(int comp_id);
         ssize_t waitForData(int comp_id, unsigned int timeout);
+
+        bool isRunning() const {
+            return running;
+        }
 
     private:
         static void cb_on_rx_data(pj_ice_strans *ice_st,
@@ -125,6 +134,9 @@ class IceTransport {
         std::string local_pwd_ {};
         pj_sockaddr remoteAddr_ {};
 
+        // set to true when on_negodone_cb_ was called.
+        std::atomic_bool running {false};
+
         struct Packet {
                 Packet(void *pkt, pj_size_t size);
                 std::unique_ptr<char> data;
@@ -134,7 +146,7 @@ class IceTransport {
                 std::mutex mutex {};
                 std::condition_variable cv {};
                 std::deque<Packet> queue {};
-
+                IceRecvCb cb {};
         };
         std::vector<ComponentIO> compIO_;
 };
