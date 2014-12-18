d691ec8b741e0c69e899bf03067a0421f71f6966
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2014-12-18 12:38:20 -0500

    header cleanup
    Change-Id: Ie9aaf7c3ce06485d9959dd59ba486abc5edc5142

--- daemon/src/sip/sipcall.cpp
+++ daemon/src/sip/sipcall.cpp
@@ -42,7 +42,6 @@
 #include "sdp.h"
 #include "manager.h"
 #include "array_size.h"
-#include "audio/audiolayer.h"
 #if USE_CCRTP
 #include "audio/audiortp/audio_rtp_factory.h" // for AudioRtpFactoryException
 #else
