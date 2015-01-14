cfb9261455b6818e73ceae51e229c3a3ce810eb8
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-01-14 11:43:10 -0500

    ringaccount: dedicated ICE transport for SIP
    Refs #63602
    
    Change-Id: I8e116fe52040383fa268a24fb93e1559dbb05aa3

--- daemon/src/sip/sipvoiplink.cpp
+++ daemon/src/sip/sipvoiplink.cpp
@@ -380,6 +380,7 @@ transaction_request_cb(pjsip_rx_data *rdata)
         RING_DBG("Initializing ICE transport");
         call->initIceTransport(false);
     }
+
     call->setupLocalSDPFromIce();
 
     ring::AudioCodec* ac = Manager::instance().audioCodecFactory.instantiateCodec(PAYLOAD_CODEC_ULAW);
@@ -640,10 +641,10 @@ SIPVoIPLink::~SIPVoIPLink()
         RING_ERR("%d SIP calls remains!",
               Manager::instance().callFactory.callCount<SIPCall>());
 
-    // destroy SIP transport before endpoint
-    sipTransport.reset();
-
     pjsip_endpt_destroy(endpt_);
+
+    // destroy SIP transport after endpoint
+    sipTransport.reset();
 
     pj_pool_release(pool_);
     pj_caching_pool_destroy(cp_);
