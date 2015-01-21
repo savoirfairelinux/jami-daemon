b9f2674c015e2a14a50e8bd1965400843414b1be
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-01-21 13:44:35 -0500

    sipcall: restart media after SDP renegotiation
    Refs #64309
    
    Change-Id: I26f8ac35d95f90da84a0f067b7c3b8109f3e581e

--- daemon/src/sip/sipcall.h
+++ daemon/src/sip/sipcall.h
@@ -178,6 +178,8 @@ class SIPCall : public Call
 
         void startAllMedia();
 
+        void onMediaUpdate();
+
     private:
         NON_COPYABLE(SIPCall);
 
@@ -185,7 +187,7 @@ class SIPCall : public Call
         std::map<std::string, std::string>
         createHistoryEntry() const;
 
-        void stopAllMedias();
+        void stopAllMedia();
 
         /**
          * Transfer method used for both type of transfer
