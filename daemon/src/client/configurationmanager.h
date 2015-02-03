16bd192778e24c2f174e29e3a2cdee36e244148d
Eloi BAIL <eloi.bail@savoirfairelinux.com>
2015-02-03 12:22:15 -0500

    daemon: use of mediacodecfactory
    DONE:
    
    * Use of mediacodecfactory :
    - collection of audio and video MediaCodecs class
    - check using libav if codec is installed
    - API to search *audio codecs* in codecs list during SDP nego
    - API to retrieve *audio codecs* info by DBUS (UI)
    
    * Remove of audiocodecfactory calls (used for plugins)
    
    TODO:
    
    * issues during codecs installation checking
        - will be solved using AvCodecId enum type in MediaCodec instead of integer
    * add and use of an API for video (as already done for audio)
    * issue seen after refactoring on client that initiate opus-to-opus call
        - stuck in media_decoder setup step
        - not going to processing step (no audio decoding on client side)
    * For the moment encoding/decoding proprieties are stored in MediaCodec object.
        -> it means that several MediaCodec would be instantiate is case of different properties
    
    Change-Id: I233889e47860b9362281d9ab3cdb506ce86ec997

--- daemon/src/client/configurationmanager.h
+++ daemon/src/client/configurationmanager.h
@@ -68,7 +68,7 @@ class ConfigurationManager
         std::vector< int32_t > getAudioCodecList();
         std::vector< std::string > getSupportedTlsMethod();
         std::vector< std::string > getSupportedCiphers(const std::string& accountID) const;
-        std::vector< std::string > getAudioCodecDetails(int32_t payload);
+        std::vector< std::string > getAudioCodecDetails(int32_t codecId);
         std::vector< int32_t > getActiveAudioCodecList(const std::string& accountID);
 
         void setActiveAudioCodecList(const std::vector< std::string >& list, const std::string& accountID);
