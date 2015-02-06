9c9bf734daafb8778559c068d351d9bb0f933432
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2015-02-06 11:28:27 -0500

    daemon: remove History module
    Its role is moved to client side.
    
    Refs #65650
    Refs #65314
    
    Change-Id: I981aa722fee23d8f222a45b54688246caa2447da

---
+++ daemon/src/client/configurationmanager.h
@@ -0,0 +1,170 @@
+/*
+ *  Copyright (C) 2004-2015 Savoir-Faire Linux Inc.
+ *  Author: Pierre-Luc Beaudoin <pierre-luc.beaudoin@savoirfairelinux.com>
+ *  Author: Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
+ *  Author: Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
+ *  Author: Guillaume Carmel-Archambault <guillaume.carmel-archambault@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  If you modify this program, or any covered work, by linking or
+ *  combining it with the OpenSSL project's OpenSSL library (or a
+ *  modified version of that library), containing parts covered by the
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  grants you additional permission to convey the resulting work.
+ *  Corresponding Source for a non-source form of such a combination
+ *  shall include the source code for the parts of OpenSSL used as well
+ *  as that of the covered work.
+ */
+
+#ifndef CONFIGURATIONMANAGER_H
+#define CONFIGURATIONMANAGER_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <vector>
+#include <map>
+#include <string>
+
+#include "dring.h"
+
+namespace ring {
+
+class ConfigurationManager
+{
+    public:
+        void registerEvHandlers(struct ring_config_ev_handlers* evHandlers);
+
+    // Methods
+    public:
+        std::map< std::string, std::string > getAccountDetails(const std::string& accountID);
+        std::map<std::string, std::string> getVolatileAccountDetails(const std::string& accountID);
+        void setAccountDetails(const std::string& accountID, const std::map< std::string, std::string >& details);
+        std::map<std::string, std::string> getAccountTemplate();
+        std::string addAccount(const std::map< std::string, std::string >& details);
+        void removeAccount(const std::string& accoundID);
+        std::vector< std::string > getAccountList();
+        void sendRegister(const std::string& accoundID, bool enable);
+        void registerAllAccounts(void);
+
+        std::map< std::string, std::string > getTlsSettingsDefault();
+
+        std::vector< int32_t > getAudioCodecList();
+        std::vector< std::string > getSupportedTlsMethod();
+        std::vector< std::string > getSupportedCiphers(const std::string& accountID) const;
+        std::vector< std::string > getAudioCodecDetails(int32_t payload);
+        std::vector< int32_t > getActiveAudioCodecList(const std::string& accountID);
+
+        void setActiveAudioCodecList(const std::vector< std::string >& list, const std::string& accountID);
+
+        std::vector< std::string > getAudioPluginList();
+        void setAudioPlugin(const std::string& audioPlugin);
+        std::vector< std::string > getAudioOutputDeviceList();
+        void setAudioOutputDevice(int32_t index);
+        void setAudioInputDevice(int32_t index);
+        void setAudioRingtoneDevice(int32_t index);
+        std::vector< std::string > getAudioInputDeviceList();
+        std::vector< std::string > getCurrentAudioDevicesIndex();
+        int32_t getAudioInputDeviceIndex(const std::string& name);
+        int32_t getAudioOutputDeviceIndex(const std::string& name);
+        std::string getCurrentAudioOutputPlugin();
+        bool getNoiseSuppressState();
+        void setNoiseSuppressState(bool state);
+
+        bool isAgcEnabled();
+        void setAgcState(bool enabled);
+
+        void muteDtmf(bool mute);
+        bool isDtmfMuted();
+
+        bool isCaptureMuted();
+        void muteCapture(bool mute);
+        bool isPlaybackMuted();
+        void mutePlayback(bool mute);
+
+        std::map<std::string, std::string> getRingtoneList();
+
+        std::string getAudioManager();
+        bool setAudioManager(const std::string& api);
+
+        int32_t isIax2Enabled();
+        std::string getRecordPath();
+        void setRecordPath(const std::string& recPath);
+        bool getIsAlwaysRecording();
+        void setIsAlwaysRecording(bool rec);
+
+        void setHistoryLimit(int32_t days);
+        int32_t getHistoryLimit();
+
+        void setAccountsOrder(const std::string& order);
+
+        std::map<std::string, std::string> getHookSettings();
+        void setHookSettings(const std::map<std::string, std::string>& settings);
+
+        std::map<std::string, std::string> getTlsSettings();
+        void setTlsSettings(const std::map< std::string, std::string >& details);
+        std::map< std::string, std::string > getIp2IpDetails();
+
+        std::vector< std::map< std::string, std::string > > getCredentials(const std::string& accountID);
+        void setCredentials(const std::string& accountID, const std::vector< std::map< std::string, std::string > >& details);
+
+        std::string getAddrFromInterfaceName(const std::string& interface);
+
+        std::vector<std::string> getAllIpInterface();
+        std::vector<std::string> getAllIpInterfaceByName();
+
+        std::map<std::string, std::string> getShortcuts();
+        void setShortcuts(const std::map<std::string, std::string> &shortcutsMap);
+
+        void setVolume(const std::string& device, double value);
+        double getVolume(const std::string& device);
+
+        /*
+         * Security
+         */
+        std::map<std::string, std::string> validateCertificate(const std::string& accountId,
+            const std::string& certificate, const std::string& privateKey);
+        std::map<std::string, std::string> getCertificateDetails(const std::string& certificate);
+
+
+    // Signals
+    public:
+        void volumeChanged(const std::string& device, double value);
+
+        void accountsChanged();
+
+        void stunStatusFailure(const std::string& accoundID);
+
+        void registrationStateChanged(const std::string& accoundID, int state);
+        void sipRegistrationStateChanged(const std::string&, const std::string&, int32_t state);
+        void volatileAccountDetailsChanged(const std::string& accountID, const std::map<std::string, std::string> &details);
+        void errorAlert(int alert);
+
+        std::vector< int32_t > getHardwareAudioFormat();
+
+    private:
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+        // Event handlers; needed by the library API
+        ring_config_ev_handlers evHandlers_{};
+#pragma GCC diagnostic warning "-Wmissing-field-initializers"
+};
+
+} // namespace ring
+
+#endif //CONFIGURATIONMANAGER_H
