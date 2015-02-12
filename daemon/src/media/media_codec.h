c269043b9a5b7fdaccd81556391d83d096b2e60b
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-02-12 18:22:44 -0500

    sdp: refactoring
    Refs #64309
    
    Change-Id: I1dbb0d7c8d48210709d01ec0bfdfff12783ce330

--- daemon/src/media/media_codec.h
+++ daemon/src/media/media_codec.h
@@ -1,6 +1,7 @@
 /*
  *  Copyright (C) 2015 Savoir-Faire Linux Inc.
  *  Author: Eloi BAIL <eloi.bail@savoirfairelinux.com>
+ *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -32,6 +33,10 @@
 #ifndef __MEDIA_CODEC_H__
 #define __MEDIA_CODEC_H__
 
+#include "audio/audiobuffer.h" // for AudioFormat
+#include "sip/sdes_negotiator.h"
+#include "ip_utils.h"
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -57,6 +62,8 @@ struct MediaCodec {
     MediaCodec(unsigned  avcodecId, const std::string name, std::string libName, MediaType mediaType, CodecType codecType = CODEC_UNDEFINED, uint16_t bitrate = 0, uint16_t payloadType = 0, bool isActive = true);
     virtual ~MediaCodec();
 
+    bool isPCMG722() const;
+
     uint16_t codecId_;
     unsigned  avcodecId_;
     std::string name_;
@@ -72,5 +79,39 @@ struct MediaCodec {
     std::string to_string();
 };
 bool operator== (MediaCodec codec1, MediaCodec codec2);
+
+struct MediaDescription {
+    MediaType type {};
+    bool enabled {false};
+    bool holding {false};
+    IpAddr addr {};
+
+    MediaCodec* codec {};
+    std::string payload_type {};
+    std::string receiving_sdp {};
+    unsigned bitrate {};
+
+    //audio
+    AudioFormat audioformat {AudioFormat::NONE()};
+    unsigned frame_size {};
+
+    //video
+    std::string parameters {};
+
+    //crypto
+    CryptoAttribute crypto {};
+};
+
+struct DeviceParams {
+    std::string input {};
+    std::string format {};
+    unsigned width {}, height {};
+    unsigned framerate {};
+    std::string video_size {};
+    std::string channel {};
+    std::string loop {};
+    std::string sdp_flags {};
+};
+
 }
 #endif // __MEDIA_CODEC_H__
