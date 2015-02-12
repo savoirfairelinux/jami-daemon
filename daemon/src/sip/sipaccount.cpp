59d24638fd243f1b67e40687259d72c3a38bbf14
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2015-02-12 11:31:05 -0500

    sipaccount: fix segfault in getSupportedCiphers()
    was broken since commit ac3bf29a5a24deacda1fb6a1475356fccfb415ea
    
    Refs #66135
    
    Change-Id: Ic18fefb97c43f26f71b8c59a5d60cda8f3f967a3

--- daemon/src/sip/sipaccount.cpp
+++ daemon/src/sip/sipaccount.cpp
@@ -1518,6 +1518,7 @@ SIPAccount::getSupportedCiphers() const
             RING_ERR("Could not determine cipher list on this system");
 
         // filter-out 0 ciphers
+        availCiphers.reserve(cipherNum);
         std::copy_if(avail_ciphers.begin(), avail_ciphers.end(),
                      availCiphers.begin(),
                      [](pj_ssl_cipher& item){ return item > 0; });
