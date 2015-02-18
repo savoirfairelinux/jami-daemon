cb5e1f389fae6cb0ca2cef1f0ff2cf27e2184524
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-02-18 00:17:51 -0500

    srtp: negotiate sdes in sdp
    Refs #66174
    
    Change-Id: I4cfea1aeabc5c9d488bd9501d3c01d0aa7fdf1ec

--- daemon/src/media/audio/audiortp/avformat_rtp_session.cpp
+++ daemon/src/media/audio/audiortp/avformat_rtp_session.cpp
@@ -401,6 +401,10 @@ AVFormatRtpSession::start(int localPort)
 
     try {
         socketPair_.reset(new SocketPair(getRemoteRtpUri().c_str(), local_.addr.getPort()));
+        if (local_.crypto and remote_.crypto) {
+            socketPair_->createSRTP(local_.crypto.getCryptoSuite().c_str(), local_.crypto.getSrtpKeyInfo().c_str(),
+                                    remote_.crypto.getCryptoSuite().c_str(), remote_.crypto.getSrtpKeyInfo().c_str());
+        }
     } catch (const std::runtime_error &e) {
         RING_ERR("Socket creation failed on port %d: %s", local_.addr.getPort(), e.what());
         return;
@@ -421,7 +425,16 @@ AVFormatRtpSession::start(std::unique_ptr<IceSocket> rtp_sock,
         return;
     }
 
-    socketPair_.reset(new SocketPair(std::move(rtp_sock), std::move(rtcp_sock)));
+    try {
+        socketPair_.reset(new SocketPair(std::move(rtp_sock), std::move(rtcp_sock)));
+        if (local_.crypto and remote_.crypto) {
+            socketPair_->createSRTP(local_.crypto.getCryptoSuite().c_str(), local_.crypto.getSrtpKeyInfo().c_str(),
+                                    remote_.crypto.getCryptoSuite().c_str(), remote_.crypto.getSrtpKeyInfo().c_str());
+        }
+    } catch (const std::runtime_error &e) {
+        RING_ERR("Socket creation failed");
+        return;
+    }
 
     startSender();
     startReceiver();
