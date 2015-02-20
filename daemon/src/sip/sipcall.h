9da3d96782c778b46e5d6528da669a275eb09492
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-02-20 17:50:19 -0500

    srtp: negotiate sdes in sdp
    Refs #66174
    
    Change-Id: I4cfea1aeabc5c9d488bd9501d3c01d0aa7fdf1ec

--- daemon/src/sip/sipcall.h
+++ daemon/src/sip/sipcall.h
@@ -118,6 +118,19 @@ class SIPCall : public Call
         struct InvSessionDeleter { void operator()(pjsip_inv_session*) {} };
         std::unique_ptr<pjsip_inv_session, InvSessionDeleter> inv {};
 
+        void setSecure(bool sec) {
+            if (srtpEnabled_)
+                return;
+            if (sec && getConnectionState() != DISCONNECTED) {
+                throw std::runtime_error("Can't enable security since call is already connected");
+            }
+            srtpEnabled_ = sec;
+        }
+
+        bool isSecure() const {
+            return srtpEnabled_;
+        }
+
         void setCallMediaLocal(const pj_sockaddr& localIP);
 
         void setContactHeader(pj_str_t *contact);
@@ -215,6 +228,8 @@ class SIPCall : public Call
          */
         video::VideoRtpSession videortp_;
 #endif
+
+        bool srtpEnabled_ {false};
 
         /**
          * Hold the transport used for SIP communication.
