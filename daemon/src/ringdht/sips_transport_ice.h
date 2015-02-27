88bd962479f8383f5b947173c43ed871a7f87539
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-02-27 11:39:24 -0500

    ringdht: TLS/ICE sip transport refactoring
    Refs #67201
    
    Change-Id: I87344b851ee34feb3f75aa66866b79943a2e60c8

--- daemon/src/ringdht/sips_transport_ice.h
+++ daemon/src/ringdht/sips_transport_ice.h
@@ -91,6 +91,7 @@ struct SipsIceTransport
     void reset();
 
     IpAddr getLocalAddress() const;
+    IpAddr getRemoteAddress() const;
 
     std::shared_ptr<IceTransport> getIceTransport() const {
         return ice_;
@@ -127,11 +128,11 @@ private:
     std::atomic<bool> canWrite_ {false};
 
     // TODO
-    pj_status_t verify_status_;
+    pj_status_t verifyStatus_ {};
     int last_err_;
 
-    pj_ssl_cert_info local_cert_info_;
-    pj_ssl_cert_info remote_cert_info_;
+    pj_ssl_cert_info localCertInfo_;
+    pj_ssl_cert_info remoteCertInfo_;
 
     std::atomic<TlsConnectionState> state_ {TlsConnectionState::DISCONNECTED};
     clock::time_point handshakeStart_;
@@ -142,9 +143,6 @@ private:
     gnutls_datum_t cookie_key_ {nullptr, 0};
     gnutls_dtls_prestate_st prestate_;
 
-    /**
-     * To be called on a regular basis to receive packets
-     */
     bool setup();
     void loop();
     void clean();
@@ -167,16 +165,17 @@ private:
     std::list<std::vector<uint8_t>> tlsInputBuff_ {};
 
     pj_status_t startTlsSession();
+    void closeTlsSession();
+
     pj_status_t tryHandshake();
     void certGetCn(const pj_str_t *gen_name, pj_str_t *cn);
-    void certGetInfo(pj_pool_t *pool, pj_ssl_cert_info *ci,
-                     gnutls_x509_crt_t cert);
+    void certGetInfo(pj_pool_t *pool, pj_ssl_cert_info *ci, const gnutls_datum_t& cert);
     void certUpdate();
     pj_bool_t onHandshakeComplete(pj_status_t status);
     int verifyCertificate();
     pj_status_t getInfo (pj_ssl_sock_info *info);
+    static pj_status_t tls_status_from_err(int err);
 
-    void close();
 };
 
 }} // namespace ring::tls
