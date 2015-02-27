ad0425ca8a14f5b9e30c1d07d15df069fe415bb1
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-02-27 17:07:44 -0500

    tls: transmit certificate and cipher to client
    Refs #66534
    
    Change-Id: I1ae97785690f37ae6a11e1632d25347ddd4115d0

--- daemon/src/dring/configurationmanager_interface.h
+++ daemon/src/dring/configurationmanager_interface.h
@@ -170,6 +170,7 @@ double getVolume(const std::string& device);
 std::map<std::string, std::string> validateCertificate(const std::string& accountId,
     const std::string& certificate, const std::string& privateKey);
 std::map<std::string, std::string> getCertificateDetails(const std::string& certificate);
+std::map<std::string, std::string> getCertificateDetailsRaw(const std::vector<uint8_t>& certificate);
 
 } // namespace DRing
 
