ee3f8a84cfeb4726fe3a41fe6fbf8befd0b172ae
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-03-01 13:17:27 -0500

    sdp: filter unused codecs for libav
    Refs #66632
    
    Change-Id: I44839120b553cba446ea51cf96c41658b59119b7

--- daemon/src/media/media_encoder.cpp
+++ daemon/src/media/media_encoder.cpp
@@ -79,6 +79,7 @@ void MediaEncoder::setDeviceOptions(const DeviceParams& args)
 
 void MediaEncoder::setOptions(const MediaDescription& args)
 {
+    av_dict_set(&options_, "payload_type", ring::to_string(args.payload_type).c_str(), 0);
     av_dict_set(&options_, "bitrate", ring::to_string(args.bitrate).c_str(), 0);
 
     auto accountAudioCodec = std::static_pointer_cast<AccountAudioCodecInfo>(args.codec);
@@ -93,9 +94,6 @@ void MediaEncoder::setOptions(const MediaDescription& args)
     if (accountAudioCodec->audioformat.sample_rate && accountAudioCodec->audioformat.nb_channels)
         av_dict_set(&options_, "frame_size",
                     ring::to_string(static_cast<unsigned>(0.02 * accountAudioCodec->audioformat.sample_rate)).c_str(), 0);
-
-    if (not args.payload_type.empty())
-        av_dict_set(&options_, "payload_type", args.payload_type.c_str(), 0);
 
     if (not args.parameters.empty())
         av_dict_set(&options_, "parameters", args.parameters.c_str(), 0);
