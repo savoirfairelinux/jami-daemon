1b245d718a96b608da6484889a7eb502d9747655
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-03-02 16:00:08 -0500

    video: renegotiate when switching input
    Change-Id: Ic0a0ad9276e1be39c25249aad66b37e74a7a74fe

--- daemon/src/sip/sipcall.cpp
+++ daemon/src/sip/sipcall.cpp
@@ -598,6 +598,14 @@ SIPCall::internalOffHold(const std::function<void()>& sdp_cb)
 }
 
 void
+SIPCall::switchInput(const std::string& resource)
+{
+    videoInput_ = resource;
+    if (SIPSessionReinvite() != PJ_SUCCESS)
+        RING_WARN("Reinvite failed");
+}
+
+void
 SIPCall::peerHungup()
 {
     // Stop all RTP streams
@@ -763,7 +771,6 @@ SIPCall::startAllMedia()
             ? static_cast<RtpSession*>(avformatrtp_.get())
             : static_cast<RtpSession*>(&videortp_);
 
-
         auto accountAudioCodec = std::static_pointer_cast<AccountAudioCodecInfo>(local.codec);
         RING_DBG("########## UPDATE MEDIA ############");
         RING_DBG("[LOCAL][codec:%s][enabled:%s][holding:%s]"
@@ -790,6 +797,11 @@ SIPCall::startAllMedia()
         RING_DBG("[REMOTE] SDP: \n %s", remote.receiving_sdp.c_str());
         RING_DBG("####################################");
 
+        if (local.type == MEDIA_VIDEO) {
+            if (videoInput_.empty())
+                videoInput_ = "v4l2://" + videoManager.videoDeviceMonitor.getDefaultDevice();
+            videortp_.setInput(videoInput_);
+        }
         rtp->updateMedia(local, remote);
         if (isIceRunning()) {
             rtp->start(newIceSocket(ice_comp_id + 0),
@@ -818,7 +830,7 @@ SIPCall::onMediaUpdate()
 
     if (startIce()) {
         auto this_ = std::static_pointer_cast<SIPCall>(shared_from_this());
-        auto iceTimeout = std::chrono::steady_clock::now() + std::chrono::seconds {10};
+        auto iceTimeout = std::chrono::steady_clock::now() + std::chrono::seconds(10);
         Manager::instance().addTask([=] {
             /* First step: wait for an ICE transport for SIP channel */
             if (this_->iceTransport_->isFailed() or std::chrono::steady_clock::now() >= iceTimeout) {
