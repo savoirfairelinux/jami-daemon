a02b5876bbb9ceb61c9d301457f87634184422d4
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-03-03 10:34:48 -0500

    use hardware audio format for main ringbuffer
    Change-Id: I1b9a42cd9ed13053da5b1747960b67ff4ad03bd4

--- daemon/src/managerimpl.cpp
+++ daemon/src/managerimpl.cpp
@@ -2354,13 +2354,13 @@ ManagerImpl::initAudioDriver()
     audiodriver_.reset(audioPreference.createAudioLayer());
 }
 
-void
+AudioFormat
 ManagerImpl::hardwareAudioFormatChanged(AudioFormat format)
 {
-    audioFormatUsed(format);
+    return audioFormatUsed(format);
 }
 
-void
+AudioFormat
 ManagerImpl::audioFormatUsed(AudioFormat format)
 {
     AudioFormat currentFormat = ringbufferpool_->getInternalAudioFormat();
@@ -2368,7 +2368,7 @@ ManagerImpl::audioFormatUsed(AudioFormat format)
     format.sample_rate = std::max(currentFormat.sample_rate, format.sample_rate);
 
     if (currentFormat == format)
-        return;
+        return format;
 
     RING_DBG("Audio format changed: %s -> %s", currentFormat.toString().c_str(), format.toString().c_str());
 
@@ -2379,6 +2379,7 @@ ManagerImpl::audioFormatUsed(AudioFormat format)
         telephoneTone_.reset(new TelephoneTone(preferences.getZoneToneChoice(), format.sample_rate));
     }
     dtmfKey_.reset(new DTMF(format.sample_rate));
+    return format;
 }
 
 void
