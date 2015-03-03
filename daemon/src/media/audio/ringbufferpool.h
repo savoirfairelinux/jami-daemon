bb5bbb1c220994a50afbb515e84087c74c2a1891
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-03-03 18:25:40 -0500

    audio: use hardware format for main ringbuffer
    Refs #67630
    
    Change-Id: I1b9a42cd9ed13053da5b1747960b67ff4ad03bd4

--- daemon/src/media/audio/ringbufferpool.h
+++ daemon/src/media/audio/ringbufferpool.h
@@ -134,6 +134,8 @@ class RingBufferPool {
     private:
         NON_COPYABLE(RingBufferPool);
 
+        static constexpr AudioFormat DEFAULT_POOL_FORMAT {16000, 1};
+
         // A set of RingBuffers readable by a call
         typedef std::set<std::shared_ptr<RingBuffer>,
             std::owner_less<std::shared_ptr<RingBuffer>> > ReadBindings;
@@ -159,7 +161,7 @@ class RingBufferPool {
 
         mutable std::recursive_mutex stateLock_ {};
 
-        AudioFormat internalAudioFormat_ {AudioFormat::MONO()};
+        AudioFormat internalAudioFormat_ {DEFAULT_POOL_FORMAT};
 
         std::shared_ptr<RingBuffer> defaultRingBuffer_;
 };
