78ce8148f3b682165758ee68d656c1ab10067b5f
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2015-03-11 17:56:02 -0400

    video: implement frame data direct access and make shm optional
    This patchset permits OSX client access.
    This one doesn't use shared memory and requiers a direct access
    to video frame.
    This is done by a client side callback.
    
    Refs #67977
    
    Change-Id: Ib6780efe4beeab027bb868d3a124d2cec59de915

--- daemon/src/media/video/sinkclient.h
+++ daemon/src/media/video/sinkclient.h
@@ -35,51 +35,52 @@
 
 #pragma once
 
-#include "noncopyable.h"
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "video_provider.h"
 #include "video_base.h"
 
 #include <string>
 #include <vector>
-
-class SHMHeader;
+#include <memory>
 
 namespace ring { namespace video {
+
+#if HAVE_SHM
+class ShmHolder;
+#endif // HAVE_SHM
 
 class SinkClient : public VideoFramePassiveReader
 {
     public:
-        SinkClient(const std::string &shm_name = "");
-        std::string openedName() const { return opened_name_; }
+        SinkClient(const std::string& id="");
         ~SinkClient();
 
-        bool start();
-        bool stop();
+        const std::string& getId() const noexcept {
+            return id_;
+        }
 
-        bool resize_area(size_t desired_length);
-
-        void render(const std::vector<unsigned char> &data);
-        void render_frame(VideoFrame& src);
-        void render_callback(VideoProvider &provider, size_t bytes);
+        const std::string& openedName() const noexcept;
 
         // as VideoFramePassiveReader
         void update(Observable<std::shared_ptr<ring::VideoFrame>>*,
                     std::shared_ptr<ring::VideoFrame> &);
 
-    private:
-        NON_COPYABLE(SinkClient);
+        bool start();
+        bool stop();
 
-        void shm_lock();
-        void shm_unlock();
-        std::string shm_name_;
-        int fd_;
-        SHMHeader *shm_area_;
-        size_t shm_area_len_;
-        std::string opened_name_;
-#ifdef DEBUG_FPS
-        unsigned frameCount_;
-        std::chrono::time_point<std::chrono::system_clock> lastFrameDebug_;
-#endif
+        void registerTarget(std::function<void(const unsigned char*)>& cb);
+
+    private:
+        const std::string id_;
+        std::function<void(const unsigned char*)> target_;
+        std::vector<unsigned char> targetData_;
+
+#if HAVE_SHM
+        ShmHolder* shm_;
+#endif // HAVE_SHM
 };
 
 }} // namespace ring::video
