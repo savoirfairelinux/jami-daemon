d05bd0b92e01ff989262e199b9528920b254a1a9
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2015-03-14 15:24:53 -0400

    daemon: update copyright dates
    Refs #68586
    
    Change-Id: I2c65640f6291d914817c395806cbc74df275d088

--- daemon/src/media/video/video_device.h
+++ daemon/src/media/video/video_device.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2014 Savoir-Faire Linux Inc.
+ *  Copyright (C) 2014-2015 Savoir-Faire Linux Inc.
  *  Author: Vivien Didelot <vivien.didelot@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
