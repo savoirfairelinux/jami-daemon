b317af0abe8da597474fb6cd3f3172545245c7d5
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2015-03-18 13:46:16 -0400

    media: set AVCodecContex thread_count
    Use std::thread::hardware_concurrency() value.
    
    Refs #68788
    
    Change-Id: Ib0930564f52b20bef2928a8229207efc5ff6bda8

--- daemon/src/media/media_decoder.cpp
+++ daemon/src/media/media_decoder.cpp
@@ -43,6 +43,7 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <thread> // hardware_concurrency
 
 namespace ring {
 
@@ -177,7 +178,7 @@ int MediaDecoder::setupFromAudioData(const AudioFormat format)
         return -1;
     }
 
-    decoderCtx_->thread_count = 1;
+    decoderCtx_->thread_count = std::thread::hardware_concurrency();
     decoderCtx_->channels = format.nb_channels;
     decoderCtx_->sample_rate = format.sample_rate;
 
