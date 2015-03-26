dc6c92a4fd70554e128f9604805c1a14533f3a4c
Eloi BAIL <eloi.bail@savoirfairelinux.com>
2015-03-26 15:44:48 -0400

    daemon: send RGBA frames for direct rendering
    Osx manages only RGB color space
    
    Refs #66543
    
    Change-Id: Ia40f2e79d054efc419eec7a246b52f2a109b0d20

--- src/media/video/video_base.h
+++ src/media/video/video_base.h
@@ -58,6 +58,7 @@ namespace ring { namespace video {
 enum VideoPixelFormat {
     VIDEO_PIXFMT_BGRA = -1,
     VIDEO_PIXFMT_YUV420P = -2,
+    VIDEO_PIXFMT_RGBA = -3,
 };
 
 template <typename T> class Observer;
