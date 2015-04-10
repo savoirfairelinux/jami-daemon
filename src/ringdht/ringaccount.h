73f65e28d302a5d20331de02999ff29539f4317c
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-04-10 10:59:43 -0400

    build: fix compilation with clang
    Refs #69759
    
    Change-Id: I31cc7ab37a756f8750d84829e187ca81aa159701

--- src/ringdht/ringaccount.h
+++ src/ringdht/ringaccount.h
@@ -79,7 +79,7 @@ class RingAccount : public SIPAccountBase {
         constexpr static const char * const ACCOUNT_TYPE = "RING";
         constexpr static const in_port_t DHT_DEFAULT_PORT = 4222;
         constexpr static const char * const DHT_DEFAULT_BOOTSTRAP = "bootstrap.ring.cx";
-        constexpr static const std::pair<uint16_t, uint16_t> DHT_PORT_RANGE {4000, 8888};
+        /* constexpr */ static const std::pair<uint16_t, uint16_t> DHT_PORT_RANGE;
 
         const char* getAccountType() const {
             return ACCOUNT_TYPE;
