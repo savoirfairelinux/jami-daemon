0b232b7782416293fae1861d6463f244382a36a5
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-04-22 16:11:34 -0400

    security: add daemon certificate store
    Refs #68196
    
    Change-Id: I9f69f680135af26fce2b6a0ca115823686f4c7a9

--- src/dring/security_const.h
+++ src/dring/security_const.h
@@ -34,6 +34,12 @@ namespace DRing {
 
 namespace Certificate {
 
+enum Status {
+    UNDEFINED = 0,
+    ALLOWED,
+    BANNED
+};
+
 /**
 * Those constantes are used by the ConfigurationManager.validateCertificate method
 */
@@ -109,6 +115,13 @@ namespace CheckValuesNames {
 } //namespace DRing::Certificate::CheckValuesNames
 
 } //namespace DRing::Certificate
+
+namespace TlsTransport {
+constexpr static char TLS_PEER_CERT        [] = "TLS_PEER_CERT"   ;
+constexpr static char TLS_PEER_CA_NUM      [] = "TLS_PEER_CA_NUM" ;
+constexpr static char TLS_PEER_CA_         [] = "TLS_PEER_CA_"    ;
+constexpr static char TLS_CIPHER           [] = "TLS_CIPHER"      ;
+} //namespace DRing::Transport
 
 } //namespace DRing
 
