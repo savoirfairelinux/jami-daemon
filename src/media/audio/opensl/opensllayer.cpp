6c2c78bf071c7904a0e3a4d0d3626e3532b58042
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-06-08 15:25:07 -0400

    dring/android: add signal to retrieve native audio format
    Refs #75003
    
    Change-Id: Id34232a91315b41cee824341c6eda93474c59205

--- src/media/audio/opensl/opensllayer.cpp
+++ src/media/audio/opensl/opensllayer.cpp
@@ -30,7 +30,7 @@
 
 #include "opensllayer.h"
 
-#include "client/configurationmanager.h"
+#include "client/ring_signal.h"
 
 #include "manager.h"
 #include "audio/resampler.h"
@@ -115,8 +115,10 @@ OpenSLLayer::startStream()
 
     RING_DBG("Start OpenSL audio layer");
 
-    std::vector<int32_t> hw_infos = Manager::instance().getConfigurationManager()->getHardwareAudioFormat();
-    hardwareFormat_ = AudioFormat(hw_infos[0], 1);  // Mono on Android
+    std::vector<int32_t> hw_infos;
+    emitSignal<DRing::ConfigurationSignal::GetHardwareAudioFormat>(&hw_infos);
+    hardwareFormat_ = AudioFormat(hw_infos[0], 1); // Mono on Android
+    //hardwareFormat_ = AudioFormat(44100, 1);
     hardwareBuffSize_ = hw_infos[1];
 
     for(auto& buf : playbackBufferStack_)
