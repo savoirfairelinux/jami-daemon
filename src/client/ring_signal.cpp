11d8d0bdc37b346bb0e15e3cf2cbbd2cf808b71d
Damien Riegel <damien.riegel@savoirfairelinux.com>
2015-06-09 16:21:11 -0400

    video: support device enumeration and capabilities
    This commit adds support in the daemon to enumerate video devices and
    capabilities. This code relies on the Java part because the daemon
    cannot access natively the camera API.
    
    Also, the video manager is instantiated statically, so it is created
    before the handlers have a chance to register. Calls to emitSignal in
    the constructors of VideoDeviceMonitor and VideoDevice silently do
    nothing. To work around that, new calls have been added to allow the
    Java code to add and remove video devices once the library is loaded and
    handlers registered. This way, we avoid polling.
    
    Refs #75493
    Change-Id: I40e6a22e6aca6084b51eb7a3cee3ca58d4cdf93e

--- src/client/ring_signal.cpp
+++ src/client/ring_signal.cpp
@@ -93,6 +93,13 @@ getSignalHandlers()
         exported_callback<DRing::VideoSignal::DeviceEvent>(),
         exported_callback<DRing::VideoSignal::DecodingStarted>(),
         exported_callback<DRing::VideoSignal::DecodingStopped>(),
+#ifdef __ANDROID__
+        exported_callback<DRing::VideoSignal::AcquireCamera>(),
+        exported_callback<DRing::VideoSignal::ReleaseCamera>(),
+        exported_callback<DRing::VideoSignal::GetCameraFormats>(),
+        exported_callback<DRing::VideoSignal::GetCameraSizes>(),
+        exported_callback<DRing::VideoSignal::GetCameraRates>(),
+#endif
 #endif
     };
 
