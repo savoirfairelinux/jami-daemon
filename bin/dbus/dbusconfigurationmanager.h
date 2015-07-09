7d75783c2e3f042a55e019cfb365be18114474f0
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-07-09 16:30:29 -0400

    security: Change the API so it can express a certificate chain
    The old API could only represent a single certificate, making
    it useless to properly validate the assets.
    
    WARNING: This commit break the API
    
    Refs #76889
    
    Change-Id: I953f62ae359b9754895c72fb2e9a2bffee32eb8d

--- bin/dbus/dbusconfigurationmanager.h
+++ bin/dbus/dbusconfigurationmanager.h
@@ -128,12 +128,12 @@ class DBusConfigurationManager :
         void setShortcuts(const std::map<std::string, std::string> &shortcutsMap);
         void setVolume(const std::string& device, const double& value);
         double getVolume(const std::string& device);
-        std::map<std::string, std::string> validateCertificate(const std::string& accountId, const std::string& certificate, const std::string& privateKey);
-        std::map<std::string, std::string> validateCertificateRaw(const std::string& accountId, const std::vector<uint8_t>& certificate);
+        std::map<std::string, std::string> validateCertificate(const std::string& accountId, const std::string& certificate);
+        std::map<std::string, std::string> validateCertificatePath(const std::string& accountId, const std::string& certificatePath, const std::string& privateKey, const std::string& caList);
         std::map<std::string, std::string> getCertificateDetails(const std::string& certificate);
-        std::map<std::string, std::string> getCertificateDetailsRaw(const std::vector<uint8_t>& certificate);
+        std::map<std::string, std::string> getCertificateDetailsPath(const std::string& certificatePath);
         std::vector<std::string> getPinnedCertificates();
-        std::string pinCertificate(const std::vector<uint8_t>& certificate, const bool& local);
+        std::vector<std::string> pinCertificate(const std::vector<uint8_t>& certificate, const bool& local);
         bool unpinCertificate(const std::string& certId);
         void pinCertificatePath(const std::string& path);
         unsigned unpinCertificatePath(const std::string& path);
