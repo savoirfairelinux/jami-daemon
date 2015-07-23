96e43a5369738ec8b27eea15a09ac77b91c19076
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2015-07-22 21:58:43 -0400

    tls/ice transport: prevent deadlock in TLS/ICE thread
    Remove the wait on rxPending_ empty queue event
    in TLS thread cleanup as this event has to be trigged
    by handleEvent() code, but this last is called by mainloop only,
    and this lst is waiting about TLS thread end by a join()... deadlock!
    
    Issue: #76205
    
    Change-Id: I895f09af51deb8bab36c3c6511606586341e2783

--- src/ringdht/sips_transport_ice.cpp
+++ src/ringdht/sips_transport_ice.cpp
@@ -200,8 +200,10 @@ SipsIceTransport::~SipsIceTransport()
     RING_DBG("~SipsIceTransport");
     auto event = state_ == TlsConnectionState::ESTABLISHED;
     shutdown();
-    tlsThread_.join();
+    if (tlsThread_.joinable()
+        tlsThread_.join();
     Manager::instance().unregisterEventHandler((uintptr_t)this);
+    handleEvents(); // process latest incoming packets
 
     pjsip_transport_add_ref(getTransportBase());
     auto state_cb = pjsip_tpmgr_get_state_cb(trData_.base.tpmgr);
@@ -895,13 +897,7 @@ SipsIceTransport::clean()
             });
     }
 
-    {
-        // make sure all incoming packets are reported before closing
-        std::unique_lock<std::mutex> l(rxMtx_);
-        rxCv_.wait(l, [&](){
-                return rxPending_.empty();
-            });
-    }
+    // note: incoming packets (rxPending_) will be processed in destructor
 
     if (cookie_key_.data) {
         gnutls_free(cookie_key_.data);
