b39e83178a5c8ca6794e4d6f7e7fafe732974c2f
Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
2015-09-03 23:46:21 -0400

    im: use pjsip api to create and parse messages
    This supports both simple and multipart content types.
    
    *WARNING* the daemon API has not changed; however the previous
    implementation was not RFC compliant and thus messages sent from or
    received by a previous version of the daemon will not be interpreted
    correctly. Additionally the behaviour of the API has slightly changed.
    It will no longer split multiple message pairs received in the
    map<string, string> into multiple, separate SIP messages if they do not
    all fit into one. If there is only one pair in the map, then a message
    with a single content-type will be created and sent. If multiple pairs
    exist, then a multipart/mixed type message will be created. If the
    created message is too large to be sent, then no parts will be sent.
    
    Support for storing the URI of the original sender is dropped for now as it
    was not being used by the clients and the implementation was buggy.
    
    The APIs for creating XML recipient lists as defined by RFC 5365 remains
    but is no longer used for now.
    
    Issue: #79657
    Change-Id: I2b00cbd797fbb423ee0a7eb24748d2362e9f9ff8

--- src/im/instant_messaging.h
+++ src/im/instant_messaging.h
@@ -2,6 +2,7 @@
  *  Copyright (C) 2004-2015 Savoir-faire Linux Inc.
  *  Author: Emmanuel Milou <emmanuel.milou@savoirfairelinux.com>
  *  Author: Alexandre Savard <alexandre.savard@savoirfairelinux.com>
+ *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,7 +23,7 @@
  *  If you modify this program, or any covered work, by linking or
  *  combining it with the OpenSSL project's OpenSSL library (or a
  *  modified version of that library), containing parts covered by the
- *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-faire Linux Inc.
  *  grants you additional permission to convey the resulting work.
  *  Corresponding Source for a non-source form of such a combination
  *  shall include the source code for the parts of OpenSSL used as well
@@ -44,19 +45,10 @@
 #endif
 
 struct pjsip_inv_session;
+struct pjsip_rx_data;
+struct pjsip_msg;
 
 namespace ring { namespace InstantMessaging {
-
-/* PJSIP's sip message limit, PJSIP_MAX_PKT_LEN is the total, but most
-   is used for the SIP header
-
-   Ring currently split the messages into smaller ones and send them. Obviously
-   it is invalid as some messages wont have the MIME boundary section.
-
-   The number set here is arbitrary, the theoretical limit is around 3000,
-   but some messages may fail.
-*/
-constexpr static unsigned MAXIMUM_MESSAGE_LENGTH = 1800;
 
 constexpr static const char* IM_XML_URI = "uri";
 
@@ -69,8 +61,37 @@ struct InstantMessageException : std::runtime_error
 using UriEntry = std::map<std::string, std::string>;
 using UriList = std::list<UriEntry>;
 
-void sendSipMessage(pjsip_inv_session* session, const std::string& id,
-                    const std::vector<std::string>& chunks);
+/**
+ * Constructs and sends a SIP message.
+ *
+ * The expected format of the map key is:
+ *     type/subtype[; *[; arg=value]]
+ *     eg: "text/plain; id=1234;part=2;of=1001"
+ *     note: all whitespace is optional
+ *
+ * If the map contains more than one pair, then a multipart/mixed message type will be created
+ * containing multiple message parts. Note that all of the message parts must be able to fit into
+ * one message... they will not be split into multple messages.
+ *
+ * @param session SIP session
+ * @param payloads a map where the mime type and optional parameters are the key
+ *                 and the message payload is the value
+ *
+ * @return boolean indicating whether or not the message was successfully sent
+ */
+bool sendSipMessage(pjsip_inv_session* session,
+                    const std::map<std::string, std::string>& payloads);
+
+/**
+ * Parses given SIP message into a map where the key is the contents of the Content-Type header
+ * (along with any parameters) and the value is the message payload.
+ *
+ * @param msg received SIP message
+ *
+ * @return map of content types and message payloads
+ */
+std::map<std::string, std::string> parseSipMessage(pjsip_msg* msg);
+
 #if HAVE_IAX
 void sendIaxMessage(iax_session* session, const std::string& id,
                     const std::vector<std::string>& chunks);
@@ -94,44 +115,5 @@ std::string generateXmlUriList(const UriList& list);
  * @return An UriList of UriEntry containing parsed XML information as a map.
  */
 UriList parseXmlUriList(const std::string &urilist);
-
-/**
- * Format text message according to RFC 5365, append recipient-list to the message
- *
- * @param Key/Value MIME pairs to be sent. If a payload doesn't fit, the message will be split
- * @param list containing the recipients
- *
- * @return formated text stored into a string to be included in sip MESSAGE
- */
-std::vector<std::string> appendMimePayloads(const std::map<std::string, std::string>& payloads,
-                                            const UriList& list = {});
-
-/**
- * Retreive the xml formated uri list in formated text data according to RFC 5365
- *
- * @param text The formated text message as retreived in the SIP message
- *
- * @return A string containing the XML content
- */
-std::string findTextUriList(const std::string& text);
-
-/**
- * Retrieve a MIME payload from the SIP container RFC5365
- *
- * @param mime the mime type
- *
- * @param encodedPayloads a MIME encoded set of payloads
- *
- * @return A string containing the actual message
- */
-std::string findMimePayload(const std::string& encodedPayloads,
-                            const std::string& mime = "text/plain");
-
-/**
- * Retrieve all MIME payloads from encodedPayloads
- *
- * @param encodedPayloads a MIME encoded set of payloads
- */
-std::map< std::string, std::string > parsePayloads(const std::string& encodedPayloads);
 
 }} // namespace ring::InstantMessaging
