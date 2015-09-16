9b3f4a27e59f77a650643ecdc9a876723b2aa67f
Eloi BAIL <eloi.bail@savoirfairelinux.com>
2015-09-16 09:44:31 -0400

    tools: refactor automatic call test tool
    Refactor of automatic call test tool
    Now do ./dringctrl --help : all test name will be listed
    ./dringctrl --test <testName> to execute a test
    
    Issue: #80498
    Change-Id: I86d16e7d96f04f040133f16856ba9ca8a061374d

--- tools/dringctrl/dringctrl.py
+++ tools/dringctrl/dringctrl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2015 Savoir-Faire Linux Inc.
+# Copyright (C) 2015 Savoir-faire Linux Inc.
 # Author: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,18 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
-#
-# Additional permission under GNU GPL version 3 section 7:
-#
-# If you modify this program, or any covered work, by linking or
-# combining it with the OpenSSL project's OpenSSL library (or a
-# modified version of that library), containing parts covered by the
-# terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
-# grants you additional permission to convey the resulting work.
-# Corresponding Source for a non-source form of such a combination
-# shall include the source code for the parts of OpenSSL used as well
-# as that of the covered work.
-#
 
 import sys
 import os
@@ -100,7 +88,7 @@ if __name__ == "__main__":
     parser.add_argument('--dtmf', help='Send DTMF', metavar='<key>')
     parser.add_argument('--toggleVideo', help='Launch toggle video  tests', action='store_true')
 
-    parser.add_argument('--test', help='Launch automatic tests', action='store_true')
+    parser.add_argument('--test', help=' '.join(str(test) for test in DRingTester().getTestName() ), metavar='<testName>')
 
     args = parser.parse_args()
 
@@ -179,7 +167,7 @@ if __name__ == "__main__":
         ctrl.Dtmf(args.dtmf)
 
     if args.test:
-        DRingTester().start(ctrl)
+        DRingTester().start(ctrl, args.test)
 
     if args.toggleVideo:
         if not ctrl.videomanager:
