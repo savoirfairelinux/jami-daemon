c1b5993dbc682c9995261ce07b289bef1c89a326
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-10-22 14:46:58 -0400

    accounts: add API-controlable "active" proprety
    * add "active" runtime property to accounts
    * accounts can only be used when active
    * "active" state is not persisted
    * add "active" state to volatile propreties map
    * add setAccountActive API
    
    The active state can be used to manage runtime states
    defining if an account should be used (e.g. on Wifi/3G etc.)
    
    Tuleap: #9
    Tuleap: #42
    Change-Id: Ie3b3aaadea6e43237fc22f6a1f96de8cbfe2bd95

--- src/manager.cpp
+++ src/manager.cpp
@@ -2382,7 +2382,7 @@ Manager::setAccountDetails(const std::string& accountID,
         // Serialize configuration to disk once it is done
         saveConfig();
 
-        if (account->isEnabled()) {
+        if (account->isUsable()) {
             account->doRegister();
         } else
             account->doUnregister();
@@ -2668,7 +2668,7 @@ Manager::registerAccounts()
 
         a->loadConfig();
 
-        if (a->isEnabled()) {
+        if (a->isUsable()) {
             a->doRegister();
         }
     }
@@ -2710,6 +2710,21 @@ Manager::sendTextMessage(const std::string& accountID, const std::string& to, co
     acc->sendTextMessage(to, message);
 }
 
+void
+Manager::setAccountActive(const std::string& accountID, bool active)
+{
+    const auto acc = getAccount(accountID);
+    if (!acc)
+        return;
+    acc->setActive(active);
+    if (acc->isEnabled()) {
+        if (active)
+            acc->doRegister();
+        else
+        acc->doUnregister();
+    }
+}
+
 std::shared_ptr<AudioLayer>
 Manager::getAudioDriver()
 {
@@ -2748,7 +2763,7 @@ Manager::newOutgoingCall(const std::string& toUrl,
     } else
         RING_WARN("IP Url detected, using IP2IP account");
 
-    if (!account) {
+    if (!account or !account->isUsable()) {
         RING_ERR("No suitable account found to create outgoing call");
         return nullptr;
     }
