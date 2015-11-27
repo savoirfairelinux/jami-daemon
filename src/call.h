2663303c30d24ba94160162b903737ec7050da6a
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-11-27 13:49:24 -0500

    call: emit signal on removal
    Tuleap: #102
    Change-Id: Id11b9047b27c33258b731f7843fd7241e4c7c07f

--- src/call.h
+++ src/call.h
@@ -84,7 +84,7 @@ class Call : public Recordable, public std::enable_shared_from_this<Call> {
          * \note modify validStateTransition/getStateStr if this enum changes
          */
         enum class CallState : unsigned {
-            INACTIVE, ACTIVE, HOLD, BUSY, MERROR, COUNT__
+            INACTIVE, ACTIVE, HOLD, BUSY, MERROR, OVER, COUNT__
         };
 
         virtual ~Call();
