0ddebbf67a160ff35fcdfab4069c0056e2dafa76
Alexandre Lision <alexandre.lision@savoirfairelinux.com>
2016-02-12 17:25:01 -0500

    account: add import/export API
    - do not read dhtPort when unserializing a Ring account, use a new
    one at runtime
    - check cache dir is created when generating dhParams
    - use key stretching for archive encryption
    
    This commit comes with a necessary bump of OpenDHT
    
    Tuleap: #335
    Change-Id: Iee67569d378baaa33e9acd7cd9557422ab8e0471

--- src/client/configurationmanager.cpp
+++ src/client/configurationmanager.cpp
@@ -34,6 +34,7 @@
 #endif
 #include "logger.h"
 #include "fileutils.h"
+#include "archiver.h"
 #include "ip_utils.h"
 #include "sip/sipaccount.h"
 #include "ringdht/ringaccount.h"
@@ -312,6 +313,21 @@ sendTrustRequest(const std::string& accountId, const std::string& to, const std:
 {
     if (auto acc = ring::Manager::instance().getAccount<ring::RingAccount>(accountId))
         acc->sendTrustRequest(to, payload);
+}
+
+/*
+ * Import/Export accounts
+ */
+int
+exportAccounts(std::vector<std::string> accountIDs, std::string filepath, std::string password)
+{
+    return ring::Archiver::instance().exportAccounts(accountIDs, filepath, password);
+}
+
+int
+importAccounts(std::string archivePath, std::string password)
+{
+    return ring::Archiver::instance().importAccounts(archivePath, password);
 }
 
 ///This function is used as a base for new accounts for clients that support it
