da11a7798d277fcd5433b55fb9ca578223285714
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2016-02-22 12:35:23 -0500

    draft: file transfer implementation
    Change-Id: Icdd2ffbabe460aa4ad641412b02b4040241b20c8

---
+++ src/client/datatransfer.cpp
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "datatransfer_interface.h"
+#include "client/ring_signal.h"
+#include "manager.h"
+
+#include "logger.h"
+
+namespace DRing {
+
+void
+registerDataXferHandlers(const std::map<std::string,
+                         std::shared_ptr<CallbackWrapperBase>>& handlers)
+{
+    auto& handlers_ = ring::getSignalHandlers();
+    for (auto& item : handlers) {
+        auto iter = handlers_.find(item.first);
+        if (iter == handlers_.end()) {
+            RING_ERR("Signal %s not supported", item.first.c_str());
+            continue;
+        }
+
+        iter->second = std::move(item.second);
+    }
+}
+
+std::string
+sendFile(const std::string& accountID, const std::string& peerUri, const std::string& filename)
+{
+    return ring::Manager::instance().sendFile(accountID, peerUri, filename);
+}
+
+} // namespace DRing
