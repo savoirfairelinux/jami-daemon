319d49b08155ad377e7c766ecb8314882d69dc3a
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2016-02-23 13:18:41 -0500

    draft: file transfer implementation
    Change-Id: Icdd2ffbabe460aa4ad641412b02b4040241b20c8

--- src/manager.cpp
+++ src/manager.cpp
@@ -1393,7 +1393,8 @@ Manager::addTask(const std::function<bool()>&& task)
 }
 
 // Must be invoked periodically by a timer from the main event loop
-void Manager::pollEvents()
+void
+Manager::pollEvents()
 {
     //-- Handlers
     {
@@ -2830,5 +2831,16 @@ Manager::getSinkClient(const std::string& id)
     return nullptr;
 }
 #endif // RING_VIDEO
+
+DRing::DataTransferId
+Manager::sendFile(const std::string& accountId,
+                  const std::string& peerUri,
+                  const std::string& pathname,
+                  const std::string& name)
+{
+    if (auto acc = getAccount(accountId))
+        return acc->sendFile(peerUri, pathname, name);
+    return {};
+}
 
 } // namespace ring
