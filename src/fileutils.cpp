5842ec9fd33a35272b4bee6deb1a4053288a18ef
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-03-03 00:50:42 -0500

    save dh params
    Change-Id: Iaea9cf24d922fc6cfc542f8fa7b0c208ebc141d2

--- src/fileutils.cpp
+++ src/fileutils.cpp
@@ -204,6 +204,34 @@ bool isDirectoryWritable(const std::string &directory)
     return access(directory.c_str(), W_OK) == 0;
 }
 
+std::chrono::system_clock::time_point
+writeTime(const std::string& path)
+{
+#ifndef WIN32
+    struct stat s;
+    auto ret = stat(path.c_str(), &s);
+    if (ret)
+        throw std::runtime_error("Can't check write time for: " + path);
+    return std::chrono::system_clock::from_time_t(s.st_mtime);
+#else
+    FILETIME lastWriteTime;
+    if (!GetFileTime(h, nullptr, nullptr, &lastWriteTime))
+        throw std::runtime_error("Can't check write time for: " + path);
+    SYSTEMTIME sTime;
+    if (!FileTimeToSystemTime(&lastWriteTime, &sTime))
+        throw std::runtime_error("Can't check write time for: " + path);
+    struct tm tm {};
+    tm.tm_year = sTime->wYear - 1900;
+    tm.tm_mon = sTime->wMonth - 1;
+    tm.tm_mday = sTime->wDay;
+    tm.tm_hour = sTime->wHour;
+    tm.tm_min = sTime->wMinute;
+    tm.tm_sec = sTime->wSecond;
+    tm.tm_isdst = -1;
+    return std::chrono::system_clock::from_time_t(_mkgmtime(&tm));
+#endif
+}
+
 std::vector<uint8_t>
 loadFile(const std::string& path)
 {
