f341eb4518fd024a7c91b3f461586651e5b295e8
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-03-27 13:58:39 -0400

    message engine
    Tuleap: #359
    Change-Id: I823ac0b9b84558d3a88d85561b506d20bd5fc8d1

---
+++ src/im/message_engine.cpp
@@ -0,0 +1,157 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "message_engine.h"
+#include "sip/sipaccountbase.h"
+#include "manager.h"
+
+#include "client/ring_signal.h"
+#include "dring/account_const.h"
+
+#include <fstream>
+
+namespace ring {
+namespace im {
+
+static std::uniform_int_distribution<uint64_t> udist {1};
+
+MessageEngine::MessageEngine(SIPAccountBase& acc, const std::string& path) : account_(acc), savePath_(path)
+{
+    load();
+}
+
+MessageToken
+MessageEngine::sendMessage(const std::string& to, const std::map<std::string, std::string>& payloads)
+{
+    if (payloads.empty() or to.empty())
+        return 0;
+    auto token = udist(account_.rand_);
+    std::lock_guard<std::mutex> lock(messagesMutex_);
+    auto m = messages_.emplace(token, Message{});
+    m.first->second.to = to;
+    m.first->second.payloads = payloads;
+    runOnMainThread([this,token]() {
+        std::lock_guard<std::mutex> lock(messagesMutex_);
+        auto m = messages_.find(token);
+        if (m != messages_.end())
+            trySend(m);
+    });
+    save();
+    return token;
+}
+
+MessageStatus
+MessageEngine::getStatus(MessageToken t) const
+{
+    std::lock_guard<std::mutex> lock(messagesMutex_);
+    const auto m = messages_.find(t);
+    return (m == messages_.end()) ? MessageStatus::UNKNOWN : m->second.status;
+}
+
+void
+MessageEngine::trySend(decltype(MessageEngine::messages_)::iterator m)
+{
+    if (m->second.status != MessageStatus::IDLE &&
+        m->second.status != MessageStatus::UNKNOWN) {
+        RING_WARN("Can't send message in status %d", m->second.status);
+        return;
+    }
+    m->second.status = MessageStatus::SENDING;
+    m->second.retried++;
+    m->second.last_op = std::chrono::system_clock::now();
+    emitSignal<DRing::ConfigurationSignal::AccountMessageStatus>(m->first, DRing::Account::MessageStates::SENDING);
+    account_.sendTextMessage(m->second.to, m->second.payloads, m->first);
+}
+
+void
+MessageEngine::onMessageSent(MessageToken token, bool ok)
+{
+    RING_WARN("sendTextMessage callback %d", ok);
+    std::lock_guard<std::mutex> lock(messagesMutex_);
+    auto f = messages_.find(token);
+    if (f != messages_.end()) {
+        if (f->second.status == MessageStatus::SENDING) {
+            if (ok) {
+                f->second.status = MessageStatus::SENT;
+                emitSignal<DRing::ConfigurationSignal::AccountMessageStatus>(token, DRing::Account::MessageStates::SENT);
+            } else if (f->second.retried == MAX_RETRIES) {
+                f->second.status = MessageStatus::FAILURE;
+                emitSignal<DRing::ConfigurationSignal::AccountMessageStatus>(token, DRing::Account::MessageStates::FAILURE);
+            } else {
+                f->second.status = MessageStatus::IDLE;
+                // TODO: reschedule sending
+            }
+        }
+    }
+}
+
+void
+MessageEngine::load()
+{
+    try {
+        std::ifstream file;
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        file.open(savePath_);
+        std::lock_guard<std::mutex> lock(messagesMutex_);
+        std::string line;
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            iss.exceptions( std::ifstream::failbit | std::ifstream::badbit );
+            MessageToken token;
+            Message msg;
+            time_t t;
+            int status;
+            std::string txt;
+            if (!(iss >> std::hex >> token >> status >> msg.retried >> msg.to >> t >> txt)) { break; }
+            msg.status = (MessageStatus)status;
+            msg.last_op = std::chrono::system_clock::from_time_t(t);
+            msg.payloads["text/plain"] = std::move(txt);
+            messages_.emplace(token, std::move(msg));
+        }
+
+        // everything when fine, removing the file
+        std::remove(savePath_.c_str());
+    } catch (const std::exception& e) {
+        RING_ERR("Could not load messages from %s", savePath_.c_str());
+    }
+}
+
+void
+MessageEngine::save() const
+{
+    try {
+        std::ofstream file;
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        file.open(savePath_, std::ios::trunc);
+        std::lock_guard<std::mutex> lock(messagesMutex_);
+        for (auto& c : messages_) {
+            auto& v = c.second;
+            file << std::hex << c.first
+                 << ' ' << (int)v.status
+                 << ' ' << v.retried
+                 << ' ' << v.to
+                 << ' ' << std::chrono::system_clock::to_time_t(v.last_op)
+                 << ' ' << v.payloads.cbegin()->second << '\n';
+        }
+    } catch (const std::exception& e) {
+        RING_ERR("Could not save messages to %s", savePath_.c_str());
+    }
+}
+
+}}
