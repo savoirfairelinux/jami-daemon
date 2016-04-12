a81118d01476482db41405ac129a7a32d98ff5f8
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-04-12 15:49:15 -0400

    sip: fix pkt receive
    - Sending profile highligthed a flaw in the processing of packet
      * The receive packet function from pjsip can't handle a packet
        larger than PJSIP_MAX_PKT_LEN (4000) but we were not enforcing
        that correctly
      * The received callback was appending packet to the last one instead
        of appending it to the received packet vector
    
    Change-Id: I603cf19cb2951708edd5cca44d71b77bb83114d6
    Tuleap: #538

--- src/ringdht/sips_transport_ice.cpp
+++ src/ringdht/sips_transport_ice.cpp
@@ -319,8 +319,8 @@ SipsIceTransport::handleEvents()
         auto& pck = *it;
         pj_pool_reset(rdata_.tp_info.pool);
         pj_gettimeofday(&rdata_.pkt_info.timestamp);
-        rdata_.pkt_info.len = pck.size();
-        std::copy_n(pck.data(), pck.size(), rdata_.pkt_info.packet);
+        rdata_.pkt_info.len = (pck.size() <= PJSIP_MAX_PKT_LEN ? pck.size() : PJSIP_MAX_PKT_LEN);
+        std::copy_n(pck.data(), rdata_.pkt_info.len, rdata_.pkt_info.packet);
         auto eaten = pjsip_tpmgr_receive_packet(trData_.base.tpmgr, &rdata_);
 
         // Uncomplet parsing? (may be a partial sip packet received)
@@ -369,12 +369,7 @@ void
 SipsIceTransport::onRxData(std::vector<uint8_t>&& buf)
 {
     std::lock_guard<std::mutex> l(rxMtx_);
-    if (rxPending_.empty())
-        rxPending_.emplace_back(std::move(buf));
-    else {
-        auto& last = rxPending_.back();
-        last.insert(std::end(last), std::begin(buf), std::end(buf));
-    }
+    rxPending_.emplace_back(std::move(buf));
 }
 
 /* Update local & remote certificates info. This function should be
