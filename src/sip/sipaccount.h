41e304a425afc2df0c088713fb8cba7473cefe58
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-04-13 17:50:11 -0400

    message engine
    Tuleap: #359
    Change-Id: I823ac0b9b84558d3a88d85561b506d20bd5fc8d1

--- src/sip/sipaccount.h
+++ src/sip/sipaccount.h
@@ -490,7 +490,8 @@ class SIPAccount : public SIPAccountBase {
         void onRegister(pjsip_regc_cbparam *param);
 
         virtual void sendTextMessage(const std::string& to,
-                                     const std::map<std::string, std::string>& payloads) override;
+                                     const std::map<std::string, std::string>& payloads,
+                                     uint64_t id) override;
 
     private:
         void doRegister1_();
