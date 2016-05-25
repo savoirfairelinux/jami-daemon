040c7b7eb336d3a0d41a795c131f8555fc9909ee
Kevin Avignon <kevin.o.avignon@gmail.com>
2016-05-25 09:36:05 -0400

    Firstr stage into defining the connection session using TelepathyQT
    Change-Id: Iacc3cee20606c4e2db9c4f070a5116e5c0598339

---
+++ TelepathyCM/Connection.h
@@ -0,0 +1,18 @@
+/**
+ *  Ring Telepathy Connection Manager
+ *
+ * Copyright (C) 2016 Kevin Avignon, All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+ * SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
