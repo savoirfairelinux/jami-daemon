2ec6d0db404779b54690c502ff1f54b718bcd99c
Simon Zeni <simonzeni@gmail.com>
2016-05-30 16:05:15 -0400

    Rested implementation
    This commit wraps up :
    - The source of the restbed implementation (in bin/restcpp)
    - The integration of the target to the configure.ac
    - A little indentation fix
    
    How to make the restcpp daemon binary?
    1 - Apply the patch that add restbed to the contribs, available
    here https://gerrit-ring.savoirfairelinux.com/#/c/4417/
    
    2 - Make the contrib, as usual.
    mkdir -p contrib/native
    cd contrib/native
    ../bootstrap
    make
    make .restbed
    
    3 - Tell configure to use restbed instead of dbus
    ./autogen.sh
    ./configure --without-dbus --with-restbed
    
    4 - Make and execute.
    make
    ./bin/dring
    
    Change-Id: Id49a1a04b05aac1e803981833abe5564785fb801

---
+++ bin/restcpp/restclient.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <memory>
+#include <chrono>
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <thread>
+
+#include <functional>
+#include <iterator>
+#include <restbed>
+
+#include "dring/dring.h"
+#include "dring/callmanager_interface.h"
+#include "dring/configurationmanager_interface.h"
+#include "dring/presencemanager_interface.h"
+#ifdef RING_VIDEO
+#include "dring/videomanager_interface.h"
+#endif
+#include "logger.h"
+#include "restconfigurationmanager.h"
+#include "restvideomanager.h"
+
+class RestClient {
+    public:
+        RestClient(int port, int flags, bool persistent);
+        ~RestClient();
+
+        int event_loop() noexcept;
+        int exit() noexcept;
+
+    private:
+        int initLib(int flags);
+        void endLib() noexcept;
+        void initResources();
+
+        bool pollNoMore_ = false;
+
+        std::unique_ptr<RestConfigurationManager> configurationManager_;
+        std::unique_ptr<RestVideoManager> videoManager_;
+
+        // Restbed attributes
+        restbed::Service service_;
+        std::shared_ptr<restbed::Settings> settings_;
+        std::thread restbed;
+};
