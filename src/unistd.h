0142008c7c2548e427cce17cc8f52ce0aac98882
atraczyk <andreastraczyk@gmail.com>
2016-06-06 16:29:34 -0400

    add windows unistd.h
    Change-Id: Ic8d03f2c661f71e021055b5f629cb6c753c64e80

---
+++ src/unistd.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef __STRICT_ANSI__
+
+#include <stdlib.h>
+#include <process.h>
+#include <direct.h>
+#include <fcntl.h>
+
+#define R_OK    4
+#define W_OK    2
+#define F_OK    0
+
+#ifndef STDIN_FILENO
+#define STDIN_FILENO 0
+#endif
+
+#ifndef STDOUT_FILENO
+#define STDOUT_FILENO 1
+#endif
+
+#ifndef STDERR_FILENO
+#define STDERR_FILENO 2
+#endif
+
+#define srandom srand
+#define random rand
+
+#define inline __inline
+typedef int mode_t;
+#include <BaseTsd.h>
+
+#endif
