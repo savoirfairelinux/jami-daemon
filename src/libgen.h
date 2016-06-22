2309d2f21e1e76588d30d856acbb194851e3369f
atraczyk <andreastraczyk@gmail.com>
2016-06-22 16:11:25 -0400

    daemon: add libgen for for Microsoft compiler support
    Add libgen replacement to provide basename and dirname functions and
    facilitate native Windows building.
    
    Change-Id: Ia8b3b7ba89a04a7b815f7b7e9ffe0b3ce56fa5f7
    Tuleap: #797

---
+++ src/libgen.h
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (C) 2015-2016 Savoir-faire Linux Inc.
+ *  Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#ifndef _LIBGEN_H
+#define _LIBGEN_H
+
+#include <string>
+
+char *basename(char *path);
+char *dirname(char *path);
+
+#endif /* _LIBGEN_H */
