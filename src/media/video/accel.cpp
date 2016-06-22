fd6f44cd6cd481c085f945d75982affeca5d6689
Philippe Gorley <philippe.gorley@savoirfairelinux.com>
2016-06-22 17:16:29 -0400

    video: hardware acceleration
    Contains code common to all hardware accelerations to be implemented.
    
    Enables the VAAPI acceleration for intel linux systems, with support
    for H.264, H.263 and MPEG4.
    
    To use VAAPI, you need libva, libva-x11 and libva-drm.
    
    Hardware acceleration is enabled by default, and can be disabled with
    "./configure --disable-hwaccel".
    
    Change-Id: Id0696465b785de0735bbce9750932ac38efe0713

---
+++ src/media/video/accel.cpp
@@ -0,0 +1,187 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "libav_deps.h" // MUST BE INCLUDED FIRST
+#include "media_buffer.h"
+
+#include "accel.h"
+
+#if defined(HAVE_VAAPI_ACCEL_X11) || defined(HAVE_VAAPI_ACCEL_DRM)
+#include "v4l2/vaapi.h"
+#endif
+
+#include "string_utils.h"
+#include "logger.h"
+
+#include <initializer_list>
+#include <algorithm>
+
+namespace ring { namespace video {
+
+static constexpr unsigned MAX_ACCEL_FAILURES {5};
+
+static AVPixelFormat
+getFormatCb(AVCodecContext* codecCtx, const AVPixelFormat* formats)
+{
+    auto accel = static_cast<HardwareAccel*>(codecCtx->opaque);
+    if (!accel) {
+        // invalid state, try to recover
+        return avcodec_default_get_format(codecCtx, formats);
+    }
+
+    RING_DBG("Acceleration supports these pixel formats:");
+    for (int i = 0; formats[i] != AV_PIX_FMT_NONE; i++) {
+        RING_DBG("    %s", av_get_pix_fmt_name(formats[i]));
+
+        if (formats[i] == accel->format()) {
+            accel->setWidth(codecCtx->coded_width);
+            accel->setHeight(codecCtx->coded_height);
+            accel->setProfile(codecCtx->profile);
+            if (accel->init(codecCtx))
+                return accel->format();
+            break;
+        }
+    }
+
+    accel->fail(true);
+    RING_WARN("Falling back to software decoding");
+    codecCtx->get_format = avcodec_default_get_format;
+    codecCtx->get_buffer2 = avcodec_default_get_buffer2;
+    for (int i = 0; formats[i] != AV_PIX_FMT_NONE; i++) {
+        auto desc = av_pix_fmt_desc_get(formats[i]);
+        if (desc && !(desc->flags & AV_PIX_FMT_FLAG_HWACCEL)) {
+            return formats[i];
+        }
+    }
+
+    return AV_PIX_FMT_NONE;
+}
+
+static int
+allocateBufferCb(AVCodecContext* codecCtx, AVFrame* frame, int flags)
+{
+    if (auto accel = static_cast<HardwareAccel*>(codecCtx->opaque)) {
+        if (!accel->hasFailed() && accel->allocateBuffer(codecCtx, frame, flags) == 0) {
+            accel->reset();
+            return 0;
+        }
+
+        accel->fail();
+    }
+
+    return avcodec_default_get_buffer2(codecCtx, frame, flags);
+}
+
+template <class T>
+static std::unique_ptr<HardwareAccel>
+makeHardwareAccel(const AccelInfo& info) {
+    return std::unique_ptr<HardwareAccel>(new T(info));
+}
+
+static const AccelInfo*
+getAccel(std::initializer_list<AccelID> codecAccels)
+{
+    static const AccelInfo accels[] = {
+#if defined(HAVE_VAAPI_ACCEL_X11) || defined(HAVE_VAAPI_ACCEL_DRM)
+        { AccelID::Vaapi, AV_PIX_FMT_VAAPI, "vaapi", makeHardwareAccel<VaapiAccel> },
+#endif
+    };
+
+    for (auto& accel : accels) {
+        for (auto& ca : codecAccels) {
+            if (accel.type == ca) {
+                RING_DBG("Found '%s' hardware acceleration", accel.name.c_str());
+                return &accel;
+            }
+        }
+    }
+
+    RING_DBG("Did not find a matching hardware acceleration");
+    return nullptr;
+}
+
+HardwareAccel::HardwareAccel(const AccelInfo& info)
+    : type_(info.type)
+    , format_(info.format)
+    , name_(info.name)
+{
+    failCount_ = 0;
+    fallback_ = false;
+    width_ = -1;
+    height_ = -1;
+    profile_ = -1;
+}
+
+void
+HardwareAccel::fail(bool forceFallback)
+{
+    ++failCount_;
+    if (failCount_ >= MAX_ACCEL_FAILURES || forceFallback) {
+        fallback_ = true;
+        failCount_ = 0;
+        // force reinit of media decoder to correctly set thread count
+    }
+}
+
+std::unique_ptr<HardwareAccel>
+makeHardwareAccel(AVCodecContext* codecCtx)
+{
+    const AccelInfo* info = nullptr;
+
+    switch (codecCtx->codec_id) {
+        case AV_CODEC_ID_H264:
+            info = getAccel({
+                AccelID::Vdpau,
+                AccelID::VideoToolbox,
+                AccelID::Dxva2,
+                AccelID::Vaapi,
+                AccelID::Vda
+            });
+            break;
+        case AV_CODEC_ID_MPEG4:
+        case AV_CODEC_ID_H263:
+        case AV_CODEC_ID_H263P:
+            info = getAccel({
+                AccelID::Vdpau,
+                AccelID::VideoToolbox,
+                AccelID::Vaapi
+            });
+            break;
+        default:
+            break;
+    }
+
+    if (info && info->type != AccelID::NoAccel) {
+        if (auto accel = info->create(*info)) {
+            codecCtx->get_format = getFormatCb;
+            codecCtx->get_buffer2 = allocateBufferCb;
+            codecCtx->thread_safe_callbacks = 1;
+            codecCtx->thread_count = 1;
+            RING_DBG("Hardware acceleration setup has succeeded");
+            return accel;
+        } else
+            RING_ERR("Failed to create %s hardware acceleration", info->name.c_str());
+    }
+
+    RING_WARN("Not using hardware acceleration");
+    return nullptr;
+}
+
+}} // namespace ring::video
