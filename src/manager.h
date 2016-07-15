70049ca78d6f8bdcf94769d9424b3dea99da27b5
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-07-15 14:48:30 -0400

    change API to avoid freeze during test of turn credential
    Change-Id: I47e8cbf90d76a7a591a4473fd801d25a84fce144
    Tuleap: #620

--- src/manager.h
+++ src/manager.h
@@ -495,7 +495,7 @@ class Manager {
 
         void setAccountActive(const std::string& accountID, bool active);
 
-        std::map<std::string, std::string> testAccountICEInitialization(const std::string& accountID);
+        void testAccountICEInitialization(const std::string& accountID);
 
         /**
          * Return a new random accountid that is not present in the list
