217583eba39b6494df1c3cdb39d402764dcacb05
Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
2016-07-15 14:48:30 -0400

    change API to avoid freeze during test of turn credential
    Change-Id: I47e8cbf90d76a7a591a4473fd801d25a84fce144
    Tuleap: #620

--- src/client/ring_signal.cpp
+++ src/client/ring_signal.cpp
@@ -74,6 +74,9 @@ getSignalHandlers()
         exported_callback<DRing::ConfigurationSignal::GetAppDataPath>(),
 #endif
 
+        /* Account */
+        exported_callback<DRing::AccountSignal::TestAccountICEInitializationResult>(),
+
         /* Presence */
         exported_callback<DRing::PresenceSignal::NewServerSubscriptionRequest>(),
         exported_callback<DRing::PresenceSignal::ServerError>(),
