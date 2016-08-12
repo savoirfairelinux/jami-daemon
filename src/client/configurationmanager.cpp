7af72f41bf2fa49b9a5ab2f4cfc47636aba08f4c
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-08-12 14:23:43 -0400

    multi-device: add getKnownRingDevices
    * adds mechanism to find other devices with a Ring account
    * adds API to list those devices
    
    Change-Id: Ic6fc9cd454f69ec81190c6e38a84f2899a66148e

--- src/client/configurationmanager.cpp
+++ src/client/configurationmanager.cpp
@@ -283,6 +283,14 @@ addRingDevice(const std::string& accountID, const std::string& password)
     return ring::Manager::instance().addRingDevice(accountID, password);
 }
 
+std::vector<std::string>
+getKnownRingDevices(const std::string& accountId)
+{
+    if (auto acc = ring::Manager::instance().getAccount<ring::RingAccount>(accountId))
+        return acc->getKnownDevices();
+    return {};
+}
+
 /* contact requests */
 std::map<std::string, std::string>
 getTrustRequests(const std::string& accountId)
