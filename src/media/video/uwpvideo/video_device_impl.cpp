6a763863b1c34385dd3e9727ac7f42658a3fda08
atraczyk <andreastraczyk@gmail.com>
2016-09-27 17:17:26 -0400

    video: starting video implementation
    Change-Id: Ia65723ed26cd10a36228010886163e008e38b52d

--- src/media/video/uwpvideo/video_device_impl.cpp
+++ src/media/video/uwpvideo/video_device_impl.cpp
@@ -1,7 +1,6 @@
 /*
  *  Copyright (C) 2015-2016 Savoir-faire Linux Inc.
  *
- *  Author: Edric Milaret <edric.ladent-milaret@savoirfairelinux.com>
  *  Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -29,97 +28,158 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <array>
 
 #include "logger.h"
 #include "../video_device.h"
+
+#include "ring_signal.h"
 
 #include <ciso646>
 
 namespace ring { namespace video {
 
+typedef struct {
+    std::string             name;
+    enum VideoPixelFormat   ring_format;
+} uwp_fmt;
+
+static const std::array<uwp_fmt, 2> uwp_formats {
+    uwp_fmt { "NV12",       VIDEO_PIXFMT_NV21    },
+    uwp_fmt { "YUY2",       VIDEO_PIXFMT_YUYV422 },
+};
+
 class VideoDeviceImpl {
     public:
-        /**
-        * @throw std::runtime_error
-        */
         VideoDeviceImpl(const std::string& path);
-        std::string device;
-        std::string name;
-        unsigned int id;
 
-        std::vector<std::string> getChannelList() const;
-        std::vector<VideoSize> getSizeList(const std::string& channel) const;
-        std::vector<VideoSize> getSizeList() const;
-        std::vector<FrameRate> getRateList(const std::string& channel, VideoSize size) const;
+        std::string name;
 
         DeviceParams getDeviceParams() const;
         void setDeviceParams(const DeviceParams&);
 
+        std::vector<VideoSize> getSizeList() const;
+        std::vector<FrameRate> getRateList() const;
+
     private:
 
-        void setup();
-        std::vector<VideoSize> sizeList_;
-        std::map<VideoSize, std::vector<FrameRate> > rateList_;
+        void selectFormat();
+        VideoSize getSize(VideoSize size) const;
+        FrameRate getRate(FrameRate rate) const;
 
-        void fail(const std::string& error);
+        std::vector<std::string> formats_ {};
+        std::vector<VideoSize> sizes_ {};
+        std::vector<FrameRate> rates_ {};
+
+        const uwp_fmt* fmt_ {nullptr};
+        VideoSize size_ {};
+        FrameRate rate_ {};
+
 };
 
-VideoDeviceImpl::VideoDeviceImpl(const std::string& id)
-    : id(atoi(id.c_str()))
+void
+VideoDeviceImpl::selectFormat()
 {
-    setup();
+    unsigned best = UINT_MAX;
+    for(auto fmt : formats_) {
+        auto f = uwp_formats.begin();
+        for (; f != uwp_formats.end(); ++f) {
+            if (f->name == fmt) {
+                auto pos = std::distance(uwp_formats.begin(), f);
+                if (pos < best)
+                    best = pos;
+                break;
+            }
+        }
+        if (f == uwp_formats.end())
+            RING_WARN("UWPVideo: No format matching %s", fmt.c_str());
+    }
+
+    if (best != UINT_MAX) {
+        fmt_ = &uwp_formats[best];
+        RING_DBG("UWPVideo: picked format %s", fmt_->name.c_str());
+    }
+    else {
+        fmt_ = &uwp_formats[0];
+        RING_ERR("UWPVideo: Could not find a known format to use");
+    }
 }
 
-void
-VideoDeviceImpl::setup()
+VideoDeviceImpl::VideoDeviceImpl(const std::string& path) : name(path)
 {
-    RING_DBG("VideoDeviceImpl::setup");
+    std::vector<unsigned> sizes;
+    std::vector<unsigned> rates;
+    formats_.reserve(16);
+    sizes.reserve(32);
+    rates.reserve(16);
+    emitSignal<DRing::VideoSignal::GetCameraInfo>(name, &formats_, &sizes, &rates);
+    for (size_t i=0, n=sizes.size(); i<n; i+=2)
+        sizes_.emplace_back(sizes[i], sizes[i+1]);
+    for (const auto& r : rates)
+        rates_.emplace_back(r, 1);
+
+    selectFormat();
 }
 
-void
-VideoDeviceImpl::fail(const std::string& error)
+VideoSize
+VideoDeviceImpl::getSize(VideoSize size) const
 {
-    throw std::runtime_error(error);
+    for (const auto &iter : sizes_) {
+        if (iter == size)
+            return iter;
+    }
+
+    return sizes_.empty() ? VideoSize{0, 0} : sizes_.back();
+}
+
+FrameRate
+VideoDeviceImpl::getRate(FrameRate rate) const
+{
+    for (const auto &iter : rates_) {
+        if (iter == rate)
+            return iter;
+    }
+
+    return rates_.empty() ? FrameRate{0, 0} : rates_.back();
+}
+
+std::vector<VideoSize>
+VideoDeviceImpl::getSizeList() const
+{
+    return sizes_;
+}
+
+std::vector<FrameRate>
+VideoDeviceImpl::getRateList() const
+{
+    return rates_;
 }
 
 DeviceParams
 VideoDeviceImpl::getDeviceParams() const
 {
     DeviceParams params;
+    std::stringstream ss1, ss2;
+
+    ss1 << fmt_->ring_format;
+    ss1 >> params.format;
+
+    params.name = name;
+    params.input = name;
+    params.channel =  0;
+    params.width = size_.first;
+    params.height = size_.second;
+    params.framerate = rate_;
+
     return params;
 }
 
 void
 VideoDeviceImpl::setDeviceParams(const DeviceParams& params)
 {
-    if (params.width and params.height) {
-    }
-}
-
-std::vector<VideoSize>
-VideoDeviceImpl::getSizeList() const
-{
-    return sizeList_;
-}
-
-std::vector<FrameRate>
-VideoDeviceImpl::getRateList(const std::string& channel, VideoSize size) const
-{
-    (void) channel;
-    return rateList_.at(size);
-}
-
-std::vector<VideoSize>
-VideoDeviceImpl::getSizeList(const std::string& channel) const
-{
-    (void) channel;
-    return sizeList_;
-}
-
-std::vector<std::string>
-VideoDeviceImpl::getChannelList() const
-{
-    return {"default"};
+    size_ = getSize({params.width, params.height});
+    rate_ = getRate(params.framerate);
+    emitSignal<DRing::VideoSignal::SetParameters>(name, fmt_->name, size_.first, size_.second, rate_.real());
 }
 
 VideoDevice::VideoDevice(const std::string& path)
@@ -144,19 +204,19 @@ VideoDevice::setDeviceParams(const DeviceParams& params)
 std::vector<std::string>
 VideoDevice::getChannelList() const
 {
-    return deviceImpl_->getChannelList();
+    return {"default"};
 }
 
 std::vector<VideoSize>
 VideoDevice::getSizeList(const std::string& channel) const
 {
-    return deviceImpl_->getSizeList(channel);
+    return deviceImpl_->getSizeList();
 }
 
 std::vector<FrameRate>
 VideoDevice::getRateList(const std::string& channel, VideoSize size) const
 {
-    return deviceImpl_->getRateList(channel, size);
+    return deviceImpl_->getRateList();
 }
 
 VideoDevice::~VideoDevice()
