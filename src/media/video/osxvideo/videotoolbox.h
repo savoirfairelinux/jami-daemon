adab28988fe8fb554af59fc4236e9288c88b7001
Philippe Gorley <gorley.philippe@gmail.com>
2016-10-03 14:43:22 -0400

    video: mac hardware acceleration
    Adds support for VideoToolbox H.264 on Mac 10.8+ and iOS 8+
    
    Change-Id: I588fcbb92809a9d6a56bb9b6a7ac3a59874c0186

---
+++ src/media/video/osxvideo/videotoolbox.h
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (C) 2016 Savoir-faire Linux Inc.
+ *
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "libav_deps.h" // MUST BE INCLUDED FIRST
+
+#include "config.h"
+
+#if defined(RING_VIDEO) && defined(RING_ACCEL)
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavcodec/videotoolbox.h>
+#include <libavutil/imgutils.h>
+}
+
+#include "video/accel.h"
+
+#include <memory>
+#include <functional>
+
+namespace ring { namespace video {
+
+class VideoToolboxAccel : public HardwareAccel {
+    public:
+        VideoToolboxAccel(AccelInfo info);
+        ~VideoToolboxAccel();
+
+        bool init(AVCodecContext* codecCtx) override;
+        int allocateBuffer(AVCodecContext* codecCtx, AVFrame* frame, int flags) override;
+        bool extractData(AVCodecContext* codecCtx, VideoFrame& container) override;
+
+    private:
+        AVCodecContext* codecCtx_;
+};
+
+}} // namespace ring::video
+
+#endif // defined(RING_VIDEO) && defined(RING_ACCEL)
