6c218eb4ffdfc2dc0d28f40b749528275e62d810
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-10-19 18:14:29 -0400

    nameservice: resolve outgoing names
    Tuleap: #938
    Change-Id: I4685a91dd7e345349855abdfbc041105bd6ae2a2

--- src/ringdht/ringaccount.h
+++ src/ringdht/ringaccount.h
@@ -376,6 +376,8 @@ class RingAccount : public SIPAccountBase {
          */
         virtual void setAccountDetails(const std::map<std::string, std::string> &details) override;
 
+        void startOutgoingCall(std::shared_ptr<SIPCall>& call, const std::string toUri);
+
         /**
          * Start a SIP Call
          * @param call  The current call
