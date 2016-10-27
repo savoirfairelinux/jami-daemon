df9951386ced6a7ffc669d36dddfa7a0e07463e3
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2016-10-27 10:40:15 -0400

    ice: new ice message format
    WARNING: this patch breaks the compatibility of ICE message format.
    
    This patch introduces a new ICE message format to serialize
    ICE transport information.
    This format permits futur evolutions of this functionality by:
     - adding a version tag to indicate the format used
     - use msgpack to have a compact and easy way to handle data
    
    Change-Id: I1df6197e487ec119cf55a34fc531d2514f6fcf16

--- src/ringdht/ringaccount.cpp
+++ src/ringdht/ringaccount.cpp
@@ -277,7 +277,7 @@ RingAccount::newOutgoingCall(const std::string& toUrl)
                     const dht::Value::Id callvid  = udist(sthis->rand_);
                     const dht::Value::Id vid  = udist(sthis->rand_);
                     const auto callkey = dht::InfoHash::get("callto:" + dev.dev.toString());
-                    dht::Value val { dht::IceCandidates(callvid, ice->getLocalAttributesAndCandidates()) };
+                    dht::Value val { dht::IceCandidates(callvid, ice->packIceMsg()) };
                     val.id = vid;
 
                     sthis->dht_.putEncrypted(
@@ -1732,7 +1732,7 @@ RingAccount::replyToIncomingIceMsg(std::shared_ptr<SIPCall> call,
                                   std::shared_ptr<dht::crypto::Certificate> peer_cert)
 {
     const auto vid = udist(rand_);
-    dht::Value val { dht::IceCandidates(peer_ice_msg.id, ice->getLocalAttributesAndCandidates()) };
+    dht::Value val { dht::IceCandidates(peer_ice_msg.id, ice->packIceMsg()) };
     val.id = vid;
 
     // Asynchronous DHT put of our local ICE data
