b1b0a939480b3d6373ed5573ed161931772251eb
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-11-04 13:48:00 -0400

    namedirectory: catch exceptions, report name error
    Tuleap: #1176
    Change-Id: Id971e66435335fe66382fa6716f41b80a0b96975

--- src/ringdht/namedirectory.cpp
+++ src/ringdht/namedirectory.cpp
@@ -52,7 +52,6 @@ void toLower(std::string& string)
 void
 NameDirectory::lookupUri(const std::string& uri, const std::string& default_server, LookupCallback cb)
 {
-    RING_WARN("lookupUri: %s", uri.c_str());
     std::smatch pieces_match;
     if (std::regex_match(uri, pieces_match, URI_VALIDATOR)) {
         if (pieces_match.size() == 4) {
@@ -64,7 +63,7 @@ NameDirectory::lookupUri(const std::string& uri, const std::string& default_serv
         }
     }
     RING_ERR("Can't parse URI: %s", uri.c_str());
-    cb("", Response::error);
+    cb("", Response::invalidName);
 }
 
 NameDirectory::NameDirectory(const std::string& s)
@@ -83,7 +82,6 @@ NameDirectory& NameDirectory::instance(const std::string& server)
     const std::string& s = server.empty() ? DEFAULT_SERVER_HOST : server;
     static std::map<std::string, NameDirectory> instances {};
     auto r = instances.emplace(s, NameDirectory{s});
-    RING_WARN("NameDirectory: %s %p", s.c_str(), &r.first->second);
     if (r.second)
         r.first->second.load();
     return r.first->second;
@@ -101,124 +99,134 @@ size_t getContentLength(restbed::Response& reply)
 
 void NameDirectory::lookupAddress(const std::string& addr, LookupCallback cb)
 {
-    auto cacheRes = nameCache_.find(addr);
-    if (cacheRes != nameCache_.end()) {
-        cb(cacheRes->second, Response::found);
-        return;
-    }
-
-    restbed::Uri uri(HTTP_PROTO + serverHost_ + QUERY_ADDR + addr);
-    auto req = std::make_shared<restbed::Request>(uri);
-    req->set_header("Accept", "*/*");
-    req->set_header("Host", serverHost_);
-
-    RING_DBG("Address lookup for %s: %s", addr.c_str(), uri.to_string().c_str());
-
-    auto ret = restbed::Http::async(req, [this,cb,addr](const std::shared_ptr<restbed::Request>,
-                                             const std::shared_ptr<restbed::Response> reply) {
-        if (reply->get_status_code() == 200) {
-            size_t length = getContentLength(*reply);
-            if (length > MAX_RESPONSE_SIZE) {
-                cb("", Response::error);
-                return;
-            }
-            restbed::Http::fetch(length, reply);
-            std::string body;
-            reply->get_body(body);
-
-            Json::Value json;
-            Json::Reader reader;
-            if (!reader.parse(body, json)) {
-                RING_ERR("Address lookup for %s: can't parse server response: %s", addr.c_str(), body.c_str());
-                cb("", Response::error);
-                return;
-            }
-            auto name = json["name"].asString();
-            if (not name.empty()) {
-                RING_DBG("Found name for %s: %s", addr.c_str(), name.c_str());
-                addrCache_.emplace(name, addr);
-                nameCache_.emplace(addr, name);
-                cb(name, Response::found);
-                saveCache();
-            } else {
-                cb("", Response::notFound);
-            }
-        } else {
-            cb("", Response::error);
+    try {
+        auto cacheRes = nameCache_.find(addr);
+        if (cacheRes != nameCache_.end()) {
+            cb(cacheRes->second, Response::found);
+            return;
         }
-    }).share();
 
-    // avoid blocking on future destruction
-    ThreadPool::instance().run([ret](){ ret.get(); });
+        restbed::Uri uri(HTTP_PROTO + serverHost_ + QUERY_ADDR + addr);
+        auto req = std::make_shared<restbed::Request>(uri);
+        req->set_header("Accept", "*/*");
+        req->set_header("Host", serverHost_);
+
+        RING_DBG("Address lookup for %s: %s", addr.c_str(), uri.to_string().c_str());
+
+        auto ret = restbed::Http::async(req, [this,cb,addr](const std::shared_ptr<restbed::Request>,
+                                                 const std::shared_ptr<restbed::Response> reply) {
+            if (reply->get_status_code() == 200) {
+                size_t length = getContentLength(*reply);
+                if (length > MAX_RESPONSE_SIZE) {
+                    cb("", Response::error);
+                    return;
+                }
+                restbed::Http::fetch(length, reply);
+                std::string body;
+                reply->get_body(body);
+
+                Json::Value json;
+                Json::Reader reader;
+                if (!reader.parse(body, json)) {
+                    RING_ERR("Address lookup for %s: can't parse server response: %s", addr.c_str(), body.c_str());
+                    cb("", Response::error);
+                    return;
+                }
+                auto name = json["name"].asString();
+                if (not name.empty()) {
+                    RING_DBG("Found name for %s: %s", addr.c_str(), name.c_str());
+                    addrCache_.emplace(name, addr);
+                    nameCache_.emplace(addr, name);
+                    cb(name, Response::found);
+                    saveCache();
+                } else {
+                    cb("", Response::notFound);
+                }
+            } else {
+                cb("", Response::error);
+            }
+        }).share();
+
+        // avoid blocking on future destruction
+        ThreadPool::instance().run([ret](){ ret.get(); });
+    } catch (const std::exception& e) {
+        RING_ERR("Error when performing address lookup: %s", e.what());
+        cb("", Response::error);
+    }
 }
 
 static const std::string HEX_PREFIX {"0x"};
 
 void NameDirectory::lookupName(const std::string& n, LookupCallback cb)
 {
-    std::string name {n};
-    if (not validateName(name)) {
-        cb(name, Response::invalidName);
-        return;
-    }
-    toLower(name);
-
-    auto cacheRes = addrCache_.find(name);
-    if (cacheRes != addrCache_.end()) {
-        cb(cacheRes->second, Response::found);
-        return;
-    }
-
-    restbed::Uri uri(HTTP_PROTO + serverHost_ + QUERY_NAME + name);
-    auto request = std::make_shared<restbed::Request>(std::move(uri));
-    request->set_header("Accept", "*/*");
-    request->set_header("Host", serverHost_);
-
-    RING_DBG("Name lookup for %s: %s", name.c_str(), uri.to_string().c_str());
-
-    auto ret = restbed::Http::async(request, [this,cb,name](const std::shared_ptr<restbed::Request>,
-                                                 const std::shared_ptr<restbed::Response> reply) {
-        auto code = reply->get_status_code();
-        if (code != 200)
-            RING_DBG("Name lookup for %s: got reply code %d", name.c_str(), code);
-        if (code >= 200 && code < 300) {
-            size_t length = getContentLength(*reply);
-            if (length > MAX_RESPONSE_SIZE) {
-                cb("", Response::error);
-                return;
-            }
-            restbed::Http::fetch(length, reply);
-            std::string body;
-            reply->get_body(body);
-
-            Json::Value json;
-            Json::Reader reader;
-            if (!reader.parse(body, json)) {
-                RING_ERR("Name lookup for %s: can't parse server response: %s", name.c_str(), body.c_str());
-                cb("", Response::error);
-                return;
-            }
-            auto addr = json["addr"].asString();
-            if (!addr.compare(0, HEX_PREFIX.size(), HEX_PREFIX))
-                addr = addr.substr(HEX_PREFIX.size());
-            if (not addr.empty()) {
-                RING_DBG("Found address for %s: %s", name.c_str(), addr.c_str());
-                addrCache_.emplace(name, addr);
-                nameCache_.emplace(addr, name);
-                cb(addr, Response::found);
-                saveCache();
-            } else {
-                cb("", Response::notFound);
-            }
-        } else if (code >= 400 && code < 500) {
-            cb("", Response::notFound);
-        } else {
-            cb("", Response::error);
+    try {
+        std::string name {n};
+        if (not validateName(name)) {
+            cb(name, Response::invalidName);
+            return;
         }
-    }).share();
+        toLower(name);
 
-    // avoid blocking on future destruction
-    ThreadPool::instance().run([ret](){ ret.get(); });
+        auto cacheRes = addrCache_.find(name);
+        if (cacheRes != addrCache_.end()) {
+            cb(cacheRes->second, Response::found);
+            return;
+        }
+
+        restbed::Uri uri(HTTP_PROTO + serverHost_ + QUERY_NAME + name);
+        auto request = std::make_shared<restbed::Request>(std::move(uri));
+        request->set_header("Accept", "*/*");
+        request->set_header("Host", serverHost_);
+
+        RING_DBG("Name lookup for %s: %s", name.c_str(), uri.to_string().c_str());
+
+        auto ret = restbed::Http::async(request, [this,cb,name](const std::shared_ptr<restbed::Request>,
+                                                     const std::shared_ptr<restbed::Response> reply) {
+            auto code = reply->get_status_code();
+            if (code != 200)
+                RING_DBG("Name lookup for %s: got reply code %d", name.c_str(), code);
+            if (code >= 200 && code < 300) {
+                size_t length = getContentLength(*reply);
+                if (length > MAX_RESPONSE_SIZE) {
+                    cb("", Response::error);
+                    return;
+                }
+                restbed::Http::fetch(length, reply);
+                std::string body;
+                reply->get_body(body);
+
+                Json::Value json;
+                Json::Reader reader;
+                if (!reader.parse(body, json)) {
+                    RING_ERR("Name lookup for %s: can't parse server response: %s", name.c_str(), body.c_str());
+                    cb("", Response::error);
+                    return;
+                }
+                auto addr = json["addr"].asString();
+                if (!addr.compare(0, HEX_PREFIX.size(), HEX_PREFIX))
+                    addr = addr.substr(HEX_PREFIX.size());
+                if (not addr.empty()) {
+                    RING_DBG("Found address for %s: %s", name.c_str(), addr.c_str());
+                    addrCache_.emplace(name, addr);
+                    nameCache_.emplace(addr, name);
+                    cb(addr, Response::found);
+                    saveCache();
+                } else {
+                    cb("", Response::notFound);
+                }
+            } else if (code >= 400 && code < 500) {
+                cb("", Response::notFound);
+            } else {
+                cb("", Response::error);
+            }
+        }).share();
+
+        // avoid blocking on future destruction
+        ThreadPool::instance().run([ret](){ ret.get(); });
+    } catch (const std::exception& e) {
+        RING_ERR("Error when performing name lookup: %s", e.what());
+        cb("", Response::error);
+    }
 }
 
 bool NameDirectory::validateName(const std::string& name) const
@@ -228,78 +236,83 @@ bool NameDirectory::validateName(const std::string& name) const
 
 void NameDirectory::registerName(const std::string& addr, const std::string& n, const std::string& owner, RegistrationCallback cb)
 {
-    std::string name {n};
-    if (not validateName(name)) {
-        cb(RegistrationResponse::invalidName);
-        return;
-    }
-    toLower(name);
-
-    auto cacheRes = addrCache_.find(name);
-    if (cacheRes != addrCache_.end()) {
-        if (cacheRes->second == addr)
-            cb(RegistrationResponse::success);
-        else
-            cb(RegistrationResponse::alreadyTaken);
-        return;
-    }
-
-    auto request = std::make_shared<restbed::Request>(restbed::Uri(HTTP_PROTO + serverHost_ + QUERY_NAME + name));
-    request->set_header("Accept", "*/*");
-    request->set_header("Host", serverHost_);
-    request->set_header("Content-Type", "application/json");
-    request->set_method("POST");
-    std::string body;
-    {
-        std::stringstream ss;
-        ss << "{\"addr\":\"" << addr << "\",\"owner\":\"" << owner << "\"}";
-        body = ss.str();
-    }
-    request->set_body(body);
-    request->set_header("Content-Length", ring::to_string(body.size()));
-
-    auto params = std::make_shared<restbed::Settings>();
-    params->set_connection_timeout(std::chrono::seconds(120));
-
-    RING_WARN("registerName: sending request %s %s", addr.c_str(), name.c_str());
-    auto ret = restbed::Http::async(request,
-                         [this,cb,addr,name](const std::shared_ptr<restbed::Request>,
-                                             const std::shared_ptr<restbed::Response> reply)
-    {
-        auto code = reply->get_status_code();
-        RING_DBG("Got reply for registration of %s -> %s: code %d", name.c_str(), addr.c_str(), code);
-        if (code >= 200 && code < 300) {
-            size_t length = getContentLength(*reply);
-            if (length > MAX_RESPONSE_SIZE) {
-                cb(RegistrationResponse::error);
-                return;
-            }
-            restbed::Http::fetch(length, reply);
-            std::string body;
-            reply->get_body(body);
-
-            Json::Value json;
-            Json::Reader reader;
-            if (!reader.parse(body, json)) {
-                cb(RegistrationResponse::error);
-                return;
-            }
-            auto success = json["success"].asBool();
-            RING_DBG("Got reply for registration of %s -> %s: %s", name.c_str(), addr.c_str(), success ? "success" : "failure");
-            if (success) {
-                addrCache_.emplace(name, addr);
-                nameCache_.emplace(addr, name);
-            }
-            cb(success ? RegistrationResponse::success : RegistrationResponse::error);
-        } else if (code >= 400 && code < 500) {
-            cb(RegistrationResponse::alreadyTaken);
-        } else {
-            cb(RegistrationResponse::error);
+    try {
+        std::string name {n};
+        if (not validateName(name)) {
+            cb(RegistrationResponse::invalidName);
+            return;
         }
-    }, params).share();
+        toLower(name);
 
-    // avoid blocking on future destruction
-    ThreadPool::instance().run([ret](){ ret.get(); });
+        auto cacheRes = addrCache_.find(name);
+        if (cacheRes != addrCache_.end()) {
+            if (cacheRes->second == addr)
+                cb(RegistrationResponse::success);
+            else
+                cb(RegistrationResponse::alreadyTaken);
+            return;
+        }
+
+        auto request = std::make_shared<restbed::Request>(restbed::Uri(HTTP_PROTO + serverHost_ + QUERY_NAME + name));
+        request->set_header("Accept", "*/*");
+        request->set_header("Host", serverHost_);
+        request->set_header("Content-Type", "application/json");
+        request->set_method("POST");
+        std::string body;
+        {
+            std::stringstream ss;
+            ss << "{\"addr\":\"" << addr << "\",\"owner\":\"" << owner << "\"}";
+            body = ss.str();
+        }
+        request->set_body(body);
+        request->set_header("Content-Length", ring::to_string(body.size()));
+
+        auto params = std::make_shared<restbed::Settings>();
+        params->set_connection_timeout(std::chrono::seconds(120));
+
+        RING_WARN("registerName: sending request %s %s", addr.c_str(), name.c_str());
+        auto ret = restbed::Http::async(request,
+                             [this,cb,addr,name](const std::shared_ptr<restbed::Request>,
+                                                 const std::shared_ptr<restbed::Response> reply)
+        {
+            auto code = reply->get_status_code();
+            RING_DBG("Got reply for registration of %s -> %s: code %d", name.c_str(), addr.c_str(), code);
+            if (code >= 200 && code < 300) {
+                size_t length = getContentLength(*reply);
+                if (length > MAX_RESPONSE_SIZE) {
+                    cb(RegistrationResponse::error);
+                    return;
+                }
+                restbed::Http::fetch(length, reply);
+                std::string body;
+                reply->get_body(body);
+
+                Json::Value json;
+                Json::Reader reader;
+                if (!reader.parse(body, json)) {
+                    cb(RegistrationResponse::error);
+                    return;
+                }
+                auto success = json["success"].asBool();
+                RING_DBG("Got reply for registration of %s -> %s: %s", name.c_str(), addr.c_str(), success ? "success" : "failure");
+                if (success) {
+                    addrCache_.emplace(name, addr);
+                    nameCache_.emplace(addr, name);
+                }
+                cb(success ? RegistrationResponse::success : RegistrationResponse::error);
+            } else if (code >= 400 && code < 500) {
+                cb(RegistrationResponse::alreadyTaken);
+            } else {
+                cb(RegistrationResponse::error);
+            }
+        }, params).share();
+
+        // avoid blocking on future destruction
+        ThreadPool::instance().run([ret](){ ret.get(); });
+    } catch (const std::exception& e) {
+        RING_ERR("Error when performing name registration: %s", e.what());
+        cb(RegistrationResponse::error);
+    }
 }
 
 void
