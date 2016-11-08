43a8f5d06ac5919c2b03774d71370df48dde1553
philippegorley <philippe.gorley@savoirfairelinux.com>
2016-11-08 10:23:19 -0500

    video: add vdpau acceleration support
    Adds the VDPAU acceleration for GNU/Linux systems and
    enable it by devault. Can be disabled using ./configure --disable-vdpau
    
    Support for H.264, H.263 and MPEG4, as long as the hardware supports
    them.
    
    Requires libvdpau and ffmpeg 3.3 (contrib bump included by this patch)
    
    [guillaume roguez: re-word ci msg to be more explicit]
    
    Change-Id: I7479f4d7e3d51caf702be8c85284a4fca01605b3
    Reviewed-by: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>

---
+++ src/media/video/v4l2/vdpau.h
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "libav_deps.h" // MUST BE INCLUDED FIRST
+
+#include "config.h"
+
+#ifdef RING_VDPAU
+
+extern "C" {
+#include <stdint.h>
+
+#include <libavcodec/vdpau.h>
+#include <libavutil/buffer.h>
+#include <libavutil/frame.h>
+#include <libavutil/hwcontext.h>
+#include <libavutil/hwcontext_vdpau.h>
+}
+
+#include "video/accel.h"
+
+#include <memory>
+#include <functional>
+
+namespace ring { namespace video {
+
+class VdpauAccel : public HardwareAccel {
+    public:
+        VdpauAccel(const std::string name, const AVPixelFormat format);
+        ~VdpauAccel();
+
+        bool check() override;
+        bool init() override;
+        int allocateBuffer(AVFrame* frame, int flags) override;
+        void extractData(VideoFrame& input, VideoFrame& output) override;
+
+    private:
+        using AVBufferRefPtr = std::unique_ptr<AVBufferRef, std::function<void(AVBufferRef*)>>;
+        AVBufferRefPtr deviceBufferRef_;
+        AVBufferRefPtr framesBufferRef_;
+};
+
+}} // namespace ring::video
+
+#endif // RING_VDPAU
