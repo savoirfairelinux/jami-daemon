ee3578da4d5860d7ab06b8b80bb2dd6a2c8f764e
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2016-11-24 13:12:14 -0500

    sip instant message refactoring
    This patch is an re-implementation of instant messaging system
    for the SIP layers, to use endpoint's request than dialog's request.
    
    This fix an issue where a packet drop during the request processing
    causes the dialog to be shutdown with error code 408 afer 30 seconds.
    This leds to shutdown of the whole attached invite session, so the
    call itself it terminated.
    
    Change-Id: I948c3eb1d6df992ed8364a01806cd92a42cf98a5
    Tuleap: #1077
    Tuleap: #1297

--- src/im/instant_messaging.h
+++ src/im/instant_messaging.h
@@ -30,10 +30,9 @@
 
 #include "config.h"
 
-struct pjsip_inv_session;
-struct pjsip_rx_data;
 struct pjsip_msg;
 struct pjsip_tx_data;
+struct pjsip_dialog;
 
 namespace ring { namespace im {
 
@@ -58,10 +57,11 @@ struct InstantMessageException : std::runtime_error
  * @param session SIP session
  * @param payloads a map where the mime type and optional parameters are the key
  *                 and the message payload is the value
+ * @param dlg SIP dialog to attach the message
  *
  * Exception: throw InstantMessageException if no message sent
  */
-void sendSipMessage(pjsip_inv_session* session, const std::map<std::string, std::string>& payloads);
+void sendSipMessage(const std::map<std::string, std::string>& payloads, pjsip_dialog* dlg);
 
 /**
  * Parses given SIP message into a map where the key is the contents of the Content-Type header
