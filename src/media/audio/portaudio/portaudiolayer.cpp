ccafafd542e0379298ffcf710a63740f00934b2e
atraczyk <andreastraczyk@gmail.com>
2016-12-13 11:44:52 -0500

    audio: ignore results of ALSA's checkSounCard
    - Portaudio device indices are initialized to preferences from
      ALSA, which is not natively available on Windows. The default
      values for alsaCardin_, alsaCardOut_, and alsaCardring_ are 0,
      unless they are overwritten by checkSoundCard which will not be
      called on native Windows builds. This patch ignores any
      preferences stored in the daemon's config file, which would all
      be zeros, causing the default audio output to function most
      likely, but not the input.
    
    Change-Id: I7516278a56354f48640c9404c9f96df563838b1c
    Tuleap: #790

--- src/media/audio/portaudio/portaudiolayer.cpp
+++ src/media/audio/portaudio/portaudiolayer.cpp
@@ -348,8 +348,13 @@ PortAudioLayer::init()
         this->terminate();
     }
 
+#ifdef RING_UWP
+    indexRing_ = indexOut_ = Pa_GetDefaultOutputDevice();
+    indexIn_ = Pa_GetDefaultInputDevice();
+#else
     indexRing_ = indexOut_ = indexOut_ == paNoDevice ? Pa_GetDefaultOutputDevice() : indexOut_;
     indexIn_ = indexIn_ == paNoDevice ? Pa_GetDefaultInputDevice() : indexIn_;
+#endif
 
     if (indexOut_ != paNoDevice) {
         if (const auto outputDeviceInfo = Pa_GetDeviceInfo(indexOut_)) {
