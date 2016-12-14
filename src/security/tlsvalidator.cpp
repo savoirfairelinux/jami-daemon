c8740c4bce7f8090e7cb35632a32c915549435ca
atraczyk <andreastraczyk@gmail.com>
2016-12-14 18:59:34 -0500

    fix: replace WIN32_NATIVE flags with RING_UWP
    - replaces the WIN32_NATIVE flags from older patches with RING_UWP
    
    Change-Id: I7e49006189111c7d05f547dc783d34703ae80f40
    Tuleap: #790

--- src/security/tlsvalidator.cpp
+++ src/security/tlsvalidator.cpp
@@ -45,7 +45,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#ifndef WIN32_NATIVE
+#ifndef RING_UWP
 #include <libgen.h>
 #endif
 
@@ -846,7 +846,7 @@ TlsValidator::CheckResult TlsValidator::privateKeyDirectoryPermissions()
     if (privateKeyPath_.empty())
        return TlsValidator::CheckResult(CheckValues::UNSUPPORTED, "");
 
-#ifndef WIN32_NATIVE
+#ifndef RING_UWP
     auto path = std::unique_ptr<char, decltype(free)&> (strdup(privateKeyPath_.c_str()), free);
     const char* dir = dirname(path.get());
 #else
@@ -869,7 +869,7 @@ TlsValidator::CheckResult TlsValidator::privateKeyDirectoryPermissions()
 
 TlsValidator::CheckResult TlsValidator::publicKeyDirectoryPermissions()
 {
-#ifndef WIN32_NATIVE
+#ifndef RING_UWP
     auto path = std::unique_ptr<char, decltype(free)&>(strdup(certificatePath_.c_str()), free);
     const char* dir = dirname(path.get());
 #else
