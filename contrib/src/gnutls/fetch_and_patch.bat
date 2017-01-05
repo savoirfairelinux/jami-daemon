181f81482bd7dcec865f174bde2d0bda160a475f
atraczyk <andreastraczyk@gmail.com>
2017-01-05 17:31:46 -0500

    contrib: add UWP support for gnutls
    Change-Id: I622bf805664ce788209ff49e2c7850fbf1cb5687
    Tuleap: #790

---
+++ contrib/src/gnutls/fetch_and_patch.bat
@@ -0,0 +1,14 @@
+set BUILD=..\..\build
+set SRC=..\..\src
+
+mkdir %BUILD%
+cd %BUILD%
+git clone https://github.com/ShiftMediaProject/gnutls.git
+
+cd gnutls
+
+git apply --reject --whitespace=fix %SRC%\gnutls\gnutls-no-egd.patch
+git apply --reject --whitespace=fix %SRC%\gnutls\read-file-limits.h.patch
+git apply --reject --whitespace=fix %SRC%\gnutls\gnutls-uwp.patch
+
+cd %SRC%
