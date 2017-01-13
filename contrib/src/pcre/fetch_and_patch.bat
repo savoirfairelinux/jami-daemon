ebc77bd68e18bb2e8fd94b34956ce21410a6f1ee
atraczyk <andreastraczyk@gmail.com>
2017-01-13 10:38:27 -0500

    contrib: add more UWP support
    Change-Id: I0e4dd4e08d7aab7625e5728d833c0f8a556a175d
    Tuleap: #790

---
+++ contrib/src/pcre/fetch_and_patch.bat
@@ -0,0 +1,21 @@
+set BUILD=%SRC%..\build
+
+set PCRE_VERSION=8.40
+set PCRE_URL=ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre
+set PCRE_NAME=pcre
+
+mkdir %BUILD%
+wget %PCRE_URL%/%PCRE_NAME%-%PCRE_VERSION%.zip
+unzip -q %PCRE_NAME%-%PCRE_VERSION%.zip -d %BUILD%
+del %PCRE_NAME%-%PCRE_VERSION%.zip
+rename %BUILD%\%PCRE_NAME%-%PCRE_VERSION% %PCRE_NAME%
+
+cd %BUILD%\%PCRE_NAME%
+
+git apply --reject --whitespace=fix %SRC%\%PCRE_NAME%\pcre-uwp.patch
+
+rename config.h.generic config.h
+rename pcre.h.in pcre.h
+rename pcre_chartables.c.dist pcre_chartables.c
+
+cd %SRC%
---
+++ contrib/src/portaudio/fetch_and_patch.bat
@@ -0,0 +1,15 @@
+set BUILD=%SRC%..\build
+
+set PA_VERSION=v190600_20161030
+set PA_URL="http://www.portaudio.com/archives/pa_stable_%PA_VERSION%.tgz"
+
+mkdir %BUILD%
+wget %PA_URL%
+7z -y e pa_stable_%PA_VERSION%.tgz && 7z -y x pa_stable_%PA_VERSION%.tar -o%BUILD%
+del pa_stable_%PA_VERSION%.tgz && del pa_stable_%PA_VERSION%.tar
+
+cd %BUILD%\portaudio
+
+git apply --reject --whitespace=fix %SRC%\portaudio\pa-uwp.patch
+
+cd %SRC%
