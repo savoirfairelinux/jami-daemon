9949220f4afce9bfe335a1a444e4de4663c2ce3f
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2017-01-27 12:17:50 -0500

    migration : add signals
    - adds a new kind of signals, called MigrationState.
    RingAccount::migrateAccount now use those signals.
    
    Change-Id: I1ef5bbd495f5f99d247549e883c8a001418a9bd2

---
+++ src/ringdht/migration_states.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Nicolas Jäger <nicolas.jager@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#ifndef MIGRATION_STATES_H_
+#define MIGRATION_STATES_H_
+
+namespace ring {
+
+/** Contains all the Migration states */
+enum class MigrationState {
+    SUCCESS,
+    INVALID
+};
+
+} // namespace ring
+
+#endif // MIGRATION_STATES_H_
