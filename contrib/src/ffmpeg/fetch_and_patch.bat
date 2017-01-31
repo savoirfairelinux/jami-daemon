9075f0ab1548d29db090b9a278a6152f340a05f5
atraczyk <andreastraczyk@gmail.com>
2017-01-31 13:10:08 -0500

    build: patch FFmpegInterop to build with all available cores
    - adds -j to the make command in the ffmpeg build script
    
    Change-Id: I3d58c6db2f30695c9d91e8b6dab87aad9c9d748b
    Tuleap: #790

--- contrib/src/ffmpeg/fetch_and_patch.bat
+++ contrib/src/ffmpeg/fetch_and_patch.bat
@@ -1,5 +1,3 @@
-git clone --recursive 
-
 set BUILD=%SRC%..\build
 
 set FFMPEG_URL=git://github.com/Microsoft/FFmpegInterop.git
@@ -9,7 +7,10 @@ cd %BUILD%
 
 git clone --recursive %FFMPEG_URL%
 
-cd FFmpegInterop\ffmpeg
+cd FFmpegInterop
+git apply --reject --whitespace=fix %SRC%\ffmpeg\ffmpeg-uwp.patch
+
+cd ffmpeg
 git apply --reject --whitespace=fix %SRC%\ffmpeg\0004-avformat-fix-find_stream_info-not-considering-extradata.patch
 
 cd %SRC%
