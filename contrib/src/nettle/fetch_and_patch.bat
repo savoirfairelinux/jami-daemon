0268fac80fac0685c52097e71425427a68a5f9c6
atraczyk <andreastraczyk@gmail.com>
2017-01-31 17:36:37 -0500

    build: change to archive downloads only on UWP build
    - Changes fetch scripts urls to archived tarballs if possible to
      facilitate caching of the contrib source on the UWP Jenkins
      build machine.
    
    Change-Id: I51eac783eea964bfe08727baf1ad7b9e08a5096d
    Tuleap: #790

--- contrib/src/nettle/fetch_and_patch.bat
+++ contrib/src/nettle/fetch_and_patch.bat
@@ -1,13 +1,16 @@
 set BUILD=%SRC%..\build
 
-set REPOURL="https://github.com/ShiftMediaProject/nettle.git"
+set NETTLE_VERSION=4e0b2723b76d4163fa37b2b456d41534154ec97c
+set NETTLE_URL=https://github.com/ShiftMediaProject/nettle/archive/%NETTLE_VERSION%.tar.gz
 
 mkdir %BUILD%
-cd %BUILD%
+wget %NETTLE_URL%
+7z -y x %NETTLE_VERSION%.tar.gz && 7z -y x %NETTLE_VERSION%.tar -o%BUILD%
+del %NETTLE_VERSION%.tar && del %NETTLE_VERSION%.tar.gz && del %BUILD%\pax_global_header
+rename %BUILD%\nettle-%NETTLE_VERSION% nettle
 
-git clone %REPOURL%
+cd %BUILD%\nettle
 
-cd nettle
 git apply --reject --whitespace=fix %SRC%\nettle\nettle-uwp.patch
 
 cd %SRC%
