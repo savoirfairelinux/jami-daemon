8d81b2d362544259ade2f2094c96084808782932
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2017-03-29 03:09:39 +0200

    contrib: bump opendht
    Bump OpenDH to fix various bugs since last bump
    and to add new API to fix issues with certificate memory management.
    
    Change-Id: I80dc2aa5113a394b725764fa6c71b35b4b18a853
    Tuleap: #1548

--- contrib/src/opendht/fetch_and_patch.bat
+++ contrib/src/opendht/fetch_and_patch.bat
@@ -1,6 +1,6 @@
 set BUILD=%SRC%..\build
 
-set OPENDHT_VERSION=b08b2f14f20e0dbaa00fc2c70347d578c70e49da
+set OPENDHT_VERSION=09b4e71dc5743d67f35a9604ec93c51c1e109ebe
 set OPENDHT_URL=https://github.com/savoirfairelinux/opendht/archive/%OPENDHT_VERSION%.tar.gz
 
 mkdir %BUILD%
