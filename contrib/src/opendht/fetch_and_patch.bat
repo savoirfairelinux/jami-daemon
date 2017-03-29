35550d22e401eec96b76107635ef8c2055712f1f
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2017-03-29 03:09:39 +0200

    contrib: bump opendht and fix uwp build
    Bump OpenDHT to commit 09b4e71d (30/03/2017) to fix various bugs since
    last bump and to add new API to fix issues with certificate memory
    management.
    
    This includes a patch for UWP to build this new version.
    
    Change-Id: I80dc2aa5113a394b725764fa6c71b35b4b18a853
    Tuleap: #1548
    Reviewed-by: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>

--- contrib/src/opendht/fetch_and_patch.bat
+++ contrib/src/opendht/fetch_and_patch.bat
@@ -1,6 +1,6 @@
 set BUILD=%SRC%..\build
 
-set OPENDHT_VERSION=b08b2f14f20e0dbaa00fc2c70347d578c70e49da
+set OPENDHT_VERSION=09b4e71dc5743d67f35a9604ec93c51c1e109ebe
 set OPENDHT_URL=https://github.com/savoirfairelinux/opendht/archive/%OPENDHT_VERSION%.tar.gz
 
 mkdir %BUILD%
@@ -14,5 +14,9 @@ if %USE_CACHE%==1 (
 7z -y x %OPENDHT_VERSION%.tar.gz && 7z -y x %OPENDHT_VERSION%.tar -o%BUILD%
 del %OPENDHT_VERSION%.tar && del %OPENDHT_VERSION%.tar.gz && del %BUILD%\pax_global_header
 rename %BUILD%\opendht-%OPENDHT_VERSION% opendht
+
+cd %BUILD%\opendht
+
+git apply --reject --whitespace=fix %SRC%\opendht\opendht-uwp.patch
 
 cd %SRC%
