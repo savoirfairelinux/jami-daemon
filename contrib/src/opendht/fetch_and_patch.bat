6c5acd22f6673bb5a0872ed83ba2d10a5e6f8901
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2017-05-13 20:34:20 -0400

    opendht: update to release 1.3.3
    * bump fixes a crash with null certificates
    * use a release tag as a best practice to help packagers
    
    Change-Id: Ief90ea9d833419eb10a9250d7e5d956f95a2a5bc

--- contrib/src/opendht/fetch_and_patch.bat
+++ contrib/src/opendht/fetch_and_patch.bat
@@ -1,6 +1,6 @@
 set BUILD=%SRC%..\build
 
-set OPENDHT_VERSION=406a1c11528fe2fd35798ac978105344663ec8b6
+set OPENDHT_VERSION=1.3.3
 set OPENDHT_URL=https://github.com/savoirfairelinux/opendht/archive/%OPENDHT_VERSION%.tar.gz
 
 mkdir %BUILD%
