19a91da6b9701da3e69cf309df9563c68526bad2
atraczyk <andreastraczyk@gmail.com>
2017-05-17 14:49:30 -0400

    contrib(UWP): add libopus
    This patch adds libopus to the UWP contrib and configures the
    ffmpeg build script to use it.
    
    Change-Id: I20e3b547eb7228be0a293a3514eb267afac1c068

---
+++ contrib/src/opus/fetch_and_patch.bat
@@ -0,0 +1,22 @@
+set BUILD=%SRC%..\build
+
+set OPUS_VERSION=b0dd440bbefa628e23ed50e418316c0afb4fe37d
+set OPUS_URL=https://github.com/ShiftMediaProject/opus/archive/%OPUS_VERSION%.tar.gz
+
+mkdir %BUILD%
+
+if %USE_CACHE%==1 (
+    copy %CACHE_DIR%\%OPUS_VERSION%.tar.gz %cd%
+) else (
+    wget %OPUS_URL%
+)
+
+7z -y x %OPUS_VERSION%.tar.gz && 7z -y x %OPUS_VERSION%.tar -o%BUILD%
+del %OPUS_VERSION%.tar && del %OPUS_VERSION%.tar.gz && del %BUILD%\pax_global_header
+rename %BUILD%\opus-%OPUS_VERSION% opus
+
+cd %BUILD%\opus
+
+rem git apply --reject --whitespace=fix %SRC%\opus\opus-uwp.patch
+
+cd %SRC%
