7a2d924ecba682b6db50003e63713a9d049a5ea9
atraczyk <andreastraczyk@gmail.com>
2017-06-13 14:27:35 -0400

    ip_utils: add GetAppHostName signal
    Add signal to retrieve the hostname on platforms where the device
    hostname must be acquired client-side.
    
    Change-Id: I81013afdd5d51f3cbd518dca22a4cfdc03f94c59

--- src/dring/configurationmanager_interface.h
+++ src/dring/configurationmanager_interface.h
@@ -294,6 +294,10 @@ struct ConfigurationSignal {
                 constexpr static const char* name = "GetAppDataPath";
                 using cb_type = void(const std::string& name, std::vector<std::string>* /* path_ret */);
         };
+        struct GetAppHostName {
+            constexpr static const char* name = "GetAppHostName";
+            using cb_type = void(std::vector<std::string>* /* path_ret */);
+        };
 #endif
 };
 
