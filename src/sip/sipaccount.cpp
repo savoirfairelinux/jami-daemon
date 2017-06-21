dc98220d977beba9a47d545f7260eb57edfb9d19
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2017-06-20 22:14:44 -0400

    upnp: do not enable if not used
    upnp is always activated even if no account uses it.
    
    Here we refactoring its creation and availability checks.
    This also removes an atomic_bool that gives no true conccurent
    protection as it protects itself, but not multiple accesses
    to upnp_ member.
    
    Change-Id: Ibf6aaee54077867cdf3029fd51ef6cae0b7488c1

--- src/sip/sipaccount.cpp
+++ src/sip/sipaccount.cpp
@@ -698,7 +698,7 @@ void SIPAccount::doRegister()
     RING_DBG("doRegister %s", hostname_.c_str());
 
     /* if UPnP is enabled, then wait for IGD to complete registration */
-    if ( upnpEnabled_ ) {
+    if (upnp_) {
         RING_DBG("UPnP: waiting for IGD to register SIP account");
         setRegistrationState(RegistrationState::TRYING);
         auto shared = shared_from_this();
@@ -842,7 +842,7 @@ void SIPAccount::doUnregister(std::function<void(bool)> released_cb)
     if (released_cb)
         released_cb(not isIP2IP());
 
-    if (upnpEnabled_) {
+    if (upnp_) {
         upnp_->setIGDListener();
         upnp_->removeMappings();
     }
