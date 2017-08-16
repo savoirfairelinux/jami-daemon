348bbad457fb2866f808a38fbc93bf818f454f6e
philippegorley <philippe.gorley@savoirfairelinux.com>
2017-08-16 16:25:43 -0400

    video: implement normalized quality/bitrate settings
    Add 3 new settings to video preferences:
    
    bitrate, quality and autoQuality
    
    They are R/W accessible into dring.yml, libring API and dbus API through
    the Configuration Manager API domain.
    
    quality and bitrate are exposed as normalized values between 0 upto 100,
    as differents codecs use different minimum and maximum values.
    
    These settings are loaded in the video rtp session, and saved by the
    configuration manager when it detects a change.
    
    CodecDetails is not changed and continue to return values in the
    related codec reference.
    
    [guillaume: ci-msg changed for clarity]
    [guillaume: API bump to 4.0.1]
    
    Change-Id: Id727adc88f6fe6f89bef0a70c343a70c5e8e73d9

--- src/manager.h
+++ src/manager.h
@@ -884,6 +884,18 @@ class Manager {
         bool getDecodingAccelerated() const;
 
         void setDecodingAccelerated(bool isAccelerated);
+
+        bool getAutoQuality() const;
+
+        void setAutoQuality(bool autoQuality);
+
+        double getQuality() const;
+
+        void setQuality(double quality);
+
+        double getBitrate() const;
+
+        void setBitrate(double bitrate);
 #endif // RING_VIDEO
 
         std::atomic<unsigned> dhtLogLevel {0}; // default = disable
