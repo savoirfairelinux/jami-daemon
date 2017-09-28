6b3a011a064ba31dedef5249e849aaec339a693e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2017-09-28 21:17:26 +0200

    eth: bump sources, remove dependency to boost, cryptopp
    Update "eth" to latest version from
    https://github.com/ethereum/cpp-ethereum
    
    This new version makes use of secp256k1 instead of custom
    code and cryptopp to handle EC generation, and reduces its dependency
    to boost to use more of the C++11 STL.
    
    This allows to trim the code to completely remove the boost and
    cryptopp dependencies.
    
    Change-Id: I19118d3b833a9425cddbeabf6d0b6e07d5a4b15e

--- src/ringdht/eth/libdevcore/Address.h
+++ src/ringdht/eth/libdevcore/Address.h
@@ -14,16 +14,25 @@
 	You should have received a copy of the GNU General Public License
 	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
 */
-/** @file Guards.cpp
- * @author Gav Wood <i@gavwood.com>
- * @date 2014
- */
+/// @file
+/// This file defined Address alias for FixedHash of 160 bits and some
+/// special Address constants.
 
-#include "Guards.h"
-using namespace std;
-using namespace dev;
+#pragma once
+
+#include "FixedHash.h"
 
 namespace dev
 {
+
+/// An Ethereum address: 20 bytes.
+/// @NOTE This is not endian-specific; it's just a bunch of bytes.
+using Address = h160;
+
+/// A vector of Ethereum addresses.
+using Addresses = h160s;
+
+/// A hash set of Ethereum addresses.
+using AddressHash = std::unordered_set<h160>;
 
 }
