c39951d3ff8ea5ae904d918004cb00d7b3cc1b9e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2017-09-28 15:58:51 +0200

    eth: bump sources, remove dependency to boost, cryptopp
    Change-Id: I19118d3b833a9425cddbeabf6d0b6e07d5a4b15e

--- src/ringdht/eth/libdevcrypto/Common.cpp
+++ src/ringdht/eth/libdevcrypto/Common.cpp
@@ -21,71 +21,52 @@
  */
 
 #include "Common.h"
-#include <cstdint>
-#include <chrono>
-#include <thread>
-#include <mutex>
-#include <libdevcore/Guards.h>
+#include <secp256k1.h>
 #include <libdevcore/SHA3.h>
-#include <libdevcore/RLP.h>
-#if ETH_HAVE_SECP256K1
-#include <secp256k1/include/secp256k1.h>
-#endif
-#include "AES.h"
-#include "CryptoPP.h"
-#include "Exceptions.h"
 using namespace std;
 using namespace dev;
 using namespace dev::crypto;
 
-#ifdef ETH_HAVE_SECP256K1
-class Secp256k1Context
+namespace
 {
-public:
-	static secp256k1_context_t const* get() { if (!s_this) s_this = new Secp256k1Context; return s_this->m_ctx; }
 
-private:
-	Secp256k1Context() { m_ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY); }
-	~Secp256k1Context() { secp256k1_context_destroy(m_ctx); }
+secp256k1_context const* getCtx()
+{
+	static std::unique_ptr<secp256k1_context, decltype(&secp256k1_context_destroy)> s_ctx{
+		secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY),
+		&secp256k1_context_destroy
+	};
+	return s_ctx.get();
+}
 
-	secp256k1_context_t* m_ctx;
-
-	static Secp256k1Context* s_this;
-};
-Secp256k1Context* Secp256k1Context::s_this = nullptr;
-#endif
+}
 
 bool dev::SignatureStruct::isValid() const noexcept
 {
-	if (v > 1 ||
-		r >= h256("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141") ||
-		s >= h256("0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141") ||
-		s < h256(1) ||
-		r < h256(1))
-		return false;
-	return true;
-}
+	static const h256 s_max{"0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"};
+	static const h256 s_zero;
 
-Public SignatureStruct::recover(h256 const& _hash) const
-{
-	return dev::recover((Signature)*this, _hash);
+	return (v <= 1 && r > s_zero && s > s_zero && r < s_max && s < s_max);
 }
-
-Address dev::ZeroAddress = Address();
 
 Public dev::toPublic(Secret const& _secret)
 {
-#ifdef ETH_HAVE_SECP256K1
-	bytes o(65);
-	int pubkeylen;
-	if (!secp256k1_ec_pubkey_create(Secp256k1Context::get(), o.data(), &pubkeylen, _secret.data(), false))
-		return Public();
-	return FixedHash<64>(o.data()+1, Public::ConstructFromPointer);
-#else
-	Public p;
-	Secp256k1PP::get()->toPublic(_secret, p);
-	return p;
-#endif
+	auto* ctx = getCtx();
+	secp256k1_pubkey rawPubkey;
+	// Creation will fail if the secret key is invalid.
+	if (!secp256k1_ec_pubkey_create(ctx, &rawPubkey, _secret.data()))
+		return {};
+	std::array<byte, 65> serializedPubkey;
+	size_t serializedPubkeySize = serializedPubkey.size();
+	secp256k1_ec_pubkey_serialize(
+			ctx, serializedPubkey.data(), &serializedPubkeySize,
+			&rawPubkey, SECP256K1_EC_UNCOMPRESSED
+	);
+	assert(serializedPubkeySize == serializedPubkey.size());
+	// Expect single byte header of value 0x04 -- uncompressed public key.
+	assert(serializedPubkey[0] == 0x04);
+	// Create the Public skipping the header.
+	return Public{&serializedPubkey[1], Public::ConstructFromPointer};
 }
 
 Address dev::toAddress(Public const& _public)
@@ -95,209 +76,26 @@ Address dev::toAddress(Public const& _public)
 
 Address dev::toAddress(Secret const& _secret)
 {
-	Public p;
-	Secp256k1PP::get()->toPublic(_secret, p);
-	return toAddress(p);
+	return toAddress(toPublic(_secret));
 }
 
-Address dev::toAddress(Address const& _from, u256 const& _nonce)
+KeyPair::KeyPair(Secret const& _sec):
+	m_secret(_sec),
+	m_public(toPublic(_sec))
 {
-	return right160(sha3(rlpList(_from, _nonce)));
-}
-
-void dev::encrypt(Public const& _k, bytesConstRef _plain, bytes& o_cipher)
-{
-	bytes io = _plain.toBytes();
-	Secp256k1PP::get()->encrypt(_k, io);
-	o_cipher = std::move(io);
-}
-
-bool dev::decrypt(Secret const& _k, bytesConstRef _cipher, bytes& o_plaintext)
-{
-	bytes io = _cipher.toBytes();
-	Secp256k1PP::get()->decrypt(_k, io);
-	if (io.empty())
-		return false;
-	o_plaintext = std::move(io);
-	return true;
-}
-
-void dev::encryptECIES(Public const& _k, bytesConstRef _plain, bytes& o_cipher)
-{
-	encryptECIES(_k, bytesConstRef(), _plain, o_cipher);
-}
-
-void dev::encryptECIES(Public const& _k, bytesConstRef _sharedMacData, bytesConstRef _plain, bytes& o_cipher)
-{
-	bytes io = _plain.toBytes();
-	Secp256k1PP::get()->encryptECIES(_k, _sharedMacData, io);
-	o_cipher = std::move(io);
-}
-
-bool dev::decryptECIES(Secret const& _k, bytesConstRef _cipher, bytes& o_plaintext)
-{
-	return decryptECIES(_k, bytesConstRef(),  _cipher, o_plaintext);
-}
-
-bool dev::decryptECIES(Secret const& _k, bytesConstRef _sharedMacData, bytesConstRef _cipher, bytes& o_plaintext)
-{
-	bytes io = _cipher.toBytes();
-	if (!Secp256k1PP::get()->decryptECIES(_k, _sharedMacData, io))
-		return false;
-	o_plaintext = std::move(io);
-	return true;
-}
-
-void dev::encryptSym(Secret const& _k, bytesConstRef _plain, bytes& o_cipher)
-{
-	// TOOD: @alex @subtly do this properly.
-	encrypt(KeyPair(_k).pub(), _plain, o_cipher);
-}
-
-bool dev::decryptSym(Secret const& _k, bytesConstRef _cipher, bytes& o_plain)
-{
-	// TODO: @alex @subtly do this properly.
-	return decrypt(_k, _cipher, o_plain);
-}
-
-std::pair<bytes, h128> dev::encryptSymNoAuth(SecureFixedHash<16> const& _k, bytesConstRef _plain)
-{
-	h128 iv(Nonce::get().makeInsecure());
-	return make_pair(encryptSymNoAuth(_k, iv, _plain), iv);
-}
-
-bytes dev::encryptAES128CTR(bytesConstRef _k, h128 const& _iv, bytesConstRef _plain)
-{
-	if (_k.size() != 16 && _k.size() != 24 && _k.size() != 32)
-		return bytes();
-	SecByteBlock key(_k.data(), _k.size());
-	try
-	{
-		CTR_Mode<AES>::Encryption e;
-		e.SetKeyWithIV(key, key.size(), _iv.data());
-		bytes ret(_plain.size());
-		e.ProcessData(ret.data(), _plain.data(), _plain.size());
-		return ret;
-	}
-	catch (CryptoPP::Exception& _e)
-	{
-		cerr << _e.what() << endl;
-		return bytes();
-	}
-}
-
-bytesSec dev::decryptAES128CTR(bytesConstRef _k, h128 const& _iv, bytesConstRef _cipher)
-{
-	if (_k.size() != 16 && _k.size() != 24 && _k.size() != 32)
-		return bytesSec();
-	SecByteBlock key(_k.data(), _k.size());
-	try
-	{
-		CTR_Mode<AES>::Decryption d;
-		d.SetKeyWithIV(key, key.size(), _iv.data());
-		bytesSec ret(_cipher.size());
-		d.ProcessData(ret.writable().data(), _cipher.data(), _cipher.size());
-		return ret;
-	}
-	catch (CryptoPP::Exception& _e)
-	{
-		cerr << _e.what() << endl;
-		return bytesSec();
-	}
-}
-
-static const Public c_zeroKey("3f17f1962b36e491b30a40b2405849e597ba5fb5");
-
-Public dev::recover(Signature const& _sig, h256 const& _message)
-{
-	Public ret;
-#ifdef ETH_HAVE_SECP256K1
-	bytes o(65);
-	int pubkeylen;
-	if (_sig[64] > 3 || !secp256k1_ecdsa_recover_compact(Secp256k1Context::get(), _message.data(), _sig.data(), o.data(), &pubkeylen, false, _sig[64]))
-		return Public();
-	ret = FixedHash<64>(o.data() + 1, Public::ConstructFromPointer);
-#else
-	ret = Secp256k1PP::get()->recover(_sig, _message.ref());
-#endif
-	if (ret == c_zeroKey)
-		return Public();
-	return ret;
-}
-
-static const u256 c_secp256k1n("115792089237316195423570985008687907852837564279074904382605163141518161494337");
-
-Signature dev::sign(Secret const& _k, h256 const& _hash)
-{
-	Signature s;
-	SignatureStruct& ss = *reinterpret_cast<SignatureStruct*>(&s);
-
-#ifdef ETH_HAVE_SECP256K1
-	int v;
-	if (!secp256k1_ecdsa_sign_compact(Secp256k1Context::get(), _hash.data(), s.data(), _k.data(), NULL, NULL, &v))
-		return Signature();
-	ss.v = v;
-#else
-	s = Secp256k1PP::get()->sign(_k, _hash);
-#endif
-	if (ss.s > c_secp256k1n / 2)
-	{
-		ss.v = ss.v ^ 1;
-		ss.s = h256(c_secp256k1n - u256(ss.s));
-	}
-	assert(ss.s <= c_secp256k1n / 2);
-	return s;
-}
-
-bool dev::verify(Public const& _p, Signature const& _s, h256 const& _hash)
-{
-	if (!_p)
-		return false;
-#ifdef ETH_HAVE_SECP256K1
-	return _p == recover(_s, _hash);
-#else
-	return Secp256k1PP::get()->verify(_p, _s, _hash.ref(), true);
-#endif
-}
-
-bytesSec dev::pbkdf2(string const& _pass, bytes const& _salt, unsigned _iterations, unsigned _dkLen)
-{
-	bytesSec ret(_dkLen);
-	if (PKCS5_PBKDF2_HMAC<SHA256>().DeriveKey(
-		ret.writable().data(),
-		_dkLen,
-		0,
-		reinterpret_cast<byte const*>(_pass.data()),
-		_pass.size(),
-		_salt.data(),
-		_salt.size(),
-		_iterations
-	) != _iterations)
-		BOOST_THROW_EXCEPTION(CryptoException() << errinfo_comment("Key derivation failed."));
-	return ret;
-}
-
-void KeyPair::populateFromSecret(Secret const& _sec)
-{
-	m_secret = _sec;
-	if (Secp256k1PP::get()->verifySecret(m_secret, m_public))
+	// Assign address only if the secret key is valid.
+	if (m_public)
 		m_address = toAddress(m_public);
 }
 
 KeyPair KeyPair::create()
 {
-	for (int i = 0; i < 100; ++i)
+	while (true)
 	{
-		KeyPair ret(Secret::random());
-		if (ret.address())
-			return ret;
+		KeyPair keyPair(Secret::random());
+		if (keyPair.address())
+			return keyPair;
 	}
-	return KeyPair();
-}
-
-KeyPair KeyPair::fromEncryptedSeed(bytesConstRef _seed, std::string const& _password)
-{
-	return KeyPair(Secret(sha3(aesDecrypt(_seed, _password))));
 }
 
 h256 crypto::kdf(Secret const& _priv, h256 const& _hash)
@@ -307,20 +105,20 @@ h256 crypto::kdf(Secret const& _priv, h256 const& _hash)
 	sha3mac(Secret::random().ref(), _priv.ref(), s.ref());
 	s ^= _hash;
 	sha3(s.ref(), s.ref());
-	
+
 	if (!s || !_hash || !_priv)
-		BOOST_THROW_EXCEPTION(InvalidState());
+		throw InvalidState();
 	return s;
 }
 
 Secret Nonce::next()
 {
-	Guard l(x_value);
+	std::lock_guard<std::mutex> l(x_value);
 	if (!m_value)
 	{
 		m_value = Secret::random();
 		if (!m_value)
-			BOOST_THROW_EXCEPTION(InvalidState());
+			throw InvalidState();
 	}
 	m_value = sha3Secure(m_value.ref());
 	return sha3(~m_value);
--- src/ringdht/eth/libdevcrypto/Common.h
+++ src/ringdht/eth/libdevcrypto/Common.h
@@ -25,9 +25,9 @@
 #pragma once
 
 #include <mutex>
-#include "libdevcore/Common.h"
-#include "libdevcore/FixedHash.h"
-#include "libdevcore/Exceptions.h"
+#include <libdevcore/Address.h>
+#include <libdevcore/Common.h>
+#include <libdevcore/FixedHash.h>
 
 namespace dev
 {
@@ -52,26 +52,10 @@ struct SignatureStruct
 	/// @returns true if r,s,v values are valid, otherwise false
 	bool isValid() const noexcept;
 
-	/// @returns the public part of the key that signed @a _hash to give this sig.
-	Public recover(h256 const& _hash) const;
-
 	h256 r;
 	h256 s;
 	byte v = 0;
 };
-
-/// An Ethereum address: 20 bytes.
-/// @NOTE This is not endian-specific; it's just a bunch of bytes.
-using Address = h160;
-
-/// The zero address.
-extern Address ZeroAddress;
-
-/// A vector of Ethereum addresses.
-using Addresses = h160s;
-
-/// A hash set of Ethereum addresses.
-using AddressHash = std::unordered_set<h160>;
 
 /// A vector of secrets.
 using Secrets = std::vector<Secret>;
@@ -86,86 +70,24 @@ Address toAddress(Public const& _public);
 /// @returns 0 if it's not a valid secret key.
 Address toAddress(Secret const& _secret);
 
-// Convert transaction from and nonce to address.
-Address toAddress(Address const& _from, u256 const& _nonce);
-
-/// Encrypts plain text using Public key.
-void encrypt(Public const& _k, bytesConstRef _plain, bytes& o_cipher);
-
-/// Decrypts cipher using Secret key.
-bool decrypt(Secret const& _k, bytesConstRef _cipher, bytes& o_plaintext);
-
-/// Symmetric encryption.
-void encryptSym(Secret const& _k, bytesConstRef _plain, bytes& o_cipher);
-
-/// Symmetric decryption.
-bool decryptSym(Secret const& _k, bytesConstRef _cipher, bytes& o_plaintext);
-
-/// Encrypt payload using ECIES standard with AES128-CTR.
-void encryptECIES(Public const& _k, bytesConstRef _plain, bytes& o_cipher);
-
-/// Encrypt payload using ECIES standard with AES128-CTR.
-/// @a _sharedMacData is shared authenticated data.
-void encryptECIES(Public const& _k, bytesConstRef _sharedMacData, bytesConstRef _plain, bytes& o_cipher);
-
-/// Decrypt payload using ECIES standard with AES128-CTR.
-bool decryptECIES(Secret const& _k, bytesConstRef _cipher, bytes& o_plaintext);
-
-/// Decrypt payload using ECIES standard with AES128-CTR.
-/// @a _sharedMacData is shared authenticated data.
-bool decryptECIES(Secret const& _k, bytesConstRef _sharedMacData, bytesConstRef _cipher, bytes& o_plaintext);
-
-/// Encrypts payload with random IV/ctr using AES128-CTR.
-std::pair<bytes, h128> encryptSymNoAuth(SecureFixedHash<16> const& _k, bytesConstRef _plain);
-
-/// Encrypts payload with specified IV/ctr using AES128-CTR.
-bytes encryptAES128CTR(bytesConstRef _k, h128 const& _iv, bytesConstRef _plain);
-
-/// Decrypts payload with specified IV/ctr using AES128-CTR.
-bytesSec decryptAES128CTR(bytesConstRef _k, h128 const& _iv, bytesConstRef _cipher);
-
-/// Encrypts payload with specified IV/ctr using AES128-CTR.
-inline bytes encryptSymNoAuth(SecureFixedHash<16> const& _k, h128 const& _iv, bytesConstRef _plain) { return encryptAES128CTR(_k.ref(), _iv, _plain); }
-inline bytes encryptSymNoAuth(SecureFixedHash<32> const& _k, h128 const& _iv, bytesConstRef _plain) { return encryptAES128CTR(_k.ref(), _iv, _plain); }
-
-/// Decrypts payload with specified IV/ctr using AES128-CTR.
-inline bytesSec decryptSymNoAuth(SecureFixedHash<16> const& _k, h128 const& _iv, bytesConstRef _cipher) { return decryptAES128CTR(_k.ref(), _iv, _cipher); }
-inline bytesSec decryptSymNoAuth(SecureFixedHash<32> const& _k, h128 const& _iv, bytesConstRef _cipher) { return decryptAES128CTR(_k.ref(), _iv, _cipher); }
-
-/// Recovers Public key from signed message hash.
-Public recover(Signature const& _sig, h256 const& _hash);
-	
-/// Returns siganture of message hash.
-Signature sign(Secret const& _k, h256 const& _hash);
-	
-/// Verify signature.
-bool verify(Public const& _k, Signature const& _s, h256 const& _hash);
-
-/// Derive key via PBKDF2.
-bytesSec pbkdf2(std::string const& _pass, bytes const& _salt, unsigned _iterations, unsigned _dkLen = 32);
-
 /// Simple class that represents a "key pair".
 /// All of the data of the class can be regenerated from the secret key (m_secret) alone.
 /// Actually stores a tuplet of secret, public and address (the right 160-bits of the public).
 class KeyPair
 {
 public:
-	/// Null constructor.
-	KeyPair() {}
-
 	/// Normal constructor - populates object from the given secret key.
-	KeyPair(Secret const& _k) { populateFromSecret(_k); }
+	/// If the secret key is invalid the constructor succeeds, but public key
+	/// and address stay "null".
+	KeyPair(Secret const& _sec);
 
 	/// Create a new, randomly generated object.
 	static KeyPair create();
 
 	/// Create from an encrypted seed.
-	static KeyPair fromEncryptedSeed(bytesConstRef _seed, std::string const& _password);
+	//static KeyPair fromEncryptedSeed(bytesConstRef _seed, std::string const& _password);
 
-	/// Retrieve the secret key.
 	Secret const& secret() const { return m_secret; }
-	/// Retrieve the secret key.
-	Secret const& sec() const { return m_secret; }
 
 	/// Retrieve the public key.
 	Public const& pub() const { return m_public; }
@@ -177,8 +99,6 @@ public:
 	bool operator!=(KeyPair const& _c) const { return m_public != _c.m_public; }
 
 private:
-	void populateFromSecret(Secret const& _k);
-
 	Secret m_secret;
 	Public m_public;
 	Address m_address;
@@ -187,7 +107,14 @@ private:
 namespace crypto
 {
 
-DEV_SIMPLE_EXCEPTION(InvalidState);
+class InvalidState : public std::runtime_error {
+public:
+	InvalidState() : runtime_error("invalid state") {};
+};
+class CryptoException : public std::runtime_error {
+public:
+	CryptoException(const char* err) : runtime_error(err) {};
+};
 
 /// Key derivation
 h256 kdf(Secret const& _priv, h256 const& _hash);
@@ -196,9 +123,9 @@ h256 kdf(Secret const& _priv, h256 const& _hash);
  * @brief Generator for non-repeating nonce material.
  * The Nonce class should only be used when a non-repeating nonce
  * is required and, in its current form, not recommended for signatures.
- * This is primarily because the key-material for signatures is 
- * encrypted on disk whereas the seed for Nonce is not. 
- * Thus, Nonce's primary intended use at this time is for networking 
+ * This is primarily because the key-material for signatures is
+ * encrypted on disk whereas the seed for Nonce is not.
+ * Thus, Nonce's primary intended use at this time is for networking
  * where the key is also stored in plaintext.
  */
 class Nonce
