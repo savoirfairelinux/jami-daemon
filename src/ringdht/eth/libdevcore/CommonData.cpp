4f6b79dedde382a14d2b70f503d74a9c2bed48ec
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2017-09-28 18:50:40 +0200

    eth: bump sources, remove dependency to boost, cryptopp
    Update "eth" to latest version from
    https://github.com/ethereum/cpp-ethereum
    
    This new version makes use of secp256k1 instead of custom
    code and cryptopp to handle EC generation, and reduces its dependency
    to boost to use more of the C++11 STL.
    
    This allows to trim the code to completely remove the boost and
    cryptopp dependencies.
    
    Change-Id: I19118d3b833a9425cddbeabf6d0b6e07d5a4b15e

--- src/ringdht/eth/libdevcore/CommonData.cpp
+++ src/ringdht/eth/libdevcore/CommonData.cpp
@@ -20,27 +20,30 @@
  */
 
 #include "CommonData.h"
-#include <random>
 
-#if defined(_MSC_VER)
-#pragma warning(push)
-#pragma warning(disable:4724) // potential mod by 0, line 78 of boost/random/uniform_int_distribution.hpp (boost 1.55)
-#endif
-#include <boost/random/uniform_int_distribution.hpp>
-#if defined(_MSC_VER)
-#pragma warning(pop)
-#endif
-#include "Exceptions.h"
 using namespace std;
 using namespace dev;
 
-bool dev::isHex(string const& _s)
+namespace
 {
-	unsigned i = (_s.size() >= 2 && _s.substr(0, 2) == "0x") ? 2 : 0;
-	for (; i < _s.size(); ++i)
-		if (fromHex(_s[i], WhenError::DontThrow) == -1)
-			return false;
-	return true;
+int fromHexChar(char _i) noexcept
+{
+	if (_i >= '0' && _i <= '9')
+		return _i - '0';
+	if (_i >= 'a' && _i <= 'f')
+		return _i - 'a' + 10;
+	if (_i >= 'A' && _i <= 'F')
+		return _i - 'A' + 10;
+	return -1;
+}
+}
+
+bool dev::isHex(string const& _s) noexcept
+{
+	auto it = _s.begin();
+	if (_s.compare(0, 2, "0x") == 0)
+		it += 2;
+	return std::all_of(it, _s.end(), [](char c){ return fromHexChar(c) != -1; });
 }
 
 std::string dev::escaped(std::string const& _s, bool _all)
@@ -71,31 +74,6 @@ std::string dev::escaped(std::string const& _s, bool _all)
 	return ret;
 }
 
-std::string dev::randomWord()
-{
-	static std::mt19937_64 s_eng(0);
-	std::string ret(boost::random::uniform_int_distribution<int>(1, 5)(s_eng), ' ');
-	char const n[] = "qwertyuiop";//asdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-	boost::random::uniform_int_distribution<int> d(0, sizeof(n) - 2);
-	for (char& c: ret)
-		c = n[d(s_eng)];
-	return ret;
-}
-
-int dev::fromHex(char _i, WhenError _throw)
-{
-	if (_i >= '0' && _i <= '9')
-		return _i - '0';
-	if (_i >= 'a' && _i <= 'f')
-		return _i - 'a' + 10;
-	if (_i >= 'A' && _i <= 'F')
-		return _i - 'A' + 10;
-	if (_throw == WhenError::Throw)
-		BOOST_THROW_EXCEPTION(BadHexCharacter() << errinfo_invalidSymbol(_i));
-	else
-		return -1;
-}
-
 bytes dev::fromHex(std::string const& _s, WhenError _throw)
 {
 	unsigned s = (_s.size() >= 2 && _s[0] == '0' && _s[1] == 'x') ? 2 : 0;
@@ -104,22 +82,22 @@ bytes dev::fromHex(std::string const& _s, WhenError _throw)
 
 	if (_s.size() % 2)
 	{
-		int h = fromHex(_s[s++], WhenError::DontThrow);
+		int h = fromHexChar(_s[s++]);
 		if (h != -1)
 			ret.push_back(h);
 		else if (_throw == WhenError::Throw)
-			BOOST_THROW_EXCEPTION(BadHexCharacter());
+			throw std::runtime_error("BadHexCharacter");
 		else
 			return bytes();
 	}
 	for (unsigned i = s; i < _s.size(); i += 2)
 	{
-		int h = fromHex(_s[i], WhenError::DontThrow);
-		int l = fromHex(_s[i + 1], WhenError::DontThrow);
+		int h = fromHexChar(_s[i]);
+		int l = fromHexChar(_s[i + 1]);
 		if (h != -1 && l != -1)
 			ret.push_back((byte)(h * 16 + l));
 		else if (_throw == WhenError::Throw)
-			BOOST_THROW_EXCEPTION(BadHexCharacter());
+			throw std::runtime_error("BadHexCharacter");
 		else
 			return bytes();
 	}
@@ -135,13 +113,5 @@ bytes dev::asNibbles(bytesConstRef const& _s)
 		ret.push_back(i / 16);
 		ret.push_back(i % 16);
 	}
-	return ret;
-}
-
-std::string dev::toString(string32 const& _s)
-{
-	std::string ret;
-	for (unsigned i = 0; i < 32 && _s[i]; ++i)
-		ret.push_back(_s[i]);
 	return ret;
 }
