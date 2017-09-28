55946fb33ce941e7f08f9b648a8d7b9ed69a42ae
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2017-09-28 21:17:26 +0200

    eth: bump sources, remove dependency to boost, cryptopp
    Update "eth" to latest version from
    https://github.com/ethereum/cpp-ethereum
    
    This new version makes use of secp256k1 instead of custom
    code and cryptopp to handle EC generation, and reduces its dependency
    to boost to use more of the C++11 STL.
    
    This allows to trim the code to completely remove the boost and
    cryptopp dependencies.
    
    Change-Id: I19118d3b833a9425cddbeabf6d0b6e07d5a4b15e

--- src/ringdht/eth/libdevcore/Common.h
+++ src/ringdht/eth/libdevcore/Common.h
@@ -23,7 +23,7 @@
 
 #pragma once
 
-// way to many unsigned to size_t warnings in 32 bit build
+// way too many unsigned to size_t warnings in 32 bit build
 #ifdef _M_IX86
 #pragma warning(disable:4244)
 #endif
@@ -45,16 +45,6 @@
 #include <functional>
 #include <string>
 #include <chrono>
-#pragma warning(push)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <boost/version.hpp>
-#if (BOOST_VERSION == 105800)
-	#include "boost_multiprecision_number_compare_bug_workaround.hpp"
-#endif
-#include <boost/multiprecision/cpp_int.hpp>
-#pragma warning(pop)
-#pragma GCC diagnostic pop
 #include "vector_ref.h"
 
 // CryptoPP defines byte in the global namespace, so must we.
@@ -66,7 +56,6 @@ using byte = uint8_t;
 
 #define DEV_IGNORE_EXCEPTIONS(X) try { X; } catch (...) {}
 
-#define DEV_IF_NO_ELSE(X) if(!(X)){}else
 #define DEV_IF_THROWS(X) try{X;}catch(...)
 
 namespace dev
@@ -74,7 +63,7 @@ namespace dev
 
 extern char const* Version;
 
-static const std::string EmptyString;
+extern std::string const EmptyString;
 
 // Binary data types.
 using bytes = std::vector<byte>;
@@ -87,8 +76,8 @@ class secure_vector
 public:
 	secure_vector() {}
 	secure_vector(secure_vector<T> const& /*_c*/) = default;  // See https://github.com/ethereum/libweb3core/pull/44
-	explicit secure_vector(unsigned _size): m_data(_size) {}
-	explicit secure_vector(unsigned _size, T _item): m_data(_size, _item) {}
+	explicit secure_vector(size_t _size): m_data(_size) {}
+	explicit secure_vector(size_t _size, T _item): m_data(_size, _item) {}
 	explicit secure_vector(std::vector<T> const& _c): m_data(_c) {}
 	explicit secure_vector(vector_ref<T> _c): m_data(_c.data(), _c.data() + _c.size()) {}
 	explicit secure_vector(vector_ref<const T> _c): m_data(_c.data(), _c.data() + _c.size()) {}
@@ -122,83 +111,23 @@ private:
 
 using bytesSec = secure_vector<byte>;
 
-// Numeric types.
-using bigint = boost::multiprecision::number<boost::multiprecision::cpp_int_backend<>>;
-using u64 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<64, 64, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>;
-using u128 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<128, 128, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>;
-using u256 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<256, 256, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>;
-using s256 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<256, 256, boost::multiprecision::signed_magnitude, boost::multiprecision::unchecked, void>>;
-using u160 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<160, 160, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>;
-using s160 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<160, 160, boost::multiprecision::signed_magnitude, boost::multiprecision::unchecked, void>>;
-using u512 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<512, 512, boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>;
-using s512 =  boost::multiprecision::number<boost::multiprecision::cpp_int_backend<512, 512, boost::multiprecision::signed_magnitude, boost::multiprecision::unchecked, void>>;
-using u256s = std::vector<u256>;
-using u160s = std::vector<u160>;
-using u256Set = std::set<u256>;
-using u160Set = std::set<u160>;
-
 // Map types.
 using StringMap = std::map<std::string, std::string>;
 using BytesMap = std::map<bytes, bytes>;
-using u256Map = std::map<u256, u256>;
 using HexMap = std::map<bytes, bytes>;
 
 // Hash types.
 using StringHashMap = std::unordered_map<std::string, std::string>;
-using u256HashMap = std::unordered_map<u256, u256>;
+//using u256HashMap = std::unordered_map<u256, u256>;
 
 // String types.
 using strings = std::vector<std::string>;
 
 // Fixed-length string types.
 using string32 = std::array<char, 32>;
-static const string32 ZeroString32 = {{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
 
 // Null/Invalid values for convenience.
-static const bytes NullBytes;
-static const std::map<u256, u256> EmptyMapU256U256;
-extern const u256 Invalid256;
-
-/// Interprets @a _u as a two's complement signed number and returns the resulting s256.
-inline s256 u2s(u256 _u)
-{
-	static const bigint c_end = bigint(1) << 256;
-	if (boost::multiprecision::bit_test(_u, 255))
-		return s256(-(c_end - _u));
-	else
-		return s256(_u);
-}
-
-/// @returns the two's complement signed representation of the signed number _u.
-inline u256 s2u(s256 _u)
-{
-	static const bigint c_end = bigint(1) << 256;
-    if (_u >= 0)
-		return u256(_u);
-    else
-		return u256(c_end + _u);
-}
-
-/// Converts given int to a string and appends one of a series of units according to its size.
-std::string inUnits(bigint const& _b, strings const& _units);
-
-/// @returns the smallest n >= 0 such that (1 << n) >= _x
-inline unsigned int toLog2(u256 _x)
-{
-	unsigned ret;
-	for (ret = 0; _x >>= 1; ++ret) {}
-	return ret;
-}
-
-template <size_t n> inline u256 exp10()
-{
-	return exp10<n - 1>() * u256(10);
-}
-
-template <> inline u256 exp10<0>()
-{
-	return u256(1);
-}
+extern bytes const NullBytes;
 
 /// @returns the absolute distance between _a and _b.
 template <class N>
@@ -224,22 +153,6 @@ class HasInvariants
 public:
 	/// Reimplement to specify the invariants.
 	virtual bool invariants() const = 0;
-};
-
-/// RAII checker for invariant assertions.
-class InvariantChecker
-{
-public:
-	InvariantChecker(HasInvariants* _this, char const* _fn, char const* _file, int _line): m_this(_this), m_function(_fn), m_file(_file), m_line(_line) { checkInvariants(_this, _fn , _file, _line, true); }
-	~InvariantChecker() { checkInvariants(m_this, m_function, m_file, m_line, false); }
-	/// Check invariants are met, throw if not.
-	static void checkInvariants(HasInvariants const* _this, char const* _fn, char const* _file, int line, bool _pre);
-
-private:
-	HasInvariants const* m_this;
-	char const* m_function;
-	char const* m_file;
-	int m_line;
 };
 
 /// Scope guard for invariant check in a class derived from HasInvariants.
@@ -310,25 +223,5 @@ enum class WithExisting: int
 
 /// Get the current time in seconds since the epoch in UTC
 uint64_t utcTime();
-
-}
-
-namespace std
-{
-
-inline dev::WithExisting max(dev::WithExisting _a, dev::WithExisting _b)
-{
-	return static_cast<dev::WithExisting>(max(static_cast<int>(_a), static_cast<int>(_b)));
-}
-
-template <> struct hash<dev::u256>
-{
-	size_t operator()(dev::u256 const& _a) const
-	{
-		unsigned size = _a.backend().size();
-		auto limbs = _a.backend().limbs();
-		return boost::hash_range(limbs, limbs + size);
-	}
-};
 
 }
