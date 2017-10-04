5a51e8dc787eb3546129b45ca9e9e9259c2d771a
atraczyk <andreastraczyk@gmail.com>
2017-10-04 17:46:44 -0400

    contrib(UWP): bump pthreads, remove uwp flags and illegal symbols
    Change-Id: I3f43a066d86d284c614521fdb016810148590e70

--- contrib/src/pthreads/fetch_and_patch.bat
+++ contrib/src/pthreads/fetch_and_patch.bat
@@ -1,19 +1,20 @@
 set BUILD=%SRC%..\build
 
-set PTHREADS_URL="ftp://sourceware.org/pub/pthreads-win32/pthreads-w32-2-9-1-release.zip"
+set PTHREADS_VERSION="pthreads4w-code-v2.10.0-rc"
+set PTHREADS_URL="https://sourceforge.net/projects/pthreads4w/files/%PTHREADS_VERSION%.zip"
 
 mkdir %BUILD%
 
 if %USE_CACHE%==1 (
-    copy %CACHE_DIR%\pthreads-w32-2-9-1-release.zip %cd%
+    copy %CACHE_DIR%\%PTHREADS_VERSION%.zip %cd%
 ) else (
     wget %PTHREADS_URL%
 )
 
-7z -y x pthreads-w32-2-9-1-release.zip -o%BUILD%
-del pthreads-w32-2-9-1-release.zip
+7z -y x %PTHREADS_VERSION%.zip -o%BUILD%
+del %PTHREADS_VERSION%.zip
 rmdir /Q /S %BUILD%\QueueUserAPCEx && rmdir /Q /S %BUILD%\Pre-built.2
-rename %BUILD%\pthreads.2 pthreads
+rename %BUILD%\pthreads4w-code-02fecc211d626f28e05ecbb0c10f739bd36d6442 pthreads
 
 cd %BUILD%\pthreads
 
