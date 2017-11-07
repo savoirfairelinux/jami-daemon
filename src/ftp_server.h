7b5692aeeef2c1fad67882ddf139f44f63b6144a
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2017-11-07 14:57:17 -0500

    data transfer: first implementation
    First implementation of Reliable Data Transfer feature in Ring.
    
    This implementation is a draft, comes with a Python script
    tools/dringctrl/sendfile.py to play with and doesn't
    implement all the API as described into "datatransfer: API proposal"
    commit.
    Transfers require a TURN server with TCP peer connections
    as described by the RFC-6062.
    
    DATA ARE NOT ENCRYPTED IN THIS VERSION.
    DO NOT SEND SENSIBLE DATA WITH IT.
    
    Testing: Currently only sendFile API is implemented and data are
    saved into a temporary file saved in "/tmp/ring_XXXXXX",
    where XXXXXX are replace by mkstemp() command.
    
    Change-Id: I5b8f48432edd58df5046e368a99f58ea44046dcd

---
+++ src/ftp_server.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "peer_connection.h"
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+namespace ring {
+
+class FtpServer final : public Stream
+{
+public:
+    FtpServer();
+
+    bool write(const std::vector<char>& buffer) override;
+    DRing::DataTransferId getId() const override;
+    void close() noexcept override;
+
+private:
+    bool parseStream(const std::vector<char>&);
+    bool parseLine(const std::string&);
+    void handleHeader(const std::string&, const std::string&);
+
+    enum class FtpState {
+        PARSE_HEADERS,
+        READ_DATA,
+    };
+
+    std::ofstream out_;
+    std::size_t fileSize_{0} ;
+    std::size_t rx_ {0};
+    std::stringstream headerStream_;
+    std::array<char, 1000> line_;
+    FtpState state_ {FtpState::PARSE_HEADERS};
+};
+
+} // namespace ring
