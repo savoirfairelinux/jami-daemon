9a3df1ac802271023f829f9089ef6277b9145d29
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2017-11-20 14:49:06 -0500

    data transfer: first implementation
    First implementation of Reliable Data Transfer feature in Ring.
    
    This implementation is a draft, comes with a Python script
    tools/dringctrl/sendfile.py to play with and doesn't
    implement all the API as described into "datatransfer: API proposal"
    commit.
    This version uses TLS over TCP-TURN sockets to encrypt data.
    
    Transfers require a TURN server with TCP peer connections
    as described by the RFC-6062.
    
    EVEN IF DATA ARE ENCRYPTED IN THIS VERSION,
    TLS CERTIFICATES ARE NOT CHECKED YET.
    DO NOT SEND SENSIBLE DATA WITH IT.
    
    Testing: Currently only sendFile API is implemented and data are
    saved into a temporary file saved in "/tmp/ring_XXXXXX",
    where XXXXXX are replace by mkstemp() command.
    
    Change-Id: I5b8f48432edd58df5046e368a99f58ea44046dcd

---
+++ src/peer_connection.h
@@ -0,0 +1,188 @@
+/*
+ *  Copyright (C) 2017 Savoir-faire Linux Inc.
+ *
+ *  Author: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "dring/datatransfer_interface.h"
+#include "ip_utils.h"
+#include "generic_io.h"
+#include "security/diffie-hellman.h"
+
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <future>
+#include <utility>
+
+namespace dht { namespace crypto {
+struct PrivateKey;
+struct Certificate;
+}}
+
+namespace ring {
+
+class Account;
+class TurnTransport;
+class ConnectedTurnTransport;
+
+//==============================================================================
+
+class Stream
+{
+public:
+    virtual ~Stream() { close(); }
+    virtual void close() noexcept { }
+    virtual DRing::DataTransferId getId() const = 0;
+    virtual bool read(std::vector<uint8_t>& buffer) const {
+        (void)buffer;
+        return false;
+    }
+    virtual bool write(const std::vector<uint8_t>& buffer) {
+        (void)buffer;
+        return false;
+    };
+};
+
+//==============================================================================
+
+/// Implement a server TLS session IO over a client TURN connection
+class TlsTurnEndpoint : public GenericSocket<uint8_t>
+{
+public:
+    using SocketType = GenericSocket<uint8_t>;
+    using Identity = std::pair<std::shared_ptr<dht::crypto::PrivateKey>,
+                               std::shared_ptr<dht::crypto::Certificate>>;
+
+    TlsTurnEndpoint(ConnectedTurnTransport& turn,
+                    const Identity& local_identity,
+                    const std::shared_future<tls::DhParams>& dh_params);
+    ~TlsTurnEndpoint();
+
+    bool isReliable() const override { return true; }
+    bool isInitiator() const override;
+    int maxPayload() const override;
+    std::size_t read(ValueType* buf, std::size_t len, std::error_code& ec) override;
+    std::size_t write(const ValueType* buf, std::size_t len, std::error_code& ec) override;
+
+    void setOnRecv(RecvCb&&) override {
+        throw std::logic_error("TlsTurnEndpoint::setOnRecv not implemented");
+    }
+    bool waitForData(unsigned) const override {
+        throw std::logic_error("TlsTurnEndpoint::waitForData not implemented");
+    }
+
+    void connect();
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+//==============================================================================
+
+/// Implement system socket IO
+class TcpSocketEndpoint : public GenericSocket<uint8_t>
+{
+public:
+    using SocketType = GenericSocket<uint8_t>;
+    explicit TcpSocketEndpoint(const IpAddr& addr);
+    ~TcpSocketEndpoint();
+
+    bool isReliable() const override { return true; }
+    bool isInitiator() const override { return false; }
+    int maxPayload() const override { return 1280; }
+    bool waitForData(unsigned ms_timeout) const override;
+    std::size_t read(ValueType* buf, std::size_t len, std::error_code& ec) override;
+    std::size_t write(const ValueType* buf, std::size_t len, std::error_code& ec) override;
+
+    void setOnRecv(RecvCb&&) override {
+        throw std::logic_error("TcpSocketEndpoint::setOnRecv not implemented");
+    }
+
+    void connect();
+
+private:
+    const IpAddr addr_;
+    int sock_ {-1};
+};
+
+//==============================================================================
+
+/// Implement a TLS session IO over a system socket
+class TlsSocketEndpoint : public GenericSocket<uint8_t>
+{
+public:
+    using SocketType = GenericSocket<uint8_t>;
+    using Identity = std::pair<std::shared_ptr<dht::crypto::PrivateKey>,
+                               std::shared_ptr<dht::crypto::Certificate>>;
+
+    TlsSocketEndpoint(TcpSocketEndpoint& parent,
+                      const Identity& local_identity,
+                      const std::shared_future<tls::DhParams>& dh_params);
+    ~TlsSocketEndpoint();
+
+    bool isReliable() const override { return true; }
+    bool isInitiator() const override { return false; }
+    int maxPayload() const override { return 1280; }
+    std::size_t read(ValueType* buf, std::size_t len, std::error_code& ec) override;
+    std::size_t write(const ValueType* buf, std::size_t len, std::error_code& ec) override;
+
+    void setOnRecv(RecvCb&&) override {
+        throw std::logic_error("TlsSocketEndpoint::setOnRecv not implemented");
+    }
+    bool waitForData(unsigned) const override {
+        throw std::logic_error("TlsSocketEndpoint::waitForData not implemented");
+    }
+
+    void connect();
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+//==============================================================================
+
+class PeerConnection
+{
+public:
+    using SocketType = GenericSocket<uint8_t>;
+
+    PeerConnection(Account& account, const std::string& peer_uri,
+                   std::unique_ptr<SocketType> endpoint);
+
+    ~PeerConnection();
+
+    void attachOutputStream(const std::shared_ptr<Stream>& stream);
+
+    void attachInputStream(const std::shared_ptr<Stream>& stream);
+
+    void refuseStream(const DRing::DataTransferId& id);
+
+    void abortStream(const DRing::DataTransferId& id);
+
+private:
+    class PeerConnectionImpl;
+    std::unique_ptr<PeerConnectionImpl> pimpl_;
+};
+
+} // namespace ring
