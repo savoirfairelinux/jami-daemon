8e9709a8025c0230ce90d778fa2bf89cd01e17ba
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-11-23 10:53:01 -0500

    [WIP]ringaccount: add the ability to use a proxy node for ring
    TODO: bump opendht version.
    
    + Bump restbed for Opendht, update patch and add patch to react
    to timeout and socket closed in restbed.
    + add startProxyClient and stopProxyClient in ringaccount and in
    configuration manager (ready for push notifications).
    
    Change-Id: I8d3a02cf54a9666a94ab992ee3fa2958e82aa7ad

--- src/ringdht/ringaccount.cpp
+++ src/ringdht/ringaccount.cpp
@@ -1833,6 +1833,11 @@ RingAccount::doRegister()
      || registrationState_ == RegistrationState::ERROR_NEED_MIGRATION)
         return;
 
+    if (proxyEnabled_)
+        startProxyClient(proxyServer_/*, deviceKey_*/);
+    else
+        stopProxyClient();
+
     if (not dhParams_.valid()) {
         generateDhParams();
     }
@@ -1850,7 +1855,6 @@ RingAccount::doRegister()
         }}.detach();
     } else
         doRegister_();
-
 }
 
 
@@ -3307,5 +3311,19 @@ RingAccount::registerDhtAddress(IceTransport& ice)
         reg_addr(ice, ip);
     }
 }
+
+void
+RingAccount::startProxyClient(const std::string& address, const std::string& /*deviceKey*/)
+{
+    dht_.setProxyServer(address/*, deviceKey */); // NOTE deviceKey will be enabled for push notifications
+    dht_.enableProxy(true);
+}
+
+void
+RingAccount::stopProxyClient()
+{
+    dht_.enableProxy(false);
+}
+
 
 } // namespace ring
