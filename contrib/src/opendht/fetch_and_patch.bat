8b700b420d8c02014f1899c84d091323a6546446
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-11-23 10:53:01 -0500

    [WIP]ringaccount: add the ability to use a proxy node for ring
    TODO: bump opendht version.
    
    + Bump restbed for Opendht, update patch and add patch to react
    to timeout and socket closed in restbed.
    + add startProxyClient and stopProxyClient in ringaccount and in
    configuration manager (ready for push notifications).
    
    Change-Id: I8d3a02cf54a9666a94ab992ee3fa2958e82aa7ad

--- contrib/src/opendht/fetch_and_patch.bat
+++ contrib/src/opendht/fetch_and_patch.bat
@@ -1,7 +1,7 @@
 set BUILD=%SRC%..\build
 
-set OPENDHT_VERSION=1.5.0
-set OPENDHT_URL=https://github.com/savoirfairelinux/opendht/archive/%OPENDHT_VERSION%.tar.gz
+set OPENDHT_VERSION=4236f65bd47a54e4d25ebd8a5e6234adaca6afaf
+set OPENDHT_URL=https://github.com/AmarOk1412/opendht/archive/%OPENDHT_VERSION%.tar.gz
 
 mkdir %BUILD%
 
