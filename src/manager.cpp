202ed7b8a3fb57e17a36f73d500f97253a3492d0
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-02-20 15:38:55 -0500

    account: fix potential deadlock during setAccountDetails
    Change-Id: I6e4feb76fe6dc4242797ffc7a466c9114313da3e

--- src/manager.cpp
+++ src/manager.cpp
@@ -2651,6 +2651,8 @@ Manager::setAccountDetails(const std::string& accountID,
 
         if (account->isUsable())
             account->doRegister();
+        else
+            account->doUnregister();
 
         // Update account details to the client side
         emitSignal<DRing::ConfigurationSignal::AccountDetailsChanged>(accountID, details);
