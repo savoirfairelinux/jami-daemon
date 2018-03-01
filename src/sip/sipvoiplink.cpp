e31a1ec4efe1ba352bc7fe120d770c20d31bb445
Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
2018-03-01 10:51:24 -0500

    pjsip bump to 2.7.2
    Change-Id: I4512dad14927f01f56d6bf4472c3e504157f1b76

--- src/sip/sipvoiplink.cpp
+++ src/sip/sipvoiplink.cpp
@@ -567,7 +567,9 @@ SIPVoIPLink::SIPVoIPLink() : pool_(nullptr, pj_pool_release)
         outgoing_request_forked_cb,
         transaction_state_changed_cb,
         sdp_request_offer_cb,
-#if PJ_VERSION_NUM > (2 << 24 | 1 << 16)
+#if PJ_VERSION_NUM > (2 << 24 | 7 << 16)
+        nullptr /* on_rx_offer2 */,
+#elif PJ_VERSION_NUM > (2 << 24 | 1 << 16)
         nullptr /* on_rx_reinvite */,
 #endif
         sdp_create_offer_cb,
@@ -1334,7 +1336,11 @@ SIPVoIPLink::findLocalAddressFromSTUN(pjsip_transport* transport,
 
     pj_sockaddr_in mapped_addr;
     pj_sock_t sipSocket = pjsip_udp_transport_get_socket(transport);
-    const pjstun_setting stunOpt = {PJ_TRUE, *stunServerName, stunPort,
+    const pjstun_setting stunOpt = {PJ_TRUE,
+#if PJ_VERSION_NUM > (2 << 24 | 7 << 16)
+                                    localIp.getFamily(),
+#endif
+                                    *stunServerName, stunPort,
                                     *stunServerName, stunPort};
     const pj_status_t stunStatus = pjstun_get_mapped_addr2(&cp_.factory,
                                                            &stunOpt, 1,
