3be430165dab88a5c491b48185892594304f7b0f
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2018-03-06 16:17:06 -0500

    namedirectory: use https instead of plain http requests
    Add libressl dependency to compile restbed with ssl support. Also,
    this patch removes a unused file.
    Bump asio to works with libressl instead of openssl.
    
    Change-Id: I5a90dba97693ccda58899f69080e545bdea926d9
    Signed-off-by: Sébastien Blin <sebastien.blin@savoirfairelinux.com>

--- contrib/src/restbed/fetch_and_patch.bat
+++ contrib/src/restbed/fetch_and_patch.bat
@@ -11,6 +11,8 @@ if %USE_CACHE%==1 (
     wget %RESTBED_URL%
 )
 
+rem ------------ restbed ------------
+
 7z -y x %RESTBED_VERSION%.tar.gz && 7z -y x %RESTBED_VERSION%.tar -o%BUILD%
 del %RESTBED_VERSION%.tar && del %RESTBED_VERSION%.tar.gz && del %BUILD%\pax_global_header
 rename %BUILD%\restbed-%RESTBED_VERSION% restbed
@@ -24,6 +26,8 @@ cd ..
 rmdir /s /q %BUILD%\restbed\dependency
 mkdir %BUILD%\restbed\dependency
 cd %BUILD%\restbed\dependency
+
+rem ------------ asio ------------
 
 set ASIO_VERSION=276846097ab5073b67e772dbdfa12596224a54a5
 set ASIO_URL=https://github.com/Corvusoft/asio-dependency/archive/%ASIO_VERSION%.tar.gz
@@ -40,8 +44,9 @@ rename asio-dependency-%ASIO_VERSION% asio
 
 cd asio
 git apply --reject --whitespace=fix %SRC%\restbed\asio-uwp.patch
-
 cd ..
+
+rem ------------ catch ------------
 
 set CATCH_VERSION=35f510545d55a831372d3113747bf1314ff4f2ef
 set CATCH_URL=https://github.com/Corvusoft/catch-dependency/archive/%CATCH_VERSION%.tar.gz
@@ -55,6 +60,23 @@ if %USE_CACHE%==1 (
 7z -y x %CATCH_VERSION%.tar.gz && 7z -y x %CATCH_VERSION%.tar
 del %CATCH_VERSION%.tar && del %CATCH_VERSION%.tar.gz && del pax_global_header
 rename catch-dependency-%CATCH_VERSION% catch
+
+rem ------------ openssl ------------
+
+set OPENSSL_VERSION=c7ba244789ce9f9b6675ff88e61dd5d5e5cac53e
+set OPENSSL_URL=https://github.com/Microsoft/openssl/archive/%OPENSSL_VERSION%.tar.gz
+
+if %USE_CACHE%==1 (
+    copy %CACHE_DIR%\%OPENSSL_VERSION%.tar.gz %cd%
+) else (
+    wget %OPENSSL_URL%
+)
+
+7z -y x %OPENSSL_VERSION%.tar.gz && 7z -y x %OPENSSL_VERSION%.tar
+del %OPENSSL_VERSION%.tar && del %OPENSSL_VERSION%.tar.gz && del pax_global_header
+rename openssl-%OPENSSL_VERSION% openssl
+
+rem ------------ kashmir ------------
 
 set KASHMIR_VERSION=2f3913f49c4ac7f9bff9224db5178f6f8f0ff3ee
 set KASHMIR_URL=https://github.com/corvusoft/kashmir-dependency/archive/%KASHMIR_VERSION%.tar.gz
