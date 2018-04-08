4f4918e96fcc0be29b6541339a951ba53068316a
philippegorley <philippe.gorley@savoirfairelinux.com>
2018-04-08 18:42:13 -0400

    media: add video recording support
    Adds video recording. Changes the internals of audio recording to use FFmpeg.
    Ring now muxes all available streams into one mkv file.
    
    Currently, there is no mixing of the video or audio.
    
    Change-Id: Ib8ee63ac71910782fb21d73d7da5fa5d19893b8e

---
+++ src/media/media_recorder.h
@@ -0,0 +1,94 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "config.h"
+#include "noncopyable.h"
+
+#include <map>
+#include <mutex>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+class AVCodecContext;
+class AVFormatContext;
+class AVStream;
+struct AVPacket;
+struct AVFrame;
+
+namespace ring {
+
+class MediaRecorder {
+    public:
+        MediaRecorder();
+        ~MediaRecorder();
+
+        std::string getFilename() const;
+
+        void audioOnly(bool audioOnly);
+
+        void setRecordingPath(const std::string& dir);
+
+        // adjust nb of streams before recording
+        // used to know when all streams are set up
+        void incrementStreams(int n);
+
+        bool isRecording() const;
+
+        bool toggleRecording();
+
+        int startRecording();
+
+        void stopRecording();
+
+        int copyStream(AVStream* input, AVPacket* packet, bool fromPeer, bool isVideo);
+
+        int recordData(AVPacket* packet, bool fromPeer, bool isVideo);
+
+        int recordData(AVFrame* frame, bool fromPeer, bool isVideo);
+
+    private:
+        NON_COPYABLE(MediaRecorder);
+
+        int recordData(AVPacket* packet, int streamIdx);
+        int recordData(AVFrame* frame, int streamIdx);
+        int flush();
+
+        AVFormatContext* outputCtx_ = nullptr;
+        std::mutex mutex_; // protect against concurrent file writes
+        std::map<std::pair<bool, bool>, int> streamMap_; // map fromPeer and isVideo to a stream index
+        std::map<int, int64_t> nextTimestamp_;
+        std::map<int, int64_t> lastDuration_;
+        std::map<int, bool> skipTSIncrement_;
+        std::map<int, AVCodecContext*> encoders_;
+
+        std::string dir_;
+        std::string filename_;
+        std::string ext_;
+
+        unsigned nbStreams_ = 0;
+        bool isRecording_ = false;
+        bool isReady_ = false;
+        bool audioOnly_ = false;
+};
+
+}; // namespace ring
