2f5bad15ddc06d8408eca37a5bd6fb0201dba447
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-06-04 10:36:46 -0400

    localrecorder: add public interface
    Initial import of the localrecorder.
    
    The local recorder provides a public interface for recording local
    video and sound files. For instance this can be used to record a
    voice/video message that could be later sent via file transfer.
    
    TODO: audio only local recording not supported yet.
    
    This patch also implements the startDecoder method in VideoInput.
    
    Change-Id: I0675fc0fb8588db24ae86302a76d4d68f5871b82

---
+++ src/media/localrecorder.h
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "media/video/video_input.h"
+#include "recordable.h"
+
+namespace ring {
+
+/*
+ * @file localrecorder.h
+ * @brief Class for recording messages locally
+ */
+
+/*
+ * The LocalRecorder class exposes the Recordable interface for
+ * recording messages locally.
+ */
+
+class LocalRecorder : public Recordable {
+    public:
+
+        /**
+         * Constructor of a LocalRecorder.
+         * Passed VideoInput pointer will be used for recording.
+         */
+        LocalRecorder(std::shared_ptr<ring::video::VideoInput> input);
+
+        /**
+         * Start local recording. Return true if recording was successfully
+         * started, false otherwise.
+         */
+        bool startRecording();
+
+        void stopRecording();
+
+    private:
+        std::weak_ptr<ring::video::VideoInput> videoInput_;
+};
+
+} // namespace ring
