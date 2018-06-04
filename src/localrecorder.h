9920f00eed6b12b86f4bbb23027c8bc5ff96ed6c
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-06-04 10:36:46 -0400

    localrecorder: add public interface
    Initial import of the localrecorder.
    
    The local recorder provides a public interface for recording local
    video and sound files. For instance this can be used to record a
    voice/video message that could be later sent via file transfer.
    
    This patch also implements the startDecoder method in VideoInput.
    
    Change-Id: I0675fc0fb8588db24ae86302a76d4d68f5871b82

---
+++ src/localrecorder.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "logger.h"
+
+#include "recordable.h"
+
+namespace ring {
+
+/*
+ * @file localrecorder.h
+ * @brief Class for recording messages locally
+ */
+
+/*
+ * The LocalRecorder class exposes the Recordable interface for
+ * recording messages locally. TODO: Explain why empty ?
+ */
+
+class LocalRecorder : public Recordable {
+    protected:
+
+        /**
+         * Constructor of a LocalRecorder
+         */
+        LocalRecorder(); // TODO
+};
+
+} // namespace ring
