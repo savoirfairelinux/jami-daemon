500e212ff9ea04de3b2dac0e61218eaefcdd1aac
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-06-04 10:36:46 -0400

    localrecorder: add public interface
    Initial import of the localrecorder.
    
    The local recorder provides a public interface for recording local
    video and sound files. For instance this can be used to record a
    voice/video message that could be later sent via file transfer.
    
    TODO: audio only local recording not supported yet.
    
    This patch also implements the startDecoder method in VideoInput.
    
    Change-Id: I0675fc0fb8588db24ae86302a76d4d68f5871b82

---
+++ src/media/localrecorder.cpp
@@ -0,0 +1,93 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "localrecorder.h"
+#include "audio/ringbufferpool.h"
+#include "audio/ringbuffer.h"
+#include "media_stream.h"
+#include "manager.h"
+#include "logger.h"
+
+namespace ring {
+
+LocalRecorder::LocalRecorder(std::shared_ptr<ring::video::VideoInput> input):
+    audioInput_(new ring::audio::AudioInput(RingBufferPool::DEFAULT_ID, false)) {
+    if (input) {
+        videoInput_ = input;
+        videoInputSet_ = true;
+    }
+}
+
+void
+LocalRecorder::setPath(const std::string& path)
+{
+    // TODO race condition
+    if (isRecording()) {
+        RING_ERR("can't set path while recording");
+        return;
+    }
+
+    if (!path.empty())
+        path_ = path;
+}
+
+bool
+LocalRecorder::startRecording()
+{
+    if (isRecording()) {
+        RING_ERR("recording already started!");
+        return false;
+    }
+
+    if (path_.empty()) {
+        RING_WARN("could not start recording: path not set");
+        return false;
+    }
+
+    const bool startRecording = Recordable::startRecording(path_);
+    if (startRecording && recorder_) {
+        // audio recording
+        audioInput_->initRecorder(recorder_);
+
+#ifdef RING_VIDEO
+        // video recording
+        if (!isAudioOnly_) {
+            if (videoInputSet_) {
+                auto videoInputShpnt = videoInput_.lock();
+                videoInputShpnt->initRecorder(recorder_);
+            } else {
+                RING_ERR("can't record video: video input pointer is null");
+            }
+        }
+#endif
+    } else {
+        RING_WARN("could not start recording");
+    }
+
+    return startRecording;
+}
+
+void
+LocalRecorder::stopRecording()
+{
+    Recordable::stopRecording();
+}
+
+} // namespace ring
