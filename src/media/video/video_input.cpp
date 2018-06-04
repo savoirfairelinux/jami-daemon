88054089f31ac28749e5ad0d1ca09fcc69b8150d
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-06-04 10:36:46 -0400

    localrecorder: add public interface
    Initial import of the localrecorder.
    
    The local recorder provides a public interface for recording local
    video and sound files. For instance this can be used to record a
    voice/video message that could be later sent via file transfer.
    
    This patch also implements the startDecoder method in VideoInput.
    
    Change-Id: I0675fc0fb8588db24ae86302a76d4d68f5871b82

--- src/media/video/video_input.cpp
+++ src/media/video/video_input.cpp
@@ -589,4 +589,11 @@ VideoInput::foundDecOpts(const DeviceParams& params)
     }
 }
 
+void
+VideoInput::startRecorder(std::shared_ptr<ring::MediaRecorder>& rec)
+{
+    if (decoder_)
+        decoder_->startRecorder(rec);
+}
+
 }} // namespace ring::video
