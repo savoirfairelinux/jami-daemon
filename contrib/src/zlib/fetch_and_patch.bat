3a80eeaa23a8a591c0378fe0fa432f0aac26847a
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-06-05 12:19:37 -0400

    DRAFT: migrate to vs2017
    Change-Id: If991ec828c34a6b51c1aa12e0f316abf76a5c781

--- contrib/src/zlib/fetch_and_patch.bat
+++ contrib/src/zlib/fetch_and_patch.bat
@@ -1,6 +1,6 @@
 set BUILD=%SRC%..\build
 
-set ZLIB_VERSION=3a062eb61d0c3d4aa30851cd1a6597b977b56597
+set ZLIB_VERSION=8e4e3ead55cdd296130242d86b44b92fde3ea4d4
 set ZLIB_URL=https://github.com/ShiftMediaProject/zlib/archive/%ZLIB_VERSION%.tar.gz
 
 mkdir %BUILD%
@@ -14,9 +14,5 @@ if %USE_CACHE%==1 (
 7z -y x %ZLIB_VERSION%.tar.gz && 7z -y x %ZLIB_VERSION%.tar -o%BUILD%
 del %ZLIB_VERSION%.tar && del %ZLIB_VERSION%.tar.gz && del %BUILD%\pax_global_header
 rename %BUILD%\zlib-%ZLIB_VERSION% zlib
-
-cd %BUILD%\zlib
-
-git apply --reject --whitespace=fix %SRC%\zlib\zlib-uwp.patch
 
 cd %SRC%
