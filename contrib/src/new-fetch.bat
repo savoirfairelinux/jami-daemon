3a9382f1d19a3773a38623b1c2bcd89e8d3491e8
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-06-05 12:19:37 -0400

    DRAFT: migrate to vs2017
    Change-Id: If991ec828c34a6b51c1aa12e0f316abf76a5c781

--- contrib/src/new-fetch.bat
+++ contrib/src/new-fetch.bat
@@ -2,20 +2,21 @@
 SETLOCAL EnableDelayedExpansion
 
 if "%USE_CACHE%"=="" (
-	set USE_CACHE=0
+    set USE_CACHE=0
 )
- 
+
 set SRC=%~dp0
 
 set DEPENDENCIES=( ^
-argon2, ^
 ffmpeg, ^
+argon2, ^
+zlib ^
 gmp, ^
-gnutls, ^
 iconv, ^
 jsoncpp, ^
 msgpack, ^
 nettle, ^
+gnutls, ^
 opendht, ^
 opus, ^
 pcre, ^
@@ -28,8 +29,11 @@ secp256k1, ^
 upnp, ^
 x264, ^
 yaml-cpp, ^
-zlib ^
 )
+
+set WGET_CMD=wget --no-check-certificate
+set PATCH_CMD=patch -flp1 -i
+set APPLY_CMD=git apply --reject --ignore-whitespace --whitespace=fix
 
 for %%I in %DEPENDENCIES% do (
     call %SRC%\%%I\fetch_and_patch.bat
