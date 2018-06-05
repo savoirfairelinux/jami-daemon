ed68875fc505a82c7f9ac20a5275be2cca344dde
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-06-05 12:19:37 -0400

    DRAFT: migrate to vs2017
    Change-Id: If991ec828c34a6b51c1aa12e0f316abf76a5c781

--- contrib/src/ffmpeg/fetch_and_patch.bat
+++ contrib/src/ffmpeg/fetch_and_patch.bat
@@ -1,37 +1,22 @@
 set BUILD=%SRC%..\build
 
-set FFMPEGINTEROP_VERSION=10879040235da3a2393ff19a7a6dc3d7f791314a
-set FFMPEGINTEROP_URL=https://github.com/Microsoft/FFmpegInterop/archive/%FFMPEGINTEROP_VERSION%.tar.gz
-
 mkdir %BUILD%
+cd %BUILD%
 
-if %USE_CACHE%==1 (
-    copy %CACHE_DIR%\%FFMPEGINTEROP_VERSION%.tar.gz %cd%
-) else (
-    wget %FFMPEGINTEROP_URL%
-)
-
-7z -y x %FFMPEGINTEROP_VERSION%.tar.gz && 7z -y x %FFMPEGINTEROP_VERSION%.tar -o%BUILD%
-del %FFMPEGINTEROP_VERSION%.tar && del %FFMPEGINTEROP_VERSION%.tar.gz && del %BUILD%\pax_global_header
-rename %BUILD%\FFmpegInterop-%FFMPEGINTEROP_VERSION% FFmpegInterop
-
-cd %BUILD%\FFmpegInterop
-
-rmdir /s /q ffmpeg
-
-set FFMPEG_VERSION=6d7192bcb7bbab17dc194e8dbb56c208bced0a92
+set FFMPEG_VERSION=5ee203076fa1b1b5da32f525f2b6df3bd5e93b09
 set FFMPEG_URL=https://github.com/FFmpeg/FFmpeg/archive/%FFMPEG_VERSION%.tar.gz
 
 if %USE_CACHE%==1 (
     copy %CACHE_DIR%\%FFMPEG_VERSION%.tar.gz %cd%
 ) else (
-    wget %FFMPEG_URL%
+    wget --no-check-certificate %FFMPEG_URL%
 )
 
 7z -y x %FFMPEG_VERSION%.tar.gz && 7z -y x %FFMPEG_VERSION%.tar
 del %FFMPEG_VERSION%.tar && del %FFMPEG_VERSION%.tar.gz && del pax_global_header
 rename FFmpeg-%FFMPEG_VERSION% ffmpeg
 
-git apply --reject --whitespace=fix %SRC%\ffmpeg\ffmpeg-uwp.patch
+cd ffmpeg
+git apply --reject --whitespace=fix %SRC%\ffmpeg\windows-no-pkg-config.patch
 
 cd %SRC%
---
+++ contrib/src/new-fetch.bat
@@ -0,0 +1,18 @@
+@echo OFF
+SETLOCAL EnableDelayedExpansion
+
+if "%USE_CACHE%"=="" (
+	set USE_CACHE=0
+)
+ 
+set SRC=%~dp0
+
+set DEPENDENCIES=( ^
+opendht, ^
+)
+
+set WGET_CMD=wget --no-check-certificate
+
+for %%I in %DEPENDENCIES% do (
+    call %SRC%\%%I\fetch_and_patch.bat
+)
