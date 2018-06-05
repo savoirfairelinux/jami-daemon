2de82beb0a5c1a2c72f62199d45466a04eaa71eb
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-06-05 12:19:37 -0400

    project: migrate to vs2017
    Change-Id: If991ec828c34a6b51c1aa12e0f316abf76a5c781

--- contrib/src/pthreads/fetch_and_patch.bat
+++ contrib/src/pthreads/fetch_and_patch.bat
@@ -1,6 +1,7 @@
 set BUILD=%SRC%..\build
 
 set PTHREADS_VERSION="pthreads4w-code-v2.10.0-rc"
+set PTHREADS_VERSION2="pthreads4w-code-02fecc211d626f28e05ecbb0c10f739bd36d6442"
 set PTHREADS_URL="https://sourceforge.net/projects/pthreads4w/files/%PTHREADS_VERSION%.zip"
 
 mkdir %BUILD%
@@ -8,16 +9,17 @@ mkdir %BUILD%
 if %USE_CACHE%==1 (
     copy %CACHE_DIR%\%PTHREADS_VERSION%.zip %cd%
 ) else (
-    wget %PTHREADS_URL%
+    %WGET_CMD% %PTHREADS_URL%
 )
 
 7z -y x %PTHREADS_VERSION%.zip -o%BUILD%
 del %PTHREADS_VERSION%.zip
-rmdir /Q /S %BUILD%\QueueUserAPCEx && rmdir /Q /S %BUILD%\Pre-built.2
-rename %BUILD%\pthreads4w-code-02fecc211d626f28e05ecbb0c10f739bd36d6442 pthreads
+rename %BUILD%\%PTHREADS_VERSION2% pthreads
 
 cd %BUILD%\pthreads
 
-git apply --reject --whitespace=fix %SRC%\pthreads\pthreads-uwp.patch
+%APPLY_CMD% %SRC%\pthreads\pthreads-windows.patch
+%APPLY_CMD% %SRC%\pthreads\pthreads-uwp.patch
+%APPLY_CMD% %SRC%\pthreads\pthreads-vs2017.patch
 
 cd %SRC%
