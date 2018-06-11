56332513ff26dd7ecad33476fa14424d97d6a62a
philippegorley <philippe.gorley@savoirfairelinux.com>
2018-06-11 17:40:01 -0400

    sipcall: add peer uri
    For SIP accounts, peer uri contains a well formed sip uri. For Ring
    accounts, contains "ring:username" if a registered name is found, else
    "ring:ringid".
    
    Change-Id: I55bc46e905adf4cf8fe8ea955a2f9cb13438de7e

--- src/sip/sipcall.cpp
+++ src/sip/sipcall.cpp
@@ -1155,8 +1155,7 @@ SIPCall::toggleRecording()
     const bool startRecording = Call::toggleRecording();
     if (startRecording) {
         std::stringstream ss;
-        ss << "Ring call between " << getSIPAccount().getUserUri() << " and "
-            << (!peerRegistredName_.empty() ? peerRegistredName_ : getPeerNumber());
+        ss << "Ring call between " << getSIPAccount().getUserUri() << " and " << peerUri_;
         recorder_->setMetadata(ss.str(), ""); // use default description
         if (avformatrtp_)
             avformatrtp_->startRecorder(recorder_);
