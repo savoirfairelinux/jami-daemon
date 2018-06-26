d5f23489dfeca21f3c8fe78d487ba63c03ce2659
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-06-26 10:23:39 -0400

    project: provide cmake path for native windows restbed build
    Change-Id: Idcce4c27fba8d6680ae451afdc07b8ea2b72a471

--- contrib/src/pthreads/fetch_and_patch.bat
+++ contrib/src/pthreads/fetch_and_patch.bat
@@ -3,6 +3,7 @@ set BUILD=%SRC%..\build
 set PTHREADS_VERSION="pthreads4w-code-v2.10.0-rc"
 set PTHREADS_VERSION2="pthreads4w-code-02fecc211d626f28e05ecbb0c10f739bd36d6442"
 set PTHREADS_URL="https://sourceforge.net/projects/pthreads4w/files/%PTHREADS_VERSION%.zip"
+set PTHREADS_URL2="https://cfhcable.dl.sourceforge.net/project/pthreads4w/%PTHREADS_VERSION%.zip"
 
 mkdir %BUILD%
 
@@ -10,6 +11,9 @@ if %USE_CACHE%==1 (
     copy %CACHE_DIR%\%PTHREADS_VERSION%.zip %cd%
 ) else (
     %WGET_CMD% %PTHREADS_URL%
+    if %ERRORLEVEL% neq 0 (
+        %WGET_CMD% %PTHREADS_URL2%
+    )
 )
 
 7z -y x %PTHREADS_VERSION%.zip -o%BUILD%
