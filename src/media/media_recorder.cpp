a6b09410811ce2645ffcf83a018907849b395d6e
philippegorley <philippe.gorley@savoirfairelinux.com>
2018-06-28 15:38:08 -0400

    recorder: better multithreading
    Better scopes mutices and adds multithreading to the filters.
    
    Change-Id: I9f96c1e90d506a8dc4a261a7e07010d42e5bf3ec

--- src/media/media_recorder.cpp
+++ src/media/media_recorder.cpp
@@ -139,12 +139,10 @@ MediaRecorder::startRecording()
 
     if (!frames_.empty()) {
         RING_WARN() << "Frame queue not empty at beginning of recording, frames will be lost";
+        std::lock_guard<std::mutex> q(qLock_);
         while (!frames_.empty())
             frames_.pop();
     }
-
-    if (!loop_.isRunning())
-        loop_.start();
 
     encoder_.reset(new MediaEncoder);
 
@@ -161,6 +159,11 @@ MediaRecorder::stopRecording()
         flush();
     }
     isRecording_ = false;
+    if (loop_.isRunning())
+        loop_.join();
+    videoFilter_.reset();
+    audioFilter_.reset();
+    encoder_.reset();
 }
 
 int
@@ -253,8 +256,6 @@ MediaRecorder::initRecord()
         encoderOptions["channels"] = std::to_string(audioStream.nbChannels);
     }
 
-    std::lock_guard<std::mutex> lk(mutex_); // lock as late as possible
-
     encoder_->openFileOutput(getPath(), encoderOptions);
 
     if (nbReceivedVideoStreams_ > 0) {
@@ -283,6 +284,11 @@ MediaRecorder::initRecord()
     isReady_ = audioIsReady && videoIsReady;
 
     if (isReady_) {
+        std::lock_guard<std::mutex> lk(mutex_); // lock as late as possible
+
+        if (!loop_.isRunning())
+            loop_.start();
+
         std::unique_ptr<MediaIOHandle> ioHandle;
         try {
             encoder_->setIOContext(ioHandle);
@@ -431,8 +437,8 @@ MediaRecorder::emptyFilterGraph()
 int
 MediaRecorder::sendToEncoder(AVFrame* frame, int streamIdx)
 {
-    std::lock_guard<std::mutex> lk(mutex_);
     try {
+        std::lock_guard<std::mutex> lk(mutex_);
         encoder_->encode(frame, streamIdx);
     } catch (const MediaEncoderException& e) {
         RING_ERR() << "MediaEncoderException: " << e.what();
@@ -449,8 +455,6 @@ MediaRecorder::flush()
     if (!isRecording_ || encoder_->getStreamCount() <= 0)
         return 0;
 
-    emptyFilterGraph();
-
     std::lock_guard<std::mutex> lk(mutex_);
     encoder_->flush();
 
@@ -460,14 +464,16 @@ MediaRecorder::flush()
 void
 MediaRecorder::process()
 {
-    std::lock_guard<std::mutex> q(qLock_);
     if (!isRecording_ || !isReady_ || frames_.empty())
         return;
 
     auto recframe = frames_.front();
-    frames_.pop();
-    AVFrame* input = recframe.frame;
+    {
+        std::lock_guard<std::mutex> q(qLock_);
+        frames_.pop();
+    }
 
+    AVFrame* input = recframe.frame;
     int streamIdx = (recframe.isVideo ? videoIdx_ : audioIdx_);
     auto filter = (recframe.isVideo ? videoFilter_.get() : audioFilter_.get());
     if (streamIdx < 0) {
