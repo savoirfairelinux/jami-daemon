ce173ac32f535817606a09b7a934c25386f2bceb
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-08-10 13:22:24 -0400

    audio: implement audio_input class
    There is currently no class allowing retrieval of local audio stream
    without being in a running call. This new audio input class implements
    such a system.
    
    Change-Id: Icdf6a9799f341367b0c50aaff76b8642f42d0d1c

---
+++ src/media/audio/audio_input.cpp
@@ -0,0 +1,132 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "audio_input.h"
+
+#include <future>
+#include "socket_pair.h"
+#include "audio/ringbufferpool.h"
+#include "manager.h"
+#include "smartools.h"
+
+namespace ring { namespace audio {
+
+AudioInput::AudioInput(const std::string& id,
+                       bool muteState) :
+    id_(id),
+    muteState_(muteState),
+    loop_([&] {},
+          std::bind(&AudioInput::process, this),
+          std::bind(&AudioInput::cleanup, this))
+{
+    loop_.start();
+}
+
+AudioInput::~AudioInput()
+{
+    if (auto rec = recorder_.lock())
+        rec->stopRecording();
+    loop_.join();
+}
+
+// seq: frame number for video, sent samples audio
+// sampleFreq: fps for video, sample rate for audio
+// clock: stream time base (packetization interval times)
+// FIXME duplicate code from media encoder
+int64_t
+getNextTimestamp(int64_t seq, rational<int64_t> sampleFreq, rational<int64_t> clock)
+{
+    return (seq / (sampleFreq * clock)).real<int64_t>();
+}
+
+void
+AudioInput::process()
+{
+    auto& mainBuffer = Manager::instance().getRingBufferPool();
+    auto mainBuffFormat = mainBuffer.getInternalAudioFormat();
+
+    // compute nb of byte to get corresponding to 1 audio frame
+    const std::size_t samplesToGet = std::chrono::duration_cast<std::chrono::seconds>(msPerPacket_ * mainBuffFormat.sample_rate).count();
+
+    if (mainBuffer.availableForGet(id_) < samplesToGet
+        && not mainBuffer.waitForDataAvailable(id_, samplesToGet, msPerPacket_)) {
+            return;
+    }
+
+    // get data
+    micData_.setFormat(mainBuffFormat);
+    micData_.resize(samplesToGet);
+    const auto samples = mainBuffer.getData(micData_, id_);
+    if (samples != samplesToGet)
+        return;
+
+    AudioBuffer buffer = micData_;
+
+    if (muteState_) // audio is muted, set samples to 0
+        buffer.reset();
+
+    // record frame
+    AVFrame* frame = buffer.toAVFrame();
+    auto ms = MediaStream("audio", buffer.getFormat());
+    frame->pts = getNextTimestamp(sent_samples, ms.sampleRate, static_cast<rational<int64_t>>(ms.timeBase));
+    sent_samples += frame->nb_samples;
+
+    {
+        auto rec = recorder_.lock();
+        if (rec && !recordingStarted_ && rec->addStream(false, false, ms) >= 0) {
+                recordingStarted_ = true;
+        }
+
+        if (rec && recordingStarted_) {
+            rec->recordData(frame, false, false);
+        } else {
+            recordingStarted_ = false;
+            recorder_ = std::weak_ptr<MediaRecorder>();
+        }
+    }
+}
+
+void
+AudioInput::cleanup()
+{
+    micData_.clear();
+}
+
+void
+AudioInput::setMuted(bool isMuted)
+{
+    muteState_ = isMuted;
+}
+
+std::shared_future<DeviceParams>
+AudioInput::switchInput(const std::string& resource)
+{
+    // TODO
+}
+
+void
+AudioInput::initRecorder(const std::shared_ptr<MediaRecorder>& rec)
+{
+    rec->incrementStreams(1);
+    recorder_ = rec;
+}
+
+}} // namespace ring::audio
