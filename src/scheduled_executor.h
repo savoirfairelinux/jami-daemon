7297a9dae538acda7895e995f3e51d54fd7f04f5
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2018-08-10 16:32:49 -0400

    ScheduledExecutor: add CancelableBag
    Change-Id: Id2de702dcfe74bc645168298825e0eec13354227

--- src/scheduled_executor.h
+++ src/scheduled_executor.h
@@ -28,6 +28,7 @@
 #include <atomic>
 #include <mutex>
 #include <condition_variable>
+#include <queue>
 
 #include "noncopyable.h"
 
@@ -77,6 +78,20 @@ public:
     }
 private:
     RepeatedJob job_;
+};
+
+class CancellableBag {
+    ~CancellableBag() {
+        while (not cancellables_.empty()) {
+            cancellables_.front()->cancel();
+            cancellables_.pop();
+        }
+    }
+    void add(std::shared_ptr<Task> t) {
+        cancellables_.push(std::move(t));
+    };
+private:
+    std::queue<std::shared_ptr<Task>> cancellables_;
 };
 
 class ScheduledExecutor {
