d44389b878a780534bf7dcf2b0e5f34faa760714
Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
2018-08-10 13:22:24 -0400

    audio: implement audio_input class
    There is currently no class allowing retrieval of local audio stream
    without being in a running call. This new audio input class implements
    such a system.
    
    Change-Id: Icdf6a9799f341367b0c50aaff76b8642f42d0d1c

---
+++ src/media/audio/audio_input.h
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Hugo Lefeuvre <hugo.lefeuvre@savoirfairelinux.com>
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include <future>
+#include "audio/audiobuffer.h"
+#include "media/media_recorder.h"
+#include "audio/resampler.h"
+
+namespace ring {
+class MediaRecorder;
+}
+
+namespace ring { namespace audio {
+
+class AudioInput
+{
+public:
+    AudioInput(const std::string& id,
+               bool muteState);
+    ~AudioInput();
+
+    std::shared_future<DeviceParams> switchInput(const std::string& resource);
+
+    void setMuted(bool isMuted);
+    void initRecorder(const std::shared_ptr<MediaRecorder>& rec);
+
+private:
+    std::weak_ptr<MediaRecorder> recorder_;
+    bool recordingStarted_{false};
+    uint64_t sent_samples = 0;
+
+    std::string id_;
+    AudioBuffer micData_;
+    bool muteState_ = false;
+
+    const std::chrono::milliseconds msPerPacket_ {20};
+
+    ThreadLoop loop_;
+    void process();
+    void cleanup();
+};
+
+}} // namespace ring::audio
