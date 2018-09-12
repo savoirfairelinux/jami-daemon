savoirfairelinux/jami-daemon

commit f6d001e3a3d3732b474225f2a0a45468cf96a100

video: use ffmpeg' AVPixelFormat
Change-Id: I019e66f12cf7d704f832bce457b63ca9aaefa15d
Reviewed-by: Philippe Gorley <philippe.gorley@savoirfairelinux.com>

src/media/video/iosvideo/video_device_impl.cpp
@@ -37,9 +37,9 @@ typedef struct
 
 static const std::array<ios_fmt, 4> ios_formats
 {
-    ios_fmt { "RGBA",       VIDEO_PIXFMT_RGBA       },
-    ios_fmt { "BGRA",       VIDEO_PIXFMT_BGRA       },
-    ios_fmt { "YUV420P",    VIDEO_PIXFMT_YUV420P    }
+    ios_fmt { "RGBA",       AV_PIX_FMT_RGBA       },
+    ios_fmt { "BGRA",       AV_PIX_FMT_BGRA       },
+    ios_fmt { "YUV420P",    AV_PIX_FMT_YUV420P    }
 };
 
 class VideoDeviceImpl

src/media/video/uwpvideo/video_device_impl.cpp
@@ -39,10 +39,10 @@ typedef struct
 // have all formats map to bgra
 static const std::array<uwp_fmt, 4> uwp_formats
 {
-    uwp_fmt { "MJPG",   VIDEO_PIXFMT_BGRA   },
-    uwp_fmt { "RGB24",  VIDEO_PIXFMT_BGRA   },
-    uwp_fmt { "NV12",   VIDEO_PIXFMT_BGRA   },
-    uwp_fmt { "YUY2",   VIDEO_PIXFMT_BGRA   }
+    uwp_fmt { "MJPG",   AV_PIX_FMT_BGRA   },
+    uwp_fmt { "RGB24",  AV_PIX_FMT_BGRA   },
+    uwp_fmt { "NV12",   AV_PIX_FMT_BGRA   },
+    uwp_fmt { "YUY2",   AV_PIX_FMT_BGRA   }
 };
 
 class VideoDeviceImpl

src/media/video/androidvideo/video_device_impl.cpp
@@ -41,12 +41,12 @@ namespace ring { namespace video {
 struct android_fmt {
     int             code;
     std::string     name;
-    enum VideoPixelFormat ring_format;
+    int ring_format;
 };
 
 static const std::array<android_fmt, 2> and_formats {
-    android_fmt { 17,           "NV21",     VIDEO_PIXFMT_NV21    },
-    android_fmt { 842094169,    "YUV420",   VIDEO_PIXFMT_YUV420P },
+    android_fmt { 17,           "NV21",     AV_PIX_FMT_NV21    },
+    android_fmt { 842094169,    "YUV420",   AV_PIX_FMT_YUV420P },
 };
 
 class VideoDeviceImpl {

src/media/libav_utils.cpp
@@ -165,27 +165,6 @@ void ring_avcodec_init()
     std::call_once(already_called, init_once);
 }
 
-
-int libav_pixel_format(int fmt)
-{
-    switch (fmt) {
-        case video::VIDEO_PIXFMT_BGRA: return AV_PIX_FMT_BGRA;
-        case video::VIDEO_PIXFMT_RGBA: return AV_PIX_FMT_RGBA;
-        case video::VIDEO_PIXFMT_YUYV422: return AV_PIX_FMT_YUYV422;
-        case video::VIDEO_PIXFMT_YUV420P: return AV_PIX_FMT_YUV420P;
-        case video::VIDEO_PIXFMT_NV21: return AV_PIX_FMT_NV21;
-    }
-    return fmt;
-}
-
-int ring_pixel_format(int fmt)
-{
-    switch (fmt) {
-        case AV_PIX_FMT_YUYV422: return video::VIDEO_PIXFMT_YUYV422;
-    }
-    return fmt;
-}
-
 void ring_url_split(const char *url,
                    char *hostname, size_t hostname_size, int *port,
                    char *path, size_t path_size)

src/media/libav_utils.h
@@ -32,9 +32,6 @@ namespace ring { namespace libav_utils {
 
     void ring_avcodec_init();
 
-    int libav_pixel_format(int fmt);
-    int ring_pixel_format(int fmt);
-
     const char *const DEFAULT_H264_PROFILE_LEVEL_ID = "profile-level-id=428029";
     const char *const MAX_H264_PROFILE_LEVEL_ID = "profile-level-id=640034";
 

src/media/media_buffer.cpp
@@ -70,7 +70,7 @@ VideoFrame::size() const noexcept
 int
 VideoFrame::format() const noexcept
 {
-    return libav_utils::ring_pixel_format(frame_->format);
+    return frame_->format;
 }
 
 int
@@ -88,7 +88,7 @@ VideoFrame::height() const noexcept
 void
 VideoFrame::setGeometry(int format, int width, int height) noexcept
 {
-    frame_->format = libav_utils::libav_pixel_format(format);
+    frame_->format = format;
     frame_->width = width;
     frame_->height = height;
 }
@@ -96,14 +96,13 @@ VideoFrame::setGeometry(int format, int width, int height) noexcept
 void
 VideoFrame::reserve(int format, int width, int height)
 {
-    auto libav_format = (AVPixelFormat)libav_utils::libav_pixel_format(format);
     auto libav_frame = frame_.get();
 
     if (allocated_) {
         // nothing to do if same properties
         if (width == libav_frame->width
             and height == libav_frame->height
-            and libav_format == libav_frame->format)
+            and format == libav_frame->format)
 #if USE_OLD_AVU
         avpicture_free((AVPicture *) libav_frame);
 #else
@@ -167,8 +166,7 @@ VideoFrame::operator =(const VideoFrame& src)
 std::size_t
 videoFrameSize(int format, int width, int height)
 {
-    return av_image_get_buffer_size((AVPixelFormat)libav_utils::libav_pixel_format(format),
-                                    width, height, 1);
+    return av_image_get_buffer_size((AVPixelFormat)format, width, height, 1);
 }
 
 #endif // RING_VIDEO

src/media/media_decoder.cpp
@@ -444,7 +444,7 @@ MediaDecoder::getTimeBase() const
 }
 
 int MediaDecoder::getPixelFormat() const
-{ return libav_utils::ring_pixel_format(decoderCtx_->pix_fmt); }
+{ return decoderCtx_->pix_fmt; }
 
 void
 MediaDecoder::writeToRingBuffer(const AudioFrame& decodedFrame,

src/media/media_encoder.cpp
@@ -278,7 +278,7 @@ MediaEncoder::addStream(const SystemCodecInfo& systemCodecInfo, std::string para
         // allocate buffers for both scaled (pre-encoder) and encoded frames
         const int width = encoderCtx->width;
         const int height = encoderCtx->height;
-        const int format = libav_utils::ring_pixel_format((int)encoderCtx->pix_fmt);
+        const int format = encoderCtx->pix_fmt;
         scaledFrameBufferSize_ = videoFrameSize(format, width, height);
         if (scaledFrameBufferSize_ <= AV_INPUT_BUFFER_MIN_SIZE)
             throw MediaEncoderException("buffer too small");

commit 2210b5403b3bd7d2d722900ff70b5b2653767f8a

build: fix uwp and ios builds
Broken by f6d001e (video: use ffmpeg' AVPixelFormat).

Change-Id: I23f103701b3a9a2e30e7648cb5862ac6ae59ad3b

src/media/video/iosvideo/video_device_impl.cpp
@@ -22,6 +22,10 @@
 
 #include <array>
 
+extern "C" {
+#include <libavutil/pixfmt.h>
+}
+
 #include "logger.h"
 #include "../video_device.h"
 
@@ -32,7 +36,7 @@ namespace ring { namespace video {
 typedef struct
 {
     std::string             name;
-    enum VideoPixelFormat   ring_format;
+    enum AVPixelFormat      pixfmt;
 } ios_fmt;
 
 static const std::array<ios_fmt, 4> ios_formats
@@ -151,7 +155,7 @@ VideoDeviceImpl::getDeviceParams() const
     DeviceParams params;
     std::stringstream ss1, ss2;
 
-    ss1 << fmt_->ring_format;
+    ss1 << fmt_->pixfmt;
     ss1 >> params.format;
 
     params.name = name;

src/media/video/uwpvideo/video_device_impl.cpp
@@ -21,6 +21,10 @@
 
 #include <array>
 
+extern "C" {
+#include <libavutil/pixfmt.h>
+}
+
 #include "logger.h"
 #include "../video_device.h"
 
@@ -33,7 +37,7 @@ namespace ring { namespace video {
 typedef struct
 {
     std::string             name;
-    enum VideoPixelFormat   ring_format;
+    enum AVPixelFormat      pixfmt;
 } uwp_fmt;
 
 // have all formats map to bgra
@@ -154,7 +158,7 @@ VideoDeviceImpl::getDeviceParams() const
     DeviceParams params;
     std::stringstream ss1, ss2;
 
-    ss1 << fmt_->ring_format;
+    ss1 << fmt_->pixfmt;
     ss1 >> params.format;
 
     params.name = name;