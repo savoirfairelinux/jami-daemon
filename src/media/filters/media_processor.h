aa10131f1e2a9ec510260cd045110c373a6e5fe3
Timothée Menais <timothee.menais@savoirfairelinux.com>
2018-09-28 11:02:15 -0400

    Witness protection mode
    Implemented : face blur
    Todo : modify voice
    
    First install Opencv
    in src/media/filtersmedia_processor modify ln 44 if needed (should not be necessary)
    
    Change-Id: I529dd7be99dc5768712f898a50fdae8a158fafd8

---
+++ src/media/filters/media_processor.h
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (C) 2015-2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Timothée Menais <timothee.menais@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+#pragma once
+
+#include <memory>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/face.hpp>
+
+#include "libav_deps.h"
+#include "media/video/video_scaler.h"
+
+
+namespace ring {
+class MediaFilter;
+
+class MediaProcessor {
+public:
+    MediaProcessor();
+    ~MediaProcessor();
+
+    void addFrame(AVFrame* frame);
+    std::shared_ptr<std::vector<cv::Rect>> getResult() { return lastOutput_; };
+    
+    
+    void stop() {
+        running = false;
+        inputCv.notify_all();
+    }
+
+private:
+	
+    std::unique_ptr<cv::CascadeClassifier> cascade_;
+    std::unique_ptr<cv::CascadeClassifier> cascade2_;
+    std::unique_ptr<cv::Mat> lastInput_;
+    std::shared_ptr<std::vector<cv::Rect>> lastOutput_ {nullptr};
+    std::unique_ptr<MediaFilter> Filter_;
+    
+    std::unique_ptr<cv::CascadeClassifier> initCascade(const std::string cvcascade);
+    std::unique_ptr<video::VideoScaler> scaler_;
+    
+
+    std::shared_ptr<std::vector<cv::Rect>> process(cv::Mat);
+
+    bool running {true};
+    std::thread t;
+
+    std::mutex inputLock;
+    std::condition_variable inputCv;
+
+};
+
+}//namespace ring
