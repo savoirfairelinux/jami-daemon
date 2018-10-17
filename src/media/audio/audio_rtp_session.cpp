1d03d60c74c770496ae38d96de19863d7a19caa0
philippegorley <philippe.gorley@savoirfairelinux.com>
2018-10-17 16:34:03 -0400

    audio: stash active inputs in videomanager
    Needed for audio streaming because the audio input needs to be accessed
    at call level so it can tell the input to switch to a file or a
    different input.
    
    Change-Id: I49be1cb3c641b50e6f70356f330d40e1c27bef61

--- src/media/audio/audio_rtp_session.cpp
+++ src/media/audio/audio_rtp_session.cpp
@@ -41,6 +41,7 @@
 #include "audio/audiobuffer.h"
 #include "audio/ringbufferpool.h"
 #include "audio/resampler.h"
+#include "client/videomanager.h"
 #include "manager.h"
 #include "observer.h"
 #include "smartools.h"
@@ -78,7 +79,7 @@ class AudioSender : public Observer<std::shared_ptr<AudioFrame>> {
         std::unique_ptr<MediaEncoder> audioEncoder_;
         std::unique_ptr<MediaIOHandle> muxContext_;
         std::unique_ptr<Resampler> resampler_;
-        std::unique_ptr<AudioInput> audioInput_;
+        std::shared_ptr<AudioInput> audioInput_;
         std::weak_ptr<MediaRecorder> recorder_;
 
         uint64_t sent_samples = 0;
@@ -147,8 +148,8 @@ AudioSender::setup(SocketPair& socketPair)
 #endif
 
     // NOTE do after encoder is ready to encode
-    audioInput_.reset(new AudioInput(id_));
     auto codec = std::static_pointer_cast<AccountAudioCodecInfo>(args_.codec);
+    audioInput_ = ring::getAudioInput(id_);
     audioInput_->setFormat(codec->audioformat);
     audioInput_->attach(this);
 
