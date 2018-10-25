afae17a4ff6609f352d8c91e5643234e34205c30
philippegorley <philippe.gorley@savoirfairelinux.com>
2018-10-25 13:28:21 -0400

    audio: add file streaming support
    When sharing a file, its audio will also be decoded and sent. Currently,
    the audio may not be in sync with the video.
    
    Device audio is muted while streaming. Mixing of the microphone with the
    file audio will be added in a later patch.
    
    Change-Id: Id0593ce4f2d32c249eb7a9672b0091c0d6e07a00

--- src/media/audio/audio_input.cpp
+++ src/media/audio/audio_input.cpp
@@ -19,9 +19,11 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
-#include "dring/media_const.h"
 #include "audio_input.h"
+#include "dring/media_const.h"
+#include "fileutils.h" // access
 #include "manager.h"
+#include "media_decoder.h"
 #include "resampler.h"
 #include "ringbufferpool.h"
 #include "smartools.h"
@@ -64,8 +66,14 @@ AudioInput::process()
     }
 
     auto frame = std::make_unique<AudioFrame>();
-    if (!nextFromDevice(*frame))
-        return; // no frame
+    bool hasFrame;
+    if (decodingFile_) {
+        hasFrame = nextFromFile(*frame);
+    } else {
+        hasFrame = nextFromDevice(*frame);
+    }
+    if (!hasFrame)
+        return;
 
     auto ms = MediaStream("a:local", format_);
     ms.firstTimestamp = sent_samples;
@@ -123,6 +131,41 @@ AudioInput::nextFromDevice(AudioFrame& frame)
 }
 
 bool
+AudioInput::nextFromFile(AudioFrame& frame)
+{
+    if (!decoder_)
+        return false;
+
+    std::lock_guard<std::mutex> lk(fmtMutex_);
+    const auto ret = decoder_->decode(frame);
+    switch(ret) {
+    case MediaDecoder::Status::ReadError:
+    case MediaDecoder::Status::DecodeError:
+        RING_ERR() << "Failed to decode frame";
+        return false;
+    case MediaDecoder::Status::RestartRequired:
+    case MediaDecoder::Status::EOFError:
+        createDecoder();
+        return false;
+    case MediaDecoder::Status::FrameFinished:
+        if (frame.pointer()->format != AV_SAMPLE_FMT_S16
+            || (unsigned)frame.pointer()->sample_rate != format_.sample_rate
+            || (unsigned)frame.pointer()->channels != format_.nb_channels) {
+            AudioFrame out;
+            out.pointer()->format = AV_SAMPLE_FMT_S16;
+            out.pointer()->sample_rate = format_.sample_rate;
+            out.pointer()->channel_layout = av_get_default_channel_layout(format_.nb_channels);
+            resampler_->resample(frame.pointer(), out.pointer());
+            frame.copyFrom(out);
+        }
+        return true;
+    case MediaDecoder::Status::Success:
+    default:
+        return false;
+    }
+}
+
+bool
 AudioInput::initDevice(const std::string& device)
 {
     devOpts_ = {};
@@ -130,6 +173,22 @@ AudioInput::initDevice(const std::string& device)
     devOpts_.channel = format_.nb_channels;
     devOpts_.framerate = format_.sample_rate;
     return true;
+}
+
+bool
+AudioInput::initFile(const std::string& path)
+{
+    if (access(path.c_str(), R_OK) != 0) {
+        RING_ERR() << "File '" << path << "' not available";
+        return false;
+    }
+
+    devOpts_ = {};
+    devOpts_.input = path;
+    devOpts_.loop = "1";
+    decodingFile_ = true;
+    createDecoder(); // sets devOpts_'s sample rate and number of channels
+    return true; // all required info found
 }
 
 std::shared_future<DeviceParams>
@@ -144,6 +203,9 @@ AudioInput::switchInput(const std::string& resource)
     }
 
     RING_DBG() << "Switching audio source to match '" << resource << "'";
+
+    decoder_.reset();
+    decodingFile_ = false;
 
     currentResource_ = resource;
     devOptsFound_ = false;
@@ -169,7 +231,13 @@ AudioInput::switchInput(const std::string& resource)
         return {};
 
     const auto suffix = resource.substr(pos + sep.size());
-    if (initDevice(suffix))
+    bool ready = false;
+    if (prefix == DRing::Media::VideoProtocolPrefix::FILE)
+        ready = initFile(suffix);
+    else
+        ready = initDevice(suffix);
+
+    if (ready)
         foundDevOpts(devOpts_);
 
     switchPending_ = true;
@@ -184,6 +252,43 @@ AudioInput::foundDevOpts(const DeviceParams& params)
         devOptsFound_ = true;
         foundDevOpts_.set_value(params);
     }
+}
+
+void
+AudioInput::createDecoder()
+{
+    decoder_.reset();
+    if (devOpts_.input.empty()) {
+        foundDevOpts(devOpts_);
+        return;
+    }
+
+    // NOTE createDecoder is currently only used for files, which require rate emulation
+    auto decoder = std::make_unique<MediaDecoder>();
+    decoder->emulateRate();
+    decoder->setInterruptCallback(
+        [](void* data) -> int { return not static_cast<AudioInput*>(data)->isCapturing(); },
+        this);
+
+    if (decoder->openInput(devOpts_) < 0) {
+        RING_ERR() << "Could not open input '" << devOpts_.input << "'";
+        foundDevOpts(devOpts_);
+        return;
+    }
+
+    if (decoder->setupFromAudioData() < 0) {
+        RING_ERR() << "Could not setup decoder for '" << devOpts_.input << "'";
+        foundDevOpts(devOpts_);
+        return;
+    }
+
+    auto ms = decoder->getStream(devOpts_.input);
+    devOpts_.channel = ms.nbChannels;
+    devOpts_.framerate = ms.sampleRate;
+    RING_DBG() << "Created audio decoder: " << ms;
+
+    decoder_ = std::move(decoder);
+    foundDevOpts(devOpts_);
 }
 
 void
