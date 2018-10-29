430c898be7761c65cf7e2ce05fa858b0a632ec28
philippegorley <philippe.gorley@savoirfairelinux.com>
2018-10-29 17:35:29 -0400

    audio: mix file with mic when streaming
    When streaming a file, its audio is written to a RingBuffer that is
    bound to the call id. The audio gets mixed when data is pulled from the
    call's RingBuffer.
    
    Change-Id: I965e8cac98f7be9a926910b063a0f182d8d71698

--- src/media/audio/audio_input.cpp
+++ src/media/audio/audio_input.cpp
@@ -25,6 +25,7 @@
 #include "manager.h"
 #include "media_decoder.h"
 #include "resampler.h"
+#include "ringbuffer.h"
 #include "ringbufferpool.h"
 #include "smartools.h"
 
@@ -39,6 +40,7 @@ AudioInput::AudioInput(const std::string& id) :
     id_(id),
     format_(Manager::instance().getRingBufferPool().getInternalAudioFormat()),
     resampler_(new Resampler),
+    fileId_(id + "_file"),
     loop_([] { return true; },
           [this] { process(); },
           [] {})
@@ -66,13 +68,12 @@ AudioInput::process()
     }
 
     auto frame = std::make_unique<AudioFrame>();
-    bool hasFrame;
     if (decodingFile_) {
-        hasFrame = nextFromFile(*frame);
-    } else {
-        hasFrame = nextFromDevice(*frame);
+        nextFromFile(*frame);
+        frame.reset(new AudioFrame); // frame was written to fileBuffer_, we can reset and reuse
     }
-    if (!hasFrame)
+    // don't notify observers on an empty frame
+    if (!nextFromDevice(*frame)) // will be mixed with file audio if streaming
         return;
 
     auto ms = MediaStream("a:local", format_);
@@ -148,16 +149,7 @@ AudioInput::nextFromFile(AudioFrame& frame)
         createDecoder();
         return false;
     case MediaDecoder::Status::FrameFinished:
-        if (frame.pointer()->format != AV_SAMPLE_FMT_S16
-            || (unsigned)frame.pointer()->sample_rate != format_.sample_rate
-            || (unsigned)frame.pointer()->channels != format_.nb_channels) {
-            AudioFrame out;
-            out.pointer()->format = AV_SAMPLE_FMT_S16;
-            out.pointer()->sample_rate = format_.sample_rate;
-            out.pointer()->channel_layout = av_get_default_channel_layout(format_.nb_channels);
-            resampler_->resample(frame.pointer(), out.pointer());
-            frame.copyFrom(out);
-        }
+        decoder_->writeToRingBuffer(frame, *fileBuffer_, format_);
         return true;
     case MediaDecoder::Status::Success:
     default:
@@ -188,7 +180,10 @@ AudioInput::initFile(const std::string& path)
     devOpts_.loop = "1";
     decodingFile_ = true;
     createDecoder(); // sets devOpts_'s sample rate and number of channels
-    return true; // all required info found
+    // set up new ring buffer for file and bind it to the call's ring buffer
+    fileBuffer_ = Manager::instance().getRingBufferPool().createRingBuffer(fileId_);
+    Manager::instance().getRingBufferPool().bindHalfDuplexOut(id_, fileId_);
+    return true;
 }
 
 std::shared_future<DeviceParams>
@@ -206,6 +201,7 @@ AudioInput::switchInput(const std::string& resource)
 
     decoder_.reset();
     decodingFile_ = false;
+    fileBuffer_.reset();
 
     currentResource_ = resource;
     devOptsFound_ = false;
