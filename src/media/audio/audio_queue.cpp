e438a1cc49dba9985c7271276c121dd64f860930
philippegorley <philippe.gorley@savoirfairelinux.com>
2018-11-01 16:46:21 -0400

    audio: add queue
    Allows buffering of samples when the frame sizes at the input and the
    output don't match.
    
    AudioQueue will be used mostly for file streaming, in case the file's
    frame size is not 20ms packets for the call's sample rate.
    
    AudioQueue::setFormat drains and resets the queue in a thread-safe way.
    
    Adds unit tests for queue.
    
    Change-Id: I568b31ba97d33bc0c1c89495e918bd10a9bf8aeb

---
+++ src/media/audio/audio_queue.cpp
@@ -0,0 +1,113 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "audio_queue.h"
+#include "libav_deps.h"
+#include "logger.h"
+
+extern "C" {
+#include <libavutil/audio_fifo.h>
+}
+
+namespace ring {
+
+AudioQueue::AudioQueue(const AudioFormat& format)
+    : format_(format)
+{
+    queue_ = av_audio_fifo_alloc(format.sampleFormat, format.nb_channels, 1);
+}
+
+AudioQueue::~AudioQueue()
+{
+    av_audio_fifo_free(queue_);
+}
+
+int
+AudioQueue::getSize()
+{
+    std::lock_guard<std::mutex> lk(mutex_);
+    return av_audio_fifo_size(queue_);
+}
+
+AudioFormat
+AudioQueue::getFormat() const
+{
+    return format_;
+}
+
+void
+AudioQueue::setFormat(const AudioFormat& format)
+{
+    std::lock_guard<std::mutex> lk(mutex_);
+    format_ = format;
+    av_audio_fifo_free(queue_);
+    queue_ = av_audio_fifo_alloc(format.sampleFormat, format.nb_channels, 1);
+}
+
+int
+AudioQueue::enqueue(AudioFrame& frame)
+{
+    int ret = 0;
+    auto f = frame.pointer();
+    if (f->format != (int)format_.sampleFormat || f->channels != (int)format_.nb_channels || f->sample_rate != (int)format_.sample_rate) {
+        RING_ERR() << "Could not write samples to audio queue: input frame is not the right format";
+        return -1;
+    }
+
+    // queue reallocates itself if need be
+    std::lock_guard<std::mutex> lk(mutex_);
+    if ((ret = av_audio_fifo_write(queue_, reinterpret_cast<void**>(f->data), f->nb_samples)) < 0) {
+        RING_ERR() << "Could not write samples to audio queue: " << libav_utils::getError(ret);
+        return -1;
+    }
+
+    return ret; // number of samples written
+}
+
+std::unique_ptr<AudioFrame>
+AudioQueue::dequeue(int nbSamples)
+{
+    // normal case, don't print anything
+    if (getSize() < nbSamples)
+        return {};
+
+    int ret;
+    auto frame = std::make_unique<AudioFrame>();
+    auto f = frame->pointer();
+    f->format = (int)format_.sampleFormat;
+    f->channels = format_.nb_channels;
+    f->channel_layout = av_get_default_channel_layout(format_.nb_channels);
+    f->sample_rate = format_.sample_rate;
+    f->nb_samples = nbSamples;
+    if ((ret = av_frame_get_buffer(f, 0)) < 0) {
+        RING_ERR() << "Failed to allocate audio buffers: " << libav_utils::getError(ret);
+        return {};
+    }
+
+    std::lock_guard<std::mutex> lk(mutex_);
+    if ((ret = av_audio_fifo_read(queue_, reinterpret_cast<void**>(f->data), nbSamples)) < 0) {
+        RING_ERR() << "Could not read samples from queue: " << libav_utils::getError(ret);
+        return {};
+    }
+
+    return frame;
+}
+
+} // namespace ring
---
+++ src/media/audio/audio_queue.h
@@ -0,0 +1,98 @@
+/*
+ *  Copyright (C) 2018 Savoir-faire Linux Inc.
+ *
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "audiobuffer.h"
+#include "media_buffer.h"
+#include "noncopyable.h"
+
+#include <mutex>
+
+struct AVAudioFifo;
+
+namespace ring {
+
+/**
+ * Buffers extra samples. This is in case an input's frame size (number of samples in
+ * a frame) and the output's frame size don't match. The queue will store the extra
+ * samples until a frame can be read.
+ *
+ * Works at frame-level instead of sample- or byte-level like FFmpeg's FIFO buffers.
+ */
+class AudioQueue {
+public:
+    AudioQueue(const AudioFormat& format);
+    ~AudioQueue();
+
+    /**
+     * Gets the numbers of samples available for reading.
+     */
+    int getSize();
+
+    /**
+     * Gets the format used by @queue_, input frames must match this format or enqueuing
+     * will fail. Returned frames are in this format.
+     */
+    AudioFormat getFormat() const;
+
+    /**
+     * Sets the format used by @queue_. This drains the queue, all stored samples will
+     * be lost.
+     */
+    void setFormat(const AudioFormat& format);
+
+    /**
+     * Write @frame's data to the queue. The internal buffer will be reallocated if
+     * there's not enough space for @frame's samples.
+     *
+     * Returns the number of samples written, or negative on error.
+     *
+     * NOTE @frame's format must match @format_, or this will fail.
+     */
+    int enqueue(AudioFrame& frame);
+
+    /**
+     * Reads @nbSamples from the queue into a frame and returns it.
+     *
+     * Returns a valid audio frame, or null if there wasn't enough data.
+     */
+    std::unique_ptr<AudioFrame> dequeue(int nbSamples);
+
+private:
+    NON_COPYABLE(AudioQueue);
+
+    /**
+     * Protects access to @queue_.
+     */
+    std::mutex mutex_ {};
+
+    /**
+     * Format used for input and output audio frames.
+     */
+    AudioFormat format_;
+
+    /**
+     * Audio queue operating on the sample level instead of byte level.
+     */
+    AVAudioFifo* queue_;
+};
+
+} // namespace ring
