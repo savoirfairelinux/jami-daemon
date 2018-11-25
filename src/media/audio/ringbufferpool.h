934bf6ca6b6e3e85a113333a0f257b1a9877a770
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2018-11-25 11:14:36 -0500

    ringbuffer, ringbufferpool: cleanup
    Change-Id: Ia7818aaa83307d5ff8bebe9681182bcec5d16e9b

--- src/media/audio/ringbufferpool.h
+++ src/media/audio/ringbufferpool.h
@@ -35,120 +35,116 @@ namespace ring {
 class RingBuffer;
 
 class RingBufferPool {
-    public:
-        static const char * const DEFAULT_ID;
+public:
+    static const char * const DEFAULT_ID;
 
-        RingBufferPool();
+    RingBufferPool();
+    ~RingBufferPool();
 
-        ~RingBufferPool();
+    int getInternalSamplingRate() const {
+        return internalAudioFormat_.sample_rate;
+    }
 
-        int getInternalSamplingRate() const {
-            return internalAudioFormat_.sample_rate;
-        }
+    AudioFormat getInternalAudioFormat() const {
+        return internalAudioFormat_;
+    }
 
-        AudioFormat getInternalAudioFormat() const {
-            return internalAudioFormat_;
-        }
+    void setInternalSamplingRate(unsigned sr);
 
-        void setInternalSamplingRate(unsigned sr);
+    void setInternalAudioFormat(AudioFormat format);
 
-        void setInternalAudioFormat(AudioFormat format);
+    /**
+     * Bind together two audio streams so that a client will be able
+     * to put and get data specifying its callid only.
+     */
+    void bindCallID(const std::string& call_id1,
+                    const std::string& call_id2);
 
-        /**
-         * Bind together two audio streams so that a client will be able
-         * to put and get data specifying its callid only.
-         */
-        void bindCallID(const std::string& call_id1,
+    /**
+     * Add a new call_id to unidirectional outgoing stream
+     * \param call_id New call id to be added for this stream
+     * \param process_id Process that require this stream
+     */
+    void bindHalfDuplexOut(const std::string& process_id,
+                            const std::string& call_id);
+
+    /**
+     * Unbind two calls
+     */
+    void unBindCallID(const std::string& call_id1,
                         const std::string& call_id2);
 
-        /**
-         * Add a new call_id to unidirectional outgoing stream
-         * \param call_id New call id to be added for this stream
-         * \param process_id Process that require this stream
-         */
-        void bindHalfDuplexOut(const std::string& process_id,
-                               const std::string& call_id);
+    /**
+     * Unbind a unidirectional stream
+     */
+    void unBindHalfDuplexOut(const std::string& process_id,
+                                const std::string& call_id);
 
-        /**
-         * Unbind two calls
-         */
-        void unBindCallID(const std::string& call_id1,
-                          const std::string& call_id2);
+    void unBindAll(const std::string& call_id);
 
-        /**
-         * Unbind a unidirectional stream
-         */
-        void unBindHalfDuplexOut(const std::string& process_id,
-                                 const std::string& call_id);
+    bool waitForDataAvailable(const std::string& call_id,
+                                const std::chrono::microseconds& max_wait) const;
 
-        void unBindAll(const std::string& call_id);
+    std::shared_ptr<AudioFrame> getData(const std::string& call_id);
 
-        bool waitForDataAvailable(const std::string& call_id,
-                                  const std::chrono::microseconds& max_wait) const;
+    std::shared_ptr<AudioFrame> getAvailableData(const std::string& call_id);
 
-        std::shared_ptr<AudioFrame> getData(const std::string& call_id);
+    size_t availableForGet(const std::string& call_id) const;
 
-        std::shared_ptr<AudioFrame> getAvailableData(const std::string& call_id);
+    size_t discard(size_t toDiscard, const std::string& call_id);
 
-        size_t availableForGet(const std::string& call_id) const;
+    void flush(const std::string& call_id);
 
-        size_t discard(size_t toDiscard, const std::string& call_id);
+    void flushAllBuffers();
 
-        void flush(const std::string& call_id);
+    /**
+     * Create a new ringbuffer with a default readoffset.
+     * This class keeps a weak reference on returned pointer,
+     * so the caller is responsible of the referred instance.
+     */
+    std::shared_ptr<RingBuffer> createRingBuffer(const std::string& id);
 
-        void flushAllBuffers();
+    /**
+     * Obtain a shared pointer on a RingBuffer given by its ID.
+     * If the ID doesn't match to any RingBuffer, the shared pointer
+     * is empty. This non-const version flushes internal weak pointers
+     * if the ID was used and the associated RingBuffer has been deleted.
+     */
+    std::shared_ptr<RingBuffer> getRingBuffer(const std::string& id);
 
-        /**
-         * Create a new ringbuffer with a default readoffset.
-         * This class keeps a weak reference on returned pointer,
-         * so the caller is responsible of the referred instance.
-         */
-        std::shared_ptr<RingBuffer> createRingBuffer(const std::string& id);
+    /**
+     * Works as non-const getRingBuffer, without the weak reference flush.
+     */
+    std::shared_ptr<RingBuffer> getRingBuffer(const std::string& id) const;
 
-        /**
-         * Obtain a shared pointer on a RingBuffer given by its ID.
-         * If the ID doesn't match to any RingBuffer, the shared pointer
-         * is empty. This non-const version flushes internal weak pointers
-         * if the ID was used and the associated RingBuffer has been deleted.
-         */
-        std::shared_ptr<RingBuffer> getRingBuffer(const std::string& id);
+private:
+    NON_COPYABLE(RingBufferPool);
 
-        /**
-         * Works as non-const getRingBuffer, without the weak reference flush.
-         */
-        std::shared_ptr<RingBuffer> getRingBuffer(const std::string& id) const;
+    // A set of RingBuffers readable by a call
+    using ReadBindings = std::set<std::shared_ptr<RingBuffer>, std::owner_less<std::shared_ptr<RingBuffer>>>;
 
-    private:
-        NON_COPYABLE(RingBufferPool);
+    const ReadBindings* getReadBindings(const std::string& call_id) const;
+    ReadBindings* getReadBindings(const std::string& call_id);
 
-        // A set of RingBuffers readable by a call
-        typedef std::set<std::shared_ptr<RingBuffer>,
-            std::owner_less<std::shared_ptr<RingBuffer>> > ReadBindings;
+    void removeReadBindings(const std::string& call_id);
 
-        const RingBufferPool::ReadBindings*
-		getReadBindings(const std::string& call_id) const;
+    void addReaderToRingBuffer(const std::shared_ptr<RingBuffer>& rbuf,
+                                const std::string& call_id);
 
-        RingBufferPool::ReadBindings* getReadBindings(const std::string& call_id);
+    void removeReaderFromRingBuffer(const std::shared_ptr<RingBuffer>& rbuf,
+                                    const std::string& call_id);
 
-        void removeReadBindings(const std::string& call_id);
+    // A cache of created RingBuffers listed by IDs.
+    std::map<std::string, std::weak_ptr<RingBuffer>> ringBufferMap_ {};
 
-        void addReaderToRingBuffer(const std::shared_ptr<RingBuffer>& rbuf,
-                                   const std::string& call_id);
+    // A map of which RingBuffers a call has some ReadOffsets
+    std::map<std::string, ReadBindings> readBindingsMap_ {};
 
-        void removeReaderFromRingBuffer(const std::shared_ptr<RingBuffer>& rbuf,
-                                        const std::string& call_id);
+    mutable std::recursive_mutex stateLock_ {};
 
-        // A cache of created RingBuffers listed by IDs.
-        std::map<std::string, std::weak_ptr<RingBuffer> > ringBufferMap_ {};
+    AudioFormat internalAudioFormat_ {AudioFormat::DEFAULT()};
 
-        // A map of which RingBuffers a call has some ReadOffsets
-        std::map<std::string, ReadBindings> readBindingsMap_ {};
-
-        mutable std::recursive_mutex stateLock_ {};
-
-        AudioFormat internalAudioFormat_ {AudioFormat::DEFAULT()};
-
-        std::shared_ptr<RingBuffer> defaultRingBuffer_;
+    std::shared_ptr<RingBuffer> defaultRingBuffer_;
 };
 
 } // namespace ring
