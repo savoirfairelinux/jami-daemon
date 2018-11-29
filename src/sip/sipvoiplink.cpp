8ebf379a4a5a85c13dc2651333793d841150fb94
Yanfei Han <kalerfu@gmail.com>
2018-11-30 03:30:01 +0800

    SIPVoIPLink initializer need register_thread first before pj_pool_create
    Change-Id: I729d8a34aa48b73fe3800119f7dac666bb9e96a9

--- src/sip/sipvoiplink.cpp
+++ src/sip/sipvoiplink.cpp
@@ -493,6 +493,7 @@ SIPVoIPLink::SIPVoIPLink() : pool_(nullptr, pj_pool_release)
     throw VoipLinkException(#ret " failed"); \
 } while (0)
 
+    sip_utils::register_thread();
     pj_caching_pool_init(&cp_, &pj_pool_factory_default_policy, 0);
     pool_.reset(pj_pool_create(&cp_.factory, PACKAGE, 64 * 1024, 4096, nullptr));
     if (!pool_)
