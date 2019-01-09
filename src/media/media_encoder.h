9a737813b538ae32b4208817d7c2fea5293354be
philippegorley <philippe.gorley@savoirfairelinux.com>
2019-01-09 11:43:56 -0500

    libav_utils: add shortcut to set dictionary values
    Change-Id: Iab880ac674e15dc2f0baba260632ead4cb6e4870
    Reviewed-by: Sébastien Blin <sebastien.blin@savoirfairelinux.com>

--- src/media/media_encoder.h
+++ src/media/media_encoder.h
@@ -2,6 +2,7 @@
  *  Copyright (C) 2013-2019 Savoir-faire Linux Inc.
  *
  *  Author: Guillaume Roguez <Guillaume.Roguez@savoirfairelinux.com>
+ *  Author: Philippe Gorley <philippe.gorley@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
