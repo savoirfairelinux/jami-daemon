184bd995d83171343fea81d9dfb11ae9ff97140a
dvidal <denys.vidal@savoirfairelinux.com>
2019-01-28 14:49:33 -0500

    video:add rotation
    Change-Id: I0b760e4ae32ab372cef49ef9c2120e722e041ef1

--- src/media/video/video_rtp_session.h
+++ src/media/video/video_rtp_session.h
@@ -69,6 +69,14 @@ public:
     void restartSender() override;
     void stop() override;
 
+    /**
+      * Set video orientation
+      *
+      * Send to the receive thread rotation to apply to the video (counterclockwise)
+      *
+      * @param rotation Rotation in degrees (counterclockwise)
+      */
+    void setRotation(int rotation);
     void forceKeyFrame();
     void bindMixer(VideoMixer* mixer);
     void unbindMixer();
