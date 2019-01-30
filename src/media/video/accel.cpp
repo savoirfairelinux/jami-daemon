1c0b1edf5d8599a9abbac808e6dc9efa7d0896ef
philippegorley <philippe.gorley@savoirfairelinux.com>
2019-01-30 16:09:03 -0500

    accel: add hardware encoding
    Adds VAAPI and VideoToolbox hardware encoders.
    
    Abstracts hardware related field accesses from the encoder and decoder
    to put them in HardwareAccel. They are freed in ~HardwareAccel.
    
    Limits bitrate when hardware encoding h264, else it can easily go up to
    a few MiB/s.
    
    Change-Id: I7d847d8ab3e4c9692341f038ce6d5dd76562e606

--- src/media/video/accel.cpp
+++ src/media/video/accel.cpp
@@ -37,6 +37,7 @@ struct HardwareAPI
 {
     std::string name;
     AVPixelFormat format;
+    AVPixelFormat swFormat;
     std::vector<AVCodecID> supportedCodecs;
 };
 
@@ -52,7 +53,7 @@ getFormatCb(AVCodecContext* codecCtx, const AVPixelFormat* formats)
             // found hardware format for codec with api
             RING_DBG() << "Found compatible hardware format for "
                 << avcodec_get_name(static_cast<AVCodecID>(accel->getCodecId()))
-                << " with " << accel->getName();
+                << " decoder with " << accel->getName();
             return formats[i];
         }
     }
@@ -61,54 +62,146 @@ getFormatCb(AVCodecContext* codecCtx, const AVPixelFormat* formats)
     return fallback;
 }
 
-HardwareAccel::HardwareAccel(AVCodecID id, const std::string& name, AVPixelFormat format)
+HardwareAccel::HardwareAccel(AVCodecID id, const std::string& name, AVPixelFormat format, AVPixelFormat swFormat, CodecType type)
     : id_(id)
     , name_(name)
     , format_(format)
+    , swFormat_(swFormat)
+    , type_(type)
 {}
+
+HardwareAccel::~HardwareAccel()
+{
+    if (deviceCtx_)
+        av_buffer_unref(&deviceCtx_);
+    if (framesCtx_)
+        av_buffer_unref(&framesCtx_);
+}
+
+std::string
+HardwareAccel::getCodecName() const
+{
+    if (type_ == CODEC_DECODER) {
+        return avcodec_get_name(id_);
+    } else if (type_ == CODEC_ENCODER) {
+        std::stringstream ss;
+        ss << avcodec_get_name(id_) << '_' << name_;
+        return ss.str();
+    }
+    return "";
+}
 
 std::unique_ptr<VideoFrame>
 HardwareAccel::transfer(const VideoFrame& frame)
 {
-    auto input = frame.pointer();
-    if (input->format != format_) {
-        RING_ERR("Frame format mismatch: expected %s, got %s",
-                 av_get_pix_fmt_name(static_cast<AVPixelFormat>(format_)),
-                 av_get_pix_fmt_name(static_cast<AVPixelFormat>(input->format)));
+    int ret = 0;
+    if (type_ == CODEC_DECODER) {
+        auto input = frame.pointer();
+        if (input->format != format_) {
+            RING_ERR() << "Frame format mismatch: expected "
+                << av_get_pix_fmt_name(format_) << ", got "
+                << av_get_pix_fmt_name(static_cast<AVPixelFormat>(input->format));
+            return nullptr;
+        }
+
+        return transferToMainMemory(frame, AV_PIX_FMT_NV12);
+    } else if (type_ == CODEC_ENCODER) {
+        auto input = frame.pointer();
+        if (input->format != swFormat_) {
+            RING_ERR() << "Frame format mismatch: expected "
+                << av_get_pix_fmt_name(swFormat_) << ", got "
+                << av_get_pix_fmt_name(static_cast<AVPixelFormat>(input->format));
+            return nullptr;
+        }
+
+        auto framePtr = std::make_unique<VideoFrame>();
+        auto hwFrame = framePtr->pointer();
+
+        if ((ret = av_hwframe_get_buffer(framesCtx_, hwFrame, 0)) < 0) {
+            RING_ERR() << "Failed to allocate hardware buffer: " << libav_utils::getError(ret);
+            return nullptr;
+        }
+
+        if (!hwFrame->hw_frames_ctx) {
+            RING_ERR() << "Failed to allocate hardware buffer: Cannot allocate memory";
+            return nullptr;
+        }
+
+        if ((ret = av_hwframe_transfer_data(hwFrame, input, 0)) < 0) {
+            RING_ERR() << "Failed to push frame to GPU: " << libav_utils::getError(ret);
+            return nullptr;
+        }
+
+        hwFrame->pts = input->pts; // transfer does not copy timestamp
+        return framePtr;
+    } else {
+        RING_ERR() << "Invalid hardware accelerator";
         return nullptr;
     }
-
-    return transferToMainMemory(frame, AV_PIX_FMT_NV12);
 }
 
-static int
-initDevice(const HardwareAPI& api, AVCodecContext* codecCtx)
+void
+HardwareAccel::setDetails(AVCodecContext* codecCtx, AVDictionary** /*d*/)
+{
+    if (type_ == CODEC_DECODER) {
+        codecCtx->hw_device_ctx = av_buffer_ref(deviceCtx_);
+        codecCtx->get_format = getFormatCb;
+        codecCtx->thread_safe_callbacks = 1;
+    } else if (type_ == CODEC_ENCODER) {
+        codecCtx->hw_device_ctx = av_buffer_ref(deviceCtx_);
+        codecCtx->hw_frames_ctx = av_buffer_ref(framesCtx_);
+    }
+}
+
+bool
+HardwareAccel::initDevice()
 {
     int ret = 0;
-    AVBufferRef* hardwareDeviceCtx = nullptr;
-    auto hwType = av_hwdevice_find_type_by_name(api.name.c_str());
+    auto hwType = av_hwdevice_find_type_by_name(name_.c_str());
 #ifdef HAVE_VAAPI_ACCEL_DRM
     // default DRM device may not work on multi GPU computers, so check all possible values
-    if (api.name == "vaapi") {
+    if (name_ == "vaapi") {
         const std::string path = "/dev/dri/";
         auto files = ring::fileutils::readDirectory(path);
         // renderD* is preferred over card*
         std::sort(files.rbegin(), files.rend());
         for (auto& entry : files) {
             std::string deviceName = path + entry;
-            if ((ret = av_hwdevice_ctx_create(&hardwareDeviceCtx, hwType, deviceName.c_str(), nullptr, 0)) >= 0) {
-                codecCtx->hw_device_ctx = hardwareDeviceCtx;
-                return ret;
+            if ((ret = av_hwdevice_ctx_create(&deviceCtx_, hwType, deviceName.c_str(), nullptr, 0)) >= 0) {
+                return true;
             }
         }
     }
 #endif
     // default device (nullptr) works for most cases
-    if ((ret = av_hwdevice_ctx_create(&hardwareDeviceCtx, hwType, nullptr, nullptr, 0)) >= 0) {
-        codecCtx->hw_device_ctx = hardwareDeviceCtx;
+    ret = av_hwdevice_ctx_create(&deviceCtx_, hwType, nullptr, nullptr, 0);
+    return ret >= 0;
+}
+
+bool
+HardwareAccel::initFrame(int width, int height)
+{
+    int ret = 0;
+    if (!deviceCtx_) {
+        RING_ERR() << "Cannot initialize hardware frames without a valid hardware device";
+        return false;
     }
 
-    return ret;
+    framesCtx_ = av_hwframe_ctx_alloc(deviceCtx_);
+    if (!framesCtx_)
+        return false;
+
+    auto ctx = reinterpret_cast<AVHWFramesContext*>(framesCtx_->data);
+    ctx->format = format_;
+    ctx->sw_format = swFormat_;
+    ctx->width = width;
+    ctx->height = height;
+    ctx->initial_pool_size = 20; // TODO try other values
+
+    if ((ret = av_hwframe_ctx_init(framesCtx_)) < 0)
+        av_buffer_unref(&framesCtx_);
+
+    return ret >= 0;
 }
 
 std::unique_ptr<VideoFrame>
@@ -137,25 +230,50 @@ HardwareAccel::transferToMainMemory(const VideoFrame& frame, AVPixelFormat desir
 }
 
 std::unique_ptr<HardwareAccel>
-HardwareAccel::setupDecoder(AVCodecContext* codecCtx)
+HardwareAccel::setupDecoder(AVCodecID id)
 {
     static const HardwareAPI apiList[] = {
-        { "vaapi", AV_PIX_FMT_VAAPI, { AV_CODEC_ID_H264, AV_CODEC_ID_MPEG4, AV_CODEC_ID_VP8, AV_CODEC_ID_MJPEG } },
-        { "vdpau", AV_PIX_FMT_VDPAU, { AV_CODEC_ID_H264, AV_CODEC_ID_MPEG4 } },
-        { "videotoolbox", AV_PIX_FMT_VIDEOTOOLBOX, { AV_CODEC_ID_H264, AV_CODEC_ID_MPEG4 } },
+        { "vaapi", AV_PIX_FMT_VAAPI, AV_PIX_FMT_NV12, { AV_CODEC_ID_H264, AV_CODEC_ID_MPEG4, AV_CODEC_ID_VP8, AV_CODEC_ID_MJPEG } },
+        { "vdpau", AV_PIX_FMT_VDPAU, AV_PIX_FMT_NV12, { AV_CODEC_ID_H264, AV_CODEC_ID_MPEG4 } },
+        { "videotoolbox", AV_PIX_FMT_VIDEOTOOLBOX, AV_PIX_FMT_NV12, { AV_CODEC_ID_H264, AV_CODEC_ID_MPEG4 } },
     };
 
     for (const auto& api : apiList) {
-        if (std::find(api.supportedCodecs.begin(), api.supportedCodecs.end(), codecCtx->codec_id) != api.supportedCodecs.end()) {
-            if (initDevice(api, codecCtx) >= 0) {
-                codecCtx->get_format = getFormatCb;
-                codecCtx->thread_safe_callbacks = 1;
-                RING_DBG() << "Attempting to use hardware accelerated decoding with " << api.name;
-                return std::make_unique<HardwareAccel>(codecCtx->codec_id, api.name, api.format);
+        if (std::find(api.supportedCodecs.begin(), api.supportedCodecs.end(), id) != api.supportedCodecs.end()) {
+            auto accel = std::make_unique<HardwareAccel>(id, api.name, api.format, api.swFormat, CODEC_DECODER);
+            if (accel->initDevice()) {
+                RING_DBG() << "Attempting to use hardware decoder " << accel->getCodecName() << " with " << api.name;
+                return accel;
             }
         }
     }
 
+    return nullptr;
+}
+
+std::unique_ptr<HardwareAccel>
+HardwareAccel::setupEncoder(AVCodecID id, int width, int height)
+{
+    static const HardwareAPI apiList[] = {
+        { "vaapi", AV_PIX_FMT_VAAPI, AV_PIX_FMT_NV12, { AV_CODEC_ID_H264, AV_CODEC_ID_MJPEG, AV_CODEC_ID_VP8 } },
+        { "videotoolbox", AV_PIX_FMT_VIDEOTOOLBOX, AV_PIX_FMT_NV12, { AV_CODEC_ID_H264 } },
+    };
+
+    for (auto api : apiList) {
+        const auto& it = std::find(api.supportedCodecs.begin(), api.supportedCodecs.end(), id);
+        if (it != api.supportedCodecs.end()) {
+            auto accel = std::make_unique<HardwareAccel>(id, api.name, api.format, api.swFormat, CODEC_ENCODER);
+            const auto& codecName = accel->getCodecName();
+            if (avcodec_find_encoder_by_name(codecName.c_str())) {
+                if (accel->initDevice() && accel->initFrame(width, height)) {
+                    RING_DBG() << "Attempting to use hardware encoder " << codecName;
+                    return accel;
+                }
+            }
+        }
+    }
+
+    RING_WARN() << "Not using hardware encoding";
     return nullptr;
 }
 
