9500a4ed9437009e4d73394fa7f65b366c6bfd8c
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-02-28 14:17:52 -0500

    videosender: avoid sending many successive keyframes
    Send a single keyframe in response to a batch of keyframe requests.
    Don't send many keyframes on call startup.
    
    Change-Id: Ic416a847314394567037408b443e48b8dd8ba0be

--- src/media/video/video_sender.cpp
+++ src/media/video/video_sender.cpp
@@ -96,9 +96,7 @@ VideoSender::encodeAndSendVideo(VideoFrame& input_frame)
     } else {
         bool is_keyframe = forceKeyFrame_ > 0
             or (keyFrameFreq_ > 0 and (frameNumber_ % keyFrameFreq_) == 0);
-
-        if (is_keyframe)
-            --forceKeyFrame_;
+        forceKeyFrame_ = 0;
 
         AVFrameSideData* side_data = av_frame_get_side_data(input_frame.pointer(), AV_FRAME_DATA_DISPLAYMATRIX);
         auto angle = side_data == nullptr ? 0 : av_display_rotation_get(reinterpret_cast<int32_t*>(side_data->data));
