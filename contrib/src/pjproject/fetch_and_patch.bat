830d4bea59457bad29dafc46b0eefbf7ae8a7cd8
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-04-03 10:30:07 -0400

    pjproject: bump to latest commit
    Bump pjproject to prepare the patch for RFC6544. Also the patch
    used to support gnutls is merged upstream so we don't need it
    anymore.
    
    Change-Id: I7d83d7693dbadd4650623baa29f1721710040a08

--- contrib/src/pjproject/fetch_and_patch.bat
+++ contrib/src/pjproject/fetch_and_patch.bat
@@ -1,19 +1,19 @@
 set BUILD=%SRC%..\build
 
-set PJPROJECT_VERSION=2.7.2
-set PJPROJECT_URL=http://www.pjsip.org/release/%PJPROJECT_VERSION%/pjproject-%PJPROJECT_VERSION%.tar.bz2
+set PJPROJECT_VERSION=6b9212dcb4b3f781c1e922ae544b063880bc46ac
+set PJPROJECT_URL=https://github.com/pjsip/pjproject/archive/%PJPROJECT_VERSION%.tar.gz
 
 mkdir %BUILD%
 
 if %USE_CACHE%==1 (
-    copy %CACHE_DIR%\pjproject-%PJPROJECT_VERSION%.tar.bz2 %cd%
+    copy %CACHE_DIR%\%PJPROJECT_VERSION%.tar.gz %cd%
 ) else (
     %WGET_CMD% %PJPROJECT_URL%
 )
 
-7z -y x pjproject-%PJPROJECT_VERSION%.tar.bz2 && 7z -y x pjproject-%PJPROJECT_VERSION%.tar -o%BUILD%
-del pjproject-%PJPROJECT_VERSION%.tar && del pjproject-%PJPROJECT_VERSION%.tar.bz2
-rename %BUILD%\pjproject-%PJPROJECT_VERSION% pjproject
+7z -y x %PJPROJECT_VERSION%.tar.gz && 7z -y x %PJPROJECT_VERSION%.tar -o%BUILD%
+del %PJPROJECT_VERSION%.tar && del %PJPROJECT_VERSION%.tar.gz
+rename %BUILD%\%PJPROJECT_VERSION% pjproject
 
 cd %BUILD%\pjproject
 
@@ -27,7 +27,6 @@ if "%NO_AUTO%"=="" (
 )
 set UNIXPATH=%SRC:\=/%
 set UNIXPATH=%ROOTPATH%%UNIXPATH:C:/=%
-bash -c "%PATCH_CMD% %UNIXPATH%pjproject/gnutls.patch"
 bash -c "%PATCH_CMD% %UNIXPATH%pjproject/ipv6.patch"
 bash -c "%PATCH_CMD% %UNIXPATH%pjproject/ice_config.patch"
 bash -c "%PATCH_CMD% %UNIXPATH%pjproject/multiple_listeners.patch"
