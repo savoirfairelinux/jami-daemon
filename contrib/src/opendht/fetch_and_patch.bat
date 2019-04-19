2e2c38500f8bd5a5a28ae913c5b2aff71afbefb6
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-04-19 16:27:42 -0400

    contrib: update OpenDHT
    Change-Id: I29bd020a3cfefb11c527a9fb860485c04367def4

--- contrib/src/opendht/fetch_and_patch.bat
+++ contrib/src/opendht/fetch_and_patch.bat
@@ -1,6 +1,6 @@
 set BUILD=%SRC%..\build
 
-set OPENDHT_VERSION=ef82652a1e9417cd20df3bf55f569aee26b6e6a8
+set OPENDHT_VERSION=1.9.2
 set OPENDHT_URL=https://github.com/savoirfairelinux/opendht/archive/%OPENDHT_VERSION%.tar.gz
 
 mkdir %BUILD%
@@ -16,7 +16,5 @@ del %OPENDHT_VERSION%.tar && del %OPENDHT_VERSION%.tar.gz && del %BUILD%\pax_glo
 rename %BUILD%\opendht-%OPENDHT_VERSION% opendht
 
 cd %BUILD%\opendht
-
-%APPLY_CMD% %SRC%\opendht\opendht-vs2017.patch
 
 cd %SRC%
