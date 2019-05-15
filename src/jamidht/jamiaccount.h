c611169f884e219031378aadbdd49b4afb7b7a0a
Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-05-15 17:17:35 -0400

    peerdiscovery: add functionality of discovering Jami Account
    Change-Id: Iee3572c7f0fa9b35ed9eb8db19e642a28d4ef981

--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -399,6 +399,18 @@ class JamiAccount : public SIPAccountBase {
 
         void saveConfig() const;
 
+#if HAVE_RINGNS
+        /**
+         * Enable Peer Discovery Service 
+         */
+        void enableDiscovery(bool enable_d);
+
+        /**
+         * Enable Peer Publish Service 
+         */
+        void enablePublish(bool enable_p);
+#endif
+
     private:
         NON_COPYABLE(JamiAccount);
 
@@ -415,6 +427,7 @@ class JamiAccount : public SIPAccountBase {
         struct DeviceAnnouncement;
         struct DeviceSync;
         struct BuddyInfo;
+        struct JamiAccountPeerInfo;
 
         void syncDevices();
         void onReceiveDeviceSync(DeviceSync&& sync);
@@ -489,6 +502,19 @@ class JamiAccount : public SIPAccountBase {
         void onPeerMessage(const dht::InfoHash& peer_device, std::function<void(const std::shared_ptr<dht::crypto::Certificate>& crt, const dht::InfoHash& account_id)>&&);
 
         void onTrustRequest(const dht::InfoHash& peer_account, const dht::InfoHash& peer_device, time_t received , bool confirm, std::vector<uint8_t>&& payload);
+
+#if HAVE_RINGNS
+        /**
+         * Start Publish the Jami Account onto the Network
+         */
+        void startJamiAccountPublish(const std::string& display_name);
+
+        /**
+         * Start Discovery the Jami Account from the Network
+         */
+        void startJamiAccountDiscovery();
+#endif
+
 
         /**
          * Maps require port via UPnP
@@ -680,6 +706,12 @@ class JamiAccount : public SIPAccountBase {
         void registerDhtAddress(IceTransport&);
 
         std::unique_ptr<DhtPeerConnector> dhtPeerConnector_;
+
+#if HAVE_RINGNS
+        std::unique_ptr<dht::PeerDiscovery> dhtJamiDiscovery_;
+        std::mutex dismtx_;
+        std::map<std::string,std::string> discoveriedPeers_;
+#endif
 
         std::shared_ptr<RepeatedTask> eventHandler {};
         void checkPendingCallsTask();
