f1210e4b8353ae89723ec31f0426b952a162e36c
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-05-16 14:03:05 -0400

    sip: negotiate both UDP and TCP for the control channel
    Our pjsip version supports the RFC6544. With this patch, when
    starting a call, the daemon is using two ICE sessions for the SIP
    channel. One is negotiating a UDP socket, and the other a TCP socket
    and transmits both SDP on the DHT.
    
    If both negotiations succeed, TCP is prefered and will be used
    to transmit SIP messages and the VCard. This should solve the 30
    seconds timeout on bad networks.
    
    Note that the media channel is still using UDP to transmit audio
    and video.
    
    MAJOR CHANGE: the SIP channel use TLS on top of TCP, no DTLS,
    so the transport is considered as reliable.
    
    Also lot of changes in rfc6544.patch to link to rfc6062. The patch
    needs to be cleaned, cf TODO notes
    
    MAJOR CHANGE: When the transport idle timeout is reached, tls_session
    will close the connection but will not wait the answer for the EOF
    from the peer.
    
    Change-Id: I55c5f51377fd8787bc951d6d282eec46f8eaf977
    Gitlab: #103

--- src/security/tls_session.cpp
+++ src/security/tls_session.cpp
@@ -725,10 +725,7 @@ TlsSession::TlsSessionImpl::cleanup()
     stateCondition_.notify_all();
 
     if (session_) {
-        if (transport_.isReliable())
-            gnutls_bye(session_, GNUTLS_SHUT_RDWR);
-        else
-            gnutls_bye(session_, GNUTLS_SHUT_WR); // not wait for a peer answer
+        gnutls_bye(session_, GNUTLS_SHUT_WR); // not wait for a peer answer
         gnutls_deinit(session_);
         session_ = nullptr;
     }
@@ -1218,7 +1215,7 @@ TlsSession::TlsSession(SocketType& transport, const TlsParams& params,
 
 TlsSession::~TlsSession()
 {
-    shutdown();
+    if (pimpl_) shutdown();
 }
 
 bool
@@ -1237,8 +1234,8 @@ int
 TlsSession::maxPayload() const
 {
     if (pimpl_->state_ == TlsSessionState::SHUTDOWN)
-        throw std::runtime_error("Getting MTU from non-valid TLS session");
-    return gnutls_dtls_get_data_mtu(pimpl_->session_);
+        throw std::runtime_error("Getting maxPayload from non-valid TLS session");
+    return pimpl_->transport_.maxPayload();
 }
 
 const char*
@@ -1302,8 +1299,10 @@ TlsSession::read(ValueType* data, std::size_t size, std::error_code& ec)
         }
 
         if (ret == 0) {
-            JAMI_DBG("[TLS] eof");
-            shutdown();
+            if (pimpl_) {
+                JAMI_ERR("[TLS] eof");
+                shutdown();
+            }
             error = std::errc::broken_pipe;
             break;
         } else if (ret == GNUTLS_E_REHANDSHAKE) {
@@ -1312,8 +1311,10 @@ TlsSession::read(ValueType* data, std::size_t size, std::error_code& ec)
             pimpl_->rxCv_.notify_one(); // unblock waiting FSM
             pimpl_->stateCondition_.notify_all();
         } else if (gnutls_error_is_fatal(ret)) {
-            JAMI_ERR("[TLS] fatal error in recv: %s", gnutls_strerror(ret));
-            shutdown();
+            if (pimpl_) {
+                JAMI_ERR("[TLS] fatal error in recv: %s", gnutls_strerror(ret));
+                shutdown();
+            }
             error = std::errc::io_error;
             break;
         }
