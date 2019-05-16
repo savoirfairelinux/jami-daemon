8e343f75b226ff3f694f36d27864ceff33474d6d
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-05-16 14:03:05 -0400

    sip: negotiate both UDP and TCP for the control channel
    Our pjsip version supports the RFC6544. With this patch, when
    starting a call, the daemon is using two ICE sessions for the SIP
    channel. One is negotiating a UDP socket, and the other a TCP socket
    and transmits both SDP on the DHT.
    
    If both negotiations succeed, TCP is prefered and will be used
    to transmit SIP messages and the VCard. This should solve the 30
    seconds timeout on bad networks.
    
    Note that the media channel is still using UDP to transmit audio
    and video.
    
    MAJOR CHANGE: the SIP channel use TLS on top of TCP, no DTLS,
    so the transport is considered as reliable.
    
    Also lot of changes in rfc6544.patch to link to rfc6062. The patch
    needs to be cleaned, cf TODO notes
    
    MAJOR CHANGE: When the transport idle timeout is reached, tls_session
    will close the connection but will not wait the answer for the EOF
    from the peer.
    
    Change-Id: I55c5f51377fd8787bc951d6d282eec46f8eaf977
    Gitlab: #103

--- src/ice_transport.cpp
+++ src/ice_transport.cpp
@@ -26,7 +26,6 @@
 #include "upnp/upnp_control.h"
 
 #include <pjlib.h>
-#include <msgpack.hpp>
 
 #include <map>
 #include <atomic>
@@ -111,7 +110,7 @@ public:
     MutexGuard lk_api{apiMutex_, std::adopt_lock};
     MutexLock lk{mutex_, std::adopt_lock};
     auto a = cv_.wait_for(lk, timeout,
-                        [this] { return stop_ or /*(data.size() != 0)*/ !stream_.eof(); });
+                        [this] { return stop_ or !stream_.eof(); });
     return a;
   }
 
@@ -212,6 +211,8 @@ public:
     mutable std::mutex iceMutex_ {};
     pj_ice_strans_cfg config_;
     std::string last_errmsg_;
+
+    std::atomic_bool is_stopped_ {false};
 
     struct Packet {
       Packet(void *pkt, pj_size_t size)
@@ -869,11 +870,12 @@ IceTransport::Impl::onReceiveData(unsigned comp_id, void *pkt, pj_size_t size)
     if (on_recv_cb_) {
       on_recv_cb_();
     }
+
     if (io.cb) {
         io.cb((uint8_t*)pkt, size);
     } else {
         MutexLock lk{apiMutex_};
-        auto &channel = peerChannels_.at(comp_id);
+        auto &channel = peerChannels_.at(comp_id-1);
         lk.unlock();
         channel << std::string(reinterpret_cast<const char *>(pkt), size);
     }
@@ -905,6 +907,13 @@ IceTransport::isRunning() const
 {
     std::lock_guard<std::mutex> lk {pimpl_->iceMutex_};
     return pimpl_->_isRunning();
+}
+
+bool
+IceTransport::isStopped() const
+{
+    std::lock_guard<std::mutex> lk {pimpl_->iceMutex_};
+    return pimpl_->is_stopped_;
 }
 
 bool
@@ -950,12 +959,14 @@ IceTransport::start(const Attribute& rem_attrs, const std::vector<IceCandidate>&
 {
     if (not isInitialized()) {
         JAMI_ERR("[ice:%p] not initialized transport", this);
+        pimpl_->is_stopped_ = true;
         return false;
     }
 
     // pj_ice_strans_start_ice crashes if remote candidates array is empty
     if (rem_candidates.empty()) {
         JAMI_ERR("[ice:%p] start failed: no remote candidates", this);
+        pimpl_->is_stopped_ = true;
         return false;
     }
 
@@ -969,62 +980,49 @@ IceTransport::start(const Attribute& rem_attrs, const std::vector<IceCandidate>&
     if (status != PJ_SUCCESS) {
         pimpl_->last_errmsg_ = sip_utils::sip_strerror(status);
         JAMI_ERR("[ice:%p] start failed: %s", this, pimpl_->last_errmsg_.c_str());
+        pimpl_->is_stopped_ = true;
         return false;
     }
     return true;
 }
 
 bool
-IceTransport::start(const std::vector<uint8_t>& rem_data)
+IceTransport::start(const SDP& sdp)
 {
-    std::string rem_ufrag;
-    std::string rem_pwd;
+    if (not isInitialized()) {
+        JAMI_ERR("[ice:%p] not initialized transport", this);
+        pimpl_->is_stopped_ = true;
+        return false;
+    }
+
+    JAMI_DBG("[ice:%p] negotiation starting (%zu remote candidates)", this, sdp.candidates.size());
+    pj_str_t ufrag, pwd;
+
     std::vector<IceCandidate> rem_candidates;
-
-    auto data = reinterpret_cast<const char*>(rem_data.data());
-    auto size = rem_data.size();
-
-    try {
-        std::size_t offset = 0;
-        auto result = msgpack::unpack(data, size, offset);
-        auto version = result.get().as<uint8_t>();
-        JAMI_DBG("[ice:%p] rx msg v%u", this, version);
-        if (version == 1) {
-            result = msgpack::unpack(data, size, offset);
-            std::tie(rem_ufrag, rem_pwd) = result.get().as<std::pair<std::string, std::string>>();
-            result = msgpack::unpack(data, size, offset);
-            auto comp_cnt = result.get().as<uint8_t>();
-            while (comp_cnt-- > 0) {
-                result = msgpack::unpack(data, size, offset);
-                IceCandidate cand;
-                for (const auto& line : result.get().as<std::vector<std::string>>()) {
-                    if (getCandidateFromSDP(line, cand))
-                        rem_candidates.emplace_back(cand);
-                }
-            }
-        } else {
-            JAMI_ERR("[ice:%p] invalid msg version", this);
-            return false;
-        }
-    } catch (const msgpack::unpack_error& e) {
-        JAMI_ERR("[ice:%p] remote msg unpack error: %s", this, e.what());
+    rem_candidates.reserve(sdp.candidates.size());
+    IceCandidate cand;
+    for (const auto &line : sdp.candidates) {
+        if (getCandidateFromSDP(line, cand))
+            rem_candidates.emplace_back(cand);
+    }
+    auto status = pj_ice_strans_start_ice(pimpl_->icest_.get(),
+                                          pj_strset(&ufrag, (char*)sdp.ufrag.c_str(), sdp.ufrag.size()),
+                                          pj_strset(&pwd, (char*)sdp.pwd.c_str(), sdp.pwd.size()),
+                                          rem_candidates.size(),
+                                          rem_candidates.data());
+    if (status != PJ_SUCCESS) {
+        pimpl_->last_errmsg_ = sip_utils::sip_strerror(status);
+        JAMI_ERR("[ice:%p] start failed: %s", this, pimpl_->last_errmsg_.c_str());
+        pimpl_->is_stopped_ = true;
         return false;
     }
-
-    if (rem_ufrag.empty() or rem_pwd.empty() or rem_candidates.empty()) {
-        JAMI_ERR("[ice:%p] invalid remote attributes", this);
-        return false;
-    }
-
-    if (pimpl_->onlyIPv4Private_)
-        JAMI_WARN("[ice:%p] no public IPv4 found, your connection may fail!", this);
-
-    return start({rem_ufrag, rem_pwd}, rem_candidates);
+    return true;
 }
 
 bool
 IceTransport::stop()
 {
+    pimpl_->is_stopped_ = true;
     if (isStarted()) {
         auto status = pj_ice_strans_stop_ice(pimpl_->icest_.get());
         if (status != PJ_SUCCESS) {
@@ -1139,20 +1137,29 @@ IceTransport::registerPublicIP(unsigned compId, const IpAddr& publicIP)
 }
 
 std::vector<uint8_t>
-IceTransport::packIceMsg() const
+IceTransport::packIceMsg(uint8_t version) const
 {
-    static constexpr uint8_t ICE_MSG_VERSION = 1;
-
     if (not isInitialized())
         return {};
 
     std::stringstream ss;
-    msgpack::pack(ss, ICE_MSG_VERSION);
-    msgpack::pack(ss, std::make_pair(pimpl_->local_ufrag_, pimpl_->local_pwd_));
-    msgpack::pack(ss, static_cast<uint8_t>(pimpl_->component_count_));
-    for (unsigned i=0; i<pimpl_->component_count_; i++)
-        msgpack::pack(ss, getLocalCandidates(i));
-
+    if (version == 1) {
+        msgpack::pack(ss, version);
+        msgpack::pack(ss, std::make_pair(pimpl_->local_ufrag_, pimpl_->local_pwd_));
+        msgpack::pack(ss, static_cast<uint8_t>(pimpl_->component_count_));
+        for (unsigned i=0; i<pimpl_->component_count_; i++)
+            msgpack::pack(ss, getLocalCandidates(i));
+    } else {
+        SDP sdp;
+        sdp.ufrag = pimpl_->local_ufrag_;
+        sdp.pwd = pimpl_->local_pwd_;
+        for (unsigned i = 0; i < pimpl_->component_count_; i++) {
+            auto candidates = getLocalCandidates(i);
+            sdp.candidates.reserve(sdp.candidates.size() + candidates.size());
+            sdp.candidates.insert(sdp.candidates.end(), candidates.begin(), candidates.end());
+        }
+        msgpack::pack(ss, sdp);
+    }
     auto str(ss.str());
     return std::vector<uint8_t>(str.begin(), str.end());
 }
@@ -1372,6 +1379,53 @@ IceTransport::waitForData(int comp_id, unsigned int timeout, std::error_code& ec
     return channel.wait(std::chrono::milliseconds(timeout));
 }
 
+std::vector<SDP>
+IceTransport::parseSDPList(const std::vector<uint8_t>& msg)
+{
+    std::vector<SDP> sdp_list;
+
+    msgpack::unpacker pac;
+    pac.reserve_buffer(msg.size());
+    memcpy(pac.buffer(), msg.data(), msg.size());
+    pac.buffer_consumed(msg.size());
+    msgpack::object_handle oh;
+
+    while (auto result = pac.next(oh)) {
+        try {
+            SDP sdp;
+            if (oh.get().type == msgpack::type::POSITIVE_INTEGER) {
+                // Version 1
+                result = pac.next(oh);
+                if (!result) break;
+                std::tie(sdp.ufrag, sdp.pwd) = oh.get().as<std::pair<std::string, std::string>>();
+                result = pac.next(oh);
+                if (!result) break;
+                auto comp_cnt = oh.get().as<uint8_t>();
+                while (comp_cnt-- > 0) {
+                    result = pac.next(oh);
+                    if (!result) break;
+                    auto candidates = oh.get().as<std::vector<std::string>>();
+                    sdp.candidates.reserve(sdp.candidates.size() + candidates.size());
+                    sdp.candidates.insert(sdp.candidates.end(), candidates.begin(), candidates.end());
+                }
+            } else {
+                oh.get().convert(sdp);
+            }
+            sdp_list.emplace_back(sdp);
+        } catch (const msgpack::unpack_error &e) {
+            break;
+        }
+    }
+
+    return sdp_list;
+}
+
+bool
+IceTransport::isTCPEnabled()
+{
+    return pimpl_->config_.protocol == PJ_ICE_TP_TCP;
+}
+
 //==============================================================================
 
 IceTransportFactory::IceTransportFactory()
@@ -1442,6 +1496,7 @@ IceSocketTransport::maxPayload() const
 int
 IceSocketTransport::waitForData(unsigned ms_timeout, std::error_code& ec) const
 {
+    if (!ice_->isRunning()) return -1;
     return ice_->waitForData(compId_, ms_timeout, ec);
 }
 
@@ -1460,13 +1515,21 @@ IceSocketTransport::write(const ValueType* buf, std::size_t len, std::error_code
 std::size_t
 IceSocketTransport::read(ValueType* buf, std::size_t len, std::error_code& ec)
 {
-    auto res = ice_->recv(compId_, buf, len);
-    if (res < 0) {
-        ec.assign(errno, std::generic_category());
-        return 0;
+    if (!ice_->isRunning()) return 0;
+    try {
+        auto res = reliable_
+                ? ice_->recvfrom(compId_, reinterpret_cast<char *>(buf), len)
+                : ice_->recv(compId_, buf, len);
+        if (res < 0) {
+            ec.assign(errno, std::generic_category());
+            return 0;
+        }
+        ec.clear();
+        return res;
+    } catch (const std::exception &e) {
+        JAMI_ERR("IceSocketTransport::read exception: %s", e.what());
     }
-    ec.clear();
-    return res;
+    return 0;
 }
 
 IpAddr
