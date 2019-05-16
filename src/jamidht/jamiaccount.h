8082b365a161aab9b5760dab0b8acd3ce74b0f3c
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-05-16 14:03:05 -0400

    sip: negotiate both UDP and TCP for the control channel
    Our pjsip version supports the RFC6544. With this patch, when
    starting a call, the daemon is using two ICE sessions for the SIP
    channel. One is negotiating a UDP socket, and the other a TCP socket
    and transmits both SDP on the DHT.
    
    If both negotiations succeed, TCP is prefered and will be used
    to transmit SIP messages and the VCard. This should solve the 30
    seconds timeout on bad networks.
    
    Note that the media channel is still using UDP to transmit audio
    and video.
    
    MAJOR CHANGE: the SIP channel use TLS on top of TCP, no DTLS,
    so the transport is considered as reliable.
    
    Change-Id: I55c5f51377fd8787bc951d6d282eec46f8eaf977
    Gitlab: #103

--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -600,6 +600,7 @@ class JamiAccount : public SIPAccountBase {
 
         void replyToIncomingIceMsg(const std::shared_ptr<SIPCall>&,
                                    const std::shared_ptr<IceTransport>&,
+                                   const std::shared_ptr<IceTransport>&,
                                    const dht::IceCandidates&,
                                    const std::shared_ptr<dht::crypto::Certificate>& from_cert,
                                    const dht::InfoHash& from);
