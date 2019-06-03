ad64b3fc192a44cf45919c01212ba41288e507e2
Eden Abitbol <eden.abitbol@savoirfairelinux.com>
2019-06-03 12:23:00 -0400

    upnp: Support libupnp and libnatnatpmp simultaneously.
    Update libupnp to version 1.8.4.
    
    For windows, the IPV6 preprocessor must be undefined. Or else
    libupnp won't initialize. Added visual studio 2017 support
    via one patch for windows that also combines previous windows
    patches.
    
    UPnPController: Class that the jami classes use to control the
    opening and closing of ports. Every  service has it's own upnp
    controller. The controller does it's actions by using the upnp
    context class.
    
    UPnPContext: Class that holds a (linked) list of discovered IGDs
    and their corresponding protocols (which discovered them).
    Whenever the controller wants to add or remove a mapping, the
    context picks a valid IGD in it's list and uses the correct
    protocol to complete the required action.
    
    UPnPProtocol: Pure virtual base class that defines the functions
    needed by the context to use the corresponding protocol.
    
    PUPnP: UPnPProtocol derived class that represents a upnp client
    that uses the portable upnp library (libupnp). Every time the
    client discovers a new IGD it uses a callback to add it to the
    context's main IGD linked list. It also has an internal list of
    IGDs that it discovered. Added features to this class include:
    	- IGD event subscription.
    	- Use UpnpInit2 function instead of deprecated UpnpInit
    	  function. It's also supposed to support IPv6.
    
    NatPmp: UPnPProtocol derived class that represents a upnp client
    that uses the NAT-PMP libarry (libnatpmp). Unlike libupnp,
    libnatpmp only supports discovering one IGD. Also uses callbacks
    to add the IGD it finds to the context's main IGD class.
    
    Gitlab: #96
    
    Change-Id: I199271edac2c6d93dc60c24e2e2aefe36de7950c

--- src/upnp/mapping/mapping.cpp
+++ src/upnp/mapping/mapping.cpp
@@ -2,6 +2,7 @@
  *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
  *
  *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *	Author: Eden Abitbol <eden.abitbol@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,16 +19,15 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
-#include "upnp_igd.h"
+#include "mapping.h"
 
 namespace jami { namespace upnp {
 
-/* move constructor and operator */
-Mapping::Mapping(Mapping&& other) noexcept
-    : port_external_(other.port_external_)
-    , port_internal_(other.port_internal_)
-    , type_(other.type_)
-    , description_(std::move(other.description_))
+Mapping::Mapping(Mapping&& other) noexcept:
+    port_external_(other.port_external_),
+    port_internal_(other.port_internal_),
+    type_(other.type_),
+    description_(std::move(other.description_))
 {
     other.port_external_ = 0;
     other.port_internal_ = 0;
@@ -35,7 +35,8 @@ Mapping::Mapping(Mapping&& other) noexcept
 
 Mapping& Mapping::operator=(Mapping&& other) noexcept
 {
-    if (this != &other) {
+    if (this != &other)
+    {
         port_external_ = other.port_external_;
         other.port_external_ = 0;
         port_internal_ = other.port_internal_;
@@ -48,10 +49,6 @@ Mapping& Mapping::operator=(Mapping&& other) noexcept
 
 bool operator== (const Mapping& cMap1, const Mapping& cMap2)
 {
-    /* we don't compare the description because it doesn't change the function of the
-     * mapping; we don't compare the IGD because for now we assume that we always
-     * use the same one and that all mappings are active
-     */
     return (cMap1.port_external_ == cMap2.port_external_ &&
             cMap1.port_internal_ == cMap2.port_internal_ &&
             cMap1.type_ == cMap2.type_);
@@ -61,5 +58,15 @@ bool operator!= (const Mapping& cMap1, const Mapping& cMap2)
 {
     return !(cMap1 == cMap2);
 }
+
+std::string Mapping::toString() const
+{
+    return getPortExternalStr() + ":" + getPortInternalStr() + ", " + getTypeStr();
+}
+
+bool Mapping::isValid() const
+{
+    return port_external_ == 0 or port_internal_ == 0 ? false : true;
+};
 
 }} // namespace jami::upnp
