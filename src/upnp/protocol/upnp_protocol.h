fec27dc97d148beec085b0544a3d87bf4e411062
Eden Abitbol <eden.abitbol@savoirfairelinux.com>
2019-06-03 12:23:00 -0400

    upnp: Support libupnp and libnatnatpmp simultaneously.
    Update libupnp to version 1.8.4.
    
    For windows, the IPV6 preprocessor must be undefined. Or else
    libupnp won't initialize. Added visual studio 2017 support
    via one patch for windows that also combines previous windows
    patches.
    
    UPnPController: Class that the jami classes use to control the
    opening and closing of ports. Every  service has it's own upnp
    controller. The controller does it's actions by using the upnp
    context class.
    
    UPnPContext: Class that holds a (linked) list of discovered IGDs
    and their corresponding protocols (which discovered them).
    Whenever the controller wants to add or remove a mapping, the
    context picks a valid IGD in it's list and uses the correct
    protocol to complete the required action.
    
    UPnPProtocol: Virtual base class that defines the functions
    needed by the context to use the corresponding protocol.
    
    PUPnP: UPnPProtocol derived class that represents a upnp client
    that uses the portable upnp library (libupnp). Every time the
    client discovers a new IGD it uses a callback to add it to the
    context's main IGD linked list. It also has an internal list of
    IGDs that it discovered. Added features to this class include:
    	- IGD event subscription.
    	- Use UpnpInit2 function instead of deprecated UpnpInit
    	  function. It's also supposed to support IPv6.
    
    NatPmp: UPnPProtocol derived class that represents a upnp client
    that uses the NAT-PMP libarry (libnatpmp). Unlike libupnp,
    libnatpmp only supports discovering one IGD. Also uses callbacks
    to add the IGD it finds to the context's main IGD class.
    
    Also inclided debug warning prints whenever a controller opens
    and closes the ports.
    
    Gitlab: #96
    
    Change-Id: I199271edac2c6d93dc60c24e2e2aefe36de7950c

---
+++ src/upnp/protocol/upnp_protocol.h
@@ -0,0 +1,93 @@
+/*
+ *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
+ *
+ *	Author: Eden Abitbol <eden.abitbol@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "igd.h"
+#include "mapping.h"
+
+#include "logger.h"
+#include "noncopyable.h"
+#include "ip_utils.h"
+#include "string_utils.h"
+
+#include <map>
+#include <string>
+#include <chrono>
+#include <functional>
+#include <condition_variable>
+
+namespace jami { namespace upnp {
+
+// UPnP device descriptions.
+constexpr static const char * UPNP_ROOT_DEVICE = "upnp:rootdevice";
+constexpr static const char * UPNP_IGD_DEVICE = "urn:schemas-upnp-org:device:InternetGatewayDevice:1";
+constexpr static const char * UPNP_WAN_DEVICE = "urn:schemas-upnp-org:device:WANDevice:1";
+constexpr static const char * UPNP_WANCON_DEVICE = "urn:schemas-upnp-org:device:WANConnectionDevice:1";
+constexpr static const char * UPNP_WANIP_SERVICE = "urn:schemas-upnp-org:service:WANIPConnection:1";
+constexpr static const char * UPNP_WANPPP_SERVICE = "urn:schemas-upnp-org:service:WANPPPConnection:1";
+
+// Pure virtual interface class that UPnPContext uses to call protocol functions.
+class UPnPProtocol
+{
+public:
+    enum class UpnpError : int {
+        INVALID_ERR = -1,
+        ERROR_OK,
+        SOAP_INVALID_ARGS,
+        ARRAY_IDX_INVALID,
+        CONFLICT_IN_MAPPING
+    };
+
+    using IgdListChangedCallback = std::function<bool(UPnPProtocol*, IGD*, IpAddr, bool)>;
+
+    UPnPProtocol(){};
+    virtual ~UPnPProtocol(){};
+
+    // Signals a change in the network.
+    virtual void connectivityChanged() = 0;
+
+    // Search for IGD.
+    virtual void searchForIGD() = 0;
+
+    // Tries to add mapping. Assumes mutex is already locked.
+    virtual Mapping addMapping(IGD* igd, uint16_t port_external, uint16_t port_internal, PortType type, UPnPProtocol::UpnpError& upnp_error) = 0;
+
+    // Removes a mapping.
+    virtual void removeMapping(const Mapping& mapping) = 0;
+
+    // Removes all local mappings of IGD that we're added by the application.
+    virtual void removeAllLocalMappings(IGD* igd) = 0;
+
+    // Set the IGD list callback handler.
+    void setOnIgdChanged(IgdListChangedCallback&& cb) { updateIgdListCb_ = std::move(cb); }
+
+protected:
+    mutable std::mutex validIGDMutex_;                      // Mutex used to access these lists and IGDs in a thread-safe manner.
+    std::condition_variable validIGDCondVar_;				// Variable to notify changes in IGDs.
+
+    IgdListChangedCallback updateIgdListCb_;				// Callback for when the IGD list changes.
+};
+
+}} // namespace jami::upnp
