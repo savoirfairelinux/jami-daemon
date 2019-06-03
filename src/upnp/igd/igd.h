c9743d40d517145a73857920a5027d0eff657137
Eden Abitbol <eden.abitbol@savoirfairelinux.com>
2019-06-03 12:23:00 -0400

    upnp: Support libupnp and libnatnatpmp simultaneously.
    Update libupnp to version 1.8.4.
    
    For windows, the IPV6 preprocessor must be undefined. Or else
    libupnp won't initialize. Added visual studio 2017 support
    via one patch for windows that also combines previous windows
    patches.
    
    UPnPController: Class that the jami classes use to control the
    opening and closing of ports. Every  service has it's own upnp
    controller. The controller does it's actions by using the upnp
    context class.
    
    UPnPContext: Class that holds a (linked) list of discovered IGDs
    and their corresponding protocols (which discovered them).
    Whenever the controller wants to add or remove a mapping, the
    context picks a valid IGD in it's list and uses the correct
    protocol to complete the required action.
    
    UPnPProtocol: Pure virtual base class that defines the functions
    needed by the context to use the corresponding protocol.
    
    PUPnP: UPnPProtocol derived class that represents a upnp client
    that uses the portable upnp library (libupnp). Every time the
    client discovers a new IGD it uses a callback to add it to the
    context's main IGD linked list. It also has an internal list of
    IGDs that it discovered. Added features to this class include:
    	- IGD event subscription.
    	- Use UpnpInit2 function instead of deprecated UpnpInit
    	  function. It's also supposed to support IPv6.
    
    NatPmp: UPnPProtocol derived class that represents a upnp client
    that uses the NAT-PMP libarry (libnatpmp). Unlike libupnp,
    libnatpmp only supports discovering one IGD. Also uses callbacks
    to add the IGD it finds to the context's main IGD class.
    
    Gitlab: #96
    
    Change-Id: I199271edac2c6d93dc60c24e2e2aefe36de7950c

---
+++ src/upnp/igd/igd.h
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
+ *
+ *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *	Author: Eden Abitbol <eden.abitbol@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+#pragma once
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string>
+#include <map>
+#include <functional>
+#include <chrono>
+
+#include "noncopyable.h"
+#include "ip_utils.h"
+#include "string_utils.h"
+
+#include "../mapping/global_mapping.h"
+
+namespace jami { namespace upnp {
+
+// Subclasses to make it easier to differentiate and cast maps of port mappings.
+class PortMapLocal : public std::map<uint16_t, Mapping> {};
+class PortMapGlobal : public std::map<uint16_t, GlobalMapping> {};
+
+using IGDFoundCallback = std::function<void()>;
+
+class IGD 
+{
+public:
+    IGD(IpAddr&& localIp = {}, IpAddr&& publicIp = {});                               
+    IGD(IGD&&) = default;               
+    virtual ~IGD() = default;
+
+    IGD& operator=(IGD&&) = default;  
+      
+    bool operator==(IGD& other) const;
+    
+public:
+    IpAddr localIp_ {};                    // Internal IP interface used to communication with IGD.
+    IpAddr publicIp_ {};                   // External IP of IGD.
+
+    PortMapGlobal udpMappings {};          // IGD UDP port mappings.
+    PortMapGlobal tcpMappings {};          // IGD TCP port mappings.
+
+private:
+    NON_COPYABLE(IGD);
+};
+
+
+}} // namespace jami::upnp
---
+++ src/upnp/igd/upnp_igd.cpp
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
+ *
+ *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *	Author: Eden Abitbol <eden.abitbol@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "upnp_igd.h"
+
+namespace jami { namespace upnp {
+
+#if HAVE_LIBUPNP
+
+UPnPIGD::UPnPIGD(std::string&& UDN, std::string&& baseURL, std::string&& friendlyName, std::string&& serviceType, std::string&& serviceId, std::string&& controlURL, std::string&& eventSubURL,
+    IpAddr&& localIp, IpAddr&& publicIp): 
+    IGD(std::move(localIp), std::move(publicIp))
+{
+    UDN_ = std::move(UDN); 
+    baseURL_ = std::move(baseURL); 
+    friendlyName_ = std::move(friendlyName); 
+    serviceType_ = std::move(serviceType);
+    serviceId_ = std::move(serviceId); 
+    controlURL_ = std::move(controlURL); 
+    eventSubURL_ = std::move(eventSubURL);
+}
+
+bool 
+UPnPIGD::operator==(IGD& other) const 
+{
+    return localIp_ == other.localIp_ and publicIp_ == other.publicIp_;
+}
+
+bool 
+UPnPIGD::operator==(UPnPIGD& other) const
+{
+    if (localIp_ and publicIp_) {
+        if (localIp_ != other.localIp_ or publicIp_ != other.publicIp_) {
+            return false;
+        }
+    }
+
+    return UDN_ == other.UDN_ and
+           baseURL_ == other.baseURL_ and
+           friendlyName_ == other.friendlyName_ and
+           serviceType_ == other.serviceType_ and
+           serviceId_ == other.serviceId_ and
+           controlURL_ == other.controlURL_ and
+           eventSubURL_ == other.eventSubURL_;
+}
+
+#endif
+
+}} // namespace jami::upnp
