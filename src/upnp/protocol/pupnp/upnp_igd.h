c8b3c61195b5bbce1dcc7482ebc84050fd9cce8e
Eden Abitbol <eden.abitbol@savoirfairelinux.com>
2019-06-03 12:23:00 -0400

    upnp: support libupnp and libnatnatpmp simultaneously
    Update libupnp to version 1.8.4.
    
    For windows, the IPV6 preprocessor must be undefined. Or else
    libupnp won't initialize. Added visual studio 2017 support
    via one patch for windows that also combines previous windows
    patches.
    
    UPnPController: Class that the jami classes use to control the
    opening and closing of ports. Every  service has it's own upnp
    controller. The controller does it's actions by using the upnp
    context class. Also refactored the functions used to add
    mappings. Instead of using two different functions with
    different types of parameters, we now use one function with
    parameters that have default values. The logic stays the same
    but the function call is more clear.
    
    UPnPContext: Class that holds a (linked) list of discovered IGDs
    and their corresponding protocols (which discovered them).
    Whenever the controller wants to add or remove a mapping, the
    context picks a valid IGD in it's list and uses the correct
    protocol to complete the required action. This class also has
    the ability to swap protocols for an IGD that was discovered
    by more then one protocol.
    
    UPnPProtocol: Virtual base class that defines the functions
    needed by the context to use the corresponding protocol.
    
    PUPnP: UPnPProtocol derived class that represents a upnp client
    that uses the portable upnp library (libupnp). Every time the
    client discovers a new IGD it uses a callback to add it to the
    context's main IGD linked list. It also has an internal list of
    IGDs that it discovered. Added features to this class include:
    	- IGD event subscription.
    	- Use UpnpInit2 function instead of deprecated UpnpInit
    	  function. It's also supposed to support IPv6.
    
    NatPmp: UPnPProtocol derived class that represents a upnp client
    that uses the NAT-PMP library (libnatpmp). Unlike libupnp,
    libnatpmp only supports discovering one IGD. Also uses callbacks
    to add the IGD it finds to the context's main IGD class.
    
    Also inclided debug warning prints whenever a controller opens
    and closes the ports. That way we can keep track of whenever
    the application opens and closes ports on the internet
    gateway device.
    
    Gitlab: #96
    
    Change-Id: I199271edac2c6d93dc60c24e2e2aefe36de7950c

---
+++ src/upnp/protocol/pupnp/upnp_igd.h
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
+ *
+ *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
+ *	Author: Eden Abitbol <eden.abitbol@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "../igd.h"
+#include "../global_mapping.h"
+
+#include "noncopyable.h"
+#include "ip_utils.h"
+#include "string_utils.h"
+
+#include <map>
+#include <string>
+#include <chrono>
+#include <functional>
+
+namespace jami { namespace upnp {
+
+class UPnPIGD : public IGD
+{
+public:
+    UPnPIGD(std::string&& UDN,
+            std::string&& baseURL,
+            std::string&& friendlyName,
+            std::string&& serviceType,
+            std::string&& serviceId,
+            std::string&& controlURL,
+            std::string&& eventSubURL,
+            IpAddr&& localIp = {},
+            IpAddr&& publicIp = {});
+    ~UPnPIGD(){}
+    const std::string& getUDN() const          { return UDN_;          };
+    const std::string& getBaseURL() const      { return baseURL_;      };
+    const std::string& getFriendlyName() const { return friendlyName_; };
+    const std::string& getServiceType() const  { return serviceType_;  };
+    const std::string& getServiceId() const    { return serviceId_;    };
+    const std::string& getControlURL() const   { return controlURL_;   };
+    const std::string& getEventSubURL() const  { return eventSubURL_;  };
+
+    bool operator==(IGD& other) const;
+    bool operator==(UPnPIGD& other) const;
+
+private:
+    std::string UDN_ {};
+    std::string baseURL_ {};
+    std::string friendlyName_ {};
+    std::string serviceType_ {};
+    std::string serviceId_ {};
+    std::string controlURL_ {};
+    std::string eventSubURL_ {};
+};
+
+}} // namespace jami::upnp
