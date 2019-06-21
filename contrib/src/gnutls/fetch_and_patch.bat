563b1bf3ea78b666004148edf50e70fc2c9e42ab
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-06-21 11:55:46 -0400

    contrib: bump gnutls and nettle on windows
    Change-Id: I8db69d51a75c9f52e0388856eb7bd1800ca80c75

--- contrib/src/gnutls/fetch_and_patch.bat
+++ contrib/src/gnutls/fetch_and_patch.bat
@@ -1,29 +1,28 @@
 set BUILD=%SRC%..\build
 
-set GNUTLS_VERSION=018a4a655ad784b1ef4f1b311ff031aeed656090
-set GNUTLS_URL=https://github.com/ShiftMediaProject/gnutls/archive/%GNUTLS_VERSION%.tar.gz
+set GNUTLS_VERSION=3.6.7
+set GNUTLS_URL=https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-%GNUTLS_VERSION%.tar.xz
 
 mkdir %BUILD%
 
 if %USE_CACHE%==1 (
-    copy %CACHE_DIR%\%GNUTLS_VERSION%.tar.gz %cd%
+    copy %CACHE_DIR%\gnutls-%GNUTLS_VERSION%.tar.xz %cd%
 ) else (
     %WGET_CMD% %GNUTLS_URL%
 )
 
-7z -y x %GNUTLS_VERSION%.tar.gz && 7z -y x %GNUTLS_VERSION%.tar -o%BUILD%
-del %GNUTLS_VERSION%.tar && del %GNUTLS_VERSION%.tar.gz && del %BUILD%\pax_global_header
+7z -y x gnutls-%GNUTLS_VERSION%.tar.xz && 7z -y x gnutls-%GNUTLS_VERSION%.tar -o%BUILD%
+del gnutls-%GNUTLS_VERSION%.tar && del gnutls-%GNUTLS_VERSION%.tar.xz
 rename %BUILD%\gnutls-%GNUTLS_VERSION% gnutls
 
 cd %BUILD%\gnutls
 
-%APPLY_CMD% %SRC%\gnutls\gnutls-no-egd.patch
 %APPLY_CMD% %SRC%\gnutls\read-file-limits.h.patch
 
 if "%1"=="uwp" (
     %APPLY_CMD% %SRC%\gnutls\gnutls-uwp.patch
 )
 
-%APPLY_CMD% %SRC%\gnutls\gnutls-mscver.patch
+rem %APPLY_CMD% %SRC%\gnutls\gnutls-mscver.patch
 
 cd %SRC%
