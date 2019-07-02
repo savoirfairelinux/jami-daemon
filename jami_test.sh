b17b6a96691b7a628d629f6b55f17a974b858a89
mfenjiro <mohamed.fenjiro@savoirfairelinux.com>
2019-07-02 14:34:50 -0400

    framework testing
    integrate monitoring script to the daemon it can be launched with 'make framework'
    this would start a daemon instance and call ./jami_test.sh
    A series of call is made and it return the failure rate.
    
    Change-Id: I2fb430bb7d4b3b3f0e79307ef9fbbbbf5f011b4e

---
+++ jami_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#=================================================================================================================================
+#description:    This script will call a dbus instance and jami_test.py script for monitoring on nagios
+#authors:        Mohamed Fenjiro <mohamed.fenjiro@savoirfairelinux.com>
+#usage:          ./jami_test.sh [-h] [--messages MESSAGES] [--duration DURATION] [--interval INTERVAL] [--peer PEER] [--calls CALLS]
+#notes:          see https://wiki.savoirfairelinux.com/wiki/Jami-monitorpeervm-01.mtl.sfl#Directives for more info
+#=================================================================================================================================
+
+if test -z "$DBUS_SESSION_BUS_ADDRESS" ; then
+	eval `dbus-launch --sh-syntax`
+	echo "D-Bus per-session daemon address is:"
+	echo "$DBUS_SESSION_BUS_ADDRESS" > ~/dbus_address
+fi
+
+TIMESTAMP=`date "+%Y-%m-%d-%H:%M:%S"`
+
+./bin/dring -cd &
+
+if [ ! -f "~/dbus_address" ]; then
+	echo "No daemon detected. Abort";
+fi
+
+TESTER="./tools/dringctrl/jami_test.py"
+DBUS_ADDR=$(cat ~/dbus_address)
+DBUS_SESSION_BUS_ADDRESS=${DBUS_ADDR} ${TESTER} "$@"
