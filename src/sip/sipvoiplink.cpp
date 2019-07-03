bf57ec10d0b2c1e693ab51fda42fb04cba020453
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-07-03 17:20:45 -0400

    call: edit call status code and how the status code is set
    - make sure that status code is correct corresponding with call stage
    
    Change-Id: I75fd260bf46af1346fa0da2bd145cf3b385c4036

--- src/sip/sipvoiplink.cpp
+++ src/sip/sipvoiplink.cpp
@@ -788,6 +788,8 @@ invite_session_state_changed_cb(pjsip_inv_session *inv, pjsip_event *ev)
                     break;
                 // When the peer manually refuse the call
                 case PJSIP_SC_DECLINE:
+                    call->onFailure(PJSIP_SC_DECLINE);
+                    break;
                 case PJSIP_SC_BUSY_EVERYWHERE:
                     if (inv->role != PJSIP_ROLE_UAC)
                         break;
@@ -796,6 +798,9 @@ invite_session_state_changed_cb(pjsip_inv_session *inv, pjsip_event *ev)
                     break;
                 // The call terminates normally - BYE / CANCEL
                 case PJSIP_SC_OK:
+                    call->onClosed();
+                    JAMI_ERR("dsadafafdadasdad");
+                    break;
                 case PJSIP_SC_REQUEST_TERMINATED:
                     call->onClosed();
                     break;
