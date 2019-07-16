1142a23a2985fd1a50fcd44f7b1a223f5c3660be
Pierre Lespagnol <pierre.lespagnol@savoirfairelinux.com>
2019-07-16 10:07:17 -0400

    autoadapt: add function to set bitrate on the fly
    Used in 'adaptQualityAndBitrate' function to decrease bitrate without restart the media (no keyframe sent)
    
    Change-Id: I1faad6e3cdc83f846d2279d9b8b997f1656f8596

--- src/media/media_encoder.h
+++ src/media/media_encoder.h
@@ -100,6 +100,8 @@ public:
     const std::string& getAudioCodec() const { return audioCodec_; }
     const std::string& getVideoCodec() const { return videoCodec_; }
 
+    void setBitrate(uint64_t mb);
+
 #ifdef RING_ACCEL
     void enableAccel(bool enableAccel);
 #endif
@@ -116,6 +118,7 @@ private:
     int initStream(const SystemCodecInfo& systemCodecInfo, AVBufferRef* framesCtx);
     void openIOContext();
     void startIO();
+    AVCodecContext* getCurrentVideoAVCtx();
 
     std::vector<AVCodecContext*> encoders_;
     AVFormatContext *outputCtx_ = nullptr;
@@ -123,6 +126,7 @@ private:
     int currentStreamIdx_ = -1;
     unsigned sent_samples = 0;
     bool initialized_ {false};
+    unsigned int currentVideoCodecID_ {0};
 
 #ifdef ENABLE_VIDEO
     video::VideoScaler scaler_;
