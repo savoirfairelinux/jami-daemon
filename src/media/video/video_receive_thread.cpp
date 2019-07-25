c675488598b84f63e483f2472db8795716a42060
amine younes <amine.younes-bouacida@savoirfairelinux.com>
2019-07-25 13:32:52 -0400

    feat(filters): move from tensorflow to tensorflowlite
    Change-Id: I73f0f2102be47e51c4a8a699a7fc571513eeae78

--- src/media/video/video_receive_thread.cpp
+++ src/media/video/video_receive_thread.cpp
@@ -78,6 +78,8 @@ bool VideoReceiveThread::setup()
     videoDecoder_.reset(new MediaDecoder([this](const std::shared_ptr<MediaFrame>& frame) mutable {
         if (auto displayMatrix = displayMatrix_)
             av_frame_new_side_data_from_buf(frame->pointer(), AV_FRAME_DATA_DISPLAYMATRIX, av_buffer_ref(displayMatrix.get()));
+        if (auto videoFrame = std::dynamic_pointer_cast<VideoFrame>(frame))
+        	mediaProcessor_.onNewFrame(*videoFrame);
         publishFrame(std::static_pointer_cast<VideoFrame>(frame));
     }));
 
