51c4054e0afd6b08e5b317788ff625fda1f5c2ed
amine younes <amine.younes-bouacida@savoirfairelinux.com>
2019-07-25 13:32:52 -0400

    feat(filters): move from tensorflow to tensorflowlite
    Change-Id: I73f0f2102be47e51c4a8a699a7fc571513eeae78

---
+++ src/media/filters/model.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// Std libraries
+#include <string>
+#include <vector>
+
+struct Model {
+    std::string modelPath =
+        "/data/user/0/cx.ring/files/tensorflow/detect_300_quant.tflite";
+    std::vector<unsigned int> normalizationValues;
+
+    // Tensorflow specific settings
+    bool useNNAPI = true;
+    bool allowFp16PrecisionForFp32 = false;
+    unsigned int numberOfThreads = 4;
+
+    // User defined details
+    bool inputFloating = false;
+    unsigned int numberOfRuns = 1;
+};
+
+struct SupervisedModel : Model {
+    std::string labelsPath =
+        "/data/user/0/cx.ring/files/tensorflow/detect_labels.txt";
+    unsigned int labelsPadding = 16;
+};
