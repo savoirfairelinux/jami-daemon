035762ba09a04b8cb8feca23f2c6ae71718e1c50
amine younes <amine.younes-bouacida@savoirfairelinux.com>
2019-07-25 13:32:52 -0400

    feat(filters): move from tensorflow to tensorflowlite
    Change-Id: I73f0f2102be47e51c4a8a699a7fc571513eeae78

---
+++ src/media/filters/media_processor.h
@@ -0,0 +1,155 @@
+#pragma once
+// System includes
+#include <condition_variable>
+#include <cstdint>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
+// OpenCV headers
+#include <opencv2/core.hpp>
+// LibFilters headers
+#include "framelistener.h"
+#include "multipleobjecttracking.h"
+// Jami Library headers
+#include "dring/videomanager_interface.h"
+#include "media/video/video_scaler.h"
+
+namespace jami {
+class FrameCopy {
+public:
+  // This frame is a resized version of the original in RGB format
+  VideoFrame resizedFrameRGB;
+  // This frame is used to draw predictions into in RGB format
+  VideoFrame predictionsFrameBGR;
+  // An instance of the scaler
+  video::VideoScaler scaler;
+
+  /**
+   * @brief createCopyFrames
+   * Takes the input frame and copies/scales the content in the
+   * frames contained by FrameCopy
+   * @param frame
+   */
+  void createCopyFrames(const VideoFrame &frame,
+                        const int resizedImageWidth,
+                        const int resizedImageHeight);
+
+  /**
+   * @brief copyFrameContent
+   * Copies the data from the input frame iFrame to the output frame oFrame
+   * Performs the scaling and format conversion
+   * @param iFrame
+   * @param oFrame
+   */
+  void copyFrameContent(const VideoFrame &iFrame, VideoFrame &oFrame);
+};
+
+class MediaProcessor : public FrameListener {
+public:
+  MediaProcessor();
+  virtual ~MediaProcessor() override;
+
+  /**
+   * @brief onNewFrame
+   * Takes a const frame and processes its data
+   * @param frame
+   */
+  void onNewFrame(const VideoFrame &frame) override;
+
+  /**
+   * @brief onNewFrame
+   * Takes a frame and updates its content
+   * @param frame
+   */
+  void onNewFrame(VideoFrame &frame) override;
+
+  /**
+   * @brief stop
+   * Notifies the processing thread to stop processing
+   */
+  void stop();
+
+  /**
+   * @brief feedInput
+   * Takes a frame and feeds it to the model storage for predictions
+   * @param frame
+   */
+  void feedInput(const VideoFrame &frame);
+
+  /**
+   * @brief computePredictions
+   * Uses the model to compute the predictions and store them in
+   * computedPredictions
+   */
+  void computePredictions();
+  /**
+   * @brief printPredictions
+   * Prints the predictions names, probabilities
+   * and bounding boxes (top left point, bottom right point)
+   */
+  void printPredictions();
+
+  /**
+   * @brief drawPredictionsOnFrame
+   * Takes a BGR frame (to conform with OpenCV format), a tuple of predictions
+   * and draws the predictions on the frame It draws the bounding box and writes
+   * text for each prediction
+   * @param frame
+   * @param predictions: tuple(BoundinxBox, probability, labelIndex)
+   * @param nbPredictions: maximum number of predictions to draw
+   * @param threshold: Only display predictions with a probability > threshold
+   */
+  void drawPredictionsOnFrame(
+      const VideoFrame &frame,
+      const std::vector<std::tuple<std::array<float, 4>, float, int>>
+          &predictions,
+      const unsigned int nbPredictions = 5, const float threshold = 0.4f);
+
+private:
+  std::chrono::steady_clock::duration delta = std::chrono::nanoseconds::zero();
+  int counter = 0;
+  // Status variables of the processing
+  bool firstRun{true};
+  bool running{true};
+  bool newFrame{false};
+  bool processing{false};
+
+  FrameCopy fcopy;
+  std::thread processFrameThread;
+
+  std::mutex inputLock;
+  std::condition_variable inputCv;
+
+  // Model being used
+  MultipleObjectTracking mot;
+
+  // Output predictions
+  std::vector<std::tuple<std::array<float, 4>, float, int>> computedPredictions;
+
+  // Colors
+  cv::Scalar colors[10] = {
+      // Red
+      cv::Scalar{75, 25, 230},
+      // Orange
+      cv::Scalar{48, 130, 245},
+      // Yellow
+      cv::Scalar{25, 255, 255},
+      // Lime
+      cv::Scalar{25, 245, 210},
+      // Green
+      cv::Scalar{75, 180, 60},
+      // Cyan
+      cv::Scalar{240, 240, 70},
+      // Blue
+      cv::Scalar{200, 130, 0},
+      // Purple
+      cv::Scalar{180, 30, 145},
+      // Magenta
+      cv::Scalar{230, 50, 240},
+      // Apricot
+      cv::Scalar{180, 215, 255},
+  };
+};
+
+} // namespace jami
