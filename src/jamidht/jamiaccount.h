ad112924693063c6b2f76e87869c4c4edf886deb
Eden Abitbol <eden.abitbol@savoirfairelinux.com>
2019-08-05 14:06:26 -0400

    upnp: asynchronous open/close ports
    The UPnP stack now opens and closes ports with an
    asynchronous mechanic and with callbacks.
    
    Every time a mapping request (open or close) is sent,
    a callback is registered and managed by the context.
    When the corresponding protocol executes the request and
    gets an answer, the right callback is found and dispatched
    to its controller. A timeout of 1 second is implemented
    on each request.
    
    Controllers can now be tracked with a unique Id (a string
    that represents its location in memory).
    
    The IGD class now tracks its current active ports and
    manages the number of users for each port.
    
    The PMPIGD class now operates using a queue of mapping
    requests. Whenever a request is made to the natpmp protocol,
    it saves the request to its corresponding list (mapToAdd,
    mapToRemove or mapToRenew in the PMPIGD class) and notifies
    the main natpmp thread. The thread then checks if there
    is any pending map requests in the PMPIGD queue and treats
    them accordingly.
    
    Change-Id: I7b09c5b7ac69276359f509392a688a847ca17a90

--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -76,6 +76,8 @@ class ContactList;
 class AccountManager;
 struct AccountInfo;
 
+using namespace std::placeholders;
+
 /**
  * @brief Ring Account is build on top of SIPAccountBase and uses DHT to handle call connectivity.
  */
@@ -494,7 +496,12 @@ private:
     /**
      * Maps require port via UPnP
      */
-    bool mapPortUPnP();
+    void mapPortUPnP();
+
+    /**
+     * Add port mapping callback function.
+     */
+    void onPortMappingAdd(uint16_t* port_used, bool success);
 
     void igdChanged();
 
@@ -554,7 +561,7 @@ private:
     std::string nameServer_;
     std::string registeredName_;
 #endif
-        std::shared_ptr<dht::Logger> logger_;
+    std::shared_ptr<dht::Logger> logger_;
 
     std::shared_ptr<dht::DhtRunner> dht_ {};
     std::unique_ptr<AccountManager> accountManager_;
