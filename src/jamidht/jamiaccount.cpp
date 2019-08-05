48e15146288664cd671ed2cef258f80f7f6d8a8b
Eden Abitbol <eden.abitbol@savoirfairelinux.com>
2019-08-05 14:06:26 -0400

    upnp: open and close ports in async manner
    Change-Id: I7b09c5b7ac69276359f509392a688a847ca17a90

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -2011,42 +2011,35 @@ JamiAccount::handlePendingCall(PendingCall& pc, bool incoming)
     return true;
     }
 
-bool
+void
 JamiAccount::mapPortUPnP()
 {
-    // return true if not using UPnP
-    bool added = true;
+    using namespace std::placeholders;
+    upnp_->addMapping(std::bind(&JamiAccount::onPortMappingAdd, this, _1, _2),
+                      dhtPort_, jami::upnp::PortType::UDP, false);
+}
 
-    if (getUPnPActive()) {
-        /* create port mapping from published port to local port to the local IP
-         * note that since different RING accounts can use the same port,
-         * it may already be open, thats OK
-         *
-         * if the desired port is taken by another client, then it will try to map
-         * a different port, if succesfull, then we have to use that port for DHT
-         */
-        uint16_t port_used;
-        std::lock_guard<std::mutex> lock(upnp_mtx);
-        upnp_->removeMappings();
-        added = upnp_->addMapping(dhtPort_, jami::upnp::PortType::UDP, false, &port_used);
-        if (added) {
-            if (port_used != dhtPort_)
-                JAMI_WARN("[Account %s] Could not map port %u for DHT, using %u instead.", getAccountID().c_str(), dhtPort_, port_used);
-            dhtPortUsed_ = port_used;
-        }
-    }
+void 
+JamiAccount::onPortMappingAdd(uint16_t* port_used, bool success)
+{
+    JAMI_WARN("JamiAccount: Port mapping added NOTIFY");
 
-    upnp_->setIGDListener([w=weak()] {
-        if (auto shared = w.lock())
-            shared->igdChanged();
-    });
-    return added;
+    auto oldPort = static_cast<in_port_t>(dhtPortUsed_);
+    auto newPort = success ? *port_used : dhtPort_;
+    if (oldPort != newPort or not dht_.isRunning()){
+        if (not dht_.isRunning())
+            JAMI_WARN("Starting DHT on port %u", newPort);
+        if (oldPort != newPort and dht_.isRunning())
+            JAMI_WARN("DHT port changed to %u: restarting network", newPort);
+        dhtPortUsed_ = newPort;
+        doRegister_();
+    } else
+        dht_.connectivityChanged();
 }
 
 void
 JamiAccount::doRegister()
 {
-    std::unique_lock<std::mutex> lock(configurationMutex_);
     if (not isUsable()) {
         JAMI_WARN("Account must be enabled and active to register, ignoring");
         return;
@@ -2066,17 +2059,9 @@ JamiAccount::doRegister()
     /* if UPnP is enabled, then wait for IGD to complete registration */
     if (upnp_) {
         JAMI_DBG("UPnP: waiting for IGD to register RING account");
-        lock.unlock();
         setRegistrationState(RegistrationState::TRYING);
-        std::thread{ [w=weak()] {
-            if (auto acc = w.lock()) {
-                if (not acc->mapPortUPnP())
-                    JAMI_WARN("UPnP: Could not successfully map DHT port with UPnP, continuing with account registration anyways.");
-                acc->doRegister_();
-            }
-        }}.detach();
+        mapPortUPnP();
     } else {
-        lock.unlock();
         doRegister_();
     }
 }
@@ -2732,8 +2717,7 @@ JamiAccount::doUnregister(std::function<void(bool)> released_cb)
     }
 
     if (upnp_) {
-        upnp_->setIGDListener();
-        upnp_->removeMappings();
+        upnp_->removeAllLocalMappings();
     }
 
     dht_.join();
@@ -3398,29 +3382,6 @@ JamiAccount::onReceiveDeviceSync(DeviceSync&& sync)
     for (const auto& tr : sync.trust_requests)
         onTrustRequest(tr.first, tr.second.device, tr.second.received, false, {});
 
-}
-
-void
-JamiAccount::igdChanged()
-{
-    if (not dht_.isRunning())
-        return;
-    if (upnp_) {
-        dht::ThreadPool::io().run([w = weak(), oldPort = static_cast<in_port_t>(dhtPortUsed_)] {
-            if (auto s = w.lock()) {
-                auto& this_ = *s;
-                if (not this_.mapPortUPnP())
-                    JAMI_WARN("UPnP: Could not map DHT port");
-                auto newPort = static_cast<in_port_t>(this_.dhtPortUsed_);
-                if (oldPort != newPort) {
-                    JAMI_WARN("DHT port changed: restarting network");
-                    this_.doRegister_();
-                } else
-                    this_.dht_.connectivityChanged();
-            }
-        });
-    } else
-        dht_.connectivityChanged();
 }
 
 void
--- src/upnp/upnp_control.h
+++ src/upnp/upnp_control.h
@@ -2,7 +2,7 @@
  *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
  *
  *  Author: Stepan Salenikovich <stepan.salenikovich@savoirfairelinux.com>
- *	Author: Eden Abitbol <eden.abitbol@savoirfairelinux.com>
+ *    Author: Eden Abitbol <eden.abitbol@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -32,8 +32,13 @@
 #include "logger.h"
 #include "ip_utils.h"
 
+#include <list>
 #include <memory>
 #include <chrono>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <functional>
 
 namespace jami {
 class IpAddr;
@@ -41,25 +46,42 @@ class IpAddr;
 
 namespace jami { namespace upnp {
 
+using namespace std::placeholders;
+
 class UPnPContext;
 
 class Controller
 {
 public:
-    Controller();
+    using NotifyServiceCallback = std::function<void(uint16_t*, bool)>;
+    using cbMap = std::map<std::pair<uint16_t, uint16_t>, NotifyServiceCallback>;
+    using cbMapItr = std::map<std::pair<uint16_t, uint16_t>, NotifyServiceCallback>::iterator;
+    using portMapItr = PortMapLocal::iterator;
+
+    struct ControllerData {
+        std::string memAddr;
+        bool keepCb;
+        NotifyControllerCallback cbAdd;
+        NotifyControllerCallback cbRm;
+    };
+
+    Controller(bool keepCb = true);
     ~Controller();
 
     // Checks if a valid IGD is available.
-    bool hasValidIGD();
+    bool hasValidIgd();
 
-    // Sets or clears a listener for valid IGDs. There is one listener per controller.
-    void setIGDListener(IgdFoundCallback&& cb = {});
+    // Sends out request to upnp protocol to open a port. Gives option to use unique port (i.e. not one that is already in use).
+    void addMapping(NotifyServiceCallback&& cb, uint16_t portDesired /*external port*/, PortType type, bool unique, uint16_t portLocal = 0 /*internal port*/);
 
-    // Adds a mapping. Gives option to use unique port (i.e. not one that is already in use).
-    bool addMapping(uint16_t port_desired, PortType type, bool unique, uint16_t* port_used, uint16_t port_local = 0);
+    // Callback function for when mapping is added.
+    void onAddMapping(Mapping* mapping, bool success);
 
-    // Removes all mappings added by this instance.
-    void removeMappings();
+    // Sends out request to upnp protocol to close all opened ports from this controller.
+    void removeAllLocalMappings();
+
+    // Callback function for when mapping is removed.
+    void onRemoveMapping(Mapping* mapping, bool success);
 
     // Gets the external ip of the first valid IGD in the list.
     IpAddr getExternalIP() const;
@@ -69,15 +91,42 @@ public:
 
 private:
     // Removes all mappings of the given type.
-    void removeMappings(PortType type);
+    void removeLocalMappings(PortType type);
+
+    // Checks if the map is present locally given a port and type.
+    bool isLocalMapPresent(const unsigned int portExternal, PortType type);
+
+    // Checks if the map is present locally given a mapping.
+    bool isLocalMapPresent(Mapping map);
+
+    // Adds a mapping locally to the list.
+    void addLocalMap(Mapping map);
+
+    // Removes a mapping locally from the list.
+    void removeLocalMap(Mapping map);
+
+    // Adds callback to callback list.
+    void registerCallback(unsigned int portExternal, unsigned int portInternal, NotifyServiceCallback&& cb);
+
+    // Removes callback from callback list.
+    void unregisterCallback(Mapping map);
+
+    // Calls corresponding callback.
+    void dispatchCallback(Mapping map, bool success);
 
 private:
-    std::shared_ptr<UPnPContext> upnpContext_;		// Context from which the controller executes the wanted commands.
+    std::shared_ptr<UPnPContext> upnpContext_;  // Context from which the controller executes the wanted commands.
 
-    PortMapLocal udpMappings_;						// List of UDP mappings created by this instance.
-    PortMapLocal tcpMappings_;						// List of TCP mappings created by this instance.
+    std::mutex mapListMutex_;
+    PortMapLocal udpMappings_;                  // List of UDP mappings created by this instance.
+    PortMapLocal tcpMappings_;                  // List of TCP mappings created by this instance.
 
-    size_t listToken_ {0};							// IGD listener token.
+    std::mutex cbListMutex_;                    // Mutex to protect local callback list.
+    bool keepCb_ {false};                       // Variable that indicates if the controller wants to keep it's callback in the list after a connectivity change.
+    cbMap mapCbList_;                           // List of mappings with their corresponding callbacks.
+
+    const void* memAddr_ {nullptr};             // Variable to store address of instance.
+    std::string memAddrStr_ {};                 // Variable to store string of address to be used as the unique identifier.
 };
 
 }} // namespace jami::upnp
