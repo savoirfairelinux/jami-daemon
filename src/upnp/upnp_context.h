da0a8f53f827816cee972c7538ce2ee96eac5ee0
Eden Abitbol <eden.abitbol@savoirfairelinux.com>
2019-08-05 14:06:26 -0400

    upnp: asynchronous open/close ports
    The UPnP stack now opens and closes ports with
    asynchronous mechanic and with callbacks.
    
    Every time a mapping request (open or close) is sent,
    a callback is registered and managed by the context.
    When the corresponding protocol executes the request and
    gets an answer, the right callback is found and dispatched
    to its controller. A timeout of 1 second is implemented
    on each request.
    
    Controllers can now be tracked with a unique Id (a string
    that represents its location in memory).
    
    The IGD class now tracks its current active ports and
    manages the number of users for each port.
    
    The PMPIGD class now operates using a queue of mapping
    requests. Whenever a request is made to the natpmp protocol,
    it saves the request to its corresponding list (mapToAdd,
    mapToRemove or mapToRenew in the PMPIGD class) and notifies
    the main natpmp thread. The thread then checks if there
    is any pending map requests in the PMPIGD queue and treats
    them accordingly.
    
    Change-Id: I7b09c5b7ac69276359f509392a688a847ca17a90

--- src/upnp/upnp_context.h
+++ src/upnp/upnp_context.h
@@ -57,34 +57,73 @@
 
 using random_device = dht::crypto::random_device;
 
-using IgdFoundCallback = std::function<void()>;
-
 namespace jami {
 class IpAddr;
 }
 
 namespace jami { namespace upnp {
 
+using namespace std::placeholders;
+
+using NotifyControllerMapCallback = std::function<void(Mapping*, bool)>;
+using NtfyCtrlrMap = NotifyControllerMapCallback;
+using NotifyControllerConnectivityChangeCallback = std::function<void()>;
+using NtfyCtrlCnxChg = NotifyControllerConnectivityChangeCallback;
+
+struct ControllerData {
+    std::string memAddr;
+    bool keepCb;
+    NtfyCtrlrMap cbAdd;
+    NtfyCtrlrMap cbRm;
+    NtfyCtrlCnxChg cbCnx;
+};
+using pControllerData = std::unique_ptr<ControllerData>;
+
+const constexpr auto MAP_REQUEST_TIMEOUT = std::chrono::seconds(1);
+
 class UPnPContext
 {
 public:
+    struct PendingMapRequest {
+        Mapping map;
+        std::shared_ptr<Task> cleanupTask;
+    };
+
+    using cbMapItr = std::map<Mapping*, pControllerData>::iterator;
+    using pendingMapItr = std::vector<PendingMapRequest>::iterator;
+
     UPnPContext();
     ~UPnPContext();
 
     // Check if there is a valid IGD in the IGD list.
-    bool hasValidIGD();
+    bool hasValidIgd();
 
-    // Add IGD listener.
-    size_t addIGDListener(IgdFoundCallback&& cb);
+    // Clears callbacks associated with map list given as parameter.
+    void clearCallbacks(const PortMapLocal& mapList, const std::string& ctrlId);
 
-    // Remove IGD listener.
-    void removeIGDListener(size_t token);
+    // Adds callback associated with a mapping and its controller.
+    void addCallback(Mapping map, pControllerData ctrlData); 
 
-    // Tries to add a valid mapping. Will return it if successful.
-    Mapping addMapping(uint16_t port_desired, uint16_t port_local, PortType type, bool unique);
+    // Informs the UPnP context that the network status has changed.
+    void connectivityChanged();
 
-    // Removes a mapping.
+    // Checks if the desired port is already in use on an IGD.
+    bool isMappingInUse(const unsigned int portDesired, PortType type);
+
+    // Increments the number of users for a given port.
+    void incrementNbOfUsers(const unsigned int portDesired, PortType type);
+
+    // Sends out a request to a protocol to add a mapping.
+    void addMapping(pControllerData ctrlData, uint16_t portDesired, uint16_t portLocal, PortType type, bool unique);
+
+    // Callback function for when mapping is added.
+    void onAddMapping(IpAddr igdIp, Mapping* mapping, bool success);
+
+    // Sends out a request to protocol to remove a mapping.
     void removeMapping(const Mapping& mapping);
+
+    // Callback function for when mapping is removed.
+    void onRemoveMapping(IpAddr igdIp, Mapping* mapping, bool success);
 
     // Get external Ip of a chosen IGD.
     IpAddr getExternalIP() const;
@@ -92,18 +131,9 @@ public:
     // Get our local Ip.
     IpAddr getLocalIP() const;
 
-    // Inform the UPnP context that the network status has changed. This clears the list of known
-    void connectivityChanged();
-
 private:
     // Checks if the IGD is in the list by checking the IGD's public Ip.
     bool isIgdInList(const IpAddr& publicIpAddr);
-
-    // Returns the protocol of the IGD.
-    UPnPProtocol::Type getIgdProtocol(IGD* igd);
-
-    // Returns a random port that is not yet used by the daemon for UPnP.
-    uint16_t chooseRandomPort(const IGD& igd, PortType type);
 
     // Tries to add or remove IGD to the list via callback.
     bool igdListChanged(UPnPProtocol* protocol, IGD* igd, const IpAddr publicIpAddr, bool added);
@@ -117,22 +147,58 @@ private:
     // Removes IGD from list by specifiying the IGD's public Ip address.
     bool removeIgdFromList(IpAddr publicIpAddr);
 
-    // Tries to add mapping. Assumes mutex is already locked.
-    Mapping addMapping(IGD* igd, uint16_t port_external, uint16_t port_internal, PortType type, UPnPProtocol::UpnpError& upnp_error);
+    // Returns the protocol of the IGD.
+    UPnPProtocol::Type getIgdProtocol(IGD* igd);
 
-public:
-    constexpr static unsigned MAX_RETRIES = 20;
+    // Tries to add a mapping to a specific IGD.
+    void addMapping(IGD* igd, uint16_t portExternal, uint16_t portInternal, PortType type);
+
+    // Adds mapping to corresponding IGD. 
+    void addMappingToIgd(IpAddr igdIp, Mapping map);
+
+    // Removes mapping from corresponding IGD. 
+    void removeMappingFromIgd(IpAddr igdIp, Mapping map);
+
+    // Add callbacks to callback list.
+    void registerCallback(Mapping map, pControllerData ctrlData);
+
+    // Removes callback from callback list given a mapping.
+    void unregisterCallback(Mapping map, bool force = false);
+
+    // Removes callback from callback list given a mapping and controller Id.
+    void unregisterCallback(Mapping map, const std::string& ctrlId);
+
+    // Calls corresponding callback.
+    void dispatchOnAddCallback(Mapping map, bool success);
+
+    // Calls corresponding callback.
+    void dispatchOnRmCallback(Mapping map, bool success);
+
+    // Registers a timeout for a given pending add map request.
+    void registerAddMappingTimeout(Mapping map);
+
+    // Unregisters a timeout for a given pending add map request.
+    void unregisterAddMappingTimeout(Mapping map);
+
+    // Registers a timeout for a given pending remove map request.
+    void registerRmMappingTimeout(Mapping map);
+
+    // Unregisters a timeout for a given pending remove map request.
+    void unregisterRmMappingTimeout(Mapping map);
 
 private:
     NON_COPYABLE(UPnPContext);
 
-    std::map<size_t, IgdFoundCallback> igdListeners_;           // Map of valid IGD listeners with their tokens.
-    size_t listenerToken_{ 0 };                                 // Last provided token for valid IGD listeners (0 is the invalid token).
+    mutable std::mutex igdListMutex_;                           // Mutex that protects IGD list.
+    std::vector<std::unique_ptr<UPnPProtocol>> protocolList_;   // Vector of available protocols.
+    std::list<std::pair<UPnPProtocol*, IGD*>> igdList_;         // List of IGDs with their corresponding protocols that found them.
+    
+    std::mutex cbListMutex_;                                    // Mutex that protects the callback list.
+    std::map<Mapping*, pControllerData> mapCbList_;             // List of mappings with their corresponding callbacks.
 
-    std::vector<std::unique_ptr<UPnPProtocol>> protocolList_;	// Vector of available protocols.
-    std::list<std::pair<UPnPProtocol*, IGD*>> igdList_;			// List of IGDs with their corresponding public IPs.
-    mutable std::mutex igdListMutex_;							// Mutex used to access these lists and IGDs in a thread-safe manner.
-
+    std::mutex pendindRequestMutex_;                            // Mutex that protects the pending map request lists.
+    std::vector<PendingMapRequest> pendingAddMapList_ {};       // Vector of pending add mapping requests.
+    std::vector<PendingMapRequest> pendingRmMapList_ {};        // Vector of pending remove mapping request.
 };
 
 std::shared_ptr<UPnPContext> getUPnPContext();
