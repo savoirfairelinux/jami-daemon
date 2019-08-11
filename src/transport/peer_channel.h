c3eed4b649f2405f19b89883cf326b2eea42832e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-08-11 16:26:25 -0400

    peer channel: add logs
    Change-Id: I4decc7c51ac89145471b9ec3a5f431a1c372103e

--- src/transport/peer_channel.h
+++ src/transport/peer_channel.h
@@ -23,12 +23,16 @@
 #include <deque>
 #include <algorithm>
 
+#include "logger.h"
+
 namespace jami {
 
 class PeerChannel
 {
 public:
-    PeerChannel() {}
+    PeerChannel() {
+        JAMI_WARN("PeerChannel::PeerChannel() %p", this);
+    }
     ~PeerChannel() {
         stop();
     }
@@ -41,23 +45,28 @@ public:
 
     ssize_t isDataAvailable() {
         std::lock_guard<std::mutex> lk{mutex_};
+        JAMI_WARN("PeerChannel::isDataAvailable() %p %zu", this, stream_.size());
         return stream_.size();
     }
 
     template <typename Duration>
     ssize_t wait(Duration timeout, std::error_code& ec) {
         std::unique_lock<std::mutex> lk {mutex_};
+        JAMI_WARN("PeerChannel::wait() %p start %lu ms", this, std::chrono::duration_cast<std::chrono::milliseconds>(timeout).count());
         cv_.wait_for(lk, timeout, [this]{ return stop_ or not stream_.empty(); });
         if (stop_) {
+            JAMI_WARN("PeerChannel::wait() %p end stopped", this);
             ec = std::make_error_code(std::errc::interrupted);
             return -1;
         }
+        JAMI_WARN("PeerChannel::wait() %p end %zu", this, stream_.size());
         ec.clear();
         return stream_.size();
     }
 
     ssize_t read(char* output, std::size_t size, std::error_code& ec) {
         std::unique_lock<std::mutex> lk {mutex_};
+        JAMI_WARN("PeerChannel::read() %p start %zu", this, size);
         cv_.wait(lk, [this]{
             return stop_ or not stream_.empty();
         });
@@ -68,13 +77,16 @@ public:
                 std::copy(stream_.begin(), endIt, output);
                 stream_.erase(stream_.begin(), endIt);
             }
+            JAMI_WARN("PeerChannel::read() %p end %zu/%zu", this, toRead, size);
             ec.clear();
             return toRead;
         }
         if (stop_) {
+            JAMI_WARN("PeerChannel::read() %p stopped", this);
             ec.clear();
             return 0;
         }
+        JAMI_WARN("PeerChannel::read() %p EAGAIN", this);
         ec = std::make_error_code(std::errc::resource_unavailable_try_again);
         return -1;
     }
@@ -82,17 +94,20 @@ public:
     ssize_t write(const char* data, std::size_t size, std::error_code& ec) {
         std::lock_guard<std::mutex> lk {mutex_};
         if (stop_) {
+            JAMI_WARN("PeerChannel::write() %p stopped", this);
             ec = std::make_error_code(std::errc::broken_pipe);
             return -1;
         }
         stream_.insert(stream_.end(), data, data+size);
         cv_.notify_all();
         ec.clear();
+        JAMI_WARN("PeerChannel::write() %p %zu", this, size);
         return size;
     }
 
     void stop() noexcept {
         std::lock_guard<std::mutex> lk {mutex_};
+        JAMI_WARN("PeerChannel::stop() %p", this);
         if (stop_)
             return;
         stop_ = true;
