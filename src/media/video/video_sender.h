3e6130c6e5ccb1e85499b9b7198b566e4d436a9a
philippegorley <philippe.gorley@savoirfairelinux.com>
2019-08-13 14:30:12 -0400

    sender: send only 1 keyframe at start
    Don't choke the network connection with multiple keyframes in
    succession. Only 1 keyframe should be necessary.
    
    Change-Id: I6c1bcc4c3973fd6cb7eb58a9928e823bc0c5fcf7

--- src/media/video/video_sender.h
+++ src/media/video/video_sender.h
@@ -63,7 +63,7 @@ public:
     int setBitrate(uint64_t br);
 
 private:
-    static constexpr int KEYFRAMES_AT_START {4}; // Number of keyframes to enforce at stream startup
+    static constexpr int KEYFRAMES_AT_START {1}; // Number of keyframes to enforce at stream startup
     static constexpr unsigned KEY_FRAME_PERIOD {0}; // seconds before forcing a keyframe
 
     NON_COPYABLE(VideoSender);
@@ -74,7 +74,6 @@ private:
     std::unique_ptr<MediaIOHandle> muxContext_ = nullptr;
     std::unique_ptr<MediaEncoder> videoEncoder_ = nullptr;
 
-    // XXX forceKeyFrame_ is always at -1, incremented to 0 when a keyframe is requested (still works though)
     std::atomic<int> forceKeyFrame_ {KEYFRAMES_AT_START};
     int keyFrameFreq_ {0}; // Set keyframe rate, 0 to disable auto-keyframe. Computed in constructor
     int64_t frameNumber_ = 0;
