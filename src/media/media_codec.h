6692adf5852fe8632ea36e440c39d0b14ed57df0
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-08-21 14:59:27 -0400

    Revert "autoadapt: enable by default"
    This reverts commit 0b9dd0e616c23bde20e4af228b9db70513a1e5bf.
    
    Change-Id: I3d42ca9673de395f8f6a36c7708e0654b638ec73

--- src/media/media_codec.h
+++ src/media/media_codec.h
@@ -190,7 +190,7 @@ struct AccountVideoCodecInfo : AccountCodecInfo
     unsigned frameRate;
     unsigned profileId;
     std::string parameters;
-    bool isAutoQualityEnabled{true};
+    bool isAutoQualityEnabled{false};
 };
 bool operator== (SystemCodecInfo codec1, SystemCodecInfo codec2);
 
