5f83171e3ba7c1992210f8de25314883887d0585
philippegorley <philippe.gorley@savoirfairelinux.com>
2019-08-22 13:32:16 -0400

    video_input: fully init decoder in switch input
    Some of the decoder's settings are only set once decoding has started,
    such as the pixel format.
    
    Change-Id: Ifb3cf4606d1dc02dc089bc8ca9ca11a7a3071caf

--- src/media/video/video_input.cpp
+++ src/media/video/video_input.cpp
@@ -367,12 +367,22 @@ VideoInput::createDecoder()
         return;
     }
 
+    decoder->decode(); // Populate AVCodecContext fields
+
     decOpts_.width = decoder->getWidth();
     decOpts_.height = decoder->getHeight();
     decOpts_.framerate = decoder->getFps();
+    AVPixelFormat fmt = decoder->getPixelFormat();
+    if (fmt != AV_PIX_FMT_NONE) {
+        decOpts_.pixel_format = av_get_pix_fmt_name(fmt);
+    } else {
+        JAMI_WARN("Could not determine pixel format, using default");
+        decOpts_.pixel_format = av_get_pix_fmt_name(AV_PIX_FMT_YUV420P);
+    }
 
-    JAMI_DBG("created decoder with video params : size=%dX%d, fps=%lf",
-             decOpts_.width, decOpts_.height, decOpts_.framerate.real());
+    JAMI_DBG("created decoder with video params : size=%dX%d, fps=%lf pix=%s",
+             decOpts_.width, decOpts_.height, decOpts_.framerate.real(),
+             decOpts_.pixel_format.c_str());
 
     decoder_ = std::move(decoder);
     foundDecOpts(decOpts_);
@@ -565,27 +575,22 @@ VideoInput::switchInput(const std::string& resource)
 
     const auto suffix = resource.substr(pos + sep.size());
 
-    bool ready = false;
 
     if (prefix == DRing::Media::VideoProtocolPrefix::CAMERA) {
         /* Video4Linux2 */
-        ready = initCamera(suffix);
+        initCamera(suffix);
     } else if (prefix == DRing::Media::VideoProtocolPrefix::DISPLAY) {
         /* X11 display name */
 #ifdef __APPLE__
-        ready = initAVFoundation(suffix);
+        initAVFoundation(suffix);
 #elif defined(_WIN32)
-        ready = initGdiGrab(suffix);
+        initGdiGrab(suffix);
 #else
-        ready = initX11(suffix);
+        initX11(suffix);
 #endif
     } else if (prefix == DRing::Media::VideoProtocolPrefix::FILE) {
         /* Pathname */
-        ready = initFile(suffix);
-    }
-
-    if (ready) {
-        foundDecOpts(decOpts_);
+        initFile(suffix);
     }
 
     switchPending_ = true;
@@ -628,7 +633,12 @@ DeviceParams VideoInput::getParams() const
 MediaStream
 VideoInput::getInfo() const
 {
-    return decoder_->getStream("v:local");
+    if (decoder_)
+        return decoder_->getStream("v:local");
+    auto opts = futureDecOpts_.get();
+    rational<int> fr(opts.framerate.numerator(), opts.framerate.denominator());
+    return MediaStream("v:local", av_get_pix_fmt(opts.pixel_format.c_str()),
+        1 / fr, opts.width, opts.height, 0, fr);
 }
 
 void
