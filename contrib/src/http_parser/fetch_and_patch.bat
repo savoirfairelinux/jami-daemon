1f9fa189446aed052fc65be8d3ac3b74a3e51c6d
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-08-27 12:07:36 -0400

    namedirectory: replace restbed by restinio
    Depends-On: If43bab09e0d209aef1e92217d145a22f0cdab466
    Change-Id: Ic8f55919f82c2e2ab088d4bc38814c99e1006062

---
+++ contrib/src/http_parser/fetch_and_patch.bat
@@ -0,0 +1,20 @@
+set BUILD=%SRC%..\build
+
+set HTTP_PARSER_VERSION=2.9.3
+set HTTP_PARSER_URL=https://github.com/binarytrails/http_parser/archive/v%HTTP_PARSER_VERSION%.tar.gz
+
+mkdir %BUILD%
+
+if %USE_CACHE%==1 (
+    copy %CACHE_DIR%\%HTTP_PARSER_VERSION%.tar.gz %cd%
+) else (
+    %WGET_CMD% %HTTP_PARSER_URL%
+)
+
+7z -y x %HTTP_PARSER_VERSION%.tar.gz && 7z -y x %HTTP_PARSER_VERSION%.tar -o%BUILD%
+del %HTTP_PARSER_VERSION%.tar && del %HTTP_PARSER_VERSION%.tar.gz && del %BUILD%\pax_global_header
+rename %BUILD%\http_parser-%HTTP_PARSER_VERSION% http_parser
+
+cd %BUILD%\http_parser
+
+cd %SRC%
