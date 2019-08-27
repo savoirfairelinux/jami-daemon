2b5b11f81cfc0090bda30a0d5a6fc76581813617
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-08-27 12:07:36 -0400

    namedirectory: replace restbed by restinio
    Depends-On: If43bab09e0d209aef1e92217d145a22f0cdab466
    Change-Id: Ic8f55919f82c2e2ab088d4bc38814c99e1006062

--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -434,6 +434,7 @@ class JamiAccount : public SIPAccountBase {
         std::string nameServer_;
         std::string registeredName_;
 #endif
+        std::shared_ptr<dht::Logger> logger_;
 
         /**
          * Compute archive encryption key and DHT storage location from password and PIN.
