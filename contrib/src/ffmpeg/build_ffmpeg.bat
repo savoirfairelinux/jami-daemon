5854bc2df2ebc4a46e8b5993358129f79ff239ba
Andreas Traczyk <andreastraczyk@gmail.com>
2019-09-02 20:56:00 -0400

    win32: add python build script
    Change-Id: I464d2f29173d2bcfdf6ddea5e26ed9e0192e98ae

---
+++ contrib/src/ffmpeg/build_ffmpeg.bat
@@ -0,0 +1,3 @@
+set MSYS2_PATH_TYPE=inherit
+IF exist C:\msys64\usr\bin ( set MSYS2_BIN="C:\msys64\usr\bin\bash.exe" ) ELSE ( set MSYS2_BIN="C:\tools\msys64\usr\bin\bash.exe" )
+%MSYS2_BIN% --login -x %CONTRIB_SRC_DIR%\ffmpeg\windows-configure-make.sh win32 x64
