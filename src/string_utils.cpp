faa8cef700d1f00dea3499a49bb1cae95e3d564b
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-09-03 12:11:51 -0400

    string utils: add string_replace
    Change-Id: I86a2b1564be127f70dd991b86fe0b14440cee299

--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -121,4 +121,13 @@ split_string_to_unsigned(const std::string &s, char delim)
     return result;
 }
 
+void string_replace(std::string& str, const std::string& from, const std::string& to)
+{
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+    }
+}
+
 } // namespace jami
