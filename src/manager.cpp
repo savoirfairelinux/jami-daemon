ccb23374bd22397c4159e0289b4676cac4478d1e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-09-05 16:21:43 -0400

    manager: cleanup, use seeded RNG from dht
    Change-Id: Ie5a16ba02023c89c78eef8d06fa13b4be56e558a

--- src/manager.cpp
+++ src/manager.cpp
@@ -409,27 +409,13 @@ struct Manager::ManagerPimpl
 Manager::ManagerPimpl::ManagerPimpl(Manager& base)
     : base_(base)
     , toneCtrl_(base.preferences)
-    , currentCallMutex_()
-    , dtmfKey_()
     , dtmfBuf_(0, AudioFormat::MONO())
-    , audioLayerMutex_()
-    , waitingCalls_()
-    , waitingCallsMutex_()
-    , path_()
     , ringbufferpool_(new RingBufferPool)
-    , conferenceMap_()
-    , ice_tf_()
+    , rand_(dht::crypto::getSeededRandomEngine<std::mt19937_64>())
 #ifdef ENABLE_VIDEO
     , videoManager_(new VideoManager)
 #endif
 {
-    // initialize random generator
-    // mt19937_64 should be seeded with 2 x 32 bits
-
-    random_device rdev;
-    std::seed_seq seed {rdev(), rdev()};
-    rand_.seed(seed);
-
     jami::libav_utils::ring_avcodec_init();
 }
 
