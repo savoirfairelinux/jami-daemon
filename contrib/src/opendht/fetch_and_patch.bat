c3b9a32e704e6a6f8173ab44a43523c4f296c0c0
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-09-19 14:24:44 -0400

    opendht: bump to latest master
    + Fix DHTLOGLEVEL
    + Fix proxy on android
    
    Change-Id: Icfe40c583333598b4528a4a05be8680242563b21

--- contrib/src/opendht/fetch_and_patch.bat
+++ contrib/src/opendht/fetch_and_patch.bat
@@ -1,6 +1,6 @@
 set BUILD=%SRC%..\build
 
-set OPENDHT_VERSION=b03a7732e3dca3a48689e64e345fe418e130b6ac
+set OPENDHT_VERSION=c564810b8cd60b3ccbbeb9b9d5e294248fed9b5b
 set OPENDHT_URL=https://github.com/savoirfairelinux/opendht/archive/%OPENDHT_VERSION%.tar.gz
 
 mkdir %BUILD%
