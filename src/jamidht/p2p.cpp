6939c59a8a0ee44186dccb5cfdf94e547256e1b4
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-09-24 14:23:12 -0400

    p2p: do not crash during TURN allocation failures
    Change-Id: Icb0fca978b51b0b2520b14add3a89f3dbbcda842

--- src/jamidht/p2p.cpp
+++ src/jamidht/p2p.cpp
@@ -570,16 +570,22 @@ DhtPeerConnector::Impl::turnConnect()
     if (turnAuthv6_ && !turnAuthv6_->isReady())
         turnAuthv6_.reset();
 
-    if (!turnAuthv4_ || !turnAuthv4_->isReady()) {
-        turn_param_v4.authorized_family = PJ_AF_INET;
-        turnAuthv4_ = std::make_unique<TurnTransport>(turn_param_v4);
+    try {
+        if (!turnAuthv4_ || !turnAuthv4_->isReady()) {
+            turn_param_v4.authorized_family = PJ_AF_INET;
+            turnAuthv4_ = std::make_unique<TurnTransport>(turn_param_v4);
+        }
+
+        if (!turnAuthv6_ || !turnAuthv6_->isReady()) {
+            auto turn_param_v6 = turn_param_v4;
+            turn_param_v6.authorized_family = PJ_AF_INET6;
+            turnAuthv6_ = std::make_unique<TurnTransport>(turn_param_v6);
+        }
+    } catch (...) {
+        JAMI_WARN("Turn allocation failed. Do not use the TURN");
+        return false;
     }
 
-    if (!turnAuthv6_ || !turnAuthv6_->isReady()) {
-        auto turn_param_v6 = turn_param_v4;
-        turn_param_v6.authorized_family = PJ_AF_INET6;
-        turnAuthv6_ = std::make_unique<TurnTransport>(turn_param_v6);
-    }
 
     // Wait until TURN server READY state (or timeout)
     Timeout<Clock> timeout {NET_CONNECTION_TIMEOUT};
