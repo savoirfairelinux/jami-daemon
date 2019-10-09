125c9aa92e19061b672bf5a2749a28f914d6e408
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-10-09 16:49:34 -0400

    add connection manager
    Change-Id: I9cdd681e91ca71f24338e728fc382349393c6912

---
+++ src/jamidht/connectionmanager.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (C) 2017-2019 Savoir-faire Linux Inc.
+ *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include <opendht/dhtrunner.h>
+#include <opendht/infohash.h>
+#include <opendht/value.h>
+#include <opendht/default_types.h>
+
+
+namespace jami {
+
+class JamiAccount;
+
+struct PeerConnectionRequest : public dht::EncryptedValue<PeerConnectionRequest>
+{
+    static const constexpr dht::ValueType& TYPE = dht::ValueType::USER_DATA;
+    static constexpr const char* key_prefix = "peer:"; ///< base to compute the DHT listen key
+    dht::InfoHash device;
+    std::string uri;
+    std::vector<std::string> addresses;
+    MSGPACK_DEFINE_MAP(device, uri)
+};
+
+class ConnectionManager {
+public:
+    ConnectionManager(JamiAccount& account);
+    ~ConnectionManager();
+
+    void connectDevice(const std::string& deviceId, const std::string& uri /* OnConnect */);
+    void onDhtConnected(const std::string& deviceId);
+
+private:
+    ConnectionManager() = delete;
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+}
