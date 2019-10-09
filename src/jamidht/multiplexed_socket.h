c22f57c0c63cb436c1f0084d44d6dce2918ea801
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-10-09 16:49:34 -0400

    add connection manager
    Change-Id: I9cdd681e91ca71f24338e728fc382349393c6912

---
+++ src/jamidht/multiplexed_socket.h
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (C) 2017-2019 Savoir-faire Linux Inc.
+ *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <opendht/default_types.h>
+
+#include "generic_io.h"
+
+namespace jami {
+
+class ChannelSocket;
+class ConnectionManager;
+class TlsSocketEndpoint;
+
+using onConnectionRequestCb = std::function<bool(const std::string& /* device id */, const uint16_t& /* id */, const std::string& /* name */)>;
+using onConnectionReadyCb = std::function<void(const std::string& /* device id */, const std::shared_ptr<ChannelSocket>&)>;
+using onChannelReadyCb = std::function<void(void)>;
+
+/**
+ * That msgpack structure is used to request a new channel (id, name)
+ * Transmitted over the TLS socket
+ */
+struct ChannelRequest {
+    std::string name {};
+    uint16_t channel {0};
+    bool isAnswer {false};
+    MSGPACK_DEFINE(name, channel, isAnswer)
+};
+
+/**
+ * A socket divided in channels over a TLS session
+ */
+class MultiplexedSocket
+{
+public:
+    MultiplexedSocket(const std::string& deviceId, std::unique_ptr<TlsSocketEndpoint> endpoint);
+    ~MultiplexedSocket();
+    std::shared_ptr<ChannelSocket> addChannel(const std::string name);
+
+    std::string deviceId() const;
+    bool isReliable() const;
+    bool isInitiator() const;
+    int maxPayload() const;
+
+    void setOnReady(onConnectionReadyCb&& cb);
+    void setOnRequest(onConnectionRequestCb&& cb);
+    void setOnChannelReady(uint16_t channel, onChannelReadyCb&& cb);
+
+    std::size_t read(const uint16_t& channel, uint8_t* buf, std::size_t len, std::error_code& ec);
+    std::size_t write(const uint16_t& channel, const uint8_t* buf, std::size_t len, std::error_code& ec);
+    int waitForData(const uint16_t& channel, std::chrono::milliseconds timeout, std::error_code&) const;
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+/**
+ * Represents a channel of the multiplexed socket (channel, name)
+ */
+class ChannelSocket : public GenericSocket<uint8_t>
+{
+public:
+    using SocketType = GenericSocket<uint8_t>;
+    ChannelSocket(MultiplexedSocket& endpoint, const std::string& name, const uint16_t& channel);
+    ~ChannelSocket();
+    std::string deviceId() const;
+    std::string name() const;
+    uint16_t channel() const;
+
+    bool isReliable() const override;
+    bool isInitiator() const override;
+    int maxPayload() const override;
+    std::size_t read(ValueType* buf, std::size_t len, std::error_code& ec) override;
+    std::size_t write(const ValueType* buf, std::size_t len, std::error_code& ec) override;
+    void setOnRecv(RecvCb&&) override {
+        throw std::logic_error("ChannelSocket::setOnRecv not implemented");
+    }
+    int waitForData(std::chrono::milliseconds timeout, std::error_code&) const override;
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+}
