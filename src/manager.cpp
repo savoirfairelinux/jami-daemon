54955e931bd60ad14b7ba6735c005ba393e534df
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2019-10-22 15:04:50 -0400

    conference: do not resend vcard
    Do not resend vCards from client hosting conference.
    
    Change-Id: I7d1c8932a36ab793b1c29bbe49541e89aaf90194

--- src/manager.cpp
+++ src/manager.cpp
@@ -1903,7 +1903,16 @@ Manager::incomingMessage(const std::string& callID,
         }
 
         JAMI_DBG("Is a conference, send incoming message to everyone");
-        pimpl_->sendTextMessageToConference(*conf, messages, from);
+        //filter out vcard messages
+        bool sendToOtherParicipants = true;
+        for (auto& message : messages) {
+            if (message.first.find("x-ring/ring.profile.vcard") != std::string::npos) {
+                sendToOtherParicipants = false;
+            }
+        }
+        if (sendToOtherParicipants) {
+            pimpl_->sendTextMessageToConference(*conf, messages, from);
+        }
 
         // in case of a conference we must notify client using conference id
         emitSignal<DRing::CallSignal::IncomingMessage>(conf->getConfID(), from, messages);
