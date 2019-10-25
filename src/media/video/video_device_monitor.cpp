0a377a73f8c80c31ba5767f7cc2a15a39661ef87
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2019-10-25 15:36:00 -0400

    videomonitor(win32) : bridge ffmpeg/win32api device ids
    Change-Id: Ibe3d79ed0632c663c2b067772ca592d5c5d466e6

--- src/media/video/video_device_monitor.cpp
+++ src/media/video/video_device_monitor.cpp
@@ -282,7 +282,7 @@ vector<VideoDevice>::iterator
 VideoDeviceMonitor::findDeviceByNode(const string& node)
 {
     for (auto it = devices_.begin(); it != devices_.end(); ++it)
-        if (it->getNode() == node)
+        if (it->getNode().find(node) != std::string::npos)
             return it;
     return devices_.end();
 }
@@ -291,7 +291,7 @@ vector<VideoDevice>::const_iterator
 VideoDeviceMonitor::findDeviceByNode(const string& node) const
 {
     for (auto it = devices_.cbegin(); it != devices_.cend(); ++it)
-        if (it->getNode() == node)
+        if (it->getNode().find(node) != std::string::npos)
             return it;
     return devices_.end();
 }
@@ -346,5 +346,5 @@ VideoDeviceMonitor::unserialize(const YAML::Node &in)
     else
         defaultDevice_ = first;
 }
-
+#pragma optimize("", on)
 }} // namespace jami::video
