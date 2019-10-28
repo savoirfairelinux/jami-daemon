966e654b9d62e94446c2cf87121ac2252e5b176a
Pierre Lespagnol <pierre.lespagnol@savoirfairelinux.com>
2019-10-28 09:30:42 -0400

    autoadapt: use Kalman to filter one way delay gradient
    Change-Id: I0897ae4dd8a274721346d84c5c392589983acfe4

--- src/media/video/video_rtp_session.h
+++ src/media/video/video_rtp_session.h
@@ -31,6 +31,7 @@
 #include <memory>
 
 namespace jami {
+class CongestionControl;
 class Conference;
 class MediaRecorder;
 } // namespace jami
@@ -130,7 +131,7 @@ private:
     void setupVideoBitrateInfo();
     void checkReceiver();
     float getPonderateLoss(float lastLoss);
-    void delayMonitor(int delay);
+    void delayMonitor(int gradient, int deltaT);
     void dropProcessing(RTCPInfo* rtcpi);
     void delayProcessing(int br);
     void setNewBitrate(unsigned int newBR);
@@ -162,16 +163,9 @@ private:
     std::chrono::seconds rtcp_checking_interval {4};
 
     time_point lastMediaRestart_ {time_point::min()};
-    time_point lastIncrease_ {time_point::min()};
     time_point lastREMB_ {time_point::min()};
 
-    int lastDelayAVG_ {0};
-    unsigned cnt_delay_callback_ {0};
-    std::pair<float, float> getDelayAvg();
-    std::pair<float, float> getDelayMedian();
-    float getRollingAvg();
-    int getRollingMedian();
-
+    CongestionControl* cc {nullptr};
 };
 
 }} // namespace jami::video
