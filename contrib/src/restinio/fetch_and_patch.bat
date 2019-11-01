86c891254fc204d642d6a7cd75b177533a8e28e6
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-11-01 12:07:57 -0400

    contrib: update restinio
    Change-Id: I3e0666d49fa4dbdb0a3ecfdabc95503a419ab142

--- contrib/src/restinio/fetch_and_patch.bat
+++ contrib/src/restinio/fetch_and_patch.bat
@@ -1,6 +1,6 @@
 set BUILD=%SRC%..\build
 
-set RESTINIO_VERSION=v.0.5.1.2
+set RESTINIO_VERSION=8d5d3e8237e0947adb9ba1ffc8281f4ad7cb2a59
 set RESTINIO_URL=https://github.com/Stiffstream/restinio/archive/%RESTINIO_VERSION%.tar.gz
 
 mkdir %BUILD%
