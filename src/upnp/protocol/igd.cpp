aca55416aab193109acb94f33e6e1be6ec95ac46
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-11-01 16:12:10 -0400

    upnp: asynchronous open/close ports
    The UPnP stack now opens and closes ports with an
    asynchronous mechanic and with callbacks.
    
    Every time a mapping request (open or close) is sent,
    a callback is registered and managed by the context.
    When the corresponding protocol executes the request and
    gets an answer, the right callback is found and dispatched
    to its controller. A timeout of 1 second is implemented
    on each request.
    
    Controllers can now be tracked with a unique Id (a string
    that represents its location in memory).
    
    The IGD class now tracks its current active ports and
    manages the number of users for each port.
    
    The PMPIGD class now operates using a queue of mapping
    requests. Whenever a request is made to the natpmp protocol,
    it saves the request to its corresponding list (mapToAdd,
    mapToRemove or mapToRenew in the PMPIGD class) and notifies
    the main natpmp thread. The thread then checks if there
    is any pending map requests in the PMPIGD queue and treats
    them accordingly.
    
    Add Nat-Pmp support for windows.
    
    Fixed bug where nat-pmp wouldn't find an IGD on a new interface
    after a connectivity change. The problem stems from the fact
    that, for one, the nat-pmp handle didn't get cleared internally
    once a connectivity change occured. This would cause the nat-
    pmp library to try and send searches on a socket that was
    bound on the old gateway. Secondly, if you don't wait for a
    little period of time, the natpmp library would discover the
    old IGD on the old interface before the connectivity change
    modifications were completed. Now we restart the nat-pmp
    process one second after the connectivity change triggers.
    
    Change-Id: I39a9b57e237deaa65eef4464a1838eaa3c5bbb03

--- src/upnp/protocol/igd.cpp
+++ src/upnp/protocol/igd.cpp
@@ -20,6 +20,7 @@
  */
 
 #include "igd.h"
+#include "logger.h"
 
 namespace jami { namespace upnp {
 
@@ -33,6 +34,159 @@ bool
 IGD::operator==(IGD& other) const
 {
     return (localIp_ == other.localIp_ and publicIp_ == other.publicIp_);
+}
+
+bool
+IGD::isMapInUse(const in_port_t externalPort, upnp::PortType type)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = type == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    return mapList.find(externalPort) != mapList.end();
+}
+
+bool
+IGD::isMapInUse(const Mapping& map)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = map.getType() == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    for (auto it = mapList.cbegin(); it != mapList.cend(); it++) {
+        if (it->second == map) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Mapping
+IGD::getMapping(in_port_t externalPort, upnp::PortType type) const
+{
+    auto& mapList = type == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    auto it = mapList.find(externalPort);
+    if (it != mapList.end()) {
+        if (it->first == externalPort) {
+            return Mapping(it->second.getPortExternal(),
+                           it->second.getPortInternal(),
+                           it->second.getType(),
+                           it->second.isUnique());
+        }
+    }
+    return Mapping(0, 0);
+}
+
+unsigned int
+IGD::getNbOfUsers(const in_port_t externalPort, upnp::PortType type)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = type == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    auto it = mapList.find(externalPort);
+    if (it != mapList.end())
+        return it->second.users;
+    return 0;
+}
+
+unsigned int
+IGD::getNbOfUsers(const Mapping& map)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = map.getType() == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    for (auto it = mapList.cbegin(); it != mapList.cend(); it++) {
+        if (it->second == map) {
+            return it->second.users;
+        }
+    }
+    return 0;
+}
+
+PortMapGlobal*
+IGD::getCurrentMappingList(upnp::PortType type)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = type == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    return &mapList;
+}
+
+void
+IGD::incrementNbOfUsers(const in_port_t externalPort, upnp::PortType type)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = type == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    auto it = mapList.find(externalPort);
+    if (it != mapList.end()) {
+        it->second.users++;
+        JAMI_DBG("IGD: Incrementing the number of users for %s to %u", it->second.toString().c_str(), it->second.users);
+        return;
+    }
+}
+
+void
+IGD::incrementNbOfUsers(const Mapping& map)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = map.getType() == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    for (auto it = mapList.begin(); it != mapList.end(); it++) {
+        if (it->second == map) {
+            it->second.users++;
+            JAMI_DBG("IGD: Incrementing the number of users for %s to %u", it->second.toString().c_str(), it->second.users);
+            return;
+        }
+    }
+    // If not found, add to mapList
+    GlobalMapping globalMap = GlobalMapping(map);
+    mapList.insert({globalMap.getPortExternal(), globalMap});
+}
+
+void
+IGD::decrementNbOfUsers(const in_port_t externalPort, upnp::PortType type)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = type == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    auto it = mapList.find(externalPort);
+    if (it != mapList.end()) {
+        if (it->second.users > 1) {
+            it->second.users--;
+            JAMI_DBG("IGD: Decrementing the number of users for %s to %u", it->second.toString().c_str(), it->second.users);
+            return;
+        } else {
+            mapList.erase(it);
+            return;
+        }
+    }
+}
+void
+IGD::decrementNbOfUsers(const Mapping& map)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = map.getType() == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    for (auto it = mapList.begin(); it != mapList.end(); it++) {
+        if (it->second == map) {
+            if (it->second.users > 1) {
+                it->second.users--;
+                JAMI_DBG("IGD: Decrementing the number of users for %s to %u", it->second.toString().c_str(), it->second.users);
+                return;
+            } else {
+                mapList.erase(it);
+                return;
+            }
+        }
+    }
+}
+
+void
+IGD::removeMapInUse(const Mapping& map)
+{
+    std::lock_guard<std::mutex> lk(mapListMutex_);
+    auto& mapList = map.getType() == upnp::PortType::UDP ? udpMappings_ : tcpMappings_;
+    for (auto it = mapList.begin(); it != mapList.end(); it++) {
+        if (it->second == map) {
+            if (it->second.users > 1) {
+                it->second.users--;
+                JAMI_DBG("IGD: Decrementing the number of users for %s to %u", it->second.toString().c_str(), it->second.users);
+            } else {
+                mapList.erase(it->first);
+            }
+            return;
+        }
+    }
 }
 
 }} // namespace jami::upnp
