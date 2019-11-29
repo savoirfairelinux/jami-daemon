607de0a92f3862c897eacf83e650cc5bd1f0be9a
amine younes <amine.younes-bouacida@savoirfairelinux.com>
2019-11-29 10:42:07 -0500

    archiver: add libarchive
    Change-Id: I2ff7d879de55e18bedcbddce74f743ffe8755ca6

--- src/plugin/pluginpreferencesmanager.h
+++ src/plugin/pluginpreferencesmanager.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
  *  Copyright (C) 2004-2019 Savoir-faire Linux Inc.
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -19,6 +19,7 @@
 #pragma once
 #include "noncopyable.h"
 #include "fileutils.h"
+#include "archiver.h"
 #include "pluginpreferencesparser.h"
 #include "pluginpreferencesvaluesmanager.h"
 
@@ -77,10 +78,11 @@ public:
      */
     std::map<std::string, std::string> getPluginDetails(const std::string& rootPath) {
         std::map<std::string, std::string> details;
-        details["name"] = "name";
+        std::string pluginName = rootPath.substr(rootPath.find_last_of("/\\") + 1);
+        details["name"] = pluginName;
         details["description"] = "A simple description";
         details["iconPath"] = rootPath + DIR_SEPARATOR_CH + "data" + DIR_SEPARATOR_CH + "icon.png";
-        details["soPath"] = rootPath;
+        details["soPath"] = rootPath + DIR_SEPARATOR_CH + "lib" + pluginName + ".so";
         return details;
     }
     
@@ -88,12 +90,28 @@ public:
      * @brief listPlugins
      * @return 
      */
-    std::vector<std::string> listPlugins(std::string arch ) {
-        std::string pluginsPath = fileutils::get_data_dir() + DIR_SEPARATOR_CH + "plugins" + DIR_SEPARATOR_CH + arch;
+    std::vector<std::string> listPlugins() {
+        std::string pluginsPath = fileutils::get_data_dir() + DIR_SEPARATOR_CH + "plugins";
         std::vector<std::string> pluginsPaths = fileutils::readDirectory(pluginsPath);
         std::for_each(pluginsPaths.begin(), pluginsPaths.end(),
                       [&pluginsPath](std::string& x){ x = pluginsPath + DIR_SEPARATOR_CH + x;});
         return pluginsPaths;
+    }
+    
+    /**
+     * @brief addPlugin
+     * @param jplPath
+     * @return 
+     */
+    int addPlugin(const std::string& jplPath) {
+        int i{0};
+        if(fileutils::isFile(jplPath)) {
+            std::string pluginName = jplPath.substr(jplPath.find_last_of("/\\") + 1);
+            pluginName = pluginName.substr(0, pluginName.find_last_of("."));
+            i = static_cast<int>(archiver::uncompressArchive(jplPath, fileutils::get_data_dir() + DIR_SEPARATOR_CH + "plugins"
+                                                                          + DIR_SEPARATOR_CH + pluginName));
+        }
+        return i;
     }
     
     /**
