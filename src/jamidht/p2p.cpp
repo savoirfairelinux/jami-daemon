e91e4f4191ece7e97a395be8b7a686728cd9f045
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2019-12-04 15:22:41 -0500

    fix: file transfer on macOS
    prevent bad access when verifying certificate.
    
    Change-Id: Ic54749f58894b4832f3331a21ca714dfa986f50e

--- src/jamidht/p2p.cpp
+++ src/jamidht/p2p.cpp
@@ -858,15 +858,16 @@ DhtPeerConnector::Impl::answerToRequest(PeerConnectionMsg&& request,
         std::unique_ptr<AbstractSocketEndpoint> peer_ep =
             std::make_unique<IceSocketEndpoint>(ice, false);
         JAMI_DBG() << account << "[CNX] start TLS session";
-        auto ph = peer_h;
         if (hasPubIp) ice->setSlaveSession();
 
         auto idx = std::make_pair(peer_h, ice->getRemoteAddress(0));
         auto it = waitForReadyEndpoints_.emplace(
             idx,
             std::make_unique<TlsSocketEndpoint>(*peer_ep, account.identity(), account.dhParams(),
-                [&, this](const dht::crypto::Certificate &cert) {
-                    return validatePeerCertificate(cert, ph);
+                [peer_h, this](const dht::crypto::Certificate &cert) {
+                    InfoHash peer_h_found;
+                    return validatePeerCertificate(cert, peer_h_found)
+                        and peer_h_found == peer_h;
                 }
             )
         );
