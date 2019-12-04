70dedfa51441079c0b728d5e6d15cdd258e32154
Ming Rui Zhang <mingrui.zhang@savoirfairelinux.com>
2019-12-04 11:45:37 -0500

    project: modify winmake.py to clear DebugInformationFormat for each vcxproj on Jenkins
    Change-Id: I362550773790643a663b6e8da70b48cb1064b391

--- MSVC/winmake.py
+++ MSVC/winmake.py
@@ -157,7 +157,7 @@ def make(pkg_info, force, sdk_version, toolset):
             os.remove(build_file)
         else:
             pkg_build_uptodate = is_build_uptodate(pkg_name, build_file)
-            with open(build_file, 'r+') as f:
+            with open(build_file, 'r+', encoding="utf8", errors='ignore') as f:
                 current_version = f.read()
                 if current_version == version:
                     pkg_ver_uptodate = True
@@ -242,7 +242,7 @@ def remove_archive_if_needed(pkg_build_path, dirty_path):
 
 
 def extract_tar(pkg_build_path, name, path):
-    with tarfile.open(path, 'r') as tarball:
+    with tarfile.open(path, 'r', encoding="utf8", errors='ignore') as tarball:
         tar_common_prefix = os.path.commonprefix(tarball.getnames())
         dirty_path = contrib_build_dir + '\\' + tar_common_prefix
         remove_archive_if_needed(pkg_build_path, dirty_path)
@@ -332,7 +332,7 @@ def get_pkg_file(pkg_name):
 
 def resolve(pkg_name, force=False, sdk_version='', toolset=''):
     pkg_json_file = get_pkg_file(pkg_name)
-    with open(pkg_json_file) as json_file:
+    with open(pkg_json_file, encoding="utf8", errors='ignore') as json_file:
         log.info('Resolving: ' + pkg_name)
         pkg_info = json.load(json_file)
         try:
@@ -345,7 +345,7 @@ def resolve(pkg_name, force=False, sdk_version='', toolset=''):
 
 def track_build(pkg_name, version):
     build_file = contrib_build_dir + '\\.' + pkg_name
-    f = open(build_file, "w+")
+    f = open(build_file, "w+", encoding="utf8", errors='ignore')
     f.write(version)
     f.close()
 
@@ -354,6 +354,8 @@ def build(pkg_name, pkg_dir, project_paths, custom_scripts, with_env, sdk,
           toolset, arch='x64', conf='Release'):
     getMSbuilder().set_msbuild_configuration(with_env, arch, conf, toolset)
     getMSbuilder().setup_vs_env(sdk)
+
+    log.info('No Error Here in 358')
 
     success = True
     build_operations = 0
@@ -368,6 +370,8 @@ def build(pkg_name, pkg_dir, project_paths, custom_scripts, with_env, sdk,
         result = getSHrunner().exec_batch(script)
         success &= not result[0]
         build_operations += 1
+
+    log.info('No Error Here in 374')
 
     # build custom step (nmake...)
     build_scripts = custom_scripts.get("build", [])
@@ -537,6 +541,11 @@ class MSbuilder:
     def build(self, pkg_name, proj_path, sdk_version, toolset):
         if not os.path.isfile(self.msbuild):
             raise IOError('msbuild.exe not found. path=' + self.msbuild)
+        if os.environ.get('JENKINS_URL'):
+            log.info("Jenkins Clear DebugInformationFormat")
+            self.__class__.replace_vs_prop(proj_path,
+                                           'DebugInformationFormat',
+                                           'None')
         # force chosen sdk
         self.__class__.replace_vs_prop(proj_path,
                                        'WindowsTargetPlatformVersion',
@@ -637,7 +646,7 @@ def main():
             getSHrunner().exec_batch('rmdir', ['/s', '/q', contrib_build_dir])
         else:
             pkg_json_file = get_pkg_file(parsed_args.clean)
-            with open(pkg_json_file) as json_file:
+            with open(pkg_json_file, encoding="utf8", errors='ignore') as json_file:
                 pkg_info = json.load(json_file)
                 dir_to_clean = contrib_build_dir + '\\' + pkg_info['name']
                 file_to_clean = contrib_build_dir + '\\.' + pkg_info['name']
