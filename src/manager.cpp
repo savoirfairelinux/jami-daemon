cbe8ccdb48d232584d440b9acb947ab266fd47d6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-12-11 17:19:21 -0500

    swarmchat: initialize empty git repository with initial commit
    This patch offers an API to create conversation. A conversation
    is represented by a git repository.
    
    The repository will contains the account certificate in /admins,
    its CRLs in /CRLs and the device key in /devices. Moreover,
    the first signed commit's hash will be the conversation's id
    
    Change-Id: Iac7ff7f6002185a5bb0f51d4c10df1afe9e059b4

--- src/manager.cpp
+++ src/manager.cpp
@@ -94,6 +94,8 @@ using random_device = dht::crypto::random_device;
 #include <asio/io_context.hpp>
 #include <asio/executor_work_guard.hpp>
 
+#include <git2.h>
+
 #ifndef WIN32
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -740,6 +742,8 @@ Manager::init(const std::string& config_file)
     // FIXME: this is no good
     initialized = true;
 
+    git_libgit2_init();
+
 #ifndef WIN32
     // Set the max number of open files.
     struct rlimit nofiles;
@@ -837,6 +841,7 @@ Manager::init(const std::string& config_file)
 void
 Manager::finish() noexcept
 {
+    git_libgit2_shutdown();
     bool expected = false;
     if (not pimpl_->finished_.compare_exchange_strong(expected, true))
         return;
