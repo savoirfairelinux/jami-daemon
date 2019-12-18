09180a9bab4160fb23d4c8f750f963b03ad52527
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2019-12-18 11:36:52 -0500

    media player: initial implementation
    Change-Id: I8cba50e1ab424d5acde9c38214af2cfe51064607

--- src/media/video/video_input.h
+++ src/media/video/video_input.h
@@ -27,6 +27,7 @@
 #include "media_stream.h"
 #include "media/media_device.h" // DeviceParams
 #include "media/video/video_base.h"
+#include "media_decoder.h"
 
 #include <map>
 #include <atomic>
@@ -65,6 +66,10 @@ public:
     AVPixelFormat getPixelFormat() const;
     const DeviceParams& getParams() const;
     MediaStream getInfo() const;
+
+    void setSink(const std::string& sinkId);
+    void updateStartTime(int64_t startTime);
+    bool configureFilePlayback(const std::string& path, std::shared_ptr<MediaDemuxer>& demuxer, int index);
 
     std::shared_future<DeviceParams> switchInput(const std::string& resource);
 #if VIDEO_CLIENT_INPUT
