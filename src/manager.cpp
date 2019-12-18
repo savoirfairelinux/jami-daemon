de02b206949d7729ea133b5e41395cc506c14203
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-12-18 14:50:09 -0500

    swarmchat: clone distant conversation
    Retrieving a conversation is done by cloning the distant repository
    
    Change-Id: Ib8e03bcde57ab4a504fca35c7b64826ba9b8325f

--- src/manager.cpp
+++ src/manager.cpp
@@ -100,6 +100,8 @@ using random_device = dht::crypto::random_device;
 
 #include <git2.h>
 
+#include "gittransport.h"
+
 #ifndef WIN32
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -745,6 +747,10 @@ Manager::init(const std::string& config_file)
     initialized = true;
 
     git_libgit2_init();
+    auto res = git_transport_register("git", p2p_transport_cb, nullptr);
+    if (res < 0) {
+        JAMI_ERR("Unable to initialize git transport");
+    }
 
 #ifndef WIN32
     // Set the max number of open files.
@@ -3233,6 +3239,14 @@ Manager::getJamiPluginManager() const
     return pimpl_->jami_plugin_manager;
 }
 #endif
+
+std::shared_ptr<ChannelSocket>
+Manager::gitSocket(const std::string& accountId, const std::string& deviceId, const std::string& conversationId)
+{
+    if (const auto acc = getAccount<JamiAccount>(accountId))
+        return acc->gitSocket(deviceId, conversationId);
+    return {};
+}
 
 std::map<std::string, std::string>
 Manager::getNearbyPeers(const std::string& accountID)
