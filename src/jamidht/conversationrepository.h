bb1a556cb207c26e072b404eb7a5f65eda4490be
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-12-18 14:50:09 -0500

    swarmchat: clone distant conversation
    Retrieving a conversation is done by cloning the distant repository
    
    Change-Id: Ib8e03bcde57ab4a504fca35c7b64826ba9b8325f

--- src/jamidht/conversationrepository.h
+++ src/jamidht/conversationrepository.h
@@ -1,6 +1,5 @@
 /*
-/*
- *  Copyright (C) 2017-2019 Savoir-faire Linux Inc.
+ *  Copyright (C) 2019 Savoir-faire Linux Inc.
  *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -26,18 +25,34 @@
 namespace jami {
 
 class JamiAccount;
+class ChannelSocket;
 
 /**
  * This class gives access to the git repository that represents the conversation
  */
-class DRING_TESTABLE ConversationRepository {
+class DRING_TESTABLE ConversationRepository
+{
 public:
     /**
      * Creates a new repository, with initial files, where the first commit hash is the conversation id
      * @param account       The related account
      * @return  the conversation repository object
      */
-    static DRING_TESTABLE std::unique_ptr<ConversationRepository> createConversation(const std::weak_ptr<JamiAccount>& account);
+    static DRING_TESTABLE std::unique_ptr<ConversationRepository> createConversation(
+        const std::weak_ptr<JamiAccount>& account);
+
+    /**
+     * Clones a conversation on a remote device
+     * @note This will use the socket registered for the conversation with JamiAccount::addGitSocket()
+     * @param account           The account getting the conversation
+     * @param deviceId          Remote device
+     * @param conversationId    Conversation to clone
+     * @param socket            Socket used to clone
+     */
+    static DRING_TESTABLE std::unique_ptr<ConversationRepository> cloneConversation(
+        const std::weak_ptr<JamiAccount>& account,
+        const std::string& deviceId,
+        const std::string& conversationId);
 
     /**
      * Open a conversation repository for an account and an id
@@ -48,9 +63,33 @@ public:
     ~ConversationRepository();
 
     /**
+     * Fetch a remote repository via the given socket
+     * @note This will use the socket registered for the conversation with JamiAccount::addGitSocket()
+     * @note will create a remote identified by the deviceId
+     * @param remoteDeviceId    Remote device id to fetch
+     * @return if the operation was successful
+     */
+    bool fetch(const std::string& remoteDeviceId);
+
+    /**
+     * Retrieve remote head. Can be useful after a fetch operation
+     * @param remoteDeviceId        The remote name
+     * @param branch                Remote branch to check (default: master)
+     * @return the commit id pointed
+     */
+    std::string remoteHead(const std::string& remoteDeviceId, const std::string& branch = "master");
+
+    /**
      * Return the conversation id
      */
     std::string id() const;
+
+    /**
+     * Add a new commit to the conversation
+     * @param msg     The msg to send
+     * @return <empty> on failure, else the message id
+     */
+    std::string sendMessage(const std::string& msg);
 
 private:
     ConversationRepository() = delete;
@@ -58,4 +97,4 @@ private:
     std::unique_ptr<Impl> pimpl_;
 };
 
-}
+} // namespace jami
