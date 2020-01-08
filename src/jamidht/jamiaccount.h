86a4a469404e0bc99ea77d39303bf23b43f31386
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-01-08 11:37:33 -0500

    group chat: start to link to dring API
    Change-Id: Ia4e687bde3448e622352f3a58541b140b18035ef

--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -4,6 +4,7 @@
  *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
  *  Author: Simon Désaulniers <simon.desaulniers@gmail.com>
  *  Author: Guillaume Roguez <guillaume.roguez@savoirfairelinux.com>
+ *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -78,6 +79,22 @@ class ContactList;
 class AccountManager;
 struct AccountInfo;
 class Conversation;
+
+/**
+ * A ConversationRequest is a request which corresponds to a trust request, but for conversations
+ * It's signed by the sender and contains the members list, the conversationId, and the metadatas
+ * such as the conversation's vcard, etc. (TODO determine)
+ * Transmitted via the UDP DHT
+ */
+struct ConversationRequest : public dht::EncryptedValue<ConversationRequest>
+{
+    static const constexpr dht::ValueType& TYPE = dht::ValueType::USER_DATA;
+    dht::Value::Id id = dht::Value::INVALID_ID;
+    std::string conversationId;
+    std::vector<std::string> members;
+    std::map<std::string, std::string> metadatas;
+    MSGPACK_DEFINE_MAP(id, conversationId, members, metadatas)
+};
 
 using GitSocketList =   std::map<
                             std::string, /* device Id */
@@ -466,6 +483,8 @@ public:
 
     // Conversation management
     std::string startConversation();
+    void acceptConversationRequest(const std::string& conversationId);
+    void declineConversationRequest(const std::string& conversationId);
     bool removeConversation(const std::string& conversationId);
 
     // Member management
@@ -475,7 +494,7 @@ public:
 
     // Message send/load
     void sendMessage(const std::string& conversationId, const std::string& message, const std::string& parent);
-    void loadConversationMessages(const std::string& conversationId, const std::string& fromMessage, size_t n);
+    void loadConversationMessages(const std::string& conversationId, const std::string& fromMessage = "", size_t n = 0);
 
 private:
     NON_COPYABLE(JamiAccount);
@@ -487,6 +506,7 @@ private:
      * Private structures
      */
     struct PendingCall;
+    struct PendingConversationClone;
     struct PendingMessage;
     struct BuddyInfo;
     struct DiscoveredPeer;
@@ -645,7 +665,7 @@ private:
     std::map<dht::InfoHash, BuddyInfo> trackedBuddies_;
 
     /** Conversations */
-    std::map<std::string, std::shared_ptr<Conversation>> conversations_;
+    std::map<std::string, std::unique_ptr<Conversation>> conversations_;
 
     mutable std::mutex dhtValuesMtx_;
     bool dhtPublicInCalls_ {true};
@@ -725,6 +745,17 @@ private:
      * Jami, or for each connectivityChange()
      */
     void cacheTurnServers();
+
+    // Conversations
+    std::mutex conversationsRequestsMtx_ {};
+    std::map<std::string, ConversationRequest> conversationsRequests_ {};
+    std::mutex pendingConversationsCloneMtx_ {};
+    std::map<std::string, PendingConversationClone> pendingConversationsClone_;
+
+    std::shared_ptr<RepeatedTask> conversationsEventHandler {};
+    void checkConversationsEvents();
+    bool handlePendingConversations();
+
 };
 
 static inline std::ostream& operator<< (std::ostream& os, const JamiAccount& acc)
