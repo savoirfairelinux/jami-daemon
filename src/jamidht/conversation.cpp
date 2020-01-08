55cf34836820b9e241499d945e5fb913de684858
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-01-08 11:37:33 -0500

    group chat: start to link to dring API
    Change-Id: Ia4e687bde3448e622352f3a58541b140b18035ef

---
+++ src/jamidht/conversation.cpp
@@ -0,0 +1,122 @@
+/*
+ *  Copyright (C) 2014-2019 Savoir-faire Linux Inc.
+ *
+ *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+ *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "conversation.h"
+
+#include "fileutils.h"
+#include "jamiaccount.h"
+#include "conversationrepository.h"
+
+namespace jami
+{
+
+Conversation::Conversation(const std::weak_ptr<JamiAccount>& account, const std::string& conversationId)
+: account_(account)
+{
+    if (conversationId.empty())
+        repository_ = ConversationRepository::createConversation(account);
+    else
+        repository_ = std::make_unique<ConversationRepository>(account, conversationId);
+}
+
+Conversation::Conversation(const std::weak_ptr<JamiAccount>& account, const std::string& remoteDevice, const std::string& conversationId)
+: account_(account)
+{
+    repository_ = ConversationRepository::cloneConversation(account, remoteDevice, conversationId);
+}
+
+
+Conversation::~Conversation()
+{
+
+}
+
+std::string
+Conversation::id() const
+{
+    return repository_? repository_->id() : "";
+}
+
+
+void
+Conversation::addMember(const std::string& contactUri)
+{
+    // Retrieve certificate
+    auto cert = tls::CertificateStore::instance().getCertificate(contactUri);
+    if (!cert) {
+        JAMI_WARN("Could not add member %s because no certificate is found", contactUri.c_str());
+        return;
+    }
+    // Add member files and commit
+    repository_->addMember(cert);
+}
+
+bool
+Conversation::removeMember(const std::string& contactUri)
+{
+    return true;
+}
+
+std::vector<std::map<std::string, std::string>>
+Conversation::getMembers()
+{
+    std::vector<std::map<std::string, std::string>> result;
+    auto shared = account_.lock();
+    if (!shared) return result;
+
+    auto repoPath = fileutils::get_data_dir()+DIR_SEPARATOR_STR+shared->getAccountID()+DIR_SEPARATOR_STR+"conversations"+DIR_SEPARATOR_STR+repository_->id();
+    auto adminsPath = repoPath+DIR_SEPARATOR_STR+"admins";
+    auto membersPath = repoPath+DIR_SEPARATOR_STR+"members";
+    for (const auto& certificate: fileutils::readDirectory(adminsPath)) {
+        if (certificate.find(".crt") == std::string::npos) {
+            JAMI_WARN("Incorrect file found: %s/%s", adminsPath.c_str(), certificate.c_str());
+        }
+        std::map<std::string, std::string> details {
+            {"uri", certificate.substr(0,certificate.size()-std::string(".crt").size())},
+            {"role","admin"}
+        };
+        result.emplace_back(details);
+    }
+    for (const auto& certificate: fileutils::readDirectory(membersPath)) {
+        if (certificate.find(".crt") == std::string::npos) {
+            JAMI_WARN("Incorrect file found: %s/%s", membersPath.c_str(), certificate.c_str());
+        }
+        std::map<std::string, std::string> details {
+            {"uri", certificate.substr(0,certificate.size()-std::string(".crt").size())},
+            {"role","member"}
+        };
+        result.emplace_back(details);
+    }
+
+    return result;
+}
+
+void
+Conversation::sendMessage(const std::string& message, const std::string& parent)
+{
+
+}
+
+void
+Conversation::loadMessages(const std::string& fromMessage, size_t n)
+{
+
+}
+
+}
