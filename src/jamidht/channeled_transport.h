0196c5ec8ea4dff2aaece58419e6d539828259c0
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-01-13 14:20:31 -0500

    sip: maintain SIP connections with contacts
    This patch includes major changes on how messages are transmitted. It's the first
    patch that uses the ConnectionManager. Each time a device is detected via the DHT,
    Jami will ask a new p2p connection with that device and then will open a new SIP
    channel. Also, the same mechanism exists when the user try to send a new message
    through the DHT.
    
    In the future, that SIP channel will be used to send vCards, presence status,
    read status, etc.
    
    Change-Id: I415553193accd8b1259ffdc037e684524fe03876

---
+++ src/jamidht/channeled_transport.h
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (C) 2020 Savoir-faire Linux Inc.
+ *
+ *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "ip_utils.h"
+#include "noncopyable.h"
+#include "scheduled_executor.h"
+#include "sip/sip_utils.h"
+
+#include <pjsip.h>
+#include <pj/pool.h>
+
+#include <atomic>
+#include <condition_variable>
+#include <chrono>
+#include <list>
+#include <memory>
+#include <thread>
+#include <type_traits>
+#include <utility>
+
+namespace jami {
+
+class ChannelSocket;
+using onShutdownCb = std::function<void(void)>;
+
+namespace tls {
+
+/**
+ * ChanneledSIPTransport
+ *
+ * Implements a pjsip_transport on top of a ChannelSocket
+ */
+class ChanneledSIPTransport
+{
+public:
+    using TransportData = struct {
+        pjsip_transport base; // do not move, SHOULD be the fist member
+        ChanneledSIPTransport* self {nullptr};
+    };
+    static_assert(std::is_standard_layout<TransportData>::value,
+                  "TranportData requires standard-layout");
+
+    ChanneledSIPTransport(pjsip_endpoint* endpt, int tp_type,
+                    const std::shared_ptr<ChannelSocket>& socket,
+                    const IpAddr& local, const IpAddr& remote,
+                    onShutdownCb&& cb);
+    ~ChanneledSIPTransport();
+
+    pjsip_transport* getTransportBase() { return &trData_.base; }
+private:
+    NON_COPYABLE(ChanneledSIPTransport);
+
+    // The SIP transport uses a ChannelSocket to send and receive datas
+    std::shared_ptr<ChannelSocket> socket_;
+    IpAddr local_;
+    IpAddr remote_;
+
+    // PJSIP transport backend
+    TransportData trData_; // uplink to "this" (used by PJSIP called C-callbacks)
+
+    std::unique_ptr<pj_pool_t, decltype(pj_pool_release)*> pool_;
+    std::unique_ptr<pj_pool_t, decltype(pj_pool_release)*> rxPool_;
+
+    std::mutex rxMtx_;
+    std::list<std::vector<uint8_t>> rxPending_;
+    pjsip_rx_data rdata_;
+
+    std::mutex txMutex_ {};
+    std::condition_variable txCv_ {};
+    std::list<pjsip_tx_data*> txQueue_ {};
+
+    ScheduledExecutor scheduler_;
+
+    pj_status_t send(pjsip_tx_data*, const pj_sockaddr_t*, int, void*, pjsip_transport_callback);
+    void handleEvents();
+
+    // Handle disconnected event
+    std::atomic_bool disconnected_ {false};
+};
+
+}} // namespace jami::tls
