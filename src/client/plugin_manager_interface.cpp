87c09a46d2972be7ac2ebda2116b49970ef213c9
ayounes <amine.younes-bouacida@savoirfairelinux.com>
2020-01-14 15:10:17 -0500

    plugins: add call handler
    Change-Id: If5296e71d4979962f71443fb298891202c8d2afe

--- src/client/plugin_manager_interface.cpp
+++ src/client/plugin_manager_interface.cpp
@@ -80,4 +80,16 @@ int installPlugin(const std::string& jplPath, bool force) {
 int uninstallPlugin(const std::string& pluginRootPath) {
     return jami::Manager::instance().getJamiPluginManager().uninstallPlugin(pluginRootPath);
 }
+
+std::vector<std::string> listCallMediaHandlers() {
+    return jami::Manager::instance().getJamiPluginManager().getCallServicesManager().listCallMediaHandlers();
+}
+
+void toggleCallMediaHandler(const std::string& id, const bool toggle) {
+    return jami::Manager::instance().getJamiPluginManager().getCallServicesManager().toggleCallMediaHandler(id, toggle);
+}
+
+std::map<std::string,std::string> getCallMediaHandlerDetails(const std::string& id) {
+    return jami::Manager::instance().getJamiPluginManager().getCallServicesManager().getCallMediaHandlerDetails(id);
+}
 }
