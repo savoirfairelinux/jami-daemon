6d3017b17807586f814b4e277f0d718b77bcdea6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-01-15 14:47:53 -0500

    sipaccountbase: avoid use after free for publishedIpAddress_
    Change-Id: I2b6b5c81798b6fb9fbd977e64705bbd2d3364d27

--- src/sip/sipaccountbase.h
+++ src/sip/sipaccountbase.h
@@ -183,7 +183,7 @@ public:
      * @return std::string The public IPv4 or IPv6 address formatted in standard notation.
      */
     std::string getPublishedAddress() const {
-        return publishedIpAddress_;
+        return publishedIpStr_;
     }
 
     IpAddr getPublishedIpAddress() const {
@@ -353,7 +353,10 @@ protected:
      */
     IpAddr publishedIp_ {};
 
+    // This will be stored in the configuration
     std::string publishedIpAddress_ {};
+    // This is used at runtime . Mainly by SIPAccount::usePublishedAddressPortInVIA()
+    std::string publishedIpStr_ {};
 
     /**
      * Published port, used only if defined by the user
