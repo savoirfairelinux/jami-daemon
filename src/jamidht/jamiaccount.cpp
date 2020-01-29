1fdf5c74fb60667ea2602aa72a752af44c0d2dd9
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2020-01-29 17:29:14 -0500

    accounts: emit signal when an avatar is received in JAMS response
    - adds signal <AccountAvatarReceived> for <account_id> with base64
      <photo>
    - handles incoming avatar during account authorization response in
      on success callback by emitting <AccountAvatarReceived>
    
    Change-Id: I8b6b9685691e1335b895a12c35ea14b53ec6260c

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -1118,6 +1118,8 @@ JamiAccount::loadAccount(const std::string& archive_password, const std::string&
 
                 fileutils::check_dir(idPath_.c_str(), 0700);
 
+                emitSignal<DRing::ConfigurationSignal::AccountAvatarReceived>(getAccountID(), info.photo);
+
                 // save the chain including CA
                 auto id = info.identity;
                 id.first = std::move(fDeviceKey.get());
@@ -3247,6 +3249,5 @@ JamiAccount::cacheSIPConnection(std::shared_ptr<ChannelSocket>&& socket, const s
     // Retry messages
     messageEngine_.onPeerOnline(peerId);
 }
-
 
 } // namespace jami
