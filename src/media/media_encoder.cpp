47c2ade497337f1ce5f5d3750ee354bfc638f275
Pierre Lespagnol <pierre.lespagnol@savoirfairelinux.com>
2020-02-25 15:46:54 -0500

    accel: enable hardware encoding with videotoolbox on macOS
    Change-Id: Icc6a87f62aaf80f867d7dd3ef6927f9940e724a0

--- src/media/media_encoder.cpp
+++ src/media/media_encoder.cpp
@@ -563,9 +563,12 @@ MediaEncoder::prepareEncoderContext(AVCodec* outputCodec, bool is_video)
         // emit one intra frame every gop_size frames
         encoderCtx->max_b_frames = 0;
         encoderCtx->pix_fmt = AV_PIX_FMT_YUV420P;
-#ifdef RING_ACCEL
+#ifdef RING_ACCEL 
+// Keep YUV format for macOS
+#if !(defined(__APPLE__) && !TARGET_OS_IOS) 
         if (accel_)
             encoderCtx->pix_fmt = accel_->getFormat();
+#endif
 #endif
 
         // Fri Jul 22 11:37:59 EDT 2011:tmatth:XXX: DON'T set this, we want our
