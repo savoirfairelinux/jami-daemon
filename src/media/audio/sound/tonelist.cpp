c3b68020e5b52b0f07abab56c97599a9bd8c6f40
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2020-05-26 10:45:56 -0400

    tone list: cleanup
    Change-Id: I0bbcdd7e5d496ce79f31ed2c172c9b530c73f160

--- src/media/audio/sound/tonelist.cpp
+++ src/media/audio/sound/tonelist.cpp
@@ -26,84 +26,31 @@
 
 namespace jami {
 
-static const char *toneZone[TelephoneTone::ZID_COUNTRIES][Tone::TONE_NULL] = {
-    {
-        // ZID_NORTH_AMERICA
-        "350+440",                // Tone::TONE_DIALTONE
-        "480+620/500,0/500",      // Tone::TONE_BUSY
-        "440+480/2000,0/4000",    // Tone::TONE_RINGTONE
-        "480+620/250,0/250",      // Tone::TONE_CONGESTION
-    },
-    {
-        //ZID_FRANCE
-        "440",
-        "440/500,0/500",
-        "440/1500,0/3500",
-        "440/250,0/250",
-    },
-    {
-        //ZID_AUSTRALIA
-        "413+438",
-        "425/375,0/375",
-        "413+438/400,0/200,413+438/400,0/2000",
-        "425/375,0/375,420/375,8/375",
-    },
-    {
-        //ZID_UNITED_KINGDOM
-        "350+440",
-        "400/375,0/375",
-        "400+450/400,0/200,400+450/400,0/2000",
-        "400/400,0/350,400/225,0/525",
-    },
-    {
-        //ZID_SPAIN
-        "425",
-        "425/200,0/200",
-        "425/1500,0/3000",
-        "425/200,0/200,425/200,0/200,425/200,0/600",
-    },
-    {
-        //ZID_ITALY
-        "425/600,0/1000,425/200,0/200",
-        "425/500,0/500",
-        "425/1000,0/4000",
-        "425/200,0/200",
-    },
-    {
-        //ZID_JAPAN
-        "400",
-        "400/500,0/500",
-        "400+15/1000,0/2000",
-        "400/500,0/500",
-    }
-};
-
-
-TelephoneTone::COUNTRYID
+TelephoneTone::CountryId
 TelephoneTone::getCountryId(const std::string& countryName)
 {
-    if (countryName == "North America")         return ZID_NORTH_AMERICA;
-    else if (countryName == "France")           return ZID_FRANCE;
-    else if (countryName == "Australia")        return ZID_AUSTRALIA;
-    else if (countryName == "United Kingdom")   return ZID_UNITED_KINGDOM;
-    else if (countryName == "Spain")            return ZID_SPAIN;
-    else if (countryName == "Italy")            return ZID_ITALY;
-    else if (countryName == "Japan")            return ZID_JAPAN;
-    else                                        return ZID_NORTH_AMERICA; // default
+    if (countryName == "North America")         return CountryId::ZID_NORTH_AMERICA;
+    else if (countryName == "France")           return CountryId::ZID_FRANCE;
+    else if (countryName == "Australia")        return CountryId::ZID_AUSTRALIA;
+    else if (countryName == "United Kingdom")   return CountryId::ZID_UNITED_KINGDOM;
+    else if (countryName == "Spain")            return CountryId::ZID_SPAIN;
+    else if (countryName == "Italy")            return CountryId::ZID_ITALY;
+    else if (countryName == "Japan")            return CountryId::ZID_JAPAN;
+    else                                        return CountryId::ZID_NORTH_AMERICA; // default
 }
 
 TelephoneTone::TelephoneTone(const std::string& countryName, unsigned int sampleRate)
     : countryId_(getCountryId(countryName))
-    , currentTone_(Tone::TONE_NULL)
+    , currentTone_(Tone::ToneId::TONE_NULL)
 {
     buildTones(sampleRate);
 }
 
 void
-TelephoneTone::setCurrentTone(Tone::TONEID toneId)
+TelephoneTone::setCurrentTone(Tone::ToneId toneId)
 {
-    if (toneId != Tone::TONE_NULL && currentTone_ != toneId)
-        tones_[toneId]->reset();
+    if (toneId != Tone::ToneId::TONE_NULL && currentTone_ != toneId)
+        tones_[(size_t)toneId]->reset();
 
     currentTone_ = toneId;
 }
@@ -117,19 +64,70 @@ TelephoneTone::setSampleRate(unsigned int sampleRate)
 std::shared_ptr<Tone>
 TelephoneTone::getCurrentTone()
 {
-    if (currentTone_ < Tone::TONE_DIALTONE or currentTone_ >= Tone::TONE_NULL)
+    if (currentTone_ < Tone::ToneId::DIALTONE or currentTone_ >= Tone::ToneId::TONE_NULL)
         return nullptr;
 
-    return tones_[currentTone_];
+    return tones_[(size_t)currentTone_];
 }
 
 void
 TelephoneTone::buildTones(unsigned int sampleRate)
 {
-    tones_[Tone::TONE_DIALTONE] = std::make_shared<Tone>(toneZone[countryId_][Tone::TONE_DIALTONE], sampleRate);
-    tones_[Tone::TONE_BUSY] = std::make_shared<Tone>(toneZone[countryId_][Tone::TONE_BUSY], sampleRate);
-    tones_[Tone::TONE_RINGTONE] = std::make_shared<Tone>(toneZone[countryId_][Tone::TONE_RINGTONE], sampleRate);
-    tones_[Tone::TONE_CONGESTION] = std::make_shared<Tone>(toneZone[countryId_][Tone::TONE_CONGESTION], sampleRate);
+    const char* toneZone[(size_t)TelephoneTone::CountryId::ZID_COUNTRIES][(size_t)Tone::ToneId::TONE_NULL] = {
+        {
+            // ZID_NORTH_AMERICA
+            "350+440",                // Tone::TONE_DIALTONE
+            "480+620/500,0/500",      // Tone::TONE_BUSY
+            "440+480/2000,0/4000",    // Tone::TONE_RINGTONE
+            "480+620/250,0/250",      // Tone::TONE_CONGESTION
+        },
+        {
+            //ZID_FRANCE
+            "440",
+            "440/500,0/500",
+            "440/1500,0/3500",
+            "440/250,0/250",
+        },
+        {
+            //ZID_AUSTRALIA
+            "413+438",
+            "425/375,0/375",
+            "413+438/400,0/200,413+438/400,0/2000",
+            "425/375,0/375,420/375,8/375",
+        },
+        {
+            //ZID_UNITED_KINGDOM
+            "350+440",
+            "400/375,0/375",
+            "400+450/400,0/200,400+450/400,0/2000",
+            "400/400,0/350,400/225,0/525",
+        },
+        {
+            //ZID_SPAIN
+            "425",
+            "425/200,0/200",
+            "425/1500,0/3000",
+            "425/200,0/200,425/200,0/200,425/200,0/600",
+        },
+        {
+            //ZID_ITALY
+            "425/600,0/1000,425/200,0/200",
+            "425/500,0/500",
+            "425/1000,0/4000",
+            "425/200,0/200",
+        },
+        {
+            //ZID_JAPAN
+            "400",
+            "400/500,0/500",
+            "400+15/1000,0/2000",
+            "400/500,0/500",
+        }
+    };
+    tones_[(size_t)Tone::ToneId::DIALTONE] = std::make_shared<Tone>(toneZone[(size_t)countryId_][(size_t)Tone::ToneId::DIALTONE], sampleRate);
+    tones_[(size_t)Tone::ToneId::BUSY] = std::make_shared<Tone>(toneZone[(size_t)countryId_][(size_t)Tone::ToneId::BUSY], sampleRate);
+    tones_[(size_t)Tone::ToneId::RINGTONE] = std::make_shared<Tone>(toneZone[(size_t)countryId_][(size_t)Tone::ToneId::RINGTONE], sampleRate);
+    tones_[(size_t)Tone::ToneId::CONGESTION] = std::make_shared<Tone>(toneZone[(size_t)countryId_][(size_t)Tone::ToneId::CONGESTION], sampleRate);
 }
 
 } // namespace jami
