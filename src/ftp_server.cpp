ea4e41c78f0bcb65963382369c64f9a9931d2083
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-06-08 16:18:20 -0400

    channeled transfer 2
    Change-Id: I8eaf7c38595bbf8e86098d6c8ad21afc9210fe6b

--- src/ftp_server.cpp
+++ src/ftp_server.cpp
@@ -35,11 +35,15 @@ namespace jami {
 //==============================================================================
 
 FtpServer::FtpServer(const std::string& account_id,
-                     const std::string& peer_uri)
+                     const std::string& peer_uri,
+                     const DRing::DataTransferId& outId)
     : Stream()
     , accountId_ {account_id}
     , peerUri_ {peer_uri}
-{}
+    , outId_ {outId}
+{
+
+}
 
 DRing::DataTransferId
 FtpServer::getId() const
@@ -67,12 +71,24 @@ FtpServer::startNewFile()
     info.totalSize = fileSize_;
     info.bytesProgress = 0;
     rx_ = 0;
-    out_ = Manager::instance().dataTransfers->onIncomingFileRequest(info); // we block here until answer from client
+    out_ = Manager::instance().dataTransfers->onIncomingFileRequest(info/* TODO, outId_*/); // we block here until answer from client
     if (!out_.stream) {
         JAMI_DBG() << "[FTP] transfer aborted by client";
         closed_ = true; // send NOK msg at next read()
     } else {
         go_ = true;
+    }
+
+    if (onRecvCb_) {
+        std::vector<uint8_t> buffer;
+        if (go_) {
+            buffer.resize(3);
+            buffer[0] = 'G'; buffer[1] = 'O'; buffer[2] = '\n';
+        } else {
+            buffer.resize(4);
+            buffer[0] = 'N'; buffer[1] = 'G'; buffer[2] = 'O'; buffer[3] = '\n';
+        }
+        onRecvCb_(std::move(buffer));
     }
     return bool(out_.stream);
 }
@@ -172,18 +188,19 @@ FtpServer::write(const std::vector<uint8_t>& buffer)
 bool
 FtpServer::parseStream(const std::vector<uint8_t>& buffer)
 {
-    headerStream_ << std::string(std::begin(buffer), std::end(buffer));
+    std::stringstream stream;
+    stream.write((const char*)buffer.data(), buffer.size());
 
     // Simple line stream parser
-    while (headerStream_.getline(&line_[0], line_.size())) {
-        if (parseLine(std::string(&line_[0], headerStream_.gcount()-1)))
-            return true; // headers EOF, data may remain in headerStream_
+    while (stream.getline(&line_[0], line_.size())) {
+        if (parseLine(std::string(&line_[0], stream.gcount()-1)))
+            return true; // headers EOF, data may remain in stream
     }
 
-    if (headerStream_.fail())
+    if (stream.fail())
         throw std::runtime_error("[FTP] header parsing error");
 
-    headerStream_.clear();
+    stream.clear();
     return false; // need more data
 }
 
--- src/jamidht/p2p.h
+++ src/jamidht/p2p.h
@@ -31,6 +31,18 @@ namespace jami {
 
 class JamiAccount;
 class PeerConnection;
+class ChannelSocket;
+class Stream;
+
+class ChanneledOutgoingTransfer {
+public:
+    ChanneledOutgoingTransfer(const std::shared_ptr<ChannelSocket>& channel);
+    
+    void linkTransfer(const std::shared_ptr<Stream>& file);
+private:
+    std::shared_ptr<ChannelSocket> channel_ {};
+    std::shared_ptr<Stream> file_;
+};
 
 class DhtPeerConnector {
 public:
@@ -38,9 +50,12 @@ public:
     ~DhtPeerConnector();
 
     void onDhtConnected(const std::string& device_id);
-    void requestConnection(const std::string& peer_id, const DRing::DataTransferId& tid, const std::function<void(PeerConnection*)>& connect_cb);
+    void requestConnection(const std::string& peer_id, const DRing::DataTransferId& tid,
+                           const std::function<void(PeerConnection*)>& connect_cb,
+                           const std::function<void(const std::shared_ptr<ChanneledOutgoingTransfer>&)>& channeledConnectedCb,
+                           const std::function<void()>& onChanneledCancelled);
     void closeConnection(const std::string& peer_id, const DRing::DataTransferId& tid);
-
+    void onIncomingConnection(const std::string& peer_id, const DRing::DataTransferId& tid, const std::shared_ptr<ChannelSocket>& channel);
 private:
     DhtPeerConnector() = delete;
 
