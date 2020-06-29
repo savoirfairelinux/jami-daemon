0a9097b0115243a17c26d4a05c47fe268ef945b5
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-06-29 11:15:26 -0400

    jamiaccount: change ICE_NEGOTIATION_TIMEOUT to 10 seconds
    60 seconds is pretty long and users will close the outgoing/incoming call
    view will close the interface before this timeout.
    
    Change-Id: I1e6cc69370f9e9be03dc04422475adf76cef846d
    Gitlab: #248

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -196,7 +196,7 @@ struct JamiAccount::DiscoveredPeer
 
 static constexpr int ICE_COMPONENTS {1};
 static constexpr int ICE_COMP_SIP_TRANSPORT {0};
-static constexpr auto ICE_NEGOTIATION_TIMEOUT = std::chrono::seconds(60);
+static constexpr auto ICE_NEGOTIATION_TIMEOUT = std::chrono::seconds(10);
 static constexpr auto TLS_TIMEOUT = std::chrono::seconds(30);
 const constexpr auto EXPORT_KEY_RENEWAL_TIME = std::chrono::minutes(20);
 
