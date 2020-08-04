85510893d5b70c494679c675acc5e4a36181097a
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2020-08-04 11:41:29 -0400

    misc: add clang format stylesheet
    - Run to clang-format all committed files.
    - Run with --all to format all cpp|cxx|cc|h|hpp|mm files in src/.
    - Run with --install to install as a pre-commit hook.
    
    Requires that clang-format be installed.
    
    Devs should either install the pre-commit hook or configure their
    editors to format on save etc.
    
    Change-Id: I73c2b19e0673115c1f7d33cbae5094f3a88d43e3

---
+++ format.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -e
+
+command_exists ()
+{
+    type "$1" &> /dev/null ;
+}
+
+CFVERSION="9"
+CLANGFORMAT=""
+if command_exists clang-format; then
+    CLANGFORMAT=clang-format
+else
+   if command_exists clang-format-${CFVERSION}; then
+      CLANGFORMAT=clang-format-${CFVERSION}
+   fi
+fi
+
+if ! command -v $CLANGFORMAT &> /dev/null; then
+    echo "Required version of clang-format not found"
+    exit 1
+fi
+
+format_file()
+{
+    if [ -f "${1}" ]; then
+        $CLANGFORMAT -i -style=file "${1}"
+    fi
+}
+
+format_files()
+{
+    for file in $1; do
+        echo -ne "Formatting: ${file}\\033[0K\\r"
+        format_file "${file}"
+    done
+}
+
+exit_if_no_files()
+{
+    echo No files to format
+    exit 0
+}
+
+install_hook()
+{
+    # check for lone repo ring-daemon
+    hooks_path=".git/hooks"
+    if [ ! -d "$hooks_path=" ]; then
+        # or ring-project
+        hooks_path="../.git/modules/daemon/hooks"
+        if [ ! -d "$hooks_path" ]; then
+            echo "Can't find a git directory"
+            exit 1
+        fi
+    fi
+    echo Installing pre-commit hook in "$hooks_path"
+    echo "/bin/bash $0" > "$hooks_path"/pre-commit
+    chmod +x "$hooks_path"/pre-commit
+}
+
+display_help()
+{
+    echo "Usage: $0 [OPTION...] -- Clang format source files with a .clang-format file" >&2
+    echo
+    echo "   --all           format all files instead of only committed ones"
+    echo "   --install       install a pre-commit hook to run this script"
+    echo
+}
+
+if [ "$1" == "--help" ]; then
+    display_help
+    exit 0
+fi
+
+case "${1}" in
+  --all )
+    files=$(find src -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\|mm\)') || true
+    echo Formatting all source files...
+    format_files "$files"
+    ;;
+  --install )
+    install_hook
+    ;;
+  * )
+    files=$(git diff-index --cached --name-only HEAD | grep -iE '\.(cpp|cxx|cc|h|hpp|mm)') || exit_if_no_files
+    echo Formatting committed source files...
+    format_files "$files"
+    ;;
+esac
