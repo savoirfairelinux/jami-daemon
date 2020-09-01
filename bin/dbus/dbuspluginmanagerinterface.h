00ddeb27e936844058d2eb743454d1d4b5772a0e
agsantos <aline.gondimsantos@savoirfairelinux.com>
2020-09-01 13:29:35 -0400

    plugin: code cleanup
    Change-Id: I5573606fae39567caf9026e9b1253b33588ab292

--- bin/dbus/dbuspluginmanagerinterface.h
+++ bin/dbus/dbuspluginmanagerinterface.h
@@ -27,7 +27,7 @@
 #include "dring/def.h"
 #include "dbus_cpp.h"
 
-#if __GNUC__ >= 5 || (__GNUC__ >=4 && __GNUC_MINOR__ >= 6)
+#if __GNUC__ >= 5 || (__GNUC__ >= 4 && __GNUC_MINOR__ >= 6)
 /* This warning option only exists for gcc 4.6.0 and greater. */
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #endif
@@ -38,37 +38,39 @@
 #pragma GCC diagnostic warning "-Wignored-qualifiers"
 #pragma GCC diagnostic warning "-Wunused-parameter"
 
-#if __GNUC__ >= 5 || (__GNUC__ >=4 && __GNUC_MINOR__ >= 6)
+#if __GNUC__ >= 5 || (__GNUC__ >= 4 && __GNUC_MINOR__ >= 6)
 /* This warning option only exists for gcc 4.6.0 and greater. */
 #pragma GCC diagnostic warning "-Wunused-but-set-variable"
 #endif
 
-class DRING_PUBLIC DBusPluginManagerInterface :
-    public cx::ring::Ring::PluginManagerInterface_adaptor,
-    public DBus::IntrospectableAdaptor,
-    public DBus::ObjectAdaptor
+class DRING_PUBLIC DBusPluginManagerInterface
+    : public cx::ring::Ring::PluginManagerInterface_adaptor,
+      public DBus::IntrospectableAdaptor,
+      public DBus::ObjectAdaptor
 {
-    public:
-        DBusPluginManagerInterface(DBus::Connection& connection);
+public:
+    DBusPluginManagerInterface(DBus::Connection& connection);
 
-        // Methods
-        bool loadPlugin(const std::string& path);
-        bool unloadPlugin(const std::string& path);
-        void togglePlugin(const std::string& path, const bool& toggle);
-        std::map<std::string,std::string> getPluginDetails(const std::string& path);
-        std::vector<std::map<std::string,std::string>> getPluginPreferences(const std::string& path);
-        bool setPluginPreference(const std::string& path, const std::string& key, const std::string& value);
-        std::map<std::string,std::string> getPluginPreferencesValues(const std::string& path);
-        bool resetPluginPreferencesValues(const std::string& path);
-        std::vector<std::string> listAvailablePlugins();
-        std::vector<std::string> listLoadedPlugins();
-        int installPlugin(const std::string& jplPath, const bool& force);
-        int uninstallPlugin(const std::string& pluginRootPath);
-        std::vector<std::string> listCallMediaHandlers();
-        void toggleCallMediaHandler(const std::string& id, const bool& toggle);
-        std::map<std::string,std::string> getCallMediaHandlerDetails(const std::string& id);
+    // Methods
+    bool loadPlugin(const std::string& path);
+    bool unloadPlugin(const std::string& path);
+    void togglePlugin(const std::string& path, const bool& toggle);
+    std::map<std::string, std::string> getPluginDetails(const std::string& path);
+    std::vector<std::map<std::string, std::string>> getPluginPreferences(const std::string& path);
+    bool setPluginPreference(const std::string& path,
+                             const std::string& key,
+                             const std::string& value);
+    std::map<std::string, std::string> getPluginPreferencesValues(const std::string& path);
+    bool resetPluginPreferencesValues(const std::string& path);
+    std::vector<std::string> listAvailablePlugins();
+    std::vector<std::string> listLoadedPlugins();
+    int installPlugin(const std::string& jplPath, const bool& force);
+    int uninstallPlugin(const std::string& pluginRootPath);
+    std::vector<std::string> listCallMediaHandlers();
+    void toggleCallMediaHandler(const std::string& id, const bool& toggle);
+    std::map<std::string, std::string> getCallMediaHandlerDetails(const std::string& id);
 
-        bool getPluginsEnabled();
-        void setPluginsEnabled(const bool& state);
-        std::map<std::string,std::string> getCallMediaHandlerStatus();
+    bool getPluginsEnabled();
+    void setPluginsEnabled(const bool& state);
+    std::map<std::string, std::string> getCallMediaHandlerStatus();
 };
