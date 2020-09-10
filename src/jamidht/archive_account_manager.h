9f4bc8cc7c81bb8264f9bc3433bfc3a0b19c75bd
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-09-10 10:46:56 -0400

    swarm: synchronize conversations between devices
    Change-Id: I9116f86f9bd008be14c0ccfaebd831839a5e4f63
    Gitlab: #311

--- src/jamidht/archive_account_manager.h
+++ src/jamidht/archive_account_manager.h
@@ -49,7 +49,7 @@ public:
                             AuthFailureCallback onFailure,
                             OnChangeCallback onChange) override;
 
-    void startSync() override;
+    void startSync(const OnNewDeviceCb&) override;
 
     bool changePassword(const std::string& password_old, const std::string& password_new) override;
 
