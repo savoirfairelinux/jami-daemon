b416ecf60d26f63518310d64799bb6fef999cab5
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-09-25 14:20:48 -0400

    connectivity: use default initializer to avoid locks
    Change-Id: Id0a13edf4f27b5f2fa11a63e4286166058c59ade

--- src/jamidht/channeled_transport.h
+++ src/jamidht/channeled_transport.h
@@ -64,25 +64,25 @@ private:
     NON_COPYABLE(ChanneledSIPTransport);
 
     // The SIP transport uses a ChannelSocket to send and receive datas
-    std::shared_ptr<ChannelSocket> socket_;
-    IpAddr local_;
-    IpAddr remote_;
+    std::shared_ptr<ChannelSocket> socket_ {};
+    IpAddr local_ {};
+    IpAddr remote_ {};
 
     // PJSIP transport backend
-    TransportData trData_; // uplink to "this" (used by PJSIP called C-callbacks)
+    TransportData trData_ {}; // uplink to "this" (used by PJSIP called C-callbacks)
 
     std::unique_ptr<pj_pool_t, decltype(pj_pool_release)*> pool_;
     std::unique_ptr<pj_pool_t, decltype(pj_pool_release)*> rxPool_;
 
-    std::mutex rxMtx_;
-    std::list<std::vector<uint8_t>> rxPending_;
-    pjsip_rx_data rdata_;
+    std::mutex rxMtx_ {};
+    std::list<std::vector<uint8_t>> rxPending_ {};
+    pjsip_rx_data rdata_ {};
 
     std::mutex txMutex_ {};
     std::condition_variable txCv_ {};
     std::list<pjsip_tx_data*> txQueue_ {};
 
-    ScheduledExecutor scheduler_;
+    ScheduledExecutor scheduler_ {};
 
     pj_status_t send(pjsip_tx_data*, const pj_sockaddr_t*, int, void*, pjsip_transport_callback);
     void handleEvents();
