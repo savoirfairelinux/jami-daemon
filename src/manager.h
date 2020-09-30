5325b647b442c7e67660d32730efe7b7ad85290e
Sébastien LE STUM <sebastien.le-stum@savoirfairelinux.com>
2020-09-30 15:52:02 -0400

    manager: fix return value for accountCount from bool to std::size_t
    This value is used in getAccountList() to reserve the size of the final
    vector that will group all the account pointers.
    
    Change-Id: Id2b0ae149b08b7962123d9e7533fe3c34e8d4750

--- src/manager.h
+++ src/manager.h
@@ -838,7 +838,7 @@ public:
     }
 
     template<class T = Account>
-    bool accountCount() const
+    std::size_t accountCount() const
     {
         return accountFactory.accountCount<T>();
     }
