59a7ab03c78c7b906ff4a1b0ee11b522963d51af
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2020-10-02 07:44:41 -0400

    Search endpoint profiles
    Change-Id: Id569c9213be79c1864d458e491b68a3e9ad94eef

--- src/jamidht/server_account_manager.cpp
+++ src/jamidht/server_account_manager.cpp
@@ -508,12 +508,12 @@ ServerAccountManager::searchUser(const std::string& query, SearchCallback cb)
                 auto& this_ = *static_cast<ServerAccountManager*>(&accountManager);
                 if (response.status_code >= 200 && response.status_code < 300) {
                     try {
-                        Json::Value::ArrayIndex rcount = json.size();
+                        Json::Value::ArrayIndex rcount = json["profiles"].size();
                         std::vector<std::map<std::string, std::string>> results;
                         results.reserve(rcount);
                         JAMI_WARN("[Search] Got server response: %s", response.body.c_str());
                         for (Json::Value::ArrayIndex i = 0; i < rcount; i++) {
-                            const auto& ruser = json[i];
+                            const auto& ruser = json["profiles"][i];
                             std::map<std::string, std::string> user;
                             for (const auto& member : ruser.getMemberNames()) {
                                 const auto& rmember = ruser[member];
