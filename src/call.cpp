0f9501bb176b7086fb5c1058a81c729b4325c191
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-10-07 11:33:44 -0400

    call: avoid to get access to invalidated account
    getSIPAccount() is returning a reference to the SIPAccount. Account
    that can be destroyed and if a mistake is made, the data can be
    invalidated, generally even when closing the daemon.
    
    This patch change the behavior of getSIPAccount() to return a
    weak_ptr to make sure that the Account is alive when performing
    actions on the call
    
    Change-Id: I1dcc05bce0c9826297535b5a5224f9ff22479d80

--- src/call.cpp
+++ src/call.cpp
@@ -81,7 +81,7 @@ hangupCalls(const Call::SubcallSet& callptr_list, int errcode)
 
 //==============================================================================
 
-Call::Call(Account& account,
+Call::Call(const std::shared_ptr<Account>& account,
            const std::string& id,
            Call::CallType type,
            const std::map<std::string, std::string>& details)
@@ -127,12 +127,14 @@ Call::Call(Account& account,
         });
 
     time(&timestamp_start_);
-    account_.attachCall(id_);
+    if (auto shared = account_.lock())
+        shared->attachCall(id_);
 }
 
 Call::~Call()
 {
-    account_.detachCall(id_);
+    if (auto shared = account_.lock())
+        shared->detachCall(id_);
 }
 
 void
@@ -145,10 +147,13 @@ Call::removeCall()
         Recordable::stopRecording();
 }
 
-const std::string&
+std::string
 Call::getAccountId() const
 {
-    return account_.getAccountID();
+    if (auto shared = account_.lock())
+        return shared->getAccountID();
+    JAMI_ERR("No account detected");
+    return {};
 }
 
 Call::ConnectionState
