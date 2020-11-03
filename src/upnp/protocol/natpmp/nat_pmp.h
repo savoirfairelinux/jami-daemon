7917dfe2dc2981bd52cfd6ec5f1bbb24421d979d
Mohamed Chibani <mohamed.chibani@savoirfairelinux.com>
2020-11-03 16:41:48 -0500

    upnp: auto-provisioning of upnp mappings
    Implement a handler for automatic provisioning of port mappings through UPNP
    and NAT-PMP protocols in NATed networks. A pool of UDP/TCP ports will be
    allocated on start-up and periodically monitored such as when port allocation
    is requested, it will be readily available. This will improve responsiveness
    with some UPNP server implementations that may take several seconds to grant
    a mapping request.
    
    Gitlab: #335
    
    Change-Id: I837945797d3b6bb337d3419bf77b4dc686558e5f

--- src/upnp/protocol/natpmp/nat_pmp.h
+++ src/upnp/protocol/natpmp/nat_pmp.h
@@ -25,7 +25,6 @@
 #endif
 
 #include "../upnp_protocol.h"
-#include "../global_mapping.h"
 #include "../igd.h"
 #include "pmp_igd.h"
 
@@ -55,10 +54,10 @@ public:
     ~NatPmp();
 
     // Returns the protocol type.
-    Type getProtocol() const override { return Type::NAT_PMP; }
+    NatProtocolType getProtocol() const override { return NatProtocolType::NAT_PMP; }
 
     // Get protocol type as string.
-    virtual std::string getProtocolName() const override { return {"NAT_PMP"};}
+    virtual std::string getProtocolName() const override { return {"NAT_PMP"}; }
 
     // Notifies a change in network.
     void clearIgds() override;
@@ -66,14 +65,17 @@ public:
     // Renew pmp_igd.
     void searchForIgd() override;
 
+    // Get the IGD list.
+    void getIgdList(std::list<std::shared_ptr<IGD>>& igdList) const override;
+
+    // Return true if it has at least one valid IGD.
+    bool hasValidIgd() const override;
+
     // Tries to add mapping.
-    void requestMappingAdd(IGD* igd, const Mapping& mapping) override;
+    void requestMappingAdd(const IGD* igd, const Mapping& mapping) override;
 
     // Removes a mapping.
     void requestMappingRemove(const Mapping& igdMapping) override;
-
-    // Removes all local mappings of IGD that we're added by the application.
-    void removeAllLocalMappings(IGD* igd) override;
 
 private:
     void searchForPmpIgd();
@@ -82,11 +84,12 @@ private:
     void addPortMapping(Mapping& mapping, bool renew);
     // Removes a port mapping.
     void removePortMapping(Mapping& mapping);
-    // void addPortMapping(const PMPIGD& pmp_igd, natpmp_t& natpmp, GlobalMapping& mapping, bool
-    // remove=false) const;
-
     // Deletes all port mappings.
     void deleteAllPortMappings(int proto);
+    // Removes all local mappings of IGD that we're added by the application.
+    void removeAllLocalMappings();
+    // True if the error is fatal.
+    bool isErrorFatal(int error);
 
 private:
     NON_COPYABLE(NatPmp);
@@ -108,6 +111,9 @@ private:
     void clearNatPmpHdl(natpmp_t& hdl);
     // Gets NAT-PMP error code string.
     const char* getNatPmpErrorStr(int errorCode);
+
+    mutable std::mutex
+        validIgdMutex_; // Mutex used to access these lists and IGDs in a thread-safe manner.
 };
 
 } // namespace upnp
