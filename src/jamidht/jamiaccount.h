aba7660439f6f688ca5071527494ec3834be00a1
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-11-05 11:32:43 -0500

    swarm: remove conversation, first version
    Link removeConversation. For this feature there is two possibilities:
    + If we are the last member, remove all the files.
    + If not, wait for another user to sync.
    
    Change-Id: I387a20fe93a72524ae701e7e98d66633d91a9ede
    GitLab: #305

--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -509,8 +509,8 @@ public:
     std::string startConversation();
     void acceptConversationRequest(const std::string& conversationId);
     void declineConversationRequest(const std::string& conversationId);
-    bool removeConversation(const std::string& conversationId);
     std::vector<std::string> getConversations();
+    bool removeConversation(const std::string& conversationId);
     std::vector<std::map<std::string, std::string>> getConversationRequests();
 
     // Member management
@@ -932,6 +932,23 @@ private:
     void syncInfos(const std::shared_ptr<ChannelSocket>& socket);
     void syncWithConnected();
     std::atomic_bool needsConvSync {true};
+
+    /**
+     * Remove a repository and all files
+     * @param convId
+     * @param sync      If we send an update to other account's devices
+     */
+    void removeRepository(const std::string& convId, bool sync);
+
+    /**
+     * Send a message notification to all members
+     * @param conversation
+     * @param commit
+     * @param sync      If we send an update to other account's devices
+     */
+    void sendMessageNotification(const Conversation& conversation,
+                                 const std::string& commitId,
+                                 bool sync);
 };
 
 static inline std::ostream&
