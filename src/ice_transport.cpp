2b36eebe9fa63d0a5d30adec857f092955385fa3
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-11-11 09:15:57 -0500

    swarm: relink filetransfer
    Change-Id: I812bfc2b9b2bb27707eaaad512fca564ccc1bc34

--- src/ice_transport.cpp
+++ src/ice_transport.cpp
@@ -982,7 +982,11 @@ IceTransport::IceTransport(const char* name,
     : pimpl_ {std::make_unique<Impl>(name, component_count, master, options)}
 {}
 
-IceTransport::~IceTransport() {}
+IceTransport::~IceTransport()
+{
+    isStopped_ = true;
+    cancelOperations();
+}
 
 bool
 IceTransport::isInitialized() const
@@ -1159,6 +1163,7 @@ IceTransport::stop()
 void
 IceTransport::cancelOperations()
 {
+    isCancelled_ = true;
     for (auto& c : pimpl_->peerChannels_) {
         c.stop();
     }
