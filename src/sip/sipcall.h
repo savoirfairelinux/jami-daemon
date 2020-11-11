3bbe0ebc29c534006aa21ca652f8aa5de632dea1
agsantos <aline.gondimsantos@savoirfairelinux.com>
2020-11-11 10:27:23 -0500

    plugins: add audio to callMediaHandler
    Change-Id: I68e277bb4b0eea97310b63e3f4e10cd1a3212825

--- src/sip/sipcall.h
+++ src/sip/sipcall.h
@@ -264,7 +264,7 @@ private:
     /**
      * Call Streams and some typedefs
      */
-    using MediaStream = Observable<std::shared_ptr<MediaFrame>>;
+    using AVMediaStream = Observable<std::shared_ptr<MediaFrame>>;
     using MediaStreamSubject = PublishMapSubject<std::shared_ptr<MediaFrame>, AVFrame*>;
 
 #ifdef ENABLE_PLUGIN
@@ -276,7 +276,7 @@ private:
      * @param mediaStreamSubject
      */
     void createCallAVStream(const StreamData& StreamData,
-                            MediaStream& streamSource,
+                            AVMediaStream& streamSource,
                             const std::shared_ptr<MediaStreamSubject>& mediaStreamSubject);
     /**
      * @brief createCallAVStreams
