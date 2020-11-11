c6e6cc9115e6cddad9791d61e185f851218d7d87
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-11-11 09:15:57 -0500

    swarm: relink filetransfer
    Change-Id: I812bfc2b9b2bb27707eaaad512fca564ccc1bc34

--- src/jamidht/contact_list.cpp
+++ src/jamidht/contact_list.cpp
@@ -291,7 +291,16 @@ ContactList::onTrustRequest(const dht::InfoHash& peer_account,
         saveTrustRequests();
     }
     // Note: call JamiAccount's callback to build ConversationRequest anyway
-    callbacks_.trustRequest(peer_account.toString(), conversationId, std::move(payload), received);
+    JAMI_ERR("@@@ onTrustRequest for contact %s, conv %s - confirm %u ; active %u",
+             peer_account.to_c_str(),
+             conversationId.c_str(),
+             confirm,
+             active);
+    if (!confirm)
+        callbacks_.trustRequest(peer_account.toString(),
+                                conversationId,
+                                std::move(payload),
+                                received);
     return accept;
 }
 
