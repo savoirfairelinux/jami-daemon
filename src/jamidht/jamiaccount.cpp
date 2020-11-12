665748021a8fd6cc7c38a23d1b418e5bba27143d
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-11-12 11:08:36 -0500

    jamiaccount: fix regex for clang
    Change-Id: Ib4782138e0c428531f15cc9b67d12f2ec7ae556a

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -218,7 +218,7 @@ static constexpr const char* DEFAULT_TURN_USERNAME = "ring";
 static constexpr const char* DEFAULT_TURN_PWD = "ring";
 static constexpr const char* DEFAULT_TURN_REALM = "ring";
 static const auto PROXY_REGEX = std::regex(
-    "(https?://)?([\\w\\.\\-\\_\\~]+)(:(\\d+)|:\\[(.+)-(.+)\\])?");
+    "(https?://)?([\\w\\.\\-_\\~]+)(:(\\d+)|:\\[(.+)-(.+)\\])?");
 static const std::string PEER_DISCOVERY_JAMI_SERVICE = "jami";
 const constexpr auto PEER_DISCOVERY_EXPIRATION = std::chrono::minutes(1);
 
