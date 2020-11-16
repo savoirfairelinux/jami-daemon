074de94034b06c56ee5ea8c0963eb8d8a921f07d
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-11-16 16:27:21 -0500

    swarm: relink setIsComposing for swarm support
    GitLab: #320
    Change-Id: I82b049cfa83f1e3e38a106bce142c1e7ce31557c

--- src/sip/sipaccount.cpp
+++ src/sip/sipaccount.cpp
@@ -2162,6 +2162,7 @@ void
 SIPAccount::sendTextMessage(const std::string& to,
                             const std::map<std::string, std::string>& payloads,
                             uint64_t id,
+                            bool,
                             bool)
 {
     if (to.empty() or payloads.empty()) {
