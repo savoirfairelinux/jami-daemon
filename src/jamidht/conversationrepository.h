2a29b7943632159c91e76a453a31412ac878930f
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-11-17 13:55:05 -0500

    swarm: remove a user/device from the conversation
    TODO
    
    Change-Id: I23b5d00e9b69dcc667cb52543d528171f9f34fdc
    GitLab: #298
    GitLab: #299

--- src/jamidht/conversationrepository.h
+++ src/jamidht/conversationrepository.h
@@ -184,6 +184,9 @@ public:
      */
     void erase();
 
+    std::string voteKick(const std::string& uri, bool isDevice);
+    std::string resolveVote(const std::string& uri, bool isDevice);
+
 private:
     ConversationRepository() = delete;
     class Impl;
