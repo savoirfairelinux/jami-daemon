2c721c1d91b72dd615d9382989aa2cb9af607542
Pierre Lespagnol <pierre.lespagnol@savoirfairelinux.com>
2020-12-02 16:43:42 -0500

    conference: add kick participant for moderator
    Change-Id: Ieb4be786a286ce52501b270e473308ad1fd5ba23

--- bin/dbus/dbuscallmanager.h
+++ bin/dbus/dbuscallmanager.h
@@ -103,6 +103,7 @@ class DRING_PUBLIC DBusCallManager :
         void stopSmartInfo();
         void setModerator(const std::string& confId, const std::string& peerId, const bool& state);
         void muteParticipant(const std::string& confId, const std::string& peerId, const bool& state);
+        void hangupParticipant(const std::string& confId, const std::string& peerId);
 };
 
 #endif // __RING_CALLMANAGER_H__
