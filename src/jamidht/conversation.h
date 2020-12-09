5bd9176db96f49cb4fc8fcca17b50a86e31bf420
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-12-09 11:47:01 -0500

    swarm: add conversation member event signal
    Change-Id: I2fb22051230d9a1afc46020f81f1d72bc99d175b
    GitLab: #362

--- src/jamidht/conversation.h
+++ src/jamidht/conversation.h
@@ -58,7 +58,7 @@ public:
      * @return a vector of member details:
      * {
      *  "uri":"xxx",
-     *  "role":"member/admin",
+     *  "role":"member/admin/invited",
      *  "lastRead":"id"
      *  ...
      * }
@@ -110,9 +110,9 @@ public:
     /**
      * Analyze if merge is possible and merge history
      * @param uri       the peer
-     * @return if the operation was successful
+     * @return new commits
      */
-    bool mergeHistory(const std::string& uri);
+    std::vector<std::map<std::string, std::string>> mergeHistory(const std::string& uri);
 
     /**
      * Fetch and merge from peer
