b5f9c5e1e89d647bd3f59fb2dd38dd581db3df46
Mohamed Chibani <mohamed.chibani@savoirfairelinux.com>
2020-12-09 14:17:40 -0500

    Multi-stream: Create and update media of an outgoing call
    Add APIs to set a list of desired media that included when making
    outgoing calls.
    Also add APIs to update the media of an outgoing call. The update
    can be used for example to "mute/un-mute" a media, or to add a new
    media (add video to an audio-only call).
    
    Gitlab: #380
    
    Change-Id: Iab32a72547f3a0a2328408aadc6a7656c971a7b0

--- src/account.h
+++ src/account.h
@@ -152,6 +152,17 @@ public:
         std::string_view toUrl, const std::map<std::string, std::string>& volatileCallDetails = {})
         = 0;
 
+    /**
+     * Create a new outgoing call.
+     *
+     * @param toUrl The address to call
+     * @param mediaList A list of media
+     * @return The created call
+     */
+    virtual std::shared_ptr<Call> newOutgoingCall(std::string_view toUrl,
+                                                  const std::vector<MediaMap>& mediaList)
+        = 0;
+
     /* Note: we forbid incoming call creation from an instance of Account.
      * This is why no newIncomingCall() method exist here.
      */
@@ -326,10 +337,11 @@ public:
     void addDefaultModerator(const std::string& peerURI);
     void removeDefaultModerator(const std::string& peerURI);
 
-    bool isLocalModeratorsEnabled() const { return localModeratorsEnabled_;  }
+    bool isLocalModeratorsEnabled() const { return localModeratorsEnabled_; }
     void enableLocalModerators(bool isModEnabled) { localModeratorsEnabled_ = isModEnabled; }
 
-public: // virtual methods that has to be implemented by concrete classes
+public:
+    // virtual methods that has to be implemented by concrete classes
     /**
      * This method is called to request removal of possible account traces on the system,
      * like internal account setup files.
