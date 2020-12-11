30098086f978ecf09ed5ba002a784feb5c74d388
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-12-11 10:11:16 -0500

    misc: add monitoring log on sockets
    Change-Id: I77388bf9a6274aca2ffc732438bd67c19c4f551a

--- bin/dbus/dbusclient.cpp
+++ bin/dbus/dbusclient.cpp
@@ -261,6 +261,8 @@ DBusClient::initLibrary(int flags)
             bind(&DBusConfigurationManager::hardwareDecodingChanged, confM, _1)),
         exportable_callback<ConfigurationSignal::HardwareEncodingChanged>(
             bind(&DBusConfigurationManager::hardwareEncodingChanged, confM, _1)),
+        exportable_callback<ConfigurationSignal::MessageSend>(
+            bind(&DBusConfigurationManager::messageSend, confM, _1)),
     };
 
     // Presence event handlers
