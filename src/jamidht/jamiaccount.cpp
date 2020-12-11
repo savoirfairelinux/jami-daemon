d1535389873bc4dd7f722f890f57bab0a776cf5d
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-12-11 10:11:16 -0500

    misc: add monitoring log on sockets
    Change-Id: I77388bf9a6274aca2ffc732438bd67c19c4f551a

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -5240,4 +5240,11 @@ JamiAccount::addCallHistoryMessage(const std::string& uri, uint64_t duration_ms)
     }
 }
 
+void
+JamiAccount::monitor() const
+{
+    if (connectionManager_)
+        connectionManager_->monitor();
+}
+
 } // namespace jami
