53d9055cc272d685d4d5b2edcc930a2938be427f
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2020-12-11 10:11:16 -0500

    misc: add monitoring log on sockets
    Change-Id: I77388bf9a6274aca2ffc732438bd67c19c4f551a

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -2331,8 +2331,8 @@ JamiAccount::doRegister_()
                 auto now = std::chrono::duration_cast<std::chrono::milliseconds>(
                                std::chrono::steady_clock::now().time_since_epoch())
                                .count();
-                jami::emitSignal<DRing::DebugSignal::MessageSend>(std::to_string(now) + " "
-                                                                  + std::string(tmp));
+                jami::emitSignal<DRing::ConfigurationSignal::MessageSend>(std::to_string(now) + " "
+                                                                          + std::string(tmp));
             };
             context.logger = std::make_shared<dht::Logger>(log_all, log_all, silent);
 #else
@@ -5274,6 +5274,13 @@ JamiAccount::addCallHistoryMessage(const std::string& uri, uint64_t duration_ms)
         value["duration"] = std::to_string(duration_ms);
         sendMessage(convId, value);
     }
+}
+
+void
+JamiAccount::monitor() const
+{
+    if (connectionManager_)
+        connectionManager_->monitor();
 }
 
 } // namespace jami
--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -563,6 +563,8 @@ public:
     // Invites
     void onConversationRequest(const std::string& from, const Json::Value&) override;
 
+    void monitor() const;
+
 private:
     NON_COPYABLE(JamiAccount);
 
