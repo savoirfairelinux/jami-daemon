fa8651ad10e71393a7d6885a7d531f10f276b0ed
Mohamed Chibani <mohamed.chibani@savoirfairelinux.com>
2020-12-29 14:48:39 -0500

    Call/SIPCall: remove template
    Gitlab: #380
    
    Change-Id: I9b6adba40262f2feb4bbc8445c98b52d0297a194

--- src/call.cpp
+++ src/call.cpp
@@ -83,10 +83,12 @@ hangupCalls(Call::SubcallSet&& callptr_list, int errcode)
 //==============================================================================
 
 Call::Call(const std::shared_ptr<Account>& account,
+           LinkType linkType,
            const std::string& id,
            Call::CallType type,
            const std::map<std::string, std::string>& details)
-    : id_(id)
+    : linkType_(linkType)
+    , id_(id)
     , type_(type)
     , account_(account)
 {
@@ -658,7 +660,8 @@ Call::setConferenceInfo(const std::string& msg)
             // confID_ empty -> participant set confInfo with the received one
             confInfo_ = std::move(newInfo);
             // Inform client that layout has changed
-            jami::emitSignal<DRing::CallSignal::OnConferenceInfosUpdated>(id_, confInfo_.toVectorMapStringString());
+            jami::emitSignal<DRing::CallSignal::OnConferenceInfosUpdated>(
+                id_, confInfo_.toVectorMapStringString());
         } else {
             // confID_ not empty -> host merge confInfo with the received confInfo
             for (auto& newI : newInfo) {
@@ -681,7 +684,6 @@ Call::setConferenceInfo(const std::string& msg)
                 conf->updateConferenceInfo(confInfo_);
         }
     }
-
 }
 
 } // namespace jami
