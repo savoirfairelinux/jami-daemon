c5365e58ca5a359633234c541ce6452326a9cd53
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-01-07 17:12:59 -0500

    swarm: add conversationId to archive and improve sync
    Some informations should be save into the archive to allow the
    daemon to be able to auto-clone from remote and re-ask for invites
    when re-importing an account. Also fix some sync issues caused
    by some locks.
    
    Change-Id: I37ba32bcd1c748c933bd264502c5aa5664875bc0

--- src/account.h
+++ src/account.h
@@ -324,7 +324,9 @@ public:
                                 const std::string& /*commitId*/) {};
 
     // Invites
-    virtual void onConversationRequest(const std::string& /* from */, const Json::Value&) {};
+    virtual void onConversationRequest(const std::string& /*from*/, const Json::Value&) {};
+    virtual void onNeedConversationRequest(const std::string& /*from*/,
+                                           const std::string& /*conversationId*/) {};
 
     /**
      * Helper function used to load the default codec order from the codec factory
