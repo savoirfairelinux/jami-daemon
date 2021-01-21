25130bfa076c0198c26f723c81cbdb9aba95541f
agsantos <aline.gondimsantos@savoirfairelinux.com>
2021-01-21 13:23:06 -0500

    plugins: add ChatHandler API
    - add internal preference for always turn on plugin
    
    Change-Id: Ic37fb486531cf436df4346246ec74f6480e4c0fd
    GitLab: jami-plugins#4 and #394

---
+++ src/plugin/PluginPreferences.h
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (C) 2020 Savoir-faire Linux Inc.
+ *
+ *  Author: Aline Gondim Santos <aline.gondimsantos@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA.
+ */
+
+#pragma once
+
+#include <json/json.h>
+#include <string>
+
+namespace jami {
+
+class PluginPreferencesManager {
+public:
+    static std::string getPreferencesConfigFilePath(const std::string& rootPath);
+
+    static std::string valuesFilePath(const std::string& rootPath);
+
+    static std::string convertArrayToString(const Json::Value& jsonArray);
+
+    static std::map<std::string, std::string> parsePreferenceConfig(const Json::Value& jsonPreference,
+                                                            const std::string& type);
+
+    static std::vector<std::map<std::string, std::string>> getPreferences(
+        const std::string& rootPath);
+
+    static std::map<std::string, std::string> getUserPreferencesValuesMap(
+        const std::string& rootPath);
+
+    static std::map<std::string, std::string> getPreferencesValuesMap(const std::string& rootPath);
+
+    static bool resetPreferencesValuesMap(const std::string& rootPath);
+private:
+    PluginPreferencesManager() {}
+    ~PluginPreferencesManager() {}
+};
+} // namespace jami
