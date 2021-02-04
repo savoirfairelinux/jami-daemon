914c9d6bc6ce8b75c5bc62c293a4430f8d704f37
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-02-04 12:55:16 -0500

    debug: remove aggressive for timing issues
    Change-Id: I07d54a1293913f5a10b8262a4b784bae461a1f83

--- src/ice_transport.h
+++ src/ice_transport.h
@@ -103,7 +103,7 @@ struct IceTransportOptions
     bool tcpEnable {false}; // If we want to use TCP
     // See https://tools.ietf.org/html/rfc5245#section-8.1.1.2
     // Make negotiation aggressive by default to avoid latencies.
-    bool aggressive {true};
+    bool aggressive {false};
     // Addresses used by the account owning the transport instance.
     IpAddr accountLocalAddr {};
     IpAddr accountPublicAddr {};
