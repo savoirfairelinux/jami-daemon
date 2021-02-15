d89fdba0bb4ebfd3e12077df0f7a1e6a60415413
Philip-Dylan Gleonec <philip-dylan.gleonec@savoirfairelinux.com>
2021-02-15 17:44:03 +0100

    ffmpeg: update fec patch to match recent ffmpeg
    An AVOption has been added to ffmpeg in sept. 2020, making the patch in
    contrib redundant. This change is integrated in the contrib ffmpeg
    starting from commit 855019860, which updates the used version.
    
    This patch adapts the patches to be applied to ffmpeg and the call to
    enable FEC in libopus. Specifically it does the following changes:
    
    - backport the mainline ffmpeg patch that add fec to libopusenc
    - add packet_loss reload to adapt its value to network
    - modify rules.mak to add patches to the correct platforms
    - make media_encoder set the right AVOption to enable fec
    
    As Darwin platform uses an older ffmpeg version, the patch from ffmpeg
    mainline is backported so the code is the exact same.
    
    Issue: #4857
    Change-Id: I614fce2f0cf7fc387973b34323138b07c1767726

--- src/media/media_encoder.cpp
+++ src/media/media_encoder.cpp
@@ -995,7 +995,7 @@ MediaEncoder::initOpus(AVCodecContext* /*encoderCtx*/)
 {
     // Enable FEC support by default with 10% packet loss
     // Disable opus FEC for now
-    // av_opt_set_int(encoderCtx, "enable_fec", 1, AV_OPT_SEARCH_CHILDREN);
+    // av_opt_set_int(encoderCtx, "fec", 1, AV_OPT_SEARCH_CHILDREN);
     // av_opt_set_int(encoderCtx, "packet_loss", 10, AV_OPT_SEARCH_CHILDREN);
 }
 
