10924b3c285455ef381343a84b971a5db5657c77
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2021-02-24 17:11:33 -0500

    aec: move speexdsp aec implementation into EchoCanceller
    Hide speexdsp's echo cancellation implementation details behind an
    EchoCanceller derived class. An AudioLayer may now instantiate
    implementations of the EchoCanceller. This may be platform specific
    compile-time or swapped out at runtime.
    
    GitLab: #454
    Change-Id: I169f1e9758afbed884fc42d9d78a69ce28d12fe2

---
+++ src/media/audio/echo-cancel/echo_canceller.h
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (C) 2021 Savoir-faire Linux Inc.
+ *
+ *  Author: Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "noncopyable.h"
+#include "audio/audio_frame_resizer.h"
+#include "audio/audiobuffer.h"
+#include "libav_deps.h"
+
+namespace jami {
+
+class EchoCanceller
+{
+private:
+    NON_COPYABLE(EchoCanceller);
+
+public:
+    EchoCanceller(AudioFormat format, unsigned frameSize)
+        : playbackQueue_(format, frameSize)
+        , recordQueue_(format, frameSize)
+        , sampleRate_(format.sample_rate)
+        , frameSize_(frameSize)
+    {}
+    virtual ~EchoCanceller() = default;
+
+    virtual void putRecorded(std::shared_ptr<AudioFrame>&& buf) = 0;
+    virtual void putPlayback(const std::shared_ptr<AudioFrame>& buf) = 0;
+    virtual std::shared_ptr<AudioFrame> getProcessed() = 0;
+    virtual void done() = 0;
+
+protected:
+    AudioFrameResizer playbackQueue_;
+    AudioFrameResizer recordQueue_;
+    unsigned sampleRate_;
+    unsigned frameSize_;
+};
+
+class NullEchoCanceller final : public EchoCanceller
+{
+    // Inherited via EchoCanceller
+    void putRecorded(std::shared_ptr<AudioFrame>&& buf) override {};
+    void putPlayback(const std::shared_ptr<AudioFrame>& buf) override {};
+    std::shared_ptr<AudioFrame> getProcessed() override { return {}; };
+    void done() override {};
+};
+
+} // namespace jami
