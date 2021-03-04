52415a8e8f7c924c2f9a50e268d012fcd8cb65a9
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-03-04 13:45:45 -0500

    misc: clean some build warnings
    Change-Id: I2f3b0a6e36d3e57a10a8fa0e83ff812cfbcc3181

--- src/ice_transport.cpp
+++ src/ice_transport.cpp
@@ -1510,7 +1510,7 @@ IceTransport::send(int comp_id, const unsigned char* buf, size_t len)
         // bytes length).
         std::unique_lock<std::mutex> lk(pimpl_->iceMutex_);
         pimpl_->waitDataCv_.wait(lk, [&] {
-            return pimpl_->lastSentLen_ >= static_cast<pj_ssize_t>(len)
+            return pimpl_->lastSentLen_ >= static_cast<pj_size_t>(len)
                    or pimpl_->destroying_.load();
         });
         pimpl_->lastSentLen_ = 0;
