3aa2ca2992a374f2254a84c8675cb29aed3e222b
Pierre Lespagnol <pierre.lespagnol@savoirfairelinux.com>
2021-03-08 16:50:15 -0500

    conference: transfert mute and hangup in conference
    When 2 conferences are merged, a host can transmit
    moderator commands to another host.
    Change remote layout is not supported, the layout
    management will be moved client side.
    
    Gitlab: #449
    Change-Id: I1550677ef7f8babf19a729dad12a224f12226fbd

--- src/manager.cpp
+++ src/manager.cpp
@@ -1483,12 +1483,7 @@ Manager::setConferenceLayout(const std::string& confId, int layout)
         std::map<std::string, std::string> messages;
         Json::Value root;
         root["layout"] = layout;
-        Json::StreamWriterBuilder wbuilder;
-        wbuilder["commentStyle"] = "None";
-        wbuilder["indentation"] = "";
-        auto output = Json::writeString(wbuilder, root);
-        messages["application/confOrder+json"] = output;
-        call->sendTextMessage(messages, call->getPeerDisplayName());
+        call->sendConfOrder(root);
     }
 }
 
@@ -1501,12 +1496,7 @@ Manager::setActiveParticipant(const std::string& confId, const std::string& part
         std::map<std::string, std::string> messages;
         Json::Value root;
         root["activeParticipant"] = participant;
-        Json::StreamWriterBuilder wbuilder;
-        wbuilder["commentStyle"] = "None";
-        wbuilder["indentation"] = "";
-        auto output = Json::writeString(wbuilder, root);
-        messages["application/confOrder+json"] = output;
-        call->sendTextMessage(messages, call->getPeerDisplayName());
+        call->sendConfOrder(root);
     }
 }
 
@@ -1519,12 +1509,7 @@ Manager::hangupParticipant(const std::string& confId, const std::string& partici
         std::map<std::string, std::string> messages;
         Json::Value root;
         root["hangupParticipant"] = participant;
-        Json::StreamWriterBuilder wbuilder;
-        wbuilder["commentStyle"] = "None";
-        wbuilder["indentation"] = "";
-        auto output = Json::writeString(wbuilder, root);
-        messages["application/confOrder+json"] = output;
-        call->sendTextMessage(messages, call->getPeerDisplayName());
+        call->sendConfOrder(root);
     }
 }
 
@@ -3257,12 +3242,7 @@ Manager::muteParticipant(const std::string& confId,
         Json::Value root;
         root["muteParticipant"] = participant;
         root["muteState"] = state ? TRUE_STR : FALSE_STR;
-        Json::StreamWriterBuilder wbuilder;
-        wbuilder["commentStyle"] = "None";
-        wbuilder["indentation"] = "";
-        auto output = Json::writeString(wbuilder, root);
-        messages["application/confOrder+json"] = output;
-        call->sendTextMessage(messages, call->getPeerDisplayName());
+        call->sendConfOrder(root);
     }
 }
 
