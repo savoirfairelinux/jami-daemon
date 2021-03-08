00b5d15134d2e9f6f97cf6b5a3e27ebf112a340c
Pierre Lespagnol <pierre.lespagnol@savoirfairelinux.com>
2021-03-08 16:50:15 -0500

    conference: transfert mute and hangup in conference
    When 2 conferences are merged, a host can transmit
    moderator commands to another host.
    
    Gitlab: #449
    Change-Id: I1550677ef7f8babf19a729dad12a224f12226fbd

--- src/conference.h
+++ src/conference.h
@@ -340,6 +340,7 @@ private:
     std::map<std::string, ConfInfo> remoteHosts_;
     std::string confInfo2str(const ConfInfo& confInfo);
     std::string_view findHostforRemoteParticipant(std::string_view uri);
+    std::shared_ptr<Call> getCallFromPeerURI(std::string_view peerURI);
 
 #ifdef ENABLE_PLUGIN
     /**
