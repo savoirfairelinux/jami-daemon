c942461daf2d861f45bb559ee924af9512164197
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-03-15 15:55:06 -0400

    data-transfer: re-ask for transfer
    add API to ask for a failed transfer. This can be used when the
    client detects a transfer they doesn't have yet. It will ask all
    members of the conversation to send the transfer back.
    
    GitLab: #136
    Change-Id: I1484298c4fa0eabb614132e22d8032612e14b949

--- src/data_transfer.h
+++ src/data_transfer.h
@@ -37,6 +37,7 @@ struct IncomingFileInfo
 };
 
 typedef std::function<void(const std::string&)> InternalCompletionCb;
+typedef std::function<bool(const std::string&)> OnVerifyCb;
 typedef std::function<void(const DRing::DataTransferId&, const DRing::DataTransferEventCode&)>
     OnStateChangedCb;
 
@@ -46,22 +47,71 @@ public:
     TransferManager(const std::string& accountId, const std::string& to, bool isConversation = true);
     ~TransferManager();
 
-    DRing::DataTransferId sendFile(const std::string& path, const InternalCompletionCb& icb = {});
+    /**
+     * Send a file
+     * @param path      of the file
+     * @param icb       used for internal files (like vcard)
+     * @param deviceId  if we only want to transmit to one device
+     * @param resendId  if we need to resend a file, just specify previous id there.
+     */
+    DRing::DataTransferId sendFile(const std::string& path,
+                                   const InternalCompletionCb& icb = {},
+                                   const std::string& deviceId = {},
+                                   DRing::DataTransferId resendId = {0});
 
+    /**
+     * Accepts a transfer
+     * @param id        of the transfer
+     * @param path      of the file
+     */
     bool acceptFile(const DRing::DataTransferId& id, const std::string& path);
 
+    /**
+     * Refuse a transfer
+     * @param id        of the transfer
+     */
     bool cancel(const DRing::DataTransferId& id);
 
+    /**
+     * Get current transfer infos
+     * @param id        of the transfer
+     * @param info      to fill
+     * @return if found
+     */
     bool info(const DRing::DataTransferId& id, DRing::DataTransferInfo& info) const noexcept;
 
+    /**
+     * Get current transfer progress
+     * @param id        of the transfer
+     * @param total     size
+     * @param progress  current progress
+     * @return if found
+     */
     bool bytesProgress(const DRing::DataTransferId& id,
                        int64_t& total,
                        int64_t& progress) const noexcept;
 
+    /**
+     * Inform the transfer manager that a new file is incoming
+     * @param info      of the transfer
+     * @param id        of the transfer
+     * @param cb        callback to trigger when connected
+     * @param icb       used for vcard
+     */
     void onIncomingFileRequest(const DRing::DataTransferInfo& info,
                                const DRing::DataTransferId& id,
                                const std::function<void(const IncomingFileInfo&)>& cb,
                                const InternalCompletionCb& icb = {});
+
+    /**
+     * Inform the transfer manager that a transfer is waited (and will be automatically accepted)
+     * @param id        of the transfer
+     * @param sha3sum   attended sha3sum
+     * @param path      where the file will be downloaded
+     */
+    void waitForTransfer(const DRing::DataTransferId& id,
+                         const std::string& sha3sum,
+                         const std::string& path);
 
 private:
     NON_COPYABLE(TransferManager);
