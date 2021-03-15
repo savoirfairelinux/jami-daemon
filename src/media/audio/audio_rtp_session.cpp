savoirfairelinux/jami-daemon

commit 37125acf5b292b69e83f5d81042fb544ed21e0d6

audio_rtp_session: cleanup
Audio sender doesn't need audioInput, audio rtp session already has one.
(Consistency with video rtp session)
muteState is not used in audio sender.

Change-Id: I7c798c94050fa6533ff9b726914f2f880e426a52

src/media/audio/audio_rtp_session.cpp
@@ -69,6 +69,8 @@ AudioRtpSession::startSender()
         if (sender_) {
             if (socketPair_)
                 socketPair_->interrupt();
+            if (audioInput_)
+                audioInput_->detach(sender_.get());
             sender_.reset();
         }
         return;
@@ -76,6 +78,8 @@ AudioRtpSession::startSender()
 
     if (sender_)
         JAMI_WARN("Restarting audio sender");
+    if (audioInput_)
+        audioInput_->detach(sender_.get());
 
     // sender sets up input correctly, we just keep a reference in case startSender is called
     audioInput_ = jami::getAudioInput(callID_);
@@ -102,11 +106,17 @@ AudioRtpSession::startSender()
         sender_.reset();
         socketPair_->stopSendOp(false);
         sender_.reset(new AudioSender(
-            callID_, getRemoteRtpUri(), send_, *socketPair_, initSeqVal_, muteState_, mtu_));
+            callID_, getRemoteRtpUri(), send_, *socketPair_, initSeqVal_, mtu_));
     } catch (const MediaEncoderException& e) {
         JAMI_ERR("%s", e.what());
         send_.enabled = false;
     }
+
+    // NOTE do after sender/encoder are ready
+    auto codec = std::static_pointer_cast<AccountAudioCodecInfo>(send_.codec);
+    audioInput_->setFormat(codec->audioformat);
+    if (audioInput_)
+        audioInput_->attach(sender_.get());
 
     if (not rtcpCheckerThread_.isRunning())
         rtcpCheckerThread_.start();
@@ -184,6 +194,9 @@ AudioRtpSession::stop()
 {
     std::lock_guard<std::recursive_mutex> lock(mutex_);
 
+    if (audioInput_)
+        audioInput_->detach(sender_.get());
+
     if (socketPair_)
         socketPair_->interrupt();
 
@@ -199,10 +212,7 @@ void
 AudioRtpSession::setMuted(bool isMuted)
 {
     std::lock_guard<std::recursive_mutex> lock(mutex_);
-    if (sender_) {
-        muteState_ = isMuted;
-        sender_->setMuted(isMuted);
-    }
+    muteState_ = isMuted;
 }
 
 bool

src/media/audio/audio_sender.cpp
@@ -19,7 +19,6 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
 
-#include "audio_input.h"
 #include "audio_sender.h"
 #include "client/videomanager.h"
 #include "libav_deps.h"
@@ -39,13 +38,11 @@ AudioSender::AudioSender(const std::string& id,
                          const MediaDescription& args,
                          SocketPair& socketPair,
                          const uint16_t seqVal,
-                         bool muteState,
                          const uint16_t mtu)
     : id_(id)
     , dest_(dest)
     , args_(args)
     , seqVal_(seqVal)
-    , muteState_(muteState)
     , mtu_(mtu)
 {
     setup(socketPair);
@@ -53,8 +50,6 @@ AudioSender::AudioSender(const std::string& id,
 
 AudioSender::~AudioSender()
 {
-    audioInput_->detach(this);
-    audioInput_.reset();
     audioEncoder_.reset();
     muxContext_.reset();
     micData_.clear();
@@ -89,12 +84,6 @@ AudioSender::setup(SocketPair& socketPair)
     audioEncoder_->print_sdp();
 #endif
 
-    // NOTE do after encoder is ready to encode
-    auto codec = std::static_pointer_cast<AccountAudioCodecInfo>(args_.codec);
-    audioInput_ = jami::getAudioInput(id_);
-    audioInput_->setFormat(codec->audioformat);
-    audioInput_->setMuted(muteState_);
-    audioInput_->attach(this);
     return true;
 }
 
@@ -107,13 +96,6 @@ AudioSender::update(Observable<std::shared_ptr<jami::MediaFrame>>* /*obs*/,
     sent_samples += frame->nb_samples;
     if (audioEncoder_->encodeAudio(*std::static_pointer_cast<AudioFrame>(framePtr)) < 0)
         JAMI_ERR("encoding failed");
-}
-
-void
-AudioSender::setMuted(bool isMuted)
-{
-    muteState_ = isMuted;
-    audioInput_->setMuted(isMuted);
 }
 
 uint16_t

src/media/audio/audio_sender.h
@@ -42,11 +42,9 @@ public:
                 const MediaDescription& args,
                 SocketPair& socketPair,
                 const uint16_t seqVal,
-                bool muteState,
                 const uint16_t mtu);
     ~AudioSender();
 
-    void setMuted(bool isMuted);
     uint16_t getLastSeqValue();
     int setPacketLoss(uint64_t pl);
 
@@ -64,14 +62,12 @@ private:
     std::unique_ptr<MediaEncoder> audioEncoder_;
     std::unique_ptr<MediaIOHandle> muxContext_;
     std::unique_ptr<Resampler> resampler_;
-    std::shared_ptr<AudioInput> audioInput_;
 
     uint64_t sent_samples = 0;
 
     AudioBuffer micData_;
     AudioBuffer resampledData_;
     const uint16_t seqVal_;
-    bool muteState_ = false;
     uint16_t mtu_;
 };
 

commit e8070b0800b9b4211ce110d25a9a43568b6edb8b

audio_rtp_session: fix setMuted
Regression was caused by 37125acf5 where the mute button didn't
work anymore.

Change-Id: I7e3d0953e65848818c9dc0e348759dce63274337

src/media/audio/audio_rtp_session.cpp
@@ -213,6 +213,8 @@ AudioRtpSession::setMuted(bool isMuted)
 {
     std::lock_guard<std::recursive_mutex> lock(mutex_);
     muteState_ = isMuted;
+    if (audioInput_)
+        audioInput_->setMuted(isMuted);
 }
 
 bool