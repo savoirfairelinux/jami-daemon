f950a2237fdecefd81361688fc4c8e3e89e76eef
Pierre Lespagnol <pierre.lespagnol@savoirfairelinux.com>
2021-03-23 12:17:32 -0400

    audio/encoder: enable encoding FEC for RTP streams only
    Fix the audio/video message playback
    
    Gitlab: #128
    Change-Id: Idce7cbe45fb05622c94ba54187e2477eb93168de

--- src/media/media_encoder.cpp
+++ src/media/media_encoder.cpp
@@ -129,6 +129,7 @@ MediaEncoder::setOptions(const MediaDescription& args)
 
     mode_ = args.mode;
     linkableHW_ = args.linkableHW;
+    fecEnabled_ = args.fecEnabled;
 }
 
 void
@@ -995,7 +996,7 @@ void
 MediaEncoder::initOpus(AVCodecContext* encoderCtx)
 {
     // Enable FEC support by default with 10% packet loss
-    av_opt_set_int(encoderCtx, "enable_fec", 1, AV_OPT_SEARCH_CHILDREN);
+    av_opt_set_int(encoderCtx, "enable_fec", fecEnabled_ ? 1 : 0, AV_OPT_SEARCH_CHILDREN);
     av_opt_set_int(encoderCtx, "packet_loss", 10, AV_OPT_SEARCH_CHILDREN);
 }
 
