894c02d3e6e348f4464f7dd0940c3cae56f87275
Mohamed Chibani <mohamed.chibani@savoirfairelinux.com>
2021-04-27 09:47:27 -0400

    SIP: use same thread for SipVoIPLink events and SIP transport
    Use the same scheduler for SipVoIPLink events and SIP transport to
    reduce data race on SIPCall class in particular.
    
    Gitlab: #529
    
    Change-Id: Ie4dc16c5535a9f892d47d6cc3e1822e9c94f996f

--- src/sip/sipvoiplink.cpp
+++ src/sip/sipvoiplink.cpp
@@ -538,11 +538,23 @@ SIPVoIPLink::SIPVoIPLink()
     TRY(pjsip_replaces_init_module(endpt_));
 #undef TRY
 
-    sipThread_ = std::thread([this] {
-        sip_utils::register_thread();
-        while (running_)
-            handleEvents();
+    sipScheduler_ = std::make_shared<ScheduledExecutor>();
+
+    sipScheduler_->run([this] {
+        if (sip_utils::register_thread() != PJ_SUCCESS) {
+            JAMI_WARN("Failed to register SIP execution thread");
+        } else {
+            JAMI_DBG("SIP thread registered. Start polling SIP events on this thread");
+        }
     });
+
+    JAMI_DBG("Start polling SIP events");
+    pollTask_ = sipScheduler_->scheduleAtFixedRate(
+        [this] {
+            handleEvents({0, 0});
+            return running_.load();
+        },
+        std::chrono::milliseconds(500));
 
     JAMI_DBG("SIPVoIPLink@%p", this);
 }
@@ -565,7 +577,6 @@ SIPVoIPLink::shutdown()
     sipTransportBroker_.reset();
 
     running_ = false;
-    sipThread_.join();
     pjsip_endpt_destroy(endpt_);
     pool_.reset();
     pj_caching_pool_destroy(&cp_);
@@ -612,9 +623,8 @@ SIPVoIPLink::guessAccount(std::string_view userName,
 
 // Called from EventThread::run (not main thread)
 void
-SIPVoIPLink::handleEvents()
+SIPVoIPLink::handleEvents(const pj_time_val timeout)
 {
-    const pj_time_val timeout = {1, 0};
     if (auto ret = pjsip_endpt_handle_events(endpt_, &timeout))
         JAMI_ERR("pjsip_endpt_handle_events failed with error %s",
                  sip_utils::sip_strerror(ret).c_str());
