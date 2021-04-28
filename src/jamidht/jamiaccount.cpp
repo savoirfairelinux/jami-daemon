41b3746a999d324f49b41e055184605e1053e5b3
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-04-28 13:19:22 -0400

    jamiaccount: re-enable sync
    Change-Id: I142e017e290c643a73f2b2b422533ec8d7cc0483

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -785,11 +785,11 @@ JamiAccount::onConnectedOutgoingCall(const std::shared_ptr<SIPCall>& call,
                 return;
 
             const auto localAddress = ip_utils::getInterfaceAddr(shared->getLocalInterface(),
-                                                                target.getFamily());
+                                                                 target.getFamily());
 
             IpAddr addrSdp = shared->getPublishedSameasLocal()
-                                ? localAddress
-                                : shared->getPublishedIpAddress(target.getFamily());
+                                 ? localAddress
+                                 : shared->getPublishedIpAddress(target.getFamily());
 
             // fallback on local address
             if (not addrSdp)
@@ -1240,7 +1240,8 @@ JamiAccount::loadAccount(const std::string& archive_password,
                         req.conversationId = conversationId;
                         req.received = std::time(nullptr);
                         auto details = vCard::utils::toMap(
-                            std::string_view(reinterpret_cast<const char*>(payload.data()), payload.size()));
+                            std::string_view(reinterpret_cast<const char*>(payload.data()),
+                                             payload.size()));
                         req.metadatas = ConversationRepository::infosFromVCard(details);
                         acc->accountManager_->addConversationRequest(conversationId, std::move(req));
                         emitSignal<DRing::ConversationSignal::ConversationRequestReceived>(
@@ -2383,8 +2384,7 @@ JamiAccount::doRegister_()
         context.identityAnnouncedCb = [this](bool ok) {
             if (!ok)
                 return;
-            accountManager_->startSync({});
-            /*[this](const std::shared_ptr<dht::crypto::Certificate>& crt) {
+            accountManager_->startSync([this](const std::shared_ptr<dht::crypto::Certificate>& crt) {
                 if (!crt)
                     return;
                 auto deviceId = crt->getId().toString();
@@ -2397,18 +2397,18 @@ JamiAccount::doRegister_()
                 auto channelName = "sync://" + deviceId;
                 if (connectionManager_->isConnecting(crt->getId(), channelName)) {
                     JAMI_INFO("[Account %s] Already connecting to %s",
-                        getAccountID().c_str(),
-                        deviceId.c_str());
+                              getAccountID().c_str(),
+                              deviceId.c_str());
                     return;
                 }
                 connectionManager_->connectDevice(crt,
-                                                channelName,
-                                                [this](std::shared_ptr<ChannelSocket> socket,
-                                                        const DeviceId& deviceId) {
-                                                    if (socket)
-                                                        syncWith(deviceId.toString(), socket);
-                                                });
-            });*/
+                                                  channelName,
+                                                  [this](std::shared_ptr<ChannelSocket> socket,
+                                                         const DeviceId& deviceId) {
+                                                      if (socket)
+                                                          syncWith(deviceId.toString(), socket);
+                                                  });
+            });
         };
 
         setRegistrationState(RegistrationState::TRYING);
@@ -2454,7 +2454,7 @@ JamiAccount::doRegister_()
                 return true;
             } else if (name == "sip") {
                 return true;
-            } /*else if (name.find("sync://") == 0) {
+            } else if (name.find("sync://") == 0) {
                 // Check if sync request is from same account
                 std::promise<bool> accept;
                 std::future<bool> fut = accept.get_future();
@@ -2472,8 +2472,7 @@ JamiAccount::doRegister_()
                 fut.wait();
                 auto result = fut.get();
                 return result;
-            } */
-            else if (isFile or isVCard) {
+            } else if (isFile or isVCard) {
                 auto tid_str = isFile ? name.substr(7) : name.substr(8);
                 uint64_t tid;
                 std::istringstream iss(tid_str);
@@ -2498,10 +2497,9 @@ JamiAccount::doRegister_()
                 auto isVCard = name.substr(0, 8) == "vcard://";
                 if (name == "sip") {
                     cacheSIPConnection(std::move(channel), peerId, deviceId);
-                } /*else if (name.find("sync://") == 0) {
+                } else if (name.find("sync://") == 0) {
                     cacheSyncConnection(std::move(channel), peerId, deviceId);
-                }*/
-                else if (isFile or isVCard) {
+                } else if (isFile or isVCard) {
                     auto tid_str = isFile ? name.substr(7) : name.substr(8);
                     std::unique_lock<std::mutex> lk(transfersMtx_);
                     auto it = incomingFileTransfers_.find(tid_str);
@@ -2800,14 +2798,14 @@ JamiAccount::incomingCall(dht::IceCandidates&& msg,
             });
         };
         auto ice = createIceTransport(("sip:" + call->getCallId()).c_str(),
-                                    ICE_COMPONENTS,
-                                    false,
-                                    iceOptions);
+                                      ICE_COMPONENTS,
+                                      false,
+                                      iceOptions);
         iceOptions.tcpEnable = true;
         auto ice_tcp = createIceTransport(("sip:" + call->getCallId()).c_str(),
-                                        ICE_COMPONENTS,
-                                        true,
-                                        iceOptions);
+                                          ICE_COMPONENTS,
+                                          true,
+                                          iceOptions);
 
         std::weak_ptr<SIPCall> wcall = call;
         Manager::instance().addTask([account = shared(), wcall, ice, ice_tcp, msg, from_cert, from] {
