c345844c2369d886e6c5f30806caec2de84b60a5
Olivier Dion <olivier.dion@polymtl.ca>
2021-05-11 08:41:22 -0400

    call: Add getCallDuration() method
    If duration_start_ is time_point::min, then when calculating the call's
    duration, there's an signed integer overflow which result in an enormous time
    duration.
    
    Signed-off-by: Olivier Dion <olivier.dion@savoirfairelinux.com>
    Change-Id: I3ad119ab0f79ef8e9c537d1879d628a29ceed4d7

--- src/sip/sipcall.cpp
+++ src/sip/sipcall.cpp
@@ -2353,7 +2353,7 @@ SIPCall::monitor() const
     }
     JAMI_DBG("- Call %s with %s:", getCallId().c_str(), getPeerNumber().c_str());
     // TODO move in getCallDuration
-    JAMI_DBG("\t- Duration: %s", dht::print_duration(clock::now() - duration_start_).c_str());
+    JAMI_DBG("\t- Duration: %s", dht::print_duration(std::chrono::milliseconds(getCallDuration())).c_str());
     for (auto& mediaAttr : getMediaAttributeList())
         JAMI_DBG("\t- Media: %s", mediaAttr.toString(true).c_str());
 #ifdef ENABLE_VIDEO
