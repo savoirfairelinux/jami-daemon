0390a7854c3dc03036c228b15cc37df8970fd1bc
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-06-03 10:42:20 -0400

    unitTest: Share common.cpp between check programs
    Change-Id: I88c44743227dc264a7bae899caccbb02b7a29a69

--- test/unitTest/common.h
+++ test/unitTest/common.h
@@ -20,177 +20,27 @@
 
 #pragma once
 
-#include <yaml-cpp/yaml.h>
+constexpr size_t WAIT_FOR_ANNOUNCEMENT_TIMEOUT = 30;
+constexpr size_t WAIT_FOR_REMOVAL_TIMEOUT = 30;
 
-/* Jami */
-#include "fileutils.h"
-#include "manager.h"
-
-/* Make GCC quiet about unused functions */
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-function"
-
-static void
+extern void
 wait_for_announcement_of(const std::vector<std::string> accountIDs,
-                         std::chrono::seconds timeout = std::chrono::seconds(30))
-{
-    std::map<std::string, std::shared_ptr<DRing::CallbackWrapperBase>> confHandlers;
-    std::mutex mtx;
-    std::unique_lock<std::mutex> lk {mtx};
-    std::condition_variable cv;
-    std::vector<std::atomic_bool> accountsReady(accountIDs.size());
+                         std::chrono::seconds timeout = std::chrono::seconds(WAIT_FOR_ANNOUNCEMENT_TIMEOUT));
 
-    confHandlers.insert(
-        DRing::exportable_callback<DRing::ConfigurationSignal::VolatileDetailsChanged>(
-            [&](const std::string& accountID, const std::map<std::string, std::string>& details) {
-                for (size_t i = 0; i < accountIDs.size(); ++i) {
-                    if (accountIDs[i] != accountID) {
-                        continue;
-                    }
-
-                    try {
-                        if ("true"
-                            != details.at(DRing::Account::VolatileProperties::DEVICE_ANNOUNCED)) {
-                            continue;
-                        }
-                    } catch (const std::out_of_range&) {
-                        continue;
-                    }
-
-                    accountsReady[i] = true;
-                    cv.notify_one();
-                }
-            }));
-
-    JAMI_DBG("Waiting for %zu account to be announced...", accountIDs.size());
-
-    DRing::registerSignalHandlers(confHandlers);
-
-    CPPUNIT_ASSERT(cv.wait_for(lk, timeout, [&] {
-        for (const auto& rdy : accountsReady) {
-            if (not rdy) {
-                return false;
-            }
-        }
-
-        return true;
-    }));
-
-    DRing::unregisterSignalHandlers();
-
-    JAMI_DBG("%zu account announced!", accountIDs.size());
-}
-
-static void
+extern void
 wait_for_announcement_of(const std::string& accountId,
-                         std::chrono::seconds timeout = std::chrono::seconds(30))
-{
-    wait_for_announcement_of(std::vector<std::string> {accountId}, timeout);
-}
+                         std::chrono::seconds timeout = std::chrono::seconds(WAIT_FOR_ANNOUNCEMENT_TIMEOUT));
 
-static void
+extern void
 wait_for_removal_of(const std::vector<std::string> accounts,
-                    std::chrono::seconds timeout = std::chrono::seconds(30))
-{
-    JAMI_INFO("Removing %zu accounts...", accounts.size());
+                    std::chrono::seconds timeout = std::chrono::seconds(WAIT_FOR_REMOVAL_TIMEOUT));
 
-    std::map<std::string, std::shared_ptr<DRing::CallbackWrapperBase>> confHandlers;
-    std::mutex mtx;
-    std::unique_lock<std::mutex> lk {mtx};
-    std::condition_variable cv;
-    std::atomic_bool accountsRemoved {false};
-
-    size_t current = jami::Manager::instance().getAccountList().size();
-
-    /* Prevent overflow */
-    CPPUNIT_ASSERT(current >= accounts.size());
-
-    size_t target = current - accounts.size();
-
-    confHandlers.insert(
-        DRing::exportable_callback<DRing::ConfigurationSignal::AccountsChanged>([&]() {
-            if (jami::Manager::instance().getAccountList().size() <= target) {
-                accountsRemoved = true;
-                cv.notify_one();
-            }
-        }));
-
-    DRing::unregisterSignalHandlers();
-    DRing::registerSignalHandlers(confHandlers);
-
-    for (const auto& account : accounts) {
-        jami::Manager::instance().removeAccount(account, true);
-    }
-
-    CPPUNIT_ASSERT(cv.wait_for(lk, timeout, [&] { return accountsRemoved.load(); }));
-
-    DRing::unregisterSignalHandlers();
-}
-
-static void
+extern void
 wait_for_removal_of(const std::string& account,
-                    std::chrono::seconds timeout = std::chrono::seconds(30))
-{
-    wait_for_removal_of(std::vector<std::string>{account}, timeout);
-}
+                    std::chrono::seconds timeout = std::chrono::seconds(WAIT_FOR_REMOVAL_TIMEOUT));
 
-static std::map<std::string, std::string>
-load_actors(const std::string& from_yaml)
-{
-    std::map<std::string, std::string> actors {};
-    std::map<std::string, std::string> default_details = DRing::getAccountTemplate("RING");
+extern std::map<std::string, std::string>
+load_actors(const std::string& from_yaml);
 
-    std::ifstream file = jami::fileutils::ifstream(from_yaml);
-
-    CPPUNIT_ASSERT(file.is_open());
-
-    YAML::Node node = YAML::Load(file);
-
-    CPPUNIT_ASSERT(node.IsMap());
-
-    auto default_account = node["default-account"];
-
-    if (default_account.IsMap()) {
-        for (const auto& kv : default_account) {
-            default_details["Account." + kv.first.as<std::string>()] = kv.second.as<std::string>();
-        }
-    }
-
-    auto accounts = node["accounts"];
-
-    CPPUNIT_ASSERT(accounts.IsMap());
-
-    for (const auto& kv : accounts) {
-        auto account_name = kv.first.as<std::string>();
-        auto account = kv.second.as<YAML::Node>();
-        auto details = std::map<std::string, std::string>(default_details);
-
-        for (const auto& detail : account) {
-            details["Account." + detail.first.as<std::string>()] = detail.second.as<std::string>();
-        }
-
-        actors[account_name] = jami::Manager::instance().addAccount(details);
-    }
-
-    return actors;
-}
-
-static std::map<std::string, std::string>
-load_actors_and_wait_for_announcement(const std::string& from_yaml)
-{
-    auto actors = load_actors(from_yaml);
-
-    std::vector<std::string> wait_for;
-
-    wait_for.reserve(actors.size());
-
-    for (auto it = actors.cbegin(); it != actors.cend(); ++it) {
-        wait_for.emplace_back(it->second);
-    }
-
-    wait_for_announcement_of(wait_for);
-
-    return actors;
-}
-
-#pragma GCC diagnostic pop
+extern std::map<std::string, std::string>
+load_actors_and_wait_for_announcement(const std::string& from_yaml);
