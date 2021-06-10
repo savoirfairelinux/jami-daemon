4682cce6fb97956a012a52eeab0127f9a79b140c
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-06-10 09:37:27 -0400

    debug win
    Change-Id: I1fc7c904115058ef9365f996a59ae7c44939ffb8

--- src/fileutils.cpp
+++ src/fileutils.cpp
@@ -326,7 +326,10 @@ createSymLink(const std::string& linkFile, const std::string& target)
     symlink(target.c_str(), linkFile.c_str());
 #else
     std::error_code ec;
-    std::filesystem::create_symlink(target, linkFile, ec);
+    std::filesystem::create_symlink(linkFile, target, ec);
+    // int test = CreateSymbolicLinkA(target.c_str(), linkFile.c_str(), 0);
+    // DWORD err = GetLastError();
+    JAMI_ERR() << "@@@ CREATE SYMLINK " << target << " -> " << linkFile;
 #endif
 }
 
@@ -998,6 +1001,7 @@ sha3File(const std::string& path)
     try {
         if (!fileutils::isFile(path))
             return {};
+        JAMI_ERR() << "@@@ SIZE: " << size(path);
         openStream(file, path);
         if (!file)
             return {};
