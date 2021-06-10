bc4a4cc6b51004a9c117a0c78ef4f1cdf7ed92b6
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-06-10 15:22:16 -0400

    fuzzing/examples: Add examples that were used as LD_PRELOAD
    Change-Id: Ieeda03d459eeefb86a19e57277bde7c194e5b8c5

---
+++ test/fuzzing/examples/fuzz-channel-request.cpp
@@ -0,0 +1,83 @@
+/*
+ *  Copyright (C) 2021 Savoir-faire Linux Inc.
+ *
+ *  Author: Olivier Dion <olivier.dion>@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include <cstdlib>
+#include <set>
+
+#include <msgpack.hpp>
+
+#include "jamidht/multiplexed_socket.h"
+
+#include "lib/gnutls.h"
+
+enum class ChannelRequestState {
+    REQUEST,
+    ACCEPT,
+    DECLINE,
+};
+
+/**
+ * That msgpack structure is used to request a new channel (id, name)
+ * Transmitted over the TLS socket
+ */
+struct ChannelRequest
+{
+    std::string name {};
+    uint16_t channel {0};
+    ChannelRequestState state {ChannelRequestState::REQUEST};
+    MSGPACK_DEFINE(name, channel, state)
+};
+
+
+
+/*
+ * Mangle channel
+ */
+bool
+mutate_gnutls_record_send(ChanneledMessage& msg)
+{
+        try {
+                msgpack::unpacked result;
+                msgpack::unpack(result, (const char*) msg.data.data(), msg.data.size(), 0);
+                auto object = result.get();
+                auto req = object.as<ChannelRequest>();
+
+                int state = rand() % 8;
+
+                static_assert(sizeof(state) == sizeof(req.state));
+                memcpy(&req.state, &state, sizeof(state));
+
+                msgpack::sbuffer buffer(512);
+                msgpack::pack(buffer, req);
+
+                msg.data.clear();
+
+                for (size_t i=0; i<buffer.size(); ++i) {
+                        msg.data.emplace_back(buffer.data()[i]);
+                }
+
+        } catch (...) {
+                return false;
+        }
+
+        return true;
+}
+
+MSGPACK_ADD_ENUM(ChannelRequestState);
---
+++ test/fuzzing/examples/fuzz-msgpack.cpp
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (C) 2021 Savoir-faire Linux Inc.
+ *
+ *  Author: Olivier Dion <olivier.dion>@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "lib/gnutls.h"
+#include "lib/syslog.h"
+
+/*
+ * Reverse channel and data in packed message
+ */
+void
+pack_gnutls_record_recv(msgpack::sbuffer& buf, const ChanneledMessage& msg)
+{
+     msgpack::packer<msgpack::sbuffer> pk(&buf);
+#if 0
+     pk.pack_array(2);
+     pk.pack_bin(msg.data.size());
+     pk.pack_bin_body((const char*) msg.data.data(), msg.data.size());
+     pk.pack(msg.channel);
+#else
+     pk.pack_array(1);
+     pk.pack(msg.channel);
+#endif
+}
+
+bool
+mutate_gnutls_record_recv(ChanneledMessage& msg)
+{
+    (void)msg;
+
+    return true;
+}
