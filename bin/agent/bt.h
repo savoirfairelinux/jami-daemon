30f4947b97869b52a222c4e38327f0d9e60afe92
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-06-17 09:55:17 -0400

    agent: Initial agent with simple behavior tree
    Change-Id: Ib56b3b034e2caf71b58330abece1a2b17584c5ec

---
+++ bin/agent/bt.h
@@ -0,0 +1,126 @@
+/*
+
+ *  Copyright (C) 2021 Savoir-faire Linux Inc.
+ *
+ *  Author: Olivier Dion <olivier.dion@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include <map>
+
+#include <yaml-cpp/yaml.h>
+
+namespace BT {
+
+std::map<std::string, std::function<bool(void)>> registered_tasks;
+
+class Node
+{
+public:
+    virtual bool operator()(void) = 0;
+
+    virtual ~Node() = default;
+};
+
+class Execute : public Node
+{
+    std::function<bool(void)> todo_ {};
+
+public:
+    Execute(const std::string& task_name)
+    {
+        try {
+            todo_ = registered_tasks.at(task_name);
+        } catch (const std::exception& E) {
+            JAMI_ERR("AGENT: Invalid task name %s: %s", task_name.c_str(), E.what());
+        }
+    }
+
+    virtual bool operator()(void) override { return todo_(); }
+};
+
+struct Sequence : public Node
+{
+    std::vector<std::unique_ptr<Node>> nodes_;
+
+public:
+    virtual bool operator()(void) override
+    {
+        for (const auto& node : nodes_) {
+            if (not(*node)()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void add(std::unique_ptr<Node> node) { nodes_.push_back(std::move(node)); }
+};
+
+struct Selector : public Node
+{
+    std::vector<std::unique_ptr<Node>> nodes_;
+
+public:
+    virtual bool operator()(void) override
+    {
+        for (const auto& node : nodes_) {
+            if ((*node)()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void add(std::unique_ptr<Node> node) { nodes_.push_back(std::move(node)); }
+};
+
+std::unique_ptr<Node>
+from_yaml(YAML::Node behavior)
+{
+    std::unique_ptr<Node> node;
+
+    if (behavior.IsSequence()) {
+        auto tmp = new Sequence();
+
+        for (const auto& sub_behavior : behavior) {
+            tmp->add(from_yaml(sub_behavior));
+        }
+
+        node.reset(dynamic_cast<Node*>(tmp));
+
+    } else if (behavior.IsMap()) {
+        auto tmp = new Selector();
+
+        for (const auto& kv : behavior) {
+            assert(kv.second.IsSequence());
+            for (const auto& sub_behavior : kv.second) {
+                tmp->add(from_yaml(sub_behavior));
+            }
+        }
+
+        node.reset(dynamic_cast<Node*>(tmp));
+
+    } else {
+        node = std::make_unique<Execute>(behavior.as<std::string>());
+    }
+
+    return node;
+}
+
+}; // namespace BT
