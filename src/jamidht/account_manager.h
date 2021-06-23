2b62e5f1324f29a3d5565fe73cd832df533d5740
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-06-23 15:47:05 -0400

    jamiaccount: fix use after move semantic
    onChange should not be moved if used
    
    Change-Id: Id76b7b12502179308e5230e5382eb2f73945c43e

--- src/jamidht/account_manager.h
+++ src/jamidht/account_manager.h
@@ -117,7 +117,7 @@ public:
                                     std::unique_ptr<AccountCredentials> credentials,
                                     AuthSuccessCallback onSuccess,
                                     AuthFailureCallback onFailure,
-                                    OnChangeCallback onChange)
+                                    const OnChangeCallback& onChange)
         = 0;
 
     virtual bool changePassword(const std::string& password_old, const std::string& password_new) = 0;
@@ -134,7 +134,7 @@ public:
                                    const std::string& receipt,
                                    const std::vector<uint8_t>& receiptSignature,
                                    const std::string& username,
-                                   OnChangeCallback&& onChange);
+                                   const OnChangeCallback& onChange);
 
     void setDht(const std::shared_ptr<dht::DhtRunner>& dht) { dht_ = dht; }
 
