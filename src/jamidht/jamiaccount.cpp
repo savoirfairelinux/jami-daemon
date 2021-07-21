863c667bfd77428edf16bf7d39de5c339da7e2e8
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-07-21 15:40:38 -0400

    jamiaccount: finish convInfos transition
    Change-Id: I9c320f52a4eea7cd5af6df67375f20cd7936511a
    GitLab: #455

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -5045,7 +5045,6 @@ void
 JamiAccount::loadConvInfos()
 {
     std::map<std::string, ConvInfo> convInfos;
-    std::vector<ConvInfo> oldCi; // TODO remove when public beta, just to avoid to break
     try {
         // read file
         auto file = fileutils::loadFile("convInfo", idPath_);
@@ -5053,20 +5052,8 @@ JamiAccount::loadConvInfos()
         msgpack::object_handle oh = msgpack::unpack((const char*) file.data(), file.size());
         oh.get().convert(convInfos);
     } catch (const std::exception& e) {
-        // TODO remove when public beta, just to avoid to break
-        try {
-            // read file
-            auto file = fileutils::loadFile("convInfo", idPath_);
-            // load values
-            msgpack::object_handle oh = msgpack::unpack((const char*) file.data(), file.size());
-            oh.get().convert(oldCi);
-            for (auto ci : oldCi) {
-                convInfos[ci.id] = ci;
-            }
-        } catch (const std::exception& e) {
-            JAMI_WARN("[convInfo] error loading convInfo: %s", e.what());
-            return;
-        }
+        JAMI_WARN("[convInfo] error loading convInfo: %s", e.what());
+        return;
     }
 
     for (auto& [key, info] : convInfos) {
