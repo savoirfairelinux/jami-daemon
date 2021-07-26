391e2614d3d970e9926bd1411f030f94b0621a1c
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-07-26 14:16:53 -0400

    swarm: add call support
    This patch introduces the ability to start calls and extends the
    usage of rendezvous points to swarm with multiple participants.
    
    When starting a call in a swarm with multiple participats, one device
    will work as the host of the conference, and the caller will
    immediately start the call alone.
    Other peers will receive a commit and a notification to be able
    to join the active call. To join a call, users needs to call
    uri/device/convId/confId to be added (if authorized) to the conf.
    
    There are some majors differences in the process.
    First, every conversation will be able to decide a default host
    for conferences. This still needs some design and will be
    introduced in another patch. For now, the caller is the host.
    Then, because all members of the call may not be interested to join
    a call, or they may want to get several calls at the same time, the
    system must be able to manage more than one active calls (e.g. a
    company with multiple projects can do several standups at the same
    time).
    Finally, in the conversation, two commits will be generated to be
    able to know what active calls are available. The first is
    announcing that a conference started, the second announces that
    the conference stopped (the host closed the call).
    However, this introduces a difficulty. The host may crash and not
    commit the end of the call in time. In this case, hostedCalls are
    stored in a file and the conversation is updated during the init
    of the daemon.
    
    Documentation: https://docs.jami.net/technical/swarm.html#call-in-swarm
    GitLab: #312
    Change-Id: If6f36a5a14be86e0eabc1e7f74adb1006f4335d7

--- src/jami/configurationmanager_interface.h
+++ src/jami/configurationmanager_interface.h
@@ -386,6 +386,12 @@ struct DRING_PUBLIC ConfigurationSignal
                              const std::string& /*message_id*/,
                              int /*state*/);
     };
+    struct DRING_PUBLIC NeedsHoster
+    {
+        constexpr static const char* name = "NeedsHoster";
+        using cb_type = void(const std::string& /*account_id*/,
+                             const std::string& /*conversation_id*/);
+    };
     struct DRING_PUBLIC ProfileReceived
     {
         constexpr static const char* name = "ProfileReceived";
