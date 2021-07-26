a6fd02bc2b86d70c7828cc873b0c6b3078ccee1b
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-07-26 14:16:53 -0400

    swarm: add call support
    This patch introduces the ability to start calls and extends the
    usage of rendezvous points to swarm with multiple participants.
    
    When starting a call in a swarm with multiple participats, one device
    will work as the host of the conference, and the caller will
    immediately start the call alone.
    Other peers will receive a commit and a notification to be able
    to join the active call. To join a call, users needs to call
    uri/device/convId/confId to be added (if authorized) to the conf.
    
    There are some majors differences in the process.
    First, every conversation will be able to decide a default host
    for conferences. This still needs some design and will be
    introduced in another patch. For now, the caller is the host.
    Then, because all members of the call may not be interested to join
    a call, or they may want to get several calls at the same time, the
    system must be able to manage more than one active calls (e.g. a
    company with multiple projects can do several standups at the same
    time).
    Finally, in the conversation, two commits will be generated to be
    able to know what active calls are available. The first is
    announcing that a conference started, the second announces that
    the conference stopped (the host closed the call).
    However, this introduces a difficulty. The host may crash and not
    commit the end of the call in time. In this case, hostedCalls are
    stored in a file and the conversation is updated during the init
    of the daemon.
    
    Documentation: https://docs.jami.net/technical/swarm.html#call-in-swarm
    GitLab: #312
    Change-Id: If6f36a5a14be86e0eabc1e7f74adb1006f4335d7

--- src/jami/configurationmanager_interface.h
+++ src/jami/configurationmanager_interface.h
@@ -386,6 +386,19 @@ struct DRING_PUBLIC ConfigurationSignal
                              const std::string& /*message_id*/,
                              int /*state*/);
     };
+    struct DRING_PUBLIC NeedsHost
+    {
+        constexpr static const char* name = "NeedsHost";
+        using cb_type = void(const std::string& /*account_id*/,
+                             const std::string& /*conversation_id*/);
+    };
+    struct DRING_PUBLIC ActiveCallChanged
+    {
+        constexpr static const char* name = "ActiveCallChanged";
+        using cb_type = void(const std::string& /*account_id*/,
+                             const std::string& /*conversation_id*/,
+                             const std::vector<std::map<std::string, std::string> >& /*activeCalls*/);
+    };
     struct DRING_PUBLIC ProfileReceived
     {
         constexpr static const char* name = "ProfileReceived";
--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -260,6 +260,10 @@ public:
 
     /**
      * Create outgoing SIPCall.
+     * @note Accepts several urls:
+     *          + jami:uri for calling someone
+     *          + swarm:id for calling a group (will host or join if an active call is detected)
+     *          + swarm:id/uri/device/confId to join a specific conference hosted on (uri, device)
      * @param[in] toUrl The address to call
      * @param[in] mediaList list of medias
      * @return A shared pointer on the created call.
@@ -631,6 +635,13 @@ public:
 
     bool isValidAccountDevice(const dht::crypto::Certificate& cert) const;
 
+    /**
+     * Join incoming call to hosted conference
+     * @param callId        The call to join
+     * @param destination   conversation/uri/device/confId to join
+     */
+    void handleIncomingConversationCall(const std::string& callId, const std::string& destination);
+
 private:
     NON_COPYABLE(JamiAccount);
 
@@ -739,10 +750,9 @@ private:
 
     template<class... Args>
     std::shared_ptr<IceTransport> createIceTransport(const Args&... args);
-    void newOutgoingCallHelper(const std::shared_ptr<SIPCall>& call, std::string_view toUri);
+    void newOutgoingCallHelper(const std::shared_ptr<SIPCall>& call, const Uri& uri);
+    void newSwarmOutgoingCallHelper(const std::shared_ptr<SIPCall>& call, const Uri& uri);
     std::shared_ptr<SIPCall> createSubCall(const std::shared_ptr<SIPCall>& mainCall);
-
-    void updateContactHeader();
 
 #if HAVE_RINGNS
     std::string nameServer_;
