ae0a85af9723d8384ef1e798d5592bff93dbf19d
Mohamed Chibani <mohamed.chibani@savoirfairelinux.com>
2021-08-17 18:08:21 -0400

    sipcall/ice: remove obsolete temporary shared pointer
    This patch removes the temporary ICE shared pointer
    
    Gitlab: #619
    
    Change-Id: Icacac9df1102327d4d1a0f0d67dfa457016048cd

--- src/sip/sipcall.h
+++ src/sip/sipcall.h
@@ -277,12 +277,6 @@ public:
     std::unique_ptr<pjsip_inv_session, InvSessionDeleter> inviteSession_;
 
 private:
-    IceTransport* getIceMediaTransport() const
-    {
-        std::lock_guard<std::mutex> lk(transportMtx_);
-        return tmpMediaTransport_ ? tmpMediaTransport_.get() : mediaTransport_.get();
-    }
-
     void generateMediaPorts();
 
     void openPortsUPnP();
@@ -436,9 +430,6 @@ private:
 
     ///< Transport used for media streams
     std::shared_ptr<IceTransport> mediaTransport_;
-
-    ///< Temporary transport for media. Replace mediaTransport_ when connected with success
-    std::unique_ptr<IceTransport> tmpMediaTransport_;
 
     std::string peerUri_ {};
 
