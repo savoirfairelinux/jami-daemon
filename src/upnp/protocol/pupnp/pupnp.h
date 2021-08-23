a644354e28e62f96a2d6788c7060e7f879972801
Mohamed Chibani <mohamed.chibani@savoirfairelinux.com>
2021-08-23 14:45:08 -0400

    upnp: restart upnp on connectivity change
    When receiving a 'connectivity change', UPNP search must be restarted
    if the host address changes and also if there is no valid IGD.
    Also reduce the number of retries on failure.
    
    Gitlab: #615
    
    Change-Id: I80e6d61ab79ef91f480bb0073b9cec8e7e67e559

--- src/upnp/protocol/pupnp/pupnp.h
+++ src/upnp/protocol/pupnp/pupnp.h
@@ -64,17 +64,6 @@ class IpAddr;
 namespace jami {
 namespace upnp {
 
-// Error codes returned by router when trying to remove ports.
-constexpr static int ARRAY_IDX_INVALID = 713;
-constexpr static int CONFLICT_IN_MAPPING = 718;
-
-// IGD search timeout (in seconds).
-constexpr static unsigned int SEARCH_TIMEOUT {60};
-// Max number of IGD search attempts before failure.
-constexpr static unsigned int PUPNP_MAX_RESTART_SEARCH_RETRIES {5};
-// Base unit for the timeout between two successive IGD search.
-constexpr static auto PUPNP_SEARCH_RETRY_UNIT {std::chrono::seconds(15)};
-
 class PUPnP : public UPnPProtocol
 {
 public:
