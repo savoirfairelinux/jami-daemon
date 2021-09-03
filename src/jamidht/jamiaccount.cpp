1fcc3a7e83b603b3fc7b063050407a73d9b8f5b3
Mohamed Chibani <mohamed.chibani@savoirfairelinux.com>
2021-09-03 14:17:38 -0400

    ice: decouple instantiation and initialization of ICE transport
    Instantiation and initialization of ICE are done in a single
    stage (in the class constructor).
    To initialize the ICE instance, connection information must
    first be gathered asynchronously.
    However, when sharing ICE media instance between subcalls,
    it's better to have a valid ICE instance as soon as possible
    (even if not fully initialized) to proceed with the call
    initialization process, then wait for ICE initialization to start
    the call.
    Thus, the ICE instantiation will be performed synchronously as
    soon as the parent (main) call is created, then it will be
    initialized asynchronously when the connection info are ready.
    
    Gitlab: #619
    
    Change-Id: I9c97516238f1a690603975ec968c8c6733155d4a

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -424,10 +424,14 @@ JamiAccount::newOutgoingCall(std::string_view toUrl,
     if (not call)
         return {};
 
-    getIceOptions([=](auto&& opts) {
-        call->initIceMediaTransport(true, std::forward<IceTransportOptions>(opts));
-        newOutgoingCallHelper(call, toUrl);
-    });
+    if (call->isIceEnabled()) {
+        call->createIceMediaTransport();
+        getIceOptions([=](auto&& opts) {
+            call->initIceMediaTransport(true, std::forward<IceTransportOptions>(opts));
+        });
+    }
+
+    newOutgoingCallHelper(call, toUrl);
 
     return call;
 }
@@ -445,10 +449,14 @@ JamiAccount::newOutgoingCall(std::string_view toUrl, const std::vector<DRing::Me
     if (not call)
         return {};
 
-    getIceOptions([=](auto&& opts) {
-        call->initIceMediaTransport(true, std::forward<IceTransportOptions>(opts));
-        newOutgoingCallHelper(call, toUrl);
-    });
+    if (call->isIceEnabled()) {
+        call->createIceMediaTransport();
+        getIceOptions([=](auto&& opts) {
+            call->initIceMediaTransport(true, std::forward<IceTransportOptions>(opts));
+        });
+    }
+
+    newOutgoingCallHelper(call, toUrl);
 
     return call;
 }
