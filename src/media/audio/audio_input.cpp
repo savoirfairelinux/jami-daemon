0a96b60897ac95dcc3176c8cfc5e5f35b638182f
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2021-09-13 16:05:02 -0400

    audioinput: use a mutex to protect against async resource changes
    Replaces an atomic flag with a mutex preventing async calls to
    switchInput from just failing or possibly causing a crash if a
    resource already in progress.
    
    https://git.jami.net/savoirfairelinux/jami-client-qt/-/issues/522
    
    Change-Id: I5f726306a2d54b1dcaa396a463d221a6efca13d8

--- src/media/audio/audio_input.cpp
+++ src/media/audio/audio_input.cpp
@@ -58,6 +58,7 @@ AudioInput::AudioInput(const std::string& id, const std::string& resource)
     : AudioInput(id)
 {
     switchInput(resource);
+    loop_.start();
 }
 
 AudioInput::~AudioInput()
@@ -71,13 +72,6 @@ AudioInput::~AudioInput()
 void
 AudioInput::process()
 {
-    if (switchPending_.exchange(false)) {
-        if (devOpts_.input.empty())
-            JAMI_DBG() << "Switching to default audio input";
-        else
-            JAMI_DBG() << "Switching audio input to '" << devOpts_.input << "'";
-    }
-
     readFromDevice();
 }
 
@@ -266,10 +260,7 @@ std::shared_future<DeviceParams>
 AudioInput::switchInput(const std::string& resource)
 {
     // Always switch inputs, even if it's the same resource, so audio will be in sync with video
-    if (switchPending_) {
-        JAMI_ERR() << "Audio switch already requested";
-        return {};
-    }
+    std::lock_guard<std::mutex> lk(resourceMutex_);
 
     JAMI_DBG() << "Switching audio source to match '" << resource << "'";
 
@@ -315,10 +306,8 @@ AudioInput::switchInput(const std::string& resource)
             foundDevOpts(devOpts_);
     }
 
-    switchPending_ = true;
     futureDevOpts_ = foundDevOpts_.get_future().share();
     wakeUp_ = std::chrono::high_resolution_clock::now() + MS_PER_PACKET;
-    loop_.start();
     if (onSuccessfulSetup_)
         onSuccessfulSetup_(MEDIA_AUDIO, 0);
     return futureDevOpts_;
