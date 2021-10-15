de90c9bed1feef07ec59beb01d74fcc2ab7d521d
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-10-15 10:30:16 -0400

    agent/jami/logger: Add (jami logger) module
    Change-Id: I1d39a963ef3efe64059da82d99e23589c4920a2d

---
+++ test/agent/jami/logger.scm
@@ -0,0 +1,27 @@
+(define-module (jami logger)
+  #:use-module ((jami logger bindings) #:prefix ffi:)
+  #:export (debug
+	    info
+	    warning
+	    error))
+
+(define-syntax-rule (logging% lvl fmt args ...)
+  (let* ((source-location (current-source-location))
+	 (filename (or (assq-ref source-location 'filename) "<guile>"))
+	 (line (or (assq-ref source-location 'line) -1)))
+    (ffi:log lvl
+	     filename
+	     (+ line 1)
+	     (format #f fmt args ...))))
+
+(define-syntax-rule (debug fmt args ...)
+  (logging% ffi:LOG_DEBUG fmt args ...))
+
+(define-syntax-rule (info fmt args ...)
+  (logging% ffi:LOG_INFO fmt args ...))
+
+(define-syntax-rule (warning fmt args ...)
+  (logging% ffi:LOG_WARNING fmt args ...))
+
+(define-syntax-rule (error fmt args ...)
+  (logging% ffi:LOG_ERR fmt args ...))
