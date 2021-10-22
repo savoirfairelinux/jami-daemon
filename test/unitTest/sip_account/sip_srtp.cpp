092056b8bbd38fc2ca3e8cbcc68de4b597ca8678
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-10-22 13:10:10 -0400

    callmanager: add accountId to identify calls, conferences
    Change-Id: I2a3a2c77e9ba78b1a280bdf8dca7eeaabb72f3fd

--- test/unitTest/sip_account/sip_srtp.cpp
+++ test/unitTest/sip_account/sip_srtp.cpp
@@ -231,7 +231,8 @@ SipSrtpTest::onIncomingCallWithMedia(const std::string& accountId,
 }
 
 void
-SipSrtpTest::onCallStateChange(const std::string& callId,
+SipSrtpTest::onCallStateChange(const std::string& accountId,
+                               const std::string& callId,
                                const std::string& state,
                                CallData& callData)
 {
@@ -389,10 +390,10 @@ SipSrtpTest::configureTest(CallData& aliceData, CallData& bobData)
         }));
 
     signalHandlers.insert(DRing::exportable_callback<DRing::CallSignal::StateChange>(
-        [&](const std::string& callId, const std::string& state, signed) {
+        [&](const std::string& accountId, const std::string& callId, const std::string& state, signed) {
             auto user = getUserAlias(callId);
             if (not user.empty())
-                onCallStateChange(callId, state, user == aliceData.alias_ ? aliceData : bobData);
+                onCallStateChange(accountId, callId, state, user == aliceData.alias_ ? aliceData : bobData);
         }));
 
     signalHandlers.insert(DRing::exportable_callback<DRing::CallSignal::MediaNegotiationStatus>(
