3eb00f4cd75054f54524b4ea136c06c080d1e819
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2021-11-05 15:14:17 -0400

    callmanager: add accountId to identify calls, conferences
    Change-Id: I2a3a2c77e9ba78b1a280bdf8dca7eeaabb72f3fd

--- src/call_factory.cpp
+++ src/call_factory.cpp
@@ -70,7 +70,7 @@ CallFactory::newSipCall(const std::shared_ptr<SIPAccountBase>& account,
     auto id = getNewCallID();
     auto call = std::make_shared<SIPCall>(account, id, type, mediaList);
     callMaps_[call->getLinkType()].emplace(id, call);
-
+    account->attach(call);
     return call;
 }
 
