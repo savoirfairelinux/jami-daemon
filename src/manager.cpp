ee18ad6d3c215d572f35ae0ac51394730429a256
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-11-05 12:20:43 -0400

    manager: emit conferenceCreated before renderer signal
    This allow clients to check if it's necessary to register to the
    sinkClient.
    
    GitLab: #568
    Change-Id: I59ad16539b3954b592fe1aeeb83bfd2c9f30a491

--- src/manager.cpp
+++ src/manager.cpp
@@ -1630,6 +1630,7 @@ Manager::joinParticipant(const std::string& callId1, const std::string& callId2,
 
     auto conf = std::make_shared<Conference>(videoEnabled);
     pimpl_->conferenceMap_.emplace(conf->getConfID(), conf);
+    emitSignal<DRing::CallSignal::ConferenceCreated>(conf->getConfID());
 
     // Bind calls according to their state
     pimpl_->bindCallToConference(*call1, *conf);
@@ -1643,7 +1644,6 @@ Manager::joinParticipant(const std::string& callId1, const std::string& callId2,
         conf->detachLocalParticipant();
     }
 
-    emitSignal<DRing::CallSignal::ConferenceCreated>(conf->getConfID());
     return true;
 }
 
@@ -2824,12 +2824,11 @@ Manager::ManagerPimpl::processIncomingCall(Call& incomCall, const std::string& a
             auto conf = std::make_shared<Conference>(acc->isVideoEnabled());
 
             conferenceMap_.emplace(conf->getConfID(), conf);
+            emitSignal<DRing::CallSignal::ConferenceCreated>(conf->getConfID());
 
             // Bind calls according to their state
             bindCallToConference(*call, *conf);
             conf->detachLocalParticipant();
-
-            emitSignal<DRing::CallSignal::ConferenceCreated>(conf->getConfID());
         });
     } else if (autoAnswer_ || account->isAutoAnswerEnabled()) {
         dht::ThreadPool::io().run([incomCallId] { Manager::instance().answerCall(incomCallId); });
