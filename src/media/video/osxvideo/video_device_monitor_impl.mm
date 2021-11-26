b4167b175b374ce84cd58ebdb2ca6cd79fc0b333
kkostiuk <kateryna.kostiuk@savoirfairelinux.com>
2021-11-26 17:36:02 -0500

    screensharing: add framerate for macOS
    Change-Id: I2a22e80a8ea826641afef9a82bf3b17ca35fcaed

--- src/media/video/osxvideo/video_device_monitor_impl.mm
+++ src/media/video/osxvideo/video_device_monitor_impl.mm
@@ -127,6 +127,7 @@ VideoDeviceMonitor::VideoDeviceMonitor() :
     monitorImpl_(new VideoDeviceMonitorImpl(this))
 {
     monitorImpl_->start();
+    addDevice(DEVICE_DESKTOP, {});
 }
 
 VideoDeviceMonitor::~VideoDeviceMonitor()
