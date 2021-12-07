f75c9426bb3053e4dbdfa34ee508f077dcd5c049
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2021-12-07 10:41:58 -0500

    media_encoder: avoid segfault on nullptr
    Catched by ut_media_encoder:
    
    READ of size 8 at 0x602000029818 thread T0
        #0 0x5649e914d6e0 in jami::MediaEncoder::send(AVPacket&, int) media/media_encoder.cpp:504
        #1 0x5649e914d05c in jami::MediaEncoder::encode(AVFrame*, int) media/media_encoder.cpp:480
        #2 0x5649e8cac119 in jami::test::MediaEncoderTest::testMultiStream() media/test_media_encoder.cpp:183
    
    startStream before checking width and height
    
    Change-Id: I3c0217ec3e1d50950bf46989e0dd6797fee8972e

--- src/media/media_encoder.cpp
+++ src/media/media_encoder.cpp
@@ -377,16 +377,15 @@ MediaEncoder::encode(const std::shared_ptr<VideoFrame>& input,
                      bool is_keyframe,
                      int64_t frame_number)
 {
+    if (!initialized_) {
+        initStream(videoCodec_, input->pointer()->hw_frames_ctx);
+        startIO();
+    }
     auto width = (input->width() >> 3) << 3;
     auto height = (input->height() >> 3) << 3;
     if (initialized_ && (getWidth() != width || getHeight() != height)) {
         resetStreams(width, height);
         is_keyframe = true;
-    }
-
-    if (!initialized_) {
-        initStream(videoCodec_, input->pointer()->hw_frames_ctx);
-        startIO();
     }
 
     std::shared_ptr<VideoFrame> output;
@@ -497,6 +496,8 @@ MediaEncoder::send(AVPacket& pkt, int streamIdx)
         streamIdx = currentStreamIdx_;
     if (streamIdx >= 0 and static_cast<size_t>(streamIdx) < encoders_.size()) {
         auto encoderCtx = encoders_[streamIdx];
+        if (!outputCtx_ || !outputCtx_->streams[streamIdx])
+            return false;
         pkt.stream_index = streamIdx;
         if (pkt.pts != AV_NOPTS_VALUE)
             pkt.pts = av_rescale_q(pkt.pts,
