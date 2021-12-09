c72fa7de6af1103d7571b9237f8fa5a9cddb2633
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-12-09 12:03:18 -0500

    agent: Use GOOPS
    Change-Id: If6a038b6d8c371f8e74849749770f1dec8559b91

--- test/agent/examples/passive-agent.scm
+++ test/agent/examples/passive-agent.scm
@@ -1,22 +1,46 @@
+#!/usr/bin/env -S ./agent.exe -s
+!#
+
 ;;; This is an example of a passive agent.
 ;;;
-;;; The passive agent ensure that an account is created and then wait for
-;;; incomming call of any peer.
+;;; The passive agent ensure that an account is created and accept all trust
+;;; requests and incoming calls.
 
-(use-modules ((agent) #:prefix agent:)
+
+(use-modules (agent)
+             ((jami account) #:prefix account:)
              ((jami signal) #:prefix jami:)
              ((jami call) #:prefix call:)
              ((jami logger) #:prefix jami:))
 
-(agent:ensure-account)
+(define this-agent (make-agent "afafafafafafafaf"))
 
-(jami:info "Agent peer-id: ~a" (agent:peer-id))
-
-(let ((account (agent:account-id)))
+(let ([account (account-id this-agent)])
+  ;; Accept all incoming calls with media.
   (jami:on-signal 'incoming-call/media
                   (lambda (account-id call-id peer media-lst)
                     (when (string= account-id account)
+                      (jami:info "Incoming [call:~a] with media ~a from peer ~a~%"
+                                 call-id media-lst peer)
+                      (call:accept account-id call-id media-lst))
+                    #t))
+  ;; Accept all incoming calls.
+  (jami:on-signal 'incoming-call
+                  (lambda (account-id call-id peer)
+                    (when (string= account-id account)
                       (jami:info "Incoming [call:~a] from peer ~a~%" call-id peer)
-                      (call:accept call-id media-lst))
+                      (call:accept account-id call-id))
+                    #t))
+
+  ;;; Accept all trust requests.
+  (jami:on-signal 'incoming-trust-request
+                  (lambda (account-id conversation-id peer-id payload received)
+                    (when (string= account-id account)
+                      (jami:info "accepting trust request: ~a ~a" account-id peer-id)
+                      (account:accept-trust-request account-id peer-id))
                     #t)))
+
+
+(jami:info "~a" this-agent)
+
 (while #t (pause))
