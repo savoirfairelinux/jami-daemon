bc051a05dda7841f322b586b49ed18a8da0e4d6e
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-12-10 12:51:23 -0500

    tracepoint: Add LTTng tracepoint generation
    Change-Id: I6358f3ab5f464f1a553814c58b22245ef64f0b52

---
+++ src/tracepoint-def.h
@@ -0,0 +1,30 @@
+#include "config.h"
+
+#ifdef ENABLE_TRACEPOINTS
+
+#undef LTTNG_UST_TRACEPOINT_PROVIDER
+#define LTTNG_UST_TRACEPOINT_PROVIDER jami
+
+#undef LTTNG_UST_TRACEPOINT_INCLUDE
+#define LTTNG_UST_TRACEPOINT_INCLUDE "src/tracepoint-def.h"
+
+#if !defined(TRACEPOINT_DEF_H) || defined(LTTNG_UST_TRACEPOINT_HEADER_MULTI_READ)
+#define TRACEPOINT_DEF_H
+
+#include <lttng/tracepoint.h>
+
+/*
+ * Use LTTNG_UST_TRACEPOINT_EVENT(), LTTNG_UST_TRACEPOINT_EVENT_CLASS(),
+ * LTTNG_UST_TRACEPOINT_EVENT_INSTANCE(), and LTTNG_UST_TRACEPOINT_LOGLEVEL()
+ * here.
+ */
+
+#endif /* TRACEPOINT_H */
+
+#include <lttng/tracepoint-event.h>
+
+#else  /* ENABLE_TRACEPOINTS */
+#  define lttng_ust_tracepoint(...) static_assert(true)
+#  define lttng_ust_do_tracepoint(...) static_assert(true)
+#  define lttng_ust_tracepoint_enabled(...) false
+#endif
