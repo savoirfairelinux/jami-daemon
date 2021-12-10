9f624a9a652345b99ca89b4eb3d48f38c7f55d14
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-12-10 12:53:31 -0500

    tracepoint: Add scheduled executor tracepoints
    The two tracepoints are used to delimit the begining and the end of the
    execution of a task by a scheduler.  The begining tracepoint contain all the
    meta informations related to the the execution context of the tast and an unique
    cookie.  This cookie is then reeused in the end tracepoint to match the pair of
    begin/end without duplicating too much informations.
    
    Change-Id: Ibbf64fe49325e7cb72c60aa6c9d8d8d1ab074f2a

--- src/tracepoint-def.h
+++ src/tracepoint-def.h
@@ -8,10 +8,11 @@
 #undef LTTNG_UST_TRACEPOINT_INCLUDE
 #define LTTNG_UST_TRACEPOINT_INCLUDE "src/tracepoint-def.h"
 
-#if !defined(TRACEPOINT_H) || defined(LTTNG_UST_TRACEPOINT_HEADER_MULTI_READ)
-#define TRACEPOINT_H
+#if !defined(TRACEPOINT_DEF_H) || defined(LTTNG_UST_TRACEPOINT_HEADER_MULTI_READ)
+#define TRACEPOINT_DEF_H
 
 #include <lttng/tracepoint.h>
+
 
 /*
  * Use LTTNG_UST_TRACEPOINT_EVENT(), LTTNG_UST_TRACEPOINT_EVENT_CLASS(),
@@ -19,7 +20,31 @@
  * here.
  */
 
-#endif /* TRACEPOINT_H */
+LTTNG_UST_TRACEPOINT_EVENT(
+    jami,
+    scheduled_executor_task_begin,
+    LTTNG_UST_TP_ARGS(
+        const char *, executor_name,
+        const char *, filename,
+        uint32_t,     linum,
+        uint64_t,     cookie
+    ),
+    LTTNG_UST_TP_FIELDS(
+        lttng_ust_field_string(executor, executor_name)
+        lttng_ust_field_string(source_filename, filename)
+        lttng_ust_field_integer(uint32_t, source_line, linum)
+        lttng_ust_field_integer(uint64_t, cookie, cookie)
+    )
+)
+
+LTTNG_UST_TRACEPOINT_EVENT(
+    jami,
+    scheduled_executor_task_end,
+    LTTNG_UST_TP_ARGS(uint64_t, cookie),
+    LTTNG_UST_TP_FIELDS(lttng_ust_field_integer(uint64_t, cookie, cookie))
+)
+
+#endif /* TRACEPOINT_DEF_H */
 
 #include <lttng/tracepoint-event.h>
 
