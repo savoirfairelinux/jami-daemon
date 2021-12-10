7b7b1afa6fc0043505b055244e5bbe2b2fa6c261
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-12-10 12:51:23 -0500

    tracepoint: Add LTTng tracepoint generation
    Change-Id: I6358f3ab5f464f1a553814c58b22245ef64f0b52

---
+++ src/jami/tracepoint-def.h
@@ -0,0 +1,26 @@
+#include "config.h"
+
+#ifdef ENABLE_TRACEPOINTS
+
+#undef LTTNG_UST_TRACEPOINT_PROVIDER
+#define LTTNG_UST_TRACEPOINT_PROVIDER jami
+
+#undef LTTNG_UST_TRACEPOINT_INCLUDE
+#define LTTNG_UST_TRACEPOINT_INCLUDE "src/jami/tracepoint-def.h"
+
+#if !defined(TRACEPOINT_DEF_H) || defined(LTTNG_UST_TRACEPOINT_HEADER_MULTI_READ)
+#define TRACEPOINT_DEF_H
+
+#include <lttng/tracepoint.h>
+
+/*
+ * Use LTTNG_UST_TRACEPOINT_EVENT(), LTTNG_UST_TRACEPOINT_EVENT_CLASS(),
+ * LTTNG_UST_TRACEPOINT_EVENT_INSTANCE(), and LTTNG_UST_TRACEPOINT_LOGLEVEL()
+ * here.
+ */
+
+#endif /* TRACEPOINT_H */
+
+#include <lttng/tracepoint-event.h>
+
+#endif
