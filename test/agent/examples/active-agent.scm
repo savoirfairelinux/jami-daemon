ef2dfe9adb87e6f8fa15dc51acbdc6c5668ba266
Olivier Dion <olivier.dion@savoirfairelinux.com>
2021-12-21 11:40:02 -0500

    agent/active-agent: Fix calling problems
    Change-Id: I3f7b3fd9e5bea986f5b600273ae4ddb07bdd787a

--- test/agent/examples/active-agent.scm
+++ test/agent/examples/active-agent.scm
@@ -11,44 +11,32 @@
  ((jami call) #:prefix call:)
  ((jami logger) #:prefix jami:))
 
-(define (make-a-call from to)
+(define* (make-a-call from to #:optional (timeout 30))
   (jami:info "Placing call from:~a to:~a" from to)
-  (let ((mtx (make-mutex))
-        (cnd (make-condition-variable))
-        (this-call-id "")
-        (success #f)
-        (over #f))
-
-    (jami:on-signal 'state-changed
-                    (lambda (call-id state code)
-                      (with-mutex mtx
-                        (let ((ret (cond
-                                    ((not (string= call-id this-call-id)) #t)
-                                    ((string= state "CURRENT") (begin
-                                                                 (set! success #t)
-                                                                 #t))
-                                    ((string= state "OVER") (begin
-                                                              (set! over #t)
-                                                              #f))
-                                    (else #t))))
-                          (signal-condition-variable cnd)
-                          ret))))
-
-    (set! this-call-id (call:place-call from to))
+  (let ([mtx (make-recursive-mutex)]
+        [cnd (make-condition-variable)]
+        [this-call-id ""]
+        [continue #t])
 
     (with-mutex mtx
-      (while (not (or success over))
-        (wait-condition-variable cnd mtx (+ (current-time) 30))))
+      (jami:on-signal 'state-changed
+                    (lambda (account-id call-id state code)
+                      (with-mutex mtx
+                        (when (and continue
+                                   (string= account-id from)
+                                   (string= call-id this-call-id)
+                                   (string= "CURRENT" state))
+                          (signal-condition-variable cnd))
+                        continue)))
 
-    (when success
-      (call:hang-up this-call-id))
+      (set! this-call-id (call:place-call/media from to))
 
-    (unless over
-      (with-mutex mtx
-        (while (not over)
-          (wait-condition-variable cnd mtx (+ (current-time) 30)))))
-
-    success))
+      (let ([success (wait-condition-variable cnd mtx
+                                     (+ (current-time) timeout))])
+        (when success
+          (call:hang-up from this-call-id))
+        (set! continue #f)
+        success))))
 
 (agent:ensure-account)
 
