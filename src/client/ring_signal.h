43408cde6f72075b44142aa19b4d77eebe517c71
Olivier Dion <olivier.dion@savoirfairelinux.com>
2022-02-04 11:46:31 -0500

    client/ring_signal: Add tracepoints
    Change-Id: I522edb40e114f03cdf6605f09cc40cdb0bd886b2

--- src/client/ring_signal.h
+++ src/client/ring_signal.h
@@ -36,6 +36,8 @@
 
 #include "jami.h"
 #include "logger.h"
+#include "trace-tools.h"
+#include "tracepoint.h"
 
 #ifdef __APPLE__
 #include <TargetConditionals.h>
@@ -56,19 +58,29 @@ extern SignalHandlerMap& getSignalHandlers();
  * Find related user given callback and call it with given
  * arguments.
  */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 template<typename Ts, typename... Args>
-static void
-emitSignal(Args... args)
+void emitSignal(Args... args)
 {
+    jami_tracepoint_if_enabled(emit_signal, demangle<Ts>().c_str());
+
     const auto& handlers = getSignalHandlers();
-    if (auto cb = *DRing::CallbackWrapper<typename Ts::cb_type>(handlers.at(Ts::name))) {
+    if (auto wrap = DRing::CallbackWrapper<typename Ts::cb_type>(handlers.at(Ts::name))) {
         try {
+            auto cb = *wrap;
+            jami_tracepoint(emit_signal_begin_callback,
+                            wrap.file_, wrap.linum_);
             cb(args...);
+            jami_tracepoint(emit_signal_end_callback);
         } catch (std::exception& e) {
             JAMI_ERR("Exception during emit signal %s:\n%s", Ts::name, e.what());
         }
     }
+
+    jami_tracepoint(emit_signal_end);
 }
+#pragma GCC diagnostic pop
 
 template<typename Ts>
 std::pair<std::string, std::shared_ptr<DRing::CallbackWrapper<typename Ts::cb_type>>>
