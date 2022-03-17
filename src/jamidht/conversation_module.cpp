3c58b3b15c498c7a338c3653bcd54df31786a0a0
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-03-17 17:37:47 -0400

    swarm: add method to save user's preferences
    This store user's preferences per conversation into
    conversation_data/<convId>/preferences
    In this way, the daemon is able to sync this file across devices
    and remove preferences at the same time we remove the conversation.
    
    For now, only support "color"
    
    TODO:
    + validate API
    + sync across devices
    + whitelist preferences?
    + signal when another device update preferences?
    
    Change-Id: I8fe74cc06733ad61d45d721e0264b1941d4cf122
    GitLab: #TODO

--- src/jamidht/conversation_module.cpp
+++ src/jamidht/conversation_module.cpp
@@ -1467,7 +1467,6 @@ ConversationModule::updateConversationInfos(const std::string& conversationId,
                                             bool sync)
 {
     std::lock_guard<std::mutex> lk(pimpl_->conversationsMtx_);
-    // Add a new member in the conversation
     auto it = pimpl_->conversations_.find(conversationId);
     if (it == pimpl_->conversations_.end()) {
         JAMI_ERR("Conversation %s doesn't exist", conversationId.c_str());
@@ -1493,7 +1492,6 @@ ConversationModule::conversationInfos(const std::string& conversationId) const
             return itReq->second.metadatas;
     }
     std::lock_guard<std::mutex> lk(pimpl_->conversationsMtx_);
-    // Add a new member in the conversation
     auto it = pimpl_->conversations_.find(conversationId);
     if (it == pimpl_->conversations_.end() or not it->second) {
         std::lock_guard<std::mutex> lkCi(pimpl_->convInfosMtx_);
@@ -1506,6 +1504,32 @@ ConversationModule::conversationInfos(const std::string& conversationId) const
     }
 
     return it->second->infos();
+}
+
+void
+ConversationModule::updateConversationPreferences(const std::string& conversationId,
+                                                  const std::map<std::string, std::string>& prefs)
+{
+    std::lock_guard<std::mutex> lk(pimpl_->conversationsMtx_);
+    auto it = pimpl_->conversations_.find(conversationId);
+    if (it == pimpl_->conversations_.end()) {
+        JAMI_ERR("Conversation %s doesn't exist", conversationId.c_str());
+        return;
+    }
+
+    it->second->updatePreferences(prefs);
+    // TODO sync across devices
+}
+
+std::map<std::string, std::string>
+ConversationModule::conversationPreferences(const std::string& conversationId) const
+{
+    std::lock_guard<std::mutex> lk(pimpl_->conversationsMtx_);
+    auto it = pimpl_->conversations_.find(conversationId);
+    if (it == pimpl_->conversations_.end() or not it->second)
+        return {};
+
+    return it->second->preferences();
 }
 
 std::vector<uint8_t>
@@ -1521,7 +1545,6 @@ ConversationModule::conversationVCard(const std::string& conversationId) const
 
     return it->second->vCard();
 }
-
 bool
 ConversationModule::isBannedDevice(const std::string& convId, const std::string& deviceId) const
 {
