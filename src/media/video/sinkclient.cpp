295d490cf9c0e76cfbca17c1c5e72d22d5cd4a32
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-05-09 17:05:38 -0400

    SinkClient: don't resize to 0 when changing size
    Change-Id: Ib5c1fd00793fa75312ec6c573805a3746fd29a22

--- src/media/video/sinkclient.cpp
+++ src/media/video/sinkclient.cpp
@@ -365,7 +365,6 @@ SinkClient::update(Observable<std::shared_ptr<MediaFrame>>* /*obs*/,
             av_frame_apply_cropping(outFrame.pointer(), AV_FRAME_CROP_UNALIGNED);
         }
         if (outFrame.height() != height_ || outFrame.width() != width_) {
-            setFrameSize(0, 0);
             setFrameSize(outFrame.width(), outFrame.height());
             return;
         }
@@ -427,7 +426,10 @@ SinkClient::update(Observable<std::shared_ptr<MediaFrame>>* /*obs*/,
 
         if (frame->height() != height_ || frame->width() != width_) {
             lock.unlock();
-            setFrameSize(0, 0);
+#if HAVE_SHM
+            if (doShmTransfer_)
+                setFrameSize(0, 0);
+#endif
             setFrameSize(frame->width(), frame->height());
             return;
         }
