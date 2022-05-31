90c99062a62740b9d012b32be8dce024191ab422
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-05-31 08:48:22 -0400

    multistream: be able to negotiate more than 2 media streams
    Current pjproject's configuration only allow the daemon to negotiate
    two medias streams. Which is not enough for multistream.
    Modify this limit to 32 streams (2**5) and avoid any potential
    crash if the remote tries to negotiate more streams.
    Also, fix some loops.
    
    Note: This may not be the best solution for what we want to achieve.
    Another approach may be to use a video mixer, mixing all the
    medias we want and always negotiate 2 media streams. However, this
    patch will not block the other approach and fix some loops.
    
    https://git.jami.net/savoirfairelinux/jami-project/-/issues/1429
    
    Change-Id: I71ff3088bbf5908945656d02872755226f452fc5

--- src/ice_transport.cpp
+++ src/ice_transport.cpp
@@ -745,20 +745,19 @@ std::string
 IceTransport::Impl::link() const
 {
     std::ostringstream out;
-    for (unsigned strm = 0; strm < streamsCount_; strm++) {
-        for (unsigned i = 1; i <= compCountPerStream_; i++) {
-            auto absIdx = strm * streamsCount_ + i;
-            auto laddr = getLocalAddress(absIdx);
-            auto raddr = getRemoteAddress(absIdx);
+    for (unsigned strm = 1; strm <= streamsCount_ * compCountPerStream_; strm++) {
+        auto absIdx = strm;
+        auto comp = (strm+1)/compCountPerStream_;
+        auto laddr = getLocalAddress(absIdx);
+        auto raddr = getRemoteAddress(absIdx);
 
-            if (laddr and laddr.getPort() != 0 and raddr and raddr.getPort() != 0) {
-                out << " [" << i << "] " << laddr.toString(true, true) << " ["
-                    << getCandidateType(getSelectedCandidate(absIdx, false)) << "] "
-                    << " <-> " << raddr.toString(true, true) << " ["
-                    << getCandidateType(getSelectedCandidate(absIdx, true)) << "] " << '\n';
-            } else {
-                out << " [" << i << "] disabled\n";
-            }
+        if (laddr and laddr.getPort() != 0 and raddr and raddr.getPort() != 0) {
+            out << " [" << comp << "] " << laddr.toString(true, true) << " ["
+                << getCandidateType(getSelectedCandidate(absIdx, false)) << "] "
+                << " <-> " << raddr.toString(true, true) << " ["
+                << getCandidateType(getSelectedCandidate(absIdx, true)) << "] " << '\n';
+        } else {
+            out << " [" << comp << "] disabled\n";
         }
     }
     return out.str();
@@ -971,6 +970,10 @@ IceTransport::Impl::addServerReflexiveCandidates(
                   compCount_);
         return;
     }
+    if (compCount_ > PJ_ICE_MAX_COMP) {
+        JAMI_ERR("[ice:%p] Too many components", this);
+        return;
+    }
 
     // Add config for server reflexive candidates (UPNP or from DHT).
     if (not addStunConfig(pj_AF_INET()))
@@ -1005,7 +1008,6 @@ IceTransport::Impl::addServerReflexiveCandidates(
     }
 
     stun.cfg.user_mapping_cnt = compCount_;
-    assert(stun.cfg.user_mapping_cnt <= PJ_ICE_MAX_COMP);
 }
 
 std::vector<std::pair<IpAddr, IpAddr>>
