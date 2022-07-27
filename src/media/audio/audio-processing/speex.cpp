00017e40ca455f6b9b6e17f3642b5ce96428f08b
Tobias Hildebrandt <tobias.hildebrandt@savoirfairelinux.com>
2022-07-27 15:10:17 -0400

    audio: add voice activity detection
    * rename EchoCanceller to AudioProcessor
    * enable echo cancellation and voice activity detection
    * add minimum active duration and trigger time to voice activation
    
    Gitlab: #741
    Change-Id: I98662462c17539fca1d042482e97fdb3eff86130

---
+++ src/media/audio/audio-processing/speex.cpp
@@ -0,0 +1,220 @@
+/*
+ *  Copyright (C) 2021-2022 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "speex.h"
+
+#include "audio/audiolayer.h"
+#include <cstdint>
+#include <memory>
+#include <speex/speex_config_types.h>
+#include <vector>
+
+extern "C" {
+#include <speex/speex_echo.h>
+#include <speex/speex_preprocess.h>
+}
+
+namespace jami {
+
+SpeexAudioProcessor::SpeexAudioProcessor(AudioFormat format, unsigned frameSize)
+    : AudioProcessor(format, frameSize)
+    , echoState(speex_echo_state_init_mc((int) frameSize,
+                                         (int) frameSize * 16,
+                                         (int) format.nb_channels,
+                                         (int) format.nb_channels),
+                &speex_echo_state_destroy)
+    , iProcBuffer(frameSize_, format)
+{
+    JAMI_DBG("[speex-dsp] SpeexAudioProcessor, frame size = %d (=%d ms), channels = %d",
+             frameSize,
+             frameDurationMs_,
+             format.nb_channels);
+    // set up speex echo state
+    speex_echo_ctl(echoState.get(), SPEEX_ECHO_SET_SAMPLING_RATE, &format_.sample_rate);
+
+    // speex specific value to turn feature on (need to pass a pointer to it)
+    spx_int32_t speexOn = 1;
+
+    // probability integers, i.e. 50 means 50%
+    // vad will be true if speex's raw probability calculation is higher than this in any case
+    spx_int32_t probStart = 99;
+
+    // vad will be true if voice was active last frame
+    //     AND speex's raw probability calculation is higher than this
+    spx_int32_t probContinue = 90;
+
+    // maximum noise suppression in dB (negative)
+    spx_int32_t maxNoiseSuppress = -50;
+
+    // set up speex preprocess states, one for each channel
+    // note that they are not enabled here, but rather in the enable* functions
+    for (unsigned int i = 0; i < format.nb_channels; i++) {
+        auto channelPreprocessorState
+            = SpeexPreprocessStatePtr(speex_preprocess_state_init((int) frameSize,
+                                                                  (int) format.sample_rate),
+                                      &speex_preprocess_state_destroy);
+
+        // set max noise suppression level
+        speex_preprocess_ctl(channelPreprocessorState.get(),
+                             SPEEX_PREPROCESS_SET_NOISE_SUPPRESS,
+                             &maxNoiseSuppress);
+
+        // set up voice activity values
+        speex_preprocess_ctl(channelPreprocessorState.get(), SPEEX_PREPROCESS_SET_VAD, &speexOn);
+        speex_preprocess_ctl(channelPreprocessorState.get(),
+                             SPEEX_PREPROCESS_SET_PROB_START,
+                             &probStart);
+        speex_preprocess_ctl(channelPreprocessorState.get(),
+                             SPEEX_PREPROCESS_SET_PROB_CONTINUE,
+                             &probContinue);
+
+        // keep track of this channel's preprocessor state
+        preprocessorStates.push_back(std::move(channelPreprocessorState));
+    }
+
+    JAMI_INFO("[speex-dsp] Done initializing");
+}
+
+void
+SpeexAudioProcessor::enableEchoCancel(bool enabled)
+{
+    JAMI_DBG("[speex-dsp] enableEchoCancel %d", enabled);
+    // need to set member variable so we know to do it in getProcessed
+    shouldAEC = enabled;
+
+    if (enabled) {
+        // reset the echo canceller
+        speex_echo_state_reset(echoState.get());
+
+        for (auto& channelPreprocessorState : preprocessorStates) {
+            // attach our already-created echo canceller
+            speex_preprocess_ctl(channelPreprocessorState.get(),
+                                 SPEEX_PREPROCESS_SET_ECHO_STATE,
+                                 echoState.get());
+        }
+    } else {
+        for (auto& channelPreprocessorState : preprocessorStates) {
+            // detach echo canceller (set it to NULL)
+            // don't destroy it though, we will reset it when necessary
+            speex_preprocess_ctl(channelPreprocessorState.get(),
+                                 SPEEX_PREPROCESS_SET_ECHO_STATE,
+                                 NULL);
+        }
+    }
+}
+
+void
+SpeexAudioProcessor::enableNoiseSuppression(bool enabled)
+{
+    JAMI_DBG("[speex-dsp] enableNoiseSuppression %d", enabled);
+    spx_int32_t speexSetValue = (spx_int32_t) enabled;
+
+    // for each preprocessor
+    for (auto& channelPreprocessorState : preprocessorStates) {
+        // set denoise status
+        speex_preprocess_ctl(channelPreprocessorState.get(),
+                             SPEEX_PREPROCESS_SET_DENOISE,
+                             &speexSetValue);
+        // set de-reverb status
+        speex_preprocess_ctl(channelPreprocessorState.get(),
+                             SPEEX_PREPROCESS_SET_DEREVERB,
+                             &speexSetValue);
+    }
+}
+
+void
+SpeexAudioProcessor::enableAutomaticGainControl(bool enabled)
+{
+    JAMI_DBG("[speex-dsp] enableAutomaticGainControl %d", enabled);
+    spx_int32_t speexSetValue = (spx_int32_t) enabled;
+
+    // for each preprocessor
+    for (auto& channelPreprocessorState : preprocessorStates) {
+        // set AGC status
+        speex_preprocess_ctl(channelPreprocessorState.get(),
+                             SPEEX_PREPROCESS_SET_AGC,
+                             &speexSetValue);
+    }
+}
+
+std::shared_ptr<AudioFrame>
+SpeexAudioProcessor::getProcessed()
+{
+    if (tidyQueues()) {
+        return {};
+    }
+
+    auto playback = playbackQueue_.dequeue();
+    auto record = recordQueue_.dequeue();
+
+    if (!playback || !record) {
+        return {};
+    }
+
+    auto processed = std::make_shared<AudioFrame>(record->getFormat(), record->getFrameSize());
+
+    if (shouldAEC) {
+        // we want to echo cancel
+        // multichannel, output into processed
+        speex_echo_cancellation(echoState.get(),
+                                (int16_t*) record->pointer()->data[0],
+                                (int16_t*) playback->pointer()->data[0],
+                                (int16_t*) processed->pointer()->data[0]);
+    } else {
+        // don't want to echo cancel, so just use record frame instead
+        processed = record;
+    }
+
+    // deinterleave processed into channels
+    std::vector<int16_t*> procData {format_.nb_channels};
+    iProcBuffer.deinterleave((const AudioSample*) processed->pointer()->data[0],
+                             frameSize_,
+                             format_.nb_channels);
+
+    // point procData to correct channels
+    for (unsigned int channel = 0; channel < format_.nb_channels; channel++) {
+        procData[channel] = iProcBuffer.getChannel(channel)->data();
+    }
+
+    // overall voice activity
+    bool overallVad = false;
+    // current channel voice activity
+    int channelVad;
+
+    // run preprocess on each channel
+    int channel = 0;
+    for (auto& channelPreprocessorState : preprocessorStates) {
+        // preprocesses in place, returns voice activity boolean
+        channelVad = speex_preprocess_run(channelPreprocessorState.get(), procData[channel]);
+
+        // boolean OR
+        overallVad |= channelVad;
+
+        channel += 1;
+    }
+
+    // reinterleave into processed
+    iProcBuffer.interleave((AudioSample*) processed->pointer()->data[0]);
+
+    // add stabilized voice activity to the AudioFrame
+    processed->has_voice = getStabilizedVoiceActivity(overallVad);
+
+    return processed;
+}
+
+} // namespace jami
---
+++ src/media/audio/audio-processing/webrtc.cpp
@@ -0,0 +1,201 @@
+/*
+ *  Copyright (C) 2021-2022 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#include "webrtc.h"
+
+#include <webrtc/modules/audio_processing/include/audio_processing.h>
+
+namespace jami {
+
+constexpr int webrtcNoError = webrtc::AudioProcessing::kNoError;
+
+WebRTCAudioProcessor::WebRTCAudioProcessor(AudioFormat format, unsigned frameSize)
+    : AudioProcessor(format, frameSize)
+    , fRecordBuffer_(format.nb_channels, std::vector<float>(frameSize_, 0))
+    , fPlaybackBuffer_(format.nb_channels, std::vector<float>(frameSize_, 0))
+    , iRecordBuffer_(frameSize_, format)
+    , iPlaybackBuffer_(frameSize_, format)
+{
+    JAMI_DBG("[webrtc-ap] WebRTCAudioProcessor, frame size = %d (=%d ms), channels = %d",
+             frameSize,
+             frameDurationMs_,
+             format.nb_channels);
+    webrtc::Config config;
+    config.Set<webrtc::ExtendedFilter>(new webrtc::ExtendedFilter(true));
+    config.Set<webrtc::DelayAgnostic>(new webrtc::DelayAgnostic(true));
+
+    apm.reset(webrtc::AudioProcessing::Create(config));
+
+    webrtc::StreamConfig streamConfig((int) format.sample_rate, (int) format.nb_channels);
+    webrtc::ProcessingConfig pconfig = {
+        streamConfig, /* input stream */
+        streamConfig, /* output stream */
+        streamConfig, /* reverse input stream */
+        streamConfig, /* reverse output stream */
+    };
+
+    if (apm->Initialize(pconfig) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error initialising audio processing module");
+    }
+
+    // voice activity
+    if (apm->voice_detection()->Enable(true) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error enabling voice detection");
+    }
+    // TODO: change likelihood?
+    if (apm->voice_detection()->set_likelihood(webrtc::VoiceDetection::kVeryLowLikelihood)
+        != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error setting voice detection likelihood");
+    }
+    // asserted to be 10 in voice_detection_impl.cc
+    if (apm->voice_detection()->set_frame_size_ms(10) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error setting voice detection frame size");
+    }
+
+    JAMI_INFO("[webrtc-ap] Done initializing");
+}
+
+void
+WebRTCAudioProcessor::enableNoiseSuppression(bool enabled)
+{
+    JAMI_DBG("[webrtc-ap] enableNoiseSuppression %d", enabled);
+    if (apm->noise_suppression()->Enable(enabled) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error enabling noise suppression");
+    }
+    if (apm->noise_suppression()->set_level(webrtc::NoiseSuppression::kVeryHigh) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error setting noise suppression level");
+    }
+    if (apm->high_pass_filter()->Enable(enabled) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error enabling high pass filter");
+    }
+}
+
+void
+WebRTCAudioProcessor::enableAutomaticGainControl(bool enabled)
+{
+    JAMI_DBG("[webrtc-ap] enableAutomaticGainControl %d", enabled);
+    if (apm->gain_control()->Enable(enabled) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error enabling automatic gain control");
+    }
+    if (apm->gain_control()->set_analog_level_limits(0, 255) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error setting automatic gain control analog level limits");
+    }
+    if (apm->gain_control()->set_mode(webrtc::GainControl::kAdaptiveAnalog) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error setting automatic gain control mode");
+    }
+}
+
+void
+WebRTCAudioProcessor::enableEchoCancel(bool enabled)
+{
+    JAMI_DBG("[webrtc-ap] enableEchoCancel %d", enabled);
+
+    if (apm->echo_cancellation()->Enable(enabled) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error enabling echo cancellation");
+    }
+    if (apm->echo_cancellation()->set_suppression_level(
+            webrtc::EchoCancellation::SuppressionLevel::kHighSuppression)
+        != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error setting echo cancellation level");
+    }
+    if (apm->echo_cancellation()->enable_drift_compensation(true) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] Error enabling echo cancellation drift compensation");
+    }
+}
+
+std::shared_ptr<AudioFrame>
+WebRTCAudioProcessor::getProcessed()
+{
+    if (tidyQueues()) {
+        return {};
+    }
+
+    int driftSamples = playbackQueue_.samples() - recordQueue_.samples();
+
+    auto playback = playbackQueue_.dequeue();
+    auto record = recordQueue_.dequeue();
+    if (!playback || !record) {
+        return {};
+    }
+
+    auto processed = std::make_shared<AudioFrame>(format_, frameSize_);
+
+    // webrtc::StreamConfig& sc = streamConfig;
+    webrtc::StreamConfig sc((int) format_.sample_rate, (int) format_.nb_channels);
+
+    // analyze deinterleaved float playback data
+    iPlaybackBuffer_.deinterleave((const AudioSample*) playback->pointer()->data[0],
+                                  frameSize_,
+                                  format_.nb_channels);
+    std::vector<float*> playData {format_.nb_channels};
+    for (unsigned channel = 0; channel < format_.nb_channels; ++channel) {
+        // point playData channel to appropriate data location
+        playData[channel] = fPlaybackBuffer_[channel].data();
+
+        // write playback to playData channel
+        iPlaybackBuffer_.channelToFloat(playData[channel], (int) channel);
+    }
+
+    // process reverse in place
+    if (apm->ProcessReverseStream(playData.data(), sc, sc, playData.data()) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] ProcessReverseStream failed");
+    }
+
+    // process deinterleaved float recorded data
+    iRecordBuffer_.deinterleave((const AudioSample*) record->pointer()->data[0],
+                                frameSize_,
+                                format_.nb_channels);
+    std::vector<float*> recData {format_.nb_channels};
+    for (unsigned int channel = 0; channel < format_.nb_channels; ++channel) {
+        // point recData channel to appropriate data location
+        recData[channel] = fRecordBuffer_[channel].data();
+
+        // write data to recData channel
+        iRecordBuffer_.channelToFloat(recData[channel], (int) channel);
+    }
+    // TODO: maybe implement this to see if it's better than automatic drift compensation
+    // (it MUST be called prior to ProcessStream)
+    // delay = (t_render - t_analyze) + (t_process - t_capture)
+    if (apm->set_stream_delay_ms(0) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] set_stream_delay_ms failed");
+    }
+
+    if (apm->gain_control()->set_stream_analog_level(analogLevel_) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] set_stream_analog_level failed");
+    }
+    apm->echo_cancellation()->set_stream_drift_samples(driftSamples);
+
+    // process in place
+    if (apm->ProcessStream(recData.data(), sc, sc, recData.data()) != webrtcNoError) {
+        JAMI_ERR("[webrtc-ap] ProcessStream failed");
+    }
+
+    analogLevel_ = apm->gain_control()->stream_analog_level();
+
+    // return interleaved s16 data
+    iRecordBuffer_.convertFloatPlanarToSigned16((uint8_t**) recData.data(),
+                                                frameSize_,
+                                                format_.nb_channels);
+    iRecordBuffer_.interleave((AudioSample*) processed->pointer()->data[0]);
+
+    processed->has_voice = getStabilizedVoiceActivity(apm->voice_detection()->stream_has_voice());
+
+    return processed;
+}
+
+} // namespace jami
