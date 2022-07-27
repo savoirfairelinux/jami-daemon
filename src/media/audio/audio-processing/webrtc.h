aa9a39e26d08d15d5c32e707e905369866d32443
Tobias Hildebrandt <tobias.hildebrandt@savoirfairelinux.com>
2022-07-27 17:43:32 -0400

    audio: integrate audio processor preferences
    * add check for PulseAudio system echo cancel module
    * audio processor preferences can currently only be changed in dring.yml
      * not yet integrated into daemon/client signaling system
    
    Gitlab: #692
    Change-Id: I57af6e844acbbfdb5a78d95a87a98873757c506d

--- src/media/audio/audio-processing/webrtc.h
+++ src/media/audio/audio-processing/webrtc.h
@@ -38,6 +38,7 @@ public:
     void enableEchoCancel(bool enabled) override;
     void enableNoiseSuppression(bool enabled) override;
     void enableAutomaticGainControl(bool enabled) override;
+    void enableVoiceActivityDetection(bool enabled) override;
 
 private:
     std::unique_ptr<webrtc::AudioProcessing> apm;
