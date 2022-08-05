0b8af8e40a4868b1c32872e8779a8943093c2394
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-08-05 10:59:57 -0400

    logger: use fmt/std.h or fmt/ostream.h depending on availability
    Change-Id: I3a1c6b59083426ceb28c8fb2fe4b3c2d7866e491

--- src/logger.cpp
+++ src/logger.cpp
@@ -30,9 +30,13 @@
 #include <fmt/core.h>
 #include <fmt/format.h>
 #include <fmt/compile.h>
+#if __has_include(<fmt/std.h>)
+#include <fmt/std.h>
+#else
+#include <fmt/ostream.h>
+#endif
 
 #ifdef _MSC_VER
-#include <fmt/std.h>
 #include <sys_time.h>
 #else
 #include <sys/time.h>
