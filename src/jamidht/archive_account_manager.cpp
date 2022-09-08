7640641104a6a8c034019c8d5c0e08a908813930
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-09-08 14:59:32 -0400

    fileutils: migrate to boost::nowide
    Change-Id: Ic243898c915b091361f03688d88baad834516d3a

--- src/jamidht/archive_account_manager.cpp
+++ src/jamidht/archive_account_manager.cpp
@@ -29,7 +29,7 @@
 #include <opendht/thread_pool.h>
 
 #include <memory>
-#include <fstream>
+#include <nowide/fstream.hpp>
 
 namespace jami {
 
@@ -823,10 +823,10 @@ ArchiveAccountManager::exportArchive(const std::string& destinationPath, const s
 
         // Export the file
         auto sourcePath = fileutils::getFullPath(path_, archivePath_);
-        std::ifstream src(sourcePath, std::ios::in | std::ios::binary);
+        nowide::ifstream src(sourcePath, std::ios::in | std::ios::binary);
         if (!src)
             return false;
-        std::ofstream dst(destinationPath, std::ios::out | std::ios::binary);
+        nowide::ofstream dst(destinationPath, std::ios::out | std::ios::binary);
         dst << src.rdbuf();
         return true;
     } catch (const std::runtime_error& ex) {
