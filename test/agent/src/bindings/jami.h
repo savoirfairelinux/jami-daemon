58296f02c6e300a1bdc34ce0b9d8561e975292a9
Olivier Dion <olivier.dion@savoirfairelinux.com>
2022-09-22 14:27:41 -0400

    libguile-jami: Expect user to call jami:init and jami:fini
    Exit hooks are only called when in a REPL.  Thus, provide the user with bindings
    for initalization and finilization of the daemon.  The syntax `with-jami' can be used to
    ensure that finilization happens if initialization succeed.
    
    Change-Id: I2f84a76844866a1de06300cde26618c8576045e5

---
+++ test/agent/src/bindings/jami.h
@@ -0,0 +1,92 @@
+/*
+ *  Copyright (C) 2021-2022 Savoir-faire Linux Inc.
+ *
+ *  Author: Olivier Dion <olivier.dion@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "jami/jami.h"
+
+#include "utils.h"
+
+static SCM init_binding(SCM flags)
+{
+        LOG_BINDING();
+
+        unsigned int flags_cast = from_guile(flags);
+
+        return to_guile(DRing::init(static_cast<DRing::InitFlag>(flags_cast)));
+}
+
+static SCM fini_binding()
+{
+        LOG_BINDING();
+
+        DRing::fini();
+
+        return SCM_UNDEFINED;
+}
+
+static SCM initialized_binding()
+{
+        LOG_BINDING();
+
+        return to_guile(DRing::initialized());
+}
+
+static SCM logging_binding(SCM whom, SCM action)
+{
+        LOG_BINDING();
+
+        DRing::logging(from_guile(whom), from_guile(action));
+
+        return SCM_UNDEFINED;
+}
+
+static SCM platform_binding()
+{
+        LOG_BINDING();
+
+        return to_guile(DRing::platform());
+}
+
+static SCM start_binding(SCM config_file)
+{
+        LOG_BINDING();
+
+        return to_guile(DRing::start(from_guile(config_file)));
+}
+
+static SCM version_binding()
+{
+        LOG_BINDING();
+
+        return to_guile(DRing::version());
+}
+
+static void
+install_jami_primitives(void *)
+{
+    define_primitive("init", 1, 0, 0, (void*) init_binding);
+    define_primitive("initialized", 0, 0, 0, (void*) initialized_binding);
+    define_primitive("fini", 0, 0, 0, (void*) fini_binding);
+    define_primitive("logging", 2, 0, 0, (void*) logging_binding);
+    define_primitive("platform", 0, 0, 0, (void*) platform_binding);
+    define_primitive("start", 1, 0, 0, (void*) start_binding);
+    define_primitive("version", 0, 0, 0, (void*) version_binding);
+}
