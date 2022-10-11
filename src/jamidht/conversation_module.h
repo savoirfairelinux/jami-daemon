8da0f389ae3dea7049224030feebd36336e7675f
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-10-11 16:22:23 -0400

    conversation: add support for message edition
    For now, only be able to edit messages from our-self and with the
    text/plain type
    
    Change-Id: I20c6ed7dad3892f0fa655812242b9ca6e77e69b3
    GitLab: #316

--- src/jamidht/conversation_module.h
+++ src/jamidht/conversation_module.h
@@ -160,6 +160,10 @@ public:
                      OnCommitCb&& onCommit = {},
                      OnDoneCb&& cb = {});
 
+    void editMessage(const std::string& conversationId,
+                     const std::string& newBody,
+                     const std::string& editedId);
+
     /**
      * Add to the related conversation the call history message
      * @param uri           Peer number
