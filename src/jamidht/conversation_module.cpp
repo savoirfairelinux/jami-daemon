6cd9bc3f9cadd6682466561fecc01ddbdd73fd66
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-10-14 10:52:37 -0400

    wip: ios: add type for cloneConversation
    If iOS is in deep sleep mode, it will only wake up the daemon if
    a new activity needs to be shown (call, message or contact).
    If the peer accepts a trust request, it will clone and join the
    conversation (so a new message will be shown). So, cloning
    a conversation should notify the iOS device that the daemon MUST
    be started.
    
    GitLab: #776
    Change-Id: I639a5f928fec2970909a29b9f3c33c165ac201c8

--- src/jamidht/conversation_module.cpp
+++ src/jamidht/conversation_module.cpp
@@ -337,7 +337,7 @@ ConversationModule::Impl::cloneConversation(const std::string& deviceId,
                 }
             }
             return false;
-        });
+        }, "application/im-gitmessage-id");
 
         JAMI_INFO("[Account %s] New conversation detected: %s. Ask device %s to clone it",
                   accountId_.c_str(),
@@ -456,7 +456,7 @@ ConversationModule::Impl::fetchNewCommits(const std::string& peer,
                     },
                     commitId);
                 return true;
-            });
+            }, "");
     } else {
         if (getRequest(conversationId) != std::nullopt)
             return;
@@ -1213,7 +1213,7 @@ ConversationModule::cloneConversationFrom(const std::string& conversationId,
                     }
                 }
                 return false;
-            });
+            }, "application/im-gitmessage-id");
         });
     ConvInfo info;
     info.id = conversationId;
