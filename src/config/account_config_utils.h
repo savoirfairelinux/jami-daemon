7fcddeb7eb41ba03daa29b1fea9139f428e8fb3c
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-10-25 16:27:39 -0400

    config: only save non-default values
    Change-Id: I099d185f1a68470274012d3a597ff0b4a9f589d5

---
+++ src/config/account_config_utils.h
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (C) 2004-2022 Savoir-faire Linux Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+#include "yamlparser.h"
+
+template<typename T>
+inline void serializeValue(YAML::Emitter& out, const char* key, const T& value, const T& def) {
+    if (value != def)
+        out << YAML::Key << key << YAML::Value << value;
+}
+
+#define SERIALIZE_CONFIG(key, name) serializeValue(out, key, name, DEFAULT_CONFIG.name)
