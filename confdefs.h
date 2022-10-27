b940f68896b0b223fea799ab6013c3d6ca045361
Thomas Ballasi <thomas.ballasi@savoirfairelinux.com>
2022-10-27 09:31:36 -0400

    connectionmanager: prevent JamiAccount usage
    GitLab: #778
    Change-Id: I5e7eb072ebda81c4ae45316cc46842dafdeaad13

---
+++ confdefs.h
@@ -0,0 +1,9 @@
+/* confdefs.h */
+#define PACKAGE_NAME "Jami Daemon"
+#define PACKAGE_TARNAME "jami"
+#define PACKAGE_VERSION "13.6.0"
+#define PACKAGE_STRING "Jami Daemon 13.6.0"
+#define PACKAGE_BUGREPORT "jami@gnu.org"
+#define PACKAGE_URL ""
+#define PACKAGE "jami"
+#define VERSION "13.6.0"
