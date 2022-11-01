ddae1a338e37f84c35d44fe4923090cd834170bd
Fadi SHEHADEH <fadi.shehadeh@savoirfairelinux.com>
2022-11-01 17:02:21 -0400

    drt: swarm components
    -created buckets for sockets
    -created routing table that can have one or more buckets
    -created a swarm protocol
    -added swarm channel handler
    -added manager which includes all the components above
    -updated the makefile
    
    Change-Id: I7973b2bb5e950a9dddd8dedb788f01e0c26f5f9f

---
+++ src/jamidht/swarm/swarm_manager.cpp
@@ -0,0 +1,273 @@
+/*
+ *  Copyright (C) 2022 Savoir-faire Linux Inc.
+ *  Author: Fadi Shehadeh <fadi.shehadeh@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "swarm_manager.h"
+#include "jamidht/multiplexed_socket.h"
+
+#include <iostream>
+
+namespace jami {
+
+using namespace swarm_protocol;
+
+SwarmManager::SwarmManager(const NodeId& id)
+    : myId(id)
+{
+    routing_table.setMyId(id);
+    // JAMI_ERROR("SwarmManager::SwarmManager {:s}", id.to_c_str());
+}
+
+void
+SwarmManager::setKnownNodes(const std::vector<NodeId>& known_nodes)
+{
+    for (const auto& nodeId : known_nodes)
+        if (myId != nodeId) {
+            auto test = routing_table.findBucket(nodeId);
+            if (!test->hasNodeId(nodeId)) {
+                addKnownNodes(nodeId);
+            }
+        }
+    maintainBuckets();
+}
+
+void
+SwarmManager::addKnownNodes(const NodeId& nodeId)
+{
+    // JAMI_ERROR("SwarmManager::addKnownNodes {:s}", nodeId.to_c_str());
+    if (myId != nodeId) {
+        /*         JAMI_ERROR("######## MY ID {:s} // ID OF NODE ADDING {:s} #########",
+         * this->getMyId().toString(), nodeId.toString());
+         */
+        routing_table.addKnownNode(nodeId);
+    }
+}
+
+void
+SwarmManager::sendRequest(const std::shared_ptr<ChannelSocketInterface>& socket,
+                          NodeId& nodeId,
+                          Query q,
+                          int numberNodes)
+{
+    msgpack::sbuffer buffer;
+    msgpack::packer<msgpack::sbuffer> pk(&buffer);
+    std::error_code ec;
+
+    Request toRequest {q, numberNodes, nodeId};
+    Message msg;
+    msg.request = toRequest;
+    pk.pack(msg);
+    /*     JAMI_ERROR("SM {:s} request to Node {:s}", myId.toString(), socket->deviceId().toString());
+     */
+    socket->write(reinterpret_cast<const unsigned char*>(buffer.data()), buffer.size(), ec);
+    if (ec) {
+        JAMI_ERR("%s", ec.message().c_str());
+        return;
+    }
+}
+
+void
+SwarmManager::sendAnswer(const std::shared_ptr<ChannelSocketInterface>& socket, const Message& msg_)
+{
+    // JAMI_ERROR("SM {:s} answer to Node {:s}", myId.toString(), socket->deviceId().toString());
+
+    Response toResponse;
+    Message msg;
+    std::vector<NodeId> nodesToSend;
+
+    /*     if (msg_.request->q == Query::FIND_KNODES) {
+            auto bucket = routing_table.findBucket(msg_.request->nodeId);
+            nodesToSend = bucket->getKnownNodesVector();
+            toResponse.q = Query::FOUND_KNODES;
+
+        } */
+
+    if (msg_.request->q == Query::FIND) {
+        /*      JAMI_ERROR("LOOKING FOR CLOSEST NODES FROM SM {:s} to {:s}",
+                        this->getMyId().toString(),
+                        socket->deviceId().toString()); */
+        nodesToSend = routing_table.closestNodes(msg_.request->nodeId, msg_.request->num);
+        toResponse.q = Query::FOUND;
+
+        toResponse.nodes = nodesToSend;
+        msg.response = toResponse;
+
+        JAMI_ERROR("Nodes Sent: ");
+        for (const auto& node : nodesToSend) {
+            JAMI_ERROR("node: {:s}", node.toString());
+        }
+        msgpack::sbuffer buffer;
+        msgpack::packer<msgpack::sbuffer> pk(&buffer);
+        std::error_code ec;
+
+        pk.pack(msg);
+
+        socket->write(reinterpret_cast<const unsigned char*>(buffer.data()), buffer.size(), ec);
+        if (ec) {
+            JAMI_ERR("%s", ec.message().c_str());
+            return;
+        }
+    }
+}
+
+void
+SwarmManager::receiveMessage(const std::shared_ptr<ChannelSocketInterface>& socket)
+{
+    socket->setOnRecv([w = weak(), socket, this](const uint8_t* buf, size_t len) {
+        auto shared = w.lock();
+        /*         JAMI_ERROR("Swarm Manager {:s} receiving message from {:s}",
+                           shared->myId.toString(),
+                           socket->deviceId().toString()); */
+
+        Message msg;
+        try {
+            msgpack::object_handle oh = msgpack::unpack(reinterpret_cast<const char*>(buf), len);
+            oh.get().convert(msg);
+
+            if (msg.request) {
+                JAMI_ERROR("Request received by {:s} from {:s}:",
+                           shared->myId.toString(),
+                           socket->deviceId().toString());
+                JAMI_ERROR("NodeId: {:s}  Number: {:d}",
+                           msg.request->nodeId.toString(),
+                           msg.request->num);
+                shared->sendAnswer(socket, msg);
+            }
+            if (msg.response) {
+                JAMI_ERROR("Response received by {:s} from {:s}:",
+                           shared->myId.toString(),
+                           socket->deviceId().toString());
+                this->setKnownNodes(msg.response->nodes);
+            }
+
+        } catch (const std::exception& e) {
+            JAMI_WARN("Error DRT recv: %s", e.what());
+            return len;
+        }
+
+        return len;
+    });
+}
+
+void
+SwarmManager::maintainBuckets()
+{
+    for (auto it = routing_table.buckets.begin(); it != routing_table.buckets.end(); ++it) {
+        auto& bucket = *it;
+        auto connecting_nodes = bucket.getConnectingNodesSize() + bucket.getNodesSize();
+        if (connecting_nodes < Bucket::BUCKET_MAX_SIZE) {
+            auto closest_nodes = bucket.getKnownNodesRandom(Bucket::BUCKET_MAX_SIZE
+                                                            - connecting_nodes);
+
+            for (auto node : closest_nodes) {
+                tryConnect(node);
+            }
+
+            /*             if (routing_table.contains(it, myId)) {
+                            auto closest_nodes = bucket.getKnownNodesRandom(Bucket::BUCKET_MAX_SIZE
+               - connecting_nodes);
+
+                            for (auto node : closest_nodes) {
+                                tryConnect(node);
+                            }
+                        } else {
+                            auto id = bucket.randomId();
+                            auto closest_nodes = bucket.getKnownNodesRandom(Bucket::BUCKET_MAX_SIZE
+               - connecting_nodes); for (auto node : closest_nodes) { tryConnect(node);
+                            }
+                        } */
+        }
+    }
+}
+
+void
+SwarmManager::tryConnect(const NodeId& nodeId)
+{
+    auto bucket = routing_table.findBucket(nodeId);
+    bucket->removeKnownNode(nodeId);
+    bucket->addConnectingNode(nodeId);
+    JAMI_ERROR("TRY CONNECT");
+
+    if (needSocketCb_)
+        needSocketCb_(nodeId.toString(), [&](const std::shared_ptr<ChannelSocketInterface>& socket) {
+            JAMI_ERROR("IN CB");
+
+            if (socket) {
+                JAMI_ERROR("IN socket");
+
+                auto bucket = routing_table.findBucket(nodeId);
+
+                if (!bucket->hasNodeId(nodeId)) {
+                    routing_table.addNode(socket, bucket);
+                    bucket->removeConnectingNode(socket->deviceId());
+                    receiveMessage(socket);
+                }
+
+                std::error_code ec;
+                resetNodeExpiry(ec, socket, myId);
+
+            } else {
+                routing_table.addKnownNode(nodeId);
+            }
+            return true;
+        });
+}
+
+void
+SwarmManager::addChannel(const std::shared_ptr<ChannelSocketInterface>& channel)
+{
+    auto bucket = routing_table.findBucket(channel->deviceId());
+    bucket->removeKnownNode(channel->deviceId());
+    bucket->removeConnectingNode(channel->deviceId());
+    routing_table.addNode(channel, bucket);
+
+    receiveMessage(channel);
+};
+
+void
+SwarmManager::resetNodeExpiry(const asio::error_code& ec,
+                              const std::shared_ptr<ChannelSocketInterface>& socket,
+                              NodeId node)
+{
+    NodeId idToFind;
+    std::list<Bucket>::iterator bucket;
+
+    if (ec == asio::error::operation_aborted)
+        return;
+
+    if (!node) {
+        bucket = routing_table.findBucket(socket->deviceId());
+        idToFind = bucket->randomId();
+    } else {
+        bucket = routing_table.findBucket(node);
+        idToFind = node;
+    }
+
+    sendRequest(socket, idToFind, Query::FIND, Bucket::BUCKET_MAX_SIZE);
+
+    auto& nodeTimer = bucket->getNodeTimer(socket);
+
+    nodeTimer.expires_after(FIND_PERIOD);
+
+    nodeTimer.async_wait(std::bind(&jami::SwarmManager::resetNodeExpiry,
+                                   this,
+                                   std::placeholders::_1,
+                                   socket,
+                                   NodeId {}));
+}
+
+} // namespace jami
