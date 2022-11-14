3c380612d803be88bd9a1157cf6211d4ca6a6d3e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-11-14 11:24:58 -0500

    misc: cleanup
    Change-Id: I6e5b31c27f3c8bd22a6d6178bf3225f0b7ebde49

--- src/config/serializable.h
+++ src/config/serializable.h
@@ -17,9 +17,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
  */
-
-#ifndef SERIALIZABLE_H__
-#define SERIALIZABLE_H__
+#pragma once
 
 namespace YAML {
 class Emitter;
@@ -37,5 +35,3 @@ public:
 };
 
 } // namespace jami
-
-#endif
