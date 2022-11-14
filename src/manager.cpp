638fdeaf40f516bf59e4f414b5425f4722cec9b0
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-11-14 11:24:58 -0500

    misc: cleanup
    Change-Id: I6e5b31c27f3c8bd22a6d6178bf3225f0b7ebde49

--- src/manager.cpp
+++ src/manager.cpp
@@ -206,15 +206,10 @@ static void
 setDhtLogLevel()
 {
 #ifndef RING_UWP
-    char* envvar = getenv(DHTLOGLEVEL);
     int level = 0;
-
-    if (envvar != nullptr) {
-        if (not(std::istringstream(envvar) >> level))
-            level = 0;
-
-        // From 0 (min) to 3 (max)
-        level = std::max(0, std::min(level, 3));
+    if (auto envvar = getenv(DHTLOGLEVEL)) {
+        level = to_int<int>(envvar, 0);
+        level = std::clamp(level, 0, 3);
         JAMI_DBG("DHTLOGLEVEL=%u", level);
     }
     Manager::instance().dhtLogLevel = level;
@@ -241,8 +236,7 @@ setSipLogLevel()
     int level = 0;
 
     if (envvar != nullptr) {
-        if (not(std::istringstream(envvar) >> level))
-            level = 0;
+        level = to_int<int>(envvar, 0);
 
         // From 0 (min) to 6 (max)
         level = std::max(0, std::min(level, 6));
@@ -284,9 +278,7 @@ setGnuTlsLogLevel()
     int level = RING_TLS_LOGLEVEL;
 
     if (envvar != nullptr) {
-        int var_level;
-        if (std::istringstream(envvar) >> var_level)
-            level = var_level;
+        level = to_int<int>(envvar);
 
         // From 0 (min) to 9 (max)
         level = std::max(0, std::min(level, 9));
@@ -2690,11 +2682,11 @@ Manager::addAccount(const std::map<std::string, std::string>& details, const std
     else
         accountType = AccountFactory::DEFAULT_ACCOUNT_TYPE;
 
-    JAMI_DBG("Adding account %s", newAccountID.c_str());
+    JAMI_DEBUG("Adding account {:s}", newAccountID);
 
     auto newAccount = accountFactory.createAccount(accountType, newAccountID);
     if (!newAccount) {
-        JAMI_ERR("Unknown %s param when calling addAccount(): %s",
+        JAMI_ERROR("Unknown {:s} param when calling addAccount(): {:s}",
                  Conf::CONFIG_ACCOUNT_TYPE,
                  accountType);
         return "";
