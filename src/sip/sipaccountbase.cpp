85d1d8b180a20392134ec42c44d4a73b280bf0d5
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2022-11-15 16:50:21 -0500

    config: use AccountConfig for all Account classes
    Change-Id: Ia7dc5c12c91c0b9ea07c41b2de3ac58df7d1239f

--- src/sip/sipaccountbase.cpp
+++ src/sip/sipaccountbase.cpp
@@ -128,203 +128,18 @@ SIPAccountBase::flush()
 {
     // Class base method
     Account::flush();
-
     fileutils::remove(fileutils::get_cache_dir() + DIR_SEPARATOR_STR + getAccountID()
                       + DIR_SEPARATOR_STR "messages");
 }
 
-template<typename T>
-static void
-validate(std::string& member, const std::string& param, const T& valid)
-{
-    const auto begin = std::begin(valid);
-    const auto end = std::end(valid);
-    if (find(begin, end, param) != end)
-        member = param;
-    else
-        JAMI_ERR("Invalid parameter \"%s\"", param.c_str());
-}
-
-static void
-updateRange(uint16_t min, uint16_t max, std::pair<uint16_t, uint16_t>& range)
-{
-    if (min > 0 and (max > min) and max <= SIPAccountBase::MAX_PORT - 2) {
-        range.first = min;
-        range.second = max;
-    }
-}
-
-static void
-unserializeRange(const YAML::Node& node,
-                 const char* minKey,
-                 const char* maxKey,
-                 std::pair<uint16_t, uint16_t>& range)
-{
-    int tmpMin = 0;
-    int tmpMax = 0;
-    yaml_utils::parseValue(node, minKey, tmpMin);
-    yaml_utils::parseValue(node, maxKey, tmpMax);
-    updateRange(tmpMin, tmpMax, range);
-}
-
-static void
-addRangeToDetails(std::map<std::string, std::string>& a,
-                  const char* minKey,
-                  const char* maxKey,
-                  const std::pair<uint16_t, uint16_t>& range)
-{
-    a.emplace(minKey, std::to_string(range.first));
-    a.emplace(maxKey, std::to_string(range.second));
-}
-
 void
-SIPAccountBase::serialize(YAML::Emitter& out) const
+SIPAccountBase::loadConfig()
 {
-    Account::serialize(out);
-
-    out << YAML::Key << Conf::AUDIO_PORT_MAX_KEY << YAML::Value << audioPortRange_.second;
-    out << YAML::Key << Conf::AUDIO_PORT_MIN_KEY << YAML::Value << audioPortRange_.first;
-    out << YAML::Key << Conf::DTMF_TYPE_KEY << YAML::Value << dtmfType_;
-    out << YAML::Key << Conf::INTERFACE_KEY << YAML::Value << interface_;
-    out << YAML::Key << Conf::PUBLISH_ADDR_KEY << YAML::Value << publishedIpAddress_;
-    out << YAML::Key << Conf::PUBLISH_PORT_KEY << YAML::Value << publishedPort_;
-    out << YAML::Key << Conf::SAME_AS_LOCAL_KEY << YAML::Value << publishedSameasLocal_;
-
-    out << YAML::Key << VIDEO_ENABLED_KEY << YAML::Value << videoEnabled_;
-    out << YAML::Key << Conf::VIDEO_PORT_MAX_KEY << YAML::Value << videoPortRange_.second;
-    out << YAML::Key << Conf::VIDEO_PORT_MIN_KEY << YAML::Value << videoPortRange_.first;
-
-    out << YAML::Key << Conf::STUN_ENABLED_KEY << YAML::Value << stunEnabled_;
-    out << YAML::Key << Conf::STUN_SERVER_KEY << YAML::Value << stunServer_;
-    out << YAML::Key << Conf::TURN_ENABLED_KEY << YAML::Value << turnEnabled_;
-    out << YAML::Key << Conf::TURN_SERVER_KEY << YAML::Value << turnServer_;
-    out << YAML::Key << Conf::TURN_SERVER_UNAME_KEY << YAML::Value << turnServerUserName_;
-    out << YAML::Key << Conf::TURN_SERVER_PWD_KEY << YAML::Value << turnServerPwd_;
-    out << YAML::Key << Conf::TURN_SERVER_REALM_KEY << YAML::Value << turnServerRealm_;
-}
-
-void
-SIPAccountBase::serializeTls(YAML::Emitter& out) const
-{
-    out << YAML::Key << Conf::CALIST_KEY << YAML::Value << tlsCaListFile_;
-    out << YAML::Key << Conf::CERTIFICATE_KEY << YAML::Value << tlsCertificateFile_;
-    out << YAML::Key << Conf::TLS_PASSWORD_KEY << YAML::Value << tlsPassword_;
-    out << YAML::Key << Conf::PRIVATE_KEY_KEY << YAML::Value << tlsPrivateKeyFile_;
-}
-
-void
-SIPAccountBase::unserialize(const YAML::Node& node)
-{
-    using yaml_utils::parseValue;
-    using yaml_utils::parseValueOptional;
-    using yaml_utils::parseVectorMap;
-
-    Account::unserialize(node);
-
-    parseValue(node, VIDEO_ENABLED_KEY, videoEnabled_);
-
-    parseValue(node, Conf::INTERFACE_KEY, interface_);
-    parseValue(node, Conf::SAME_AS_LOCAL_KEY, publishedSameasLocal_);
-    parseValue(node, Conf::PUBLISH_ADDR_KEY, publishedIpAddress_);
-    IpAddr publishedIp {publishedIpAddress_};
-    if (publishedIp and not publishedSameasLocal_)
+    Account::loadConfig();
+    const auto& conf = config();
+    IpAddr publishedIp {conf.publishedIp};
+    if (not conf.publishedSameasLocal and publishedIp)
         setPublishedAddress(publishedIp);
-
-    int port = sip_utils::DEFAULT_SIP_PORT;
-    parseValue(node, Conf::PUBLISH_PORT_KEY, port);
-    publishedPort_ = port;
-
-    parseValue(node, Conf::DTMF_TYPE_KEY, dtmfType_);
-
-    unserializeRange(node, Conf::AUDIO_PORT_MIN_KEY, Conf::AUDIO_PORT_MAX_KEY, audioPortRange_);
-    unserializeRange(node, Conf::VIDEO_PORT_MIN_KEY, Conf::VIDEO_PORT_MAX_KEY, videoPortRange_);
-
-    // ICE - STUN/TURN
-    if (not isIP2IP()) {
-        parseValue(node, Conf::STUN_ENABLED_KEY, stunEnabled_);
-        parseValue(node, Conf::STUN_SERVER_KEY, stunServer_);
-        parseValue(node, Conf::TURN_ENABLED_KEY, turnEnabled_);
-        parseValue(node, Conf::TURN_SERVER_KEY, turnServer_);
-        parseValue(node, Conf::TURN_SERVER_UNAME_KEY, turnServerUserName_);
-        parseValue(node, Conf::TURN_SERVER_PWD_KEY, turnServerPwd_);
-        parseValue(node, Conf::TURN_SERVER_REALM_KEY, turnServerRealm_);
-    }
-}
-
-void
-SIPAccountBase::setAccountDetails(const std::map<std::string, std::string>& details)
-{
-    Account::setAccountDetails(details);
-
-    parseBool(details, Conf::CONFIG_VIDEO_ENABLED, videoEnabled_);
-
-    // general sip settings
-    parseString(details, Conf::CONFIG_LOCAL_INTERFACE, interface_);
-    parseBool(details, Conf::CONFIG_PUBLISHED_SAMEAS_LOCAL, publishedSameasLocal_);
-    parseString(details, Conf::CONFIG_PUBLISHED_ADDRESS, publishedIpAddress_);
-    parseInt(details, Conf::CONFIG_PUBLISHED_PORT, publishedPort_);
-    IpAddr publishedIp {publishedIpAddress_};
-    if (publishedIp and not publishedSameasLocal_)
-        setPublishedAddress(publishedIp);
-
-    parseString(details, Conf::CONFIG_ACCOUNT_DTMF_TYPE, dtmfType_);
-
-    int tmpMin = -1;
-    parseInt(details, Conf::CONFIG_ACCOUNT_AUDIO_PORT_MIN, tmpMin);
-    int tmpMax = -1;
-    parseInt(details, Conf::CONFIG_ACCOUNT_AUDIO_PORT_MAX, tmpMax);
-    updateRange(tmpMin, tmpMax, audioPortRange_);
-#ifdef ENABLE_VIDEO
-    tmpMin = -1;
-    parseInt(details, Conf::CONFIG_ACCOUNT_VIDEO_PORT_MIN, tmpMin);
-    tmpMax = -1;
-    parseInt(details, Conf::CONFIG_ACCOUNT_VIDEO_PORT_MAX, tmpMax);
-    updateRange(tmpMin, tmpMax, videoPortRange_);
-#endif
-
-    // ICE - STUN
-    parseBool(details, Conf::CONFIG_STUN_ENABLE, stunEnabled_);
-    parseString(details, Conf::CONFIG_STUN_SERVER, stunServer_);
-
-    // ICE - TURN
-    parseBool(details, Conf::CONFIG_TURN_ENABLE, turnEnabled_);
-    parseString(details, Conf::CONFIG_TURN_SERVER, turnServer_);
-    parseString(details, Conf::CONFIG_TURN_SERVER_UNAME, turnServerUserName_);
-    parseString(details, Conf::CONFIG_TURN_SERVER_PWD, turnServerPwd_);
-    parseString(details, Conf::CONFIG_TURN_SERVER_REALM, turnServerRealm_);
-}
-
-std::map<std::string, std::string>
-SIPAccountBase::getAccountDetails() const
-{
-    auto a = Account::getAccountDetails();
-    a.emplace(Conf::CONFIG_VIDEO_ENABLED, videoEnabled_ ? TRUE_STR : FALSE_STR);
-
-    addRangeToDetails(a,
-                      Conf::CONFIG_ACCOUNT_AUDIO_PORT_MIN,
-                      Conf::CONFIG_ACCOUNT_AUDIO_PORT_MAX,
-                      audioPortRange_);
-#ifdef ENABLE_VIDEO
-    addRangeToDetails(a,
-                      Conf::CONFIG_ACCOUNT_VIDEO_PORT_MIN,
-                      Conf::CONFIG_ACCOUNT_VIDEO_PORT_MAX,
-                      videoPortRange_);
-#endif
-
-    a.emplace(Conf::CONFIG_ACCOUNT_DTMF_TYPE, dtmfType_);
-    a.emplace(Conf::CONFIG_LOCAL_INTERFACE, interface_);
-    a.emplace(Conf::CONFIG_PUBLISHED_PORT, std::to_string(publishedPort_));
-    a.emplace(Conf::CONFIG_PUBLISHED_SAMEAS_LOCAL, publishedSameasLocal_ ? TRUE_STR : FALSE_STR);
-    a.emplace(Conf::CONFIG_PUBLISHED_ADDRESS, publishedIpAddress_);
-    a.emplace(Conf::CONFIG_STUN_ENABLE, stunEnabled_ ? TRUE_STR : FALSE_STR);
-    a.emplace(Conf::CONFIG_STUN_SERVER, stunServer_);
-    a.emplace(Conf::CONFIG_TURN_ENABLE, turnEnabled_ ? TRUE_STR : FALSE_STR);
-    a.emplace(Conf::CONFIG_TURN_SERVER, turnServer_);
-    a.emplace(Conf::CONFIG_TURN_SERVER_UNAME, turnServerUserName_);
-    a.emplace(Conf::CONFIG_TURN_SERVER_PWD, turnServerPwd_);
-    a.emplace(Conf::CONFIG_TURN_SERVER_REALM, turnServerRealm_);
-
-    return a;
 }
 
 std::map<std::string, std::string>
@@ -404,14 +219,14 @@ SIPAccountBase::releasePort(uint16_t port) noexcept
 uint16_t
 SIPAccountBase::generateAudioPort() const
 {
-    return acquireRandomEvenPort(audioPortRange_);
+    return acquireRandomEvenPort(config().audioPortRange);
 }
 
 #ifdef ENABLE_VIDEO
 uint16_t
 SIPAccountBase::generateVideoPort() const
 {
-    return acquireRandomEvenPort(videoPortRange_);
+    return acquireRandomEvenPort(config().videoPortRange);
 }
 #endif
 
@@ -421,18 +236,18 @@ SIPAccountBase::getIceOptions() const noexcept
     IceTransportOptions opts;
     opts.upnpEnable = getUPnPActive();
 
-    if (stunEnabled_)
-        opts.stunServers.emplace_back(StunServerInfo().setUri(stunServer_));
-    if (turnEnabled_) {
+    //if (config().stunEnabled)
+    //    opts.stunServers.emplace_back(StunServerInfo().setUri(stunServer_));
+    if (config().turnEnabled) {
         auto cached = false;
         std::lock_guard<std::mutex> lk(cachedTurnMutex_);
         cached = cacheTurnV4_ || cacheTurnV6_;
         if (cacheTurnV4_ && *cacheTurnV4_) {
             opts.turnServers.emplace_back(TurnServerInfo()
                                               .setUri(cacheTurnV4_->toString(true))
-                                              .setUsername(turnServerUserName_)
-                                              .setPassword(turnServerPwd_)
-                                              .setRealm(turnServerRealm_));
+                                              .setUsername(config().turnServerUserName)
+                                              .setPassword(config().turnServerPwd)
+                                              .setRealm(config().turnServerRealm));
         }
         // NOTE: first test with ipv6 turn was not concluant and resulted in multiple
         // co issues. So this needs some debug. for now just disable
@@ -446,10 +261,10 @@ SIPAccountBase::getIceOptions() const noexcept
         // Nothing cached, so do the resolution
         if (!cached) {
             opts.turnServers.emplace_back(TurnServerInfo()
-                                              .setUri(turnServer_)
-                                              .setUsername(turnServerUserName_)
-                                              .setPassword(turnServerPwd_)
-                                              .setRealm(turnServerRealm_));
+                                              .setUri(config().turnServer)
+                                              .setUsername(config().turnServerUserName)
+                                              .setPassword(config().turnServerPwd)
+                                              .setRealm(config().turnServerRealm));
         }
     }
     return opts;
