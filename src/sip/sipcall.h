19f7a394efcf821ba169ee91ce8cc0114250154c
Ezra Pierce <ezra.pierce@savoirfairelinux.com>
2022-11-16 13:13:19 -0500

    SIPCall: make rtpStreams_ thread safe
    Protecting list of rtp streams within SIPCall.
    
    GitLab: #771
    Change-Id: I062e149c17bee3013f0aad4ad001c97bb153ed51

--- src/sip/sipcall.h
+++ src/sip/sipcall.h
@@ -428,6 +428,7 @@ private:
                              const MediaDescription& remoteMedia);
     // Find the stream index with the matching label
     int findRtpStreamIndex(const std::string& label) const;
+    size_t getRtpStreamsSize() const;
 
     std::vector<IceCandidate> getAllRemoteCandidates(IceTransport& transport) const;
 
@@ -516,6 +517,7 @@ private:
     void detachAudioFromConference();
 
     std::mutex setupSuccessMutex_;
+    mutable std::recursive_mutex rtpStreamsMutex_;
 #ifdef ENABLE_VIDEO
     int rotation_ {0};
 #endif
