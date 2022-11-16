0664b2905126d45eff94bf9ca10414430f22f00c
Fadi SHEHADEH <fadi.shehadeh@savoirfairelinux.com>
2022-11-16 16:49:00 -0500

    drt: conv devices
    - changed string to string view for gitsocket function and conv repo management functions
    - getter for conversation's devices
    - git transport update
    - added public to ChannelSocket/ChannelSocketInterface
    - indentation fixes
    
    Change-Id: I3ee87ea09fe4274b14b86f6419ea524e1c6add02

--- src/jamidht/conversationrepository.h
+++ src/jamidht/conversationrepository.h
@@ -45,6 +45,8 @@ using GitIndexConflictIterator
 
 namespace jami {
 
+using DeviceId = dht::PkId;
+
 constexpr auto EFETCH = 1;
 constexpr auto EINVALIDMODE = 2;
 constexpr auto EVALIDFETCH = 3;
@@ -305,7 +307,7 @@ public:
      * @param type      device, members, admins or invited
      * @return the commit id or empty if failed
      */
-    std::string voteUnban(const std::string& uri, const std::string& type);
+    std::string voteUnban(const std::string& uri, const std::string_view type);
     /**
      * Validate if a vote is finished
      * @param uri       identified of the user/device
@@ -314,7 +316,7 @@ public:
      * @return the commit id or empty if failed
      */
     std::string resolveVote(const std::string& uri,
-                            const std::string& type,
+                            const std::string_view type,
                             const std::string& voteType);
 
     /**
@@ -343,6 +345,12 @@ public:
      * @return members
      */
     std::vector<ConversationMember> members() const;
+
+    /**
+     * Get conversation's devices
+     * @return members
+     */
+    std::map<std::string, std::vector<DeviceId>> devices() const;
 
     /**
      * @param filter           If we want to remove one member
@@ -379,12 +387,6 @@ public:
     static std::map<std::string, std::string> infosFromVCard(
         std::map<std::string, std::string>&& details);
 
-    /**
-     * Retrieve account's URI from deviceId
-     * @param deviceId
-     * @return account's URI
-     */
-    std::string uriFromDevice(const std::string& deviceId) const;
     /**
      * Convert ConversationCommit to MapStringString for the client
      */
