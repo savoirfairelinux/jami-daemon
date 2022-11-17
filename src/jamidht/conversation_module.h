a873bd501a4a1bfa9b2c957b667666614f731489
Fadi SHEHADEH <fadi.shehadeh@savoirfairelinux.com>
2022-11-17 13:56:22 -0500

    DRT: implement Distributed Routing Table
    This patch adds the minimal code for the DRT. This component
    should avoid the fact that every peer in a swarm must connects
    to every other peer.
    
    The idea is to get a similar algorithm we use in OpenDHT, meaning
    to store known devices in buckets and try to dynamically connect
    to some of the peers, creating a routing table, so every peer
    in a conversation should connect to a subset of peers.
    
    The big difference is that we must includes mobile devices (for
    OpenDHT push/baterry optimizations can be taken into accounts).
    For this, we consider a special type of node in the DRT that is
    considered as reachable but not connected and stored in the mobile
    nodes. A mobile announces itself as a mobile after bootstraping
    via a message in the swarm channel. In this case, if they disconnect
    the node is moved to a special state (mobile node) instead (known nodes).
    
    A user can know to which devices they can send messages via Routing
    Table's mobile nodes and connected nodes.
    
    Docs: TODO add link
    GitLab: #297
    Change-Id: Ic061c58ca487698afee712cb2eef5e57f9c208fb

--- src/jamidht/conversation_module.h
+++ src/jamidht/conversation_module.h
@@ -36,9 +36,9 @@ class SIPCall;
 
 struct SyncMsg
 {
-    jami::DeviceSync ds;
-    std::map<std::string, jami::ConvInfo> c;
-    std::map<std::string, jami::ConversationRequest> cr;
+    DeviceSync ds;
+    std::map<std::string, ConvInfo> c;
+    std::map<std::string, ConversationRequest> cr;
     // p is conversation's preferences. It's not stored in c, as
     // we can update the preferences without touching any confInfo.
     std::map<std::string, std::map<std::string, std::string>> p;
@@ -62,6 +62,7 @@ public:
                        NeedsSyncingCb&& needsSyncingCb,
                        SengMsgCb&& sendMsgCb,
                        NeedSocketCb&& onNeedSocket,
+                       NeedSocketCb&& onNeedSwarmSocket,
                        UpdateConvReq&& updateConvReqCb);
     ~ConversationModule() = default;
 
@@ -429,6 +430,38 @@ public:
     static std::map<std::string, ConversationRequest> convRequestsFromPath(const std::string& path);
     void addConvInfo(const ConvInfo& info);
     void setConversationMembers(const std::string& convId, const std::vector<std::string>& members);
+
+    /**
+     * Get a conversation
+     * @param convId
+     */
+    std::shared_ptr<Conversation> getConversation(const std::string& convId);
+    /**
+     * Return current git socket used for a conversation
+     * @param deviceId          Related device
+     * @param conversationId    Related conversation
+     * @return the related socket
+     */
+    std::shared_ptr<ChannelSocket> gitSocket(std::string_view deviceId,
+                                             std::string_view convId) const;
+    void removeGitSocket(std::string_view deviceId, std::string_view convId);
+    void addGitSocket(std::string_view deviceId,
+                      std::string_view convId,
+                      const std::shared_ptr<ChannelSocket>& channel);
+    /**
+     * Clear all connection (swarm channels)
+     */
+    void shutdownConnections();
+    /**
+     * Add a swarm connection
+     * @param conversationId
+     * @param socket
+     */
+    void addSwarmChannel(const std::string& conversationId, std::shared_ptr<ChannelSocket> socket);
+    /**
+     * Triggers a bucket maintainance for DRTs
+     */
+    void connectivityChanged();
 
 private:
     class Impl;
