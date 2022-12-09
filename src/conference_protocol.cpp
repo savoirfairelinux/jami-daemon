5a948fdad0844e71a372d1c1d5ca019df007a60d
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2022-12-09 15:01:36 -0500

    wip: conference: homogeneize 1:1 and conference
    Change-Id: I36698c41d3118f852d25c222d23d6d1f67fa8eb5

--- src/conference_protocol.cpp
+++ src/conference_protocol.cpp
@@ -134,7 +134,7 @@ ConfProtocolParser::parseV1()
                     if (deviceValue.isMember(ProtocolKeys::RAISEHAND)) {
                         auto newState = deviceValue[ProtocolKeys::RAISEHAND].asBool();
                         if (peerId_ == accountUri || (!newState && isPeerModerator))
-                            raiseHand_(deviceId, newState);
+                            raiseHand_(peerId_, deviceId, newState);
                     }
                     if (isPeerModerator && deviceValue.isMember(ProtocolKeys::HANGUP)) {
                         hangupParticipant_(accountUri, deviceId);
