4841d479e40df4f1d96bccd87d788f68c49c97f6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2023-01-24 11:31:30 -0500

    swarm-call: avoid duplicate active call on merge
    If two users were starting a call at the same time, activeCalls
    was filled with a duplicate active call. This was due because
    the merge commit re-announce all commits where the linearized
    parent potentially changed, causing a call to be added two times
    in the vector.
    
    Change-Id: Ie419a419c13abcfb44e3c758fa40845f492504ae

--- src/jamidht/conversation.cpp
+++ src/jamidht/conversation.cpp
@@ -271,15 +271,21 @@ public:
             auto confId = commit.at("confId");
             auto uri = commit.at("uri");
             auto device = commit.at("device");
+            std::lock_guard<std::mutex> lk(activeCallsMtx_);
+            auto itActive = std::find_if(activeCalls_.begin(),
+                                            activeCalls_.end(),
+                                            [&](auto value) {
+                                                return value["id"] == confId && value["uri"] == uri
+                                                    && value["device"] == device;
+                                            });
             if (commit.find("duration") == commit.end()) {
-                if (!eraseOnly) {
+                if (itActive == activeCalls_.end() && !eraseOnly) {
                     JAMI_DEBUG(
                         "swarm:{:s} new current call detected: {:s} on device {:s}, account {:s}",
                         convId,
                         confId,
                         device,
                         uri);
-                    std::lock_guard<std::mutex> lk(activeCallsMtx_);
                     std::map<std::string, std::string> activeCall;
                     activeCall["id"] = confId;
                     activeCall["uri"] = uri;
@@ -291,13 +297,6 @@ public:
                                                                                  activeCalls_);
                 }
             } else {
-                std::lock_guard<std::mutex> lk(activeCallsMtx_);
-                auto itActive = std::find_if(activeCalls_.begin(),
-                                             activeCalls_.end(),
-                                             [&](auto value) {
-                                                 return value["id"] == confId && value["uri"] == uri
-                                                        && value["device"] == device;
-                                             });
                 if (itActive != activeCalls_.end()) {
                     activeCalls_.erase(itActive);
                     if (eraseOnly) {
