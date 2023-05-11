a31f21ede39e894d375020339c396a088268d3ad
Fadi SHEHADEH <fadi.shehadeh@savoirfairelinux.com>
2023-05-11 10:25:41 -0400

    transifex: auto translations script
    - script using nllb model for translating
    - pull and push on transifex automaticaly
    
    Change-Id: I885142c0eb842e89ec75f9cc706c71d626c75e5f

---
+++ tools/autotranslate/tools.py
@@ -0,0 +1,139 @@
+"""
+Copyright (C) 2023 Savoir-faire Linux Inc.
+Author: Fadi Shehadeh <fadi.shehadeh@savoirfairelinux.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+from transifex.api import transifex_api
+import datetime
+import os
+import traceback
+import pandas as pd
+
+class bcolors:
+    OK = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+
+
+def printc(text, color=bcolors.OK):
+    print(color + text + bcolors.ENDC)
+
+
+class Transifex:
+    def __init__(self, organization_="savoirfairelinux", project_="jami"):
+        TOKEN = os.environ['TRANSIFEX_TOKEN']
+
+        try:
+            print("Authenticating to Transifex API...")
+            transifex_api.setup(auth=TOKEN)
+            self.organization = transifex_api.Organization.get(slug=organization_)
+            printc(self.organization.slug + " fetched successfully")
+
+            self.project = self.organization.fetch("projects").get(slug=project_)
+            print(self.project.slug)
+
+        except Exception as e:
+            traceback.print_exc()
+            printc(f"Exiting program from transifex {e}", bcolors.FAIL)
+            exit()
+
+    def get_languages(self):
+        self.languages = self.project.fetch("languages")
+        for l in self.languages:
+            print(l.code)
+        return self.languages
+
+    def get_resources(self):
+        self.resources = self.project.fetch("resources")
+        for r in self.resources:
+            print(r.slug)
+        return self.resources
+
+    def get_toTranslate_strings(self, resource_name, languageCode):
+        toTransalte = []
+        translated = []
+        toTranslateCount = 0
+        translatedCount = 0
+        resource = None
+
+        for r in self.resources:
+            if r.slug == resource_name:
+                resource = r
+                break
+
+        try:
+            language = transifex_api.Language.get(code=languageCode)
+            print("Resource fetched")
+            translations = transifex_api.ResourceTranslation.filter(
+                resource=resource, language=language
+            ).include("resource_string")
+
+            for t in translations.all():
+                if t.strings and t.strings.get("other") is not None:
+                    translatedCount += 1
+                    translated.append(t)
+                else:
+                    toTranslateCount += 1
+                    toTransalte.append(t)
+
+            printc(f"{translatedCount} strings translated")
+            printc(f"{toTranslateCount} strings to translate", bcolors.WARNING)
+            return toTransalte, translated
+
+        except Exception as e:
+            traceback.print_exc()
+            printc(f"Problem detected in get_toTranslate_strings: {e}", bcolors.FAIL)
+
+    def create_resource_file(self, resource_name, translated_strings, language):
+        directory = resource_name  # Nom du répertoire à créer
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        if not os.path.exists(directory):
+            os.makedirs(directory)  # Créer le répertoire s'il n'existe pas déjà
+
+        filename = f"{language}_translated_strings_{date}.xlsx"
+        filepath = os.path.join(
+            directory, filename
+        )
+
+        df = pd.DataFrame(
+            {
+                "stringToTranslate": [
+                    item.resource_string.strings["other"] for item in translated_strings
+                ],
+                "translatedString": [
+                    item.strings["other"] for item in translated_strings
+                ],
+            }
+        )
+
+        # Ajouter les informations d'en-tête
+        header = pd.DataFrame(
+            {
+                "Organization": ["savoirfairelinux"],
+                "Project": ["jami"],
+                "Resource": [resource_name],
+                "Date": [datetime.datetime.now().strftime("%Y-%m-%d")],
+                "Time": [datetime.datetime.now().strftime("%H:%M:%S")],
+            }
+        )
+
+        # Ajouter le header et les colonnes au dataframe
+        df = pd.concat([header, df], axis=1)
+
+        # Écrire le DataFrame dans le fichier Excel
+        df.to_excel(filepath, index=False)
