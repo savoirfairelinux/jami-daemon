5e319bdc17d05b8577f40376bd96143f5e5684b4
Matheo Joseph <matheo.joseph@savoirfairelinux.com>
2023-07-25 11:04:52 -0400

    jams: synchronize the blueprint config changes on Jami launch
    Change-Id: I102d66970aaa90764d88e194fa2d3a1b8794bfb2

--- src/jamidht/server_account_manager.cpp
+++ src/jamidht/server_account_manager.cpp
@@ -41,6 +41,7 @@ constexpr std::string_view PATH_DEVICE = JAMI_PATH_AUTH "/device";
 constexpr std::string_view PATH_DEVICES = JAMI_PATH_AUTH "/devices";
 constexpr std::string_view PATH_SEARCH = JAMI_PATH_AUTH "/directory/search";
 constexpr std::string_view PATH_CONTACTS = JAMI_PATH_AUTH "/contacts";
+constexpr std::string_view PATH_BLUEPRINT = JAMI_PATH_AUTH "/policyData";
 
 ServerAccountManager::ServerAccountManager(const std::string& path,
                                            OnAsync&& onAsync,
@@ -461,6 +462,39 @@ ServerAccountManager::syncDevices()
                 } else if (response.status_code == 401)
                     this_.authError(TokenScope::Device);
 
+                this_.clearRequest(response.request);
+            });
+        },
+        logger_));
+}
+
+void
+ServerAccountManager::syncBlueprintConfig(SyncBlueprintCallback onSuccess)
+{
+    auto syncBlueprintCallback = std::make_shared<SyncBlueprintCallback>();
+    *syncBlueprintCallback = std::move(onSuccess);
+    const std::string urlBlueprints = managerHostname_ + PATH_BLUEPRINT;
+    JAMI_WARN("[Auth] syncBlueprintConfig %s", urlBlueprints.c_str());
+    sendDeviceRequest(std::make_shared<Request>(
+        *Manager::instance().ioContext(),
+        urlBlueprints,
+        [syncBlueprintCallback, onAsync = onAsync_](Json::Value json, const dht::http::Response& response) {
+            onAsync([=](AccountManager& accountManager) {
+                JAMI_DBG("[Auth] Got sync request callback with status code=%u", response.status_code);
+                auto& this_ = *static_cast<ServerAccountManager*>(&accountManager);
+                if (response.status_code >= 200 && response.status_code < 300) {
+                    try {
+                        std::map<std::string, std::string> config;
+                        for (auto itr = json.begin(); itr != json.end(); ++itr) {
+                            const auto& name = itr.name();
+                            config.emplace(name, itr->asString());
+                        }
+                        (*syncBlueprintCallback)(config);
+                    } catch (const std::exception& e) {
+                        JAMI_ERR("Error when iterating blueprint config json: %s", e.what());
+                    }
+                } else if (response.status_code == 401)
+                    this_.authError(TokenScope::Device);
                 this_.clearRequest(response.request);
             });
         },
