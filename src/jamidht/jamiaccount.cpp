e82995655b9b334786342532f22800640ff1f35e
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-08-01 11:23:54 -0400

    refactor ConversationModule, add OnMembersChanged callback
    Change-Id: Ibb66889194af618a1666695dd7641a3aca98900a

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -106,9 +106,7 @@ namespace jami {
 constexpr pj_str_t STR_MESSAGE_ID = jami::sip_utils::CONST_PJ_STR("Message-ID");
 static constexpr const char MIME_TYPE_IMDN[] {"message/imdn+xml"};
 static constexpr const char MIME_TYPE_IM_COMPOSING[] {"application/im-iscomposing+xml"};
-static constexpr const char MIME_TYPE_INVITE[] {"application/invite"};
 static constexpr const char MIME_TYPE_INVITE_JSON[] {"application/invite+json"};
-static constexpr const char MIME_TYPE_GIT[] {"application/im-gitmessage-id"};
 static constexpr const char FILE_URI[] {"file://"};
 static constexpr const char VCARD_URI[] {"vcard://"};
 static constexpr const char DATA_TRANSFER_URI[] {"data-transfer://"};
@@ -3433,13 +3431,6 @@ JamiAccount::setActiveCodecs(const std::vector<unsigned>& list)
         setCodecActive(AV_CODEC_ID_VP8);
     }
     config_->activeCodecs = getActiveCodecs(MEDIA_ALL);
-}
-
-// Member management
-void
-JamiAccount::saveMembers(const std::string& convId, const std::vector<std::string>& members)
-{
-    convModule()->setConversationMembers(convId, members);
 }
 
 void
