5e32c8d08525def0abc95d080d78f4cd2e6c0504
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2023-08-12 16:54:17 -0400

    audio input: only resample if required
    Change-Id: I0f6ef10ad5c4575ca820a2ffa83a837ef6df1f2e

--- src/media/audio/audio_input.cpp
+++ src/media/audio/audio_input.cpp
@@ -51,7 +51,7 @@ AudioInput::AudioInput(const std::string& id)
     , deviceGuard_()
     , loop_([] { return true; }, [this] { process(); }, [] {})
 {
-    JAMI_DBG() << "Creating audio input with id: " << id;
+    JAMI_LOG("Creating audio input with id: {} and format {}",  id, format_.toString());
 }
 
 AudioInput::AudioInput(const std::string& id, const std::string& resource)
@@ -133,7 +133,7 @@ AudioInput::readFromDevice()
     }
 
     std::lock_guard<std::mutex> lk(fmtMutex_);
-    if (bufferPool.getInternalAudioFormat() != format_)
+    if (audioFrame->getFormat() != format_)
         audioFrame = resampler_->resample(std::move(audioFrame), format_);
     resizer_->enqueue(std::move(audioFrame));
 
