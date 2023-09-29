4814dbdb6c7a9924c24f10c9845a61333bd73808
Alexander Lussier-Cullen <alexander.lussier-cullen@savoirfairelinux.com>
2023-09-29 09:14:38 -0400

    video_rtp_session: allow socket transmissions for response packets
    Bitrate adjustment (REMB) packets in video calls are sent back on the
    same socket pair as the video itself.
    Video being enabled or disabled should thusly not stop communication
    on these sockets fully as they could still be needed for these packets.
    
    Change-Id: I3789aae44884ad5acfe5e1b014767ff2bf84ba33

--- src/media/video/video_rtp_session.cpp
+++ src/media/video/video_rtp_session.cpp
@@ -217,6 +217,9 @@ VideoRtpSession::startSender()
             rtcpCheckerThread_.start();
         else if (not autoQuality and rtcpCheckerThread_.isRunning())
             rtcpCheckerThread_.join();
+        // Block reads to received feedback packets
+        if(socketPair_)
+            socketPair_->setReadBlockingMode(true);
     }
 }
 
@@ -238,7 +241,7 @@ VideoRtpSession::restartSender()
 }
 
 void
-VideoRtpSession::stopSender()
+VideoRtpSession::stopSender(bool forceStopSocket)
 {
     // Concurrency protection must be done by caller.
 
@@ -255,8 +258,13 @@ VideoRtpSession::stopSender()
         sender_.reset();
     }
 
-    if (socketPair_)
-        socketPair_->stopSendOp();
+    if (socketPair_) {
+        bool const isReceivingVideo = receive_.enabled && !receive_.onHold;
+        if(forceStopSocket || !isReceivingVideo) {
+            socketPair_->stopSendOp();
+            socketPair_->setReadBlockingMode(false);
+        }
+    }
 }
 
 void
@@ -312,7 +320,7 @@ VideoRtpSession::startReceiver()
 }
 
 void
-VideoRtpSession::stopReceiver()
+VideoRtpSession::stopReceiver(bool forceStopSocket)
 {
     // Concurrency protection must be done by caller.
 
@@ -334,8 +342,13 @@ VideoRtpSession::stopReceiver()
     // We need to disable the read operation, otherwise the
     // receiver thread will block since the peer stopped sending
     // RTP packets.
-    if (socketPair_)
-        socketPair_->setReadBlockingMode(false);
+    bool const isSendingVideo = send_.enabled && !send_.onHold;
+    if (socketPair_) {
+        if (forceStopSocket || !isSendingVideo) {
+            socketPair_->setReadBlockingMode(false);
+            socketPair_->stopSendOp();
+        }
+    }
 
     auto ms = receiveThread_->getInfo();
     if (auto ob = recorder_->getStream(ms.name)) {
@@ -343,7 +356,8 @@ VideoRtpSession::stopReceiver()
         recorder_->removeStream(ms);
     }
 
-    receiveThread_->stopLoop();
+    if(forceStopSocket || !isSendingVideo)
+        receiveThread_->stopLoop();
     receiveThread_->stopSink();
 }
 
@@ -409,8 +423,8 @@ VideoRtpSession::stop()
 {
     std::lock_guard<std::recursive_mutex> lock(mutex_);
 
-    stopSender();
-    stopReceiver();
+    stopSender(true);
+    stopReceiver(true);
 
     if (socketPair_)
         socketPair_->interrupt();
