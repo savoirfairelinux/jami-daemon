f30eb0e4b6cfd711f0bd1d3a88acc93739f9aa64
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2024-01-03 22:06:09 -0500

    (WIP) ArchiveAccountManager: add support for p2p
    Change-Id: Ib90430d7a97b33292923f240c235d61f4135572c

---
+++ src/jamidht/auth_channel_handler.h
@@ -0,0 +1,71 @@
+/*
+ *  Copyright (C) 2021-2023 Savoir-faire Linux Inc.
+ *
+ *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+ */
+
+#pragma once
+
+#include "jamidht/channel_handler.h"
+#include "jamidht/jamiaccount.h"
+#include <dhtnet/connectionmanager.h>
+// #include <opendht/crypto.h>
+
+namespace jami {
+
+/**
+ * Manages channels for syncing informations
+ */
+class AuthChannelHandler : public ChannelHandlerInterface
+{
+public:
+    AuthChannelHandler(const std::shared_ptr<JamiAccount>& acc, dhtnet::ConnectionManager& cm);
+    ~AuthChannelHandler();
+
+    /**
+     * Ask for a new sync channel
+     * @param deviceId      The device to connect
+     * @param name          (Unused, generated from deviceId)
+     * @param cb            The callback to call when connected (can be immediate if already connected)
+     */
+    void connect(const DeviceId& deviceId, const std::string&, ConnectCb&& cb) override;
+    void connect(const dht::InfoHash& infoHash, const std::string&, ConnectCallbackLegacy&& cb) override;
+
+    /**
+     * Determine if we accept or not the sync request
+     * @param deviceId      Device who asked
+     * @param name          Name asked
+     * @return if the channel is for a valid conversation and device not banned
+     */
+    bool onRequest(const std::shared_ptr<dht::crypto::Certificate>& peer, const std::string& name) override;
+
+    /**
+     * Launch sync process
+     * @param deviceId      Device who asked
+     * @param name          Name asked
+     * @param channel       Channel used to sync
+     */
+    void onReady(const std::shared_ptr<dht::crypto::Certificate>& peer,
+                 const std::string& name,
+                 std::shared_ptr<dhtnet::ChannelSocket> channel) override;
+
+private:
+    std::weak_ptr<JamiAccount> account_;
+    dhtnet::ConnectionManager& connectionManager_;
+};
+
+} // namespace jami
