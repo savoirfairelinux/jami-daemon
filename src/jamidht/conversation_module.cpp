57c02acb3c542b2e6309d4affd1ca339cb04c626
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2024-01-03 22:06:09 -0500

    (WIP) ArchiveAccountManager: add support for p2p
    Change-Id: Ib90430d7a97b33292923f240c235d61f4135572c

--- src/jamidht/conversation_module.cpp
+++ src/jamidht/conversation_module.cpp
@@ -2233,7 +2233,7 @@ ConversationModule::loadConversationMessages(const std::string& conversationId,
     if (auto conv = pimpl_->getConversation(conversationId)) {
         std::lock_guard lk(conv->mtx);
         if (conv->conversation) {
-            const uint32_t id = std::uniform_int_distribution<uint32_t> {}(acc->rand);
+            const uint32_t id = std::uniform_int_distribution<uint32_t> {1}(acc->rand);
             LogOptions options;
             options.from = fromMessage;
             options.nbOfCommits = n;
@@ -2271,7 +2271,7 @@ ConversationModule::loadConversation(const std::string& conversationId,
     if (auto conv = pimpl_->getConversation(conversationId)) {
         std::lock_guard lk(conv->mtx);
         if (conv->conversation) {
-            const uint32_t id = std::uniform_int_distribution<uint32_t> {}(acc->rand);
+            const uint32_t id = std::uniform_int_distribution<uint32_t> {1}(acc->rand);
             LogOptions options;
             options.from = fromMessage;
             options.nbOfCommits = n;
@@ -2298,7 +2298,7 @@ ConversationModule::loadConversationUntil(const std::string& conversationId,
     if (auto conv = pimpl_->getConversation(conversationId)) {
         std::lock_guard lk(conv->mtx);
         if (conv->conversation) {
-            const uint32_t id = std::uniform_int_distribution<uint32_t> {}(acc->rand);
+            const uint32_t id = std::uniform_int_distribution<uint32_t> {1}(acc->rand);
             LogOptions options;
             options.from = fromMessage;
             options.to = toMessage;
