0b2dc792438afdfc57f2d51adfb3425ee1062b9a
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2024-02-07 15:38:53 -0500

    Add conversation Id to connection request
    Change-Id: I164017db6883bfb22c49fa8f9d5f802bcd8e0a93

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -1993,7 +1993,6 @@ JamiAccount::doRegister_()
                 JAMI_WARN("[Account %s] New channel asked with name %s",
                           getAccountID().c_str(),
                           name.c_str());
-
                 if (this->config().turnEnabled && turnCache_) {
                     auto addr = turnCache_->getResolvedTurn();
                     if (addr == std::nullopt) {
@@ -2212,7 +2211,6 @@ JamiAccount::convModule(bool noCreation)
                         cb({});
                         return;
                     }
-
                     shared->connectionManager_->connectDevice(
                         DeviceId(deviceId),
                         "git://" + deviceId + "/" + convId,
@@ -3197,6 +3195,22 @@ JamiAccount::sendMessage(const std::string& to,
 
                 // Else, ask for a channel and send a DHT message
                 auto payload_type = payloads.cbegin()->first;
+                Json::Value parsed;
+                Json::CharReaderBuilder readerBuilder;
+                std::unique_ptr<Json::CharReader> const reader(readerBuilder.newCharReader());
+                std::string errors;
+
+                bool parsingSuccessful = reader->parse(payloads.cbegin()->second.c_str(),
+                                                       payloads.cbegin()->second.c_str()
+                                                           + payloads.cbegin()->second.size(),
+                                                       &parsed,
+                                                       &errors);
+
+                // Extract the id value
+                if (parsed.isMember("id")) {
+                    auto id = parsed["id"].asString();
+                    payload_type = payload_type + "/" + id;
+                }
                 requestSIPConnection(to, deviceId, payload_type);
                 {
                     std::lock_guard lock(messageMutex_);
@@ -3314,6 +3328,22 @@ JamiAccount::sendMessage(const std::string& to,
         // Set message as not sent in order to be re-triggered
         messageEngine_.onMessageSent(to, token, false, deviceId);
         auto payload_type = payloads.cbegin()->first;
+        Json::Value parsed;
+        Json::CharReaderBuilder readerBuilder;
+        std::unique_ptr<Json::CharReader> const reader(readerBuilder.newCharReader());
+        std::string errors;
+
+        bool parsingSuccessful = reader->parse(payloads.cbegin()->second.c_str(),
+                                               payloads.cbegin()->second.c_str()
+                                                   + payloads.cbegin()->second.size(),
+                                               &parsed,
+                                               &errors);
+
+        // Extract the id value
+        if (parsed.isMember("id")) {
+            auto id = parsed["id"].asString();
+            payload_type = payload_type + "/" + id;
+        }
         requestSIPConnection(to, DeviceId(deviceId), payload_type);
     }
 }
