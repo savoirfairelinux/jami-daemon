c30901fcfa2b2e2da34a59a67e5b37faf9557aaf
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2024-11-09 16:04:26 -0500

    update license
    Change-Id: I8b9e25f99da5945684e265d3923b74065c8ef8eb

--- src/sip/sdes_negotiator.cpp
+++ src/sip/sdes_negotiator.cpp
@@ -1,14 +1,9 @@
 /*
  *  Copyright (C) 2004-2024 Savoir-faire Linux Inc.
  *
- *  Author: Pierre-Luc Bacon <pierre-luc.bacon@savoirfairelinux.com>
- *  Author: Alexandre Savard <alexandre.savard@savoirfairelinux.com>
- *  Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
- *  Author: Mingrui Zhang <mingrui.zhang@savoirfairelinux.com>
- *
- *  This program is free software; you can redistribute it and/or modify
+ *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -17,8 +12,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "sdes_negotiator.h"
