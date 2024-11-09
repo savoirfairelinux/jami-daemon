cd7247c49b84ebdb3fce8570e976840678e341c6
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2024-11-09 17:42:44 -0500

    std::filesystem: always use non-throw version when possible
    Change-Id: Ibc5410929a4f7b5e56433d883763bed5400dd2ea

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -4239,7 +4239,9 @@ JamiAccount::sendFile(const std::string& conversationId,
                                 "Unable to create symlink for file transfer {} - {}. Copy file",
                                 filelinkPath,
                                 path);
-                            if (!std::filesystem::copy_file(path, filelinkPath)) {
+                            std::error_code ec;
+                            auto success = std::filesystem::copy_file(path, filelinkPath, ec);
+                            if (ec || !success) {
                                 JAMI_ERROR("Unable to copy file for file transfer {} - {}",
                                            filelinkPath,
                                            path);
