f89a4880209ba4a32957d633f2cf3d7f8d3d6a6a
ovari123 <ovari123@zoho.com>
2024-11-26 18:57:56 -0400

    misc: unify terminology
    try → attempt
    un-hold → unhold
    won't → unable to
    
    Change-Id: I5a012dd22b40027a9470b42fb7ae0b512619c8ee
    GitLab: https://git.jami.net/savoirfairelinux/jami-client-qt/issues/1730

--- src/media/media_attribute.h
+++ src/media/media_attribute.h
@@ -86,7 +86,7 @@ public:
     bool onHold_ {false};
 
     // NOTE: the hold and mute attributes are related but not
-    // tightly coupled. A hold/un-hold operation should always
+    // tightly coupled. A hold/unhold operation should always
     // trigger a new re-invite to notify the change in media
     // direction.For instance, on an active call, the hold action
     // would change the media direction attribute from "sendrecv"
