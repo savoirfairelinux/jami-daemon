5e8c060a7f07f9b00f65cc9e3cfdf03fb2713fb4
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2025-03-04 16:15:06 -0500

    JamiAccount: use shared_mutex for connManager
    Change-Id: I73eeb7a3c8f09e147a8ca2713028c94de6a72aa7

--- src/jamidht/jamiaccount.h
+++ src/jamidht/jamiaccount.h
@@ -60,6 +60,7 @@
 #include <optional>
 #include <vector>
 #include <filesystem>
+#include <shared_mutex>
 
 #if HAVE_RINGNS
 #include "namedirectory.h"
@@ -789,7 +790,10 @@ private:
 
     pjsip_transport* via_tp_ {nullptr};
 
-    mutable std::mutex connManagerMtx_ {};
+    /** ConnectionManager is thread-safe.
+     * The shared mutex protects the pointer while allowing
+     * multiple threads to access the ConnectionManager concurrently  */
+    mutable std::shared_mutex connManagerMtx_ {};
     std::unique_ptr<dhtnet::ConnectionManager> connectionManager_;
 
     virtual void updateUpnpController() override;
