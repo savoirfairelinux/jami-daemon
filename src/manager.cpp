eec358c1e99b44aab3e161d43e8be7c24b38b60c
Kateryna Kostiuk <kateryna.kostiuk@savoirfairelinux.com>
2025-04-16 11:04:47 -0400

    iOS notification: ensure account is registered
    When a notification is received, the account should be
    registered, regardless of whether we need to load conversations.
    
    Change-Id: Icb68605d0b7bbee94ce4b49ba34e9ff878c66215

--- src/manager.cpp
+++ src/manager.cpp
@@ -3016,10 +3016,6 @@ Manager::loadAccountAndConversation(const std::string& accountId,
         return;
     }
 
-    if (convId.empty() && !loadAll) {
-        // Nothing to load. Only need to start account
-        return;
-    }
     if (auto jamiAcc = std::dynamic_pointer_cast<JamiAccount>(account)) {
         jamiAcc->setActive(true);
         jamiAcc->reloadContacts();
@@ -3029,7 +3025,7 @@ Manager::loadAccountAndConversation(const std::string& accountId,
             convModule->reloadRequests();
             if (loadAll) {
                 convModule->loadConversations();
-            } else {
+            } else if (!convId.empty()) {
                 jamiAcc->loadConversation(convId);
             }
         }
