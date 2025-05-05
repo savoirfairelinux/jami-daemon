7625709a216d661b6114b8f7a2ec782ef3c41e1e
pmagnier-slimani <page.magnier-slimani@savoirfairelinux.com>
2025-05-05 16:00:07 -0400

    autoanswer: allow to choose 2nd call behavior
    Add a setting to choose if the user want 2nd calls to be hanged up
    while already in one auto-answered.
    
    Change-Id: I2149c057827ee9bf678e97ba64c31c8952aaed0c

--- src/account.h
+++ src/account.h
@@ -323,6 +323,10 @@ public:
 
     bool isRendezVous() const { return config().isRendezVous; }
     bool isAutoAnswerEnabled() const { return config().autoAnswerEnabled; }
+    bool isDenySecondCallEnabled() const
+    {
+        return config().denySecondCallEnabled;
+    }
     bool isReadReceiptEnabled() const { return config().sendReadReceipt; }
     bool isComposingEnabled() const { return config().sendComposing; }
 
