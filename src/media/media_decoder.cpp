38ac794295e9075e94bf24b58e1053f66b921350
pknellore <pavankoushik.nellore@savoirfairelinux.com>
2025-06-12 15:15:07 -0400

    optimize SDP input probing for video streams
    Set max_ts_probe and fpsprobesize to 0 for SDP
    video to reduce stream initialization latency.
    
    Change-Id: Idfda66cfc72cb1a9d45c6da4e13c73f82859b1de

--- src/media/media_decoder.cpp
+++ src/media/media_decoder.cpp
@@ -175,11 +175,14 @@ MediaDemuxer::openInput(const DeviceParams& params)
     }
 
     // Ask FFmpeg to open the input using the options set above
-    av_opt_set_int(
-        inputCtx_,
-        "fpsprobesize",
-        1,
-        AV_OPT_SEARCH_CHILDREN); // Don't waste time fetching framerate when finding stream info
+    if (params.disable_dts_probe_delay && params.format == "sdp") {
+        av_opt_set_int(inputCtx_, "max_ts_probe", 0, AV_OPT_SEARCH_CHILDREN);
+        av_opt_set_int(inputCtx_, "fpsprobesize", 0, AV_OPT_SEARCH_CHILDREN);
+    } else {
+        // Don't waste time fetching framerate when finding stream info
+        av_opt_set_int(inputCtx_, "fpsprobesize", 1, AV_OPT_SEARCH_CHILDREN);
+    }
+
     int ret = avformat_open_input(&inputCtx_, input.c_str(), iformat, options_ ? &options_ : NULL);
 
     if (ret) {
