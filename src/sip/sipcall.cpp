7e7d08bd1ca4223fb4635055717c4ede5a65b4cf
pknellore <pavankoushik.nellore@savoirfairelinux.com>
2025-09-03 12:21:36 -0400

    SIPCall: Check readyToRecord_
    Prevents state change detection and recording toggling
    from being lost if reportMediaNegotiationStatus is
    called before the call is ready to record.
    
    Change-Id: I3525c864df54474495cf6e2291903aa666b622e7

--- src/sip/sipcall.cpp
+++ src/sip/sipcall.cpp
@@ -2757,8 +2757,13 @@ SIPCall::reportMediaNegotiationStatus()
         callId,
         libjami::Media::MediaNegotiationStatusEvents::NEGOTIATION_SUCCESS,
         currentMediaList());
+    std::lock_guard lk {mediaStateMutex_};
     auto previousState = isAudioOnly_;
     auto newState = !hasVideo();
+
+    if (!readyToRecord_) {
+        return;
+    }
 
     if (previousState != newState && Call::isRecording()) {
         deinitRecorder();
@@ -3694,7 +3699,7 @@ SIPCall::newIceSocket(unsigned compId)
 void
 SIPCall::rtpSetupSuccess()
 {
-    std::lock_guard lk {setupSuccessMutex_};
+    std::lock_guard lk {mediaStateMutex_};
 
     readyToRecord_ = true; // We're ready to record whenever a stream is ready
 
