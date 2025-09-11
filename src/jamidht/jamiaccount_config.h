9ac55e9af18730e26c3a54eba06773aa87b0c18c
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2025-09-11 17:06:41 -0400

    jamiaccount: disable incoming calls form unknown users by default
    Change-Id: I0c975c992e1233462296e4ca485dc67b179518bd

--- src/jamidht/jamiaccount_config.h
+++ src/jamidht/jamiaccount_config.h
@@ -82,7 +82,7 @@ struct JamiAccountConfig : public SipAccountBaseConfig
     std::string receipt {};
     std::vector<uint8_t> receiptSignature {};
 
-    bool dhtPublicInCalls {true};
+    bool dhtPublicInCalls {false};
 };
 
 } // namespace jami
