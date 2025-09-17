c1735527038370aa6c6aed3338073f0a554f4752
Ilyas Erdogan <ilyas.erdogan@savoirfairelinux.com>
2025-09-17 13:08:26 -0400

    ConversationRepository: implement DST
    An attempt to implement deterministic simulation testing.
    Current state:
    - Generates a specified number of randomly generated events based on a
    non-deterministically randomly generated seed
    - Seed can be reused to reproduce same sequence of events
    - Schedules git operations based on conversation events
    - Make accounts reusable (w/ ability to clear out repos)
    - Signal validation for received messages
    - Signal validation for loading entire conversations
    - Added replayability via unit test configuration for easier sharing and
    debugging
    - Script for simple configuration and running
    
    Change-Id: Id1da25582efe3f2f4e882c69e5ef552cb21f4d28

---
+++ test/dst/main.cpp
@@ -0,0 +1,27 @@
+/*
+ *  Copyright (C) 2026 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "dst.h"
+
+int
+main()
+{
+    jami::test::ConversationDST dst;
+    dst.setUp();
+    dst.tearDown();
+    return 0;
+}
