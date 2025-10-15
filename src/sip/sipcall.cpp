372677519d39f6de024fd9a2b2d9cd25d317747e
ebjarstal <eric.bjarstal@savoirfairelinux.com>
2025-10-15 10:01:15 -0400

    sipcall.cpp: fix jami crash on media update
    This handles the exceptions thrown by createRtpSession in the two unique methods that call it. On failure, the associated Rtp stream is removed from vector of Rtp streams.
    
    Crash scenario example:
    When making a media change request, if peerSupportMultiIce_ == false (for example, if peer's daemon version is old), the new media list will be only the first Audio media (audio_0) and the first Video media (video_0), and will ignore the rest of the list.
    
    However, if no audio media is found in the list, a default MediaAttribute with type_==MEDIA_NONE is added.
    
    Later, when updateAllMediaStreams is called, it will attempt to create an RTP session for this default audio stream with createRtpSession, but it will throw a runtime exception "Unsupported media type" due to stream.mediaAttribute_->type_ == MediaType::MEDIA_NONE.
    
    Since this exception is not handled by updateAllMediaStreams or higher in the call stack, Jami will inevitably crash due to this exception.
    
    Therefore there are two points to address:
    - There needs to be error handling for createRtpSession, which is currently NOT the case
    - When peerSupportMultiIce_ is false and a default audio attribute is added to the media list, it should either still have a type_ == MEDIA_AUDIO, or it should simply not be added to the list
    
    The second point will be addressed in another incoming patch
    
    Change-Id: I14d82640fe0c8f0d84979ac03b1170a74b93a65b

--- src/sip/sipcall.cpp
+++ src/sip/sipcall.cpp
@@ -2014,12 +2014,19 @@ SIPCall::initMediaStreams(const std::vector<MediaAttribute>& mediaAttrList)
 
         addMediaStream(mediaAttr);
         auto& stream = rtpStreams_.back();
-        createRtpSession(stream);
-
-        JAMI_DEBUG("[call:{:s}] Added media @{:d}: {:s}",
-                   getCallId(),
-                   idx,
-                   stream.mediaAttribute_->toString(true));
+        try {
+            createRtpSession(stream);
+            JAMI_DEBUG("[call:{:s}] Added media @{:d}: {:s}",
+                        getCallId(),
+                        idx,
+                        stream.mediaAttribute_->toString(true));
+        } catch (const std::exception& e) {
+            JAMI_ERROR("[call:{:s}] Failed to create RTP session for media @{:d}: {:s}. Ignoring the media",
+                        getCallId(),
+                        idx,
+                        e.what());
+            rtpStreams_.pop_back();
+        }
     }
 
     JAMI_DEBUG("[call:{:s}] Created {:d} media stream(s)", getCallId(), rtpStreams_.size());
@@ -2441,11 +2448,19 @@ SIPCall::updateAllMediaStreams(const std::vector<MediaAttribute>& mediaAttrList,
             auto& stream = rtpStreams_.back();
             // If the remote asks for a new stream, our side sends nothing
             stream.mediaAttribute_->muted_ = isRemote ? true : stream.mediaAttribute_->muted_;
-            createRtpSession(stream);
-            JAMI_DBG("[call:%s] Added a new media stream [%s] @ index %i",
-                     getCallId().c_str(),
-                     stream.mediaAttribute_->label_.c_str(),
-                     streamIdx);
+            try {
+                createRtpSession(stream);
+                JAMI_DEBUG("[call:{:s}] Added a new media stream @{:d}: {:s}",
+                            getCallId(),
+                            idx,
+                            stream.mediaAttribute_->toString(true));
+            } catch (const std::exception& e) {
+                JAMI_ERROR("[call:{:s}] Failed to create RTP session for media @{:d}: {:s}. Ignoring the media",
+                            getCallId(),
+                            idx,
+                            e.what());
+                rtpStreams_.pop_back();
+            }
         } else {
             updateMediaStream(newAttr, streamIdx);
         }
