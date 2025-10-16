1a6db969bc8388578f7cc9dc19aab4225705a491
ebjarstal <eric.bjarstal@savoirfairelinux.com>
2025-10-16 16:42:10 -0400

    audio-loopback: add native pipewire support with capture methods
    This patch implements the PipeWire audio layer with the audio loopback
    functionality. The audio loopback will be used for recording desktop
    audio and sharing it during a screenshare in a call.
    
    https://git.jami.net/savoirfairelinux/jami-client-qt/-/work_items/2102
    Change-Id: I36d16de04576e61ac3a9fd511d7d360b4e56400f

---
+++ src/media/audio/pipewire/pipelayer.h
@@ -0,0 +1,145 @@
+/*
+ *  Copyright (C) 2004-2024 Savoir-faire Linux Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "noncopyable.h"
+#include "logger.h"
+#include "audio/audiolayer.h"
+#include "audio/pipewire/pipeloopbackcapture.h"
+
+#include <list>
+#include <string>
+#include <memory>
+#include <thread>
+#include <sstream>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+#include <spa/utils/hook.h>
+#include <pipewire/core.h>
+extern "C" {
+struct pw_context;
+struct pw_core;
+struct pw_thread_loop;
+struct pw_buffer;
+struct pw_registry;
+struct pw_client;
+struct pw_registry_events;
+struct spa_dict;
+}
+
+namespace jami {
+
+class PipeWireStream;
+struct PwDeviceInfo;
+
+class PipeWireLayer : public AudioLayer 
+{
+public:
+    PipeWireLayer(AudioPreference& pref);
+    ~PipeWireLayer();
+
+    // Implement AudioLayer virtual methods
+    std::vector<std::string> getCaptureDeviceList() const override;
+    std::vector<std::string> getPlaybackDeviceList() const override;
+    void startStream(AudioDeviceType stream = AudioDeviceType::ALL) override;
+
+    /**
+     * Start capturing audio from PipeWire nodes in the 'Stream/Output/Audio' class.
+     * 'Jami Daemon' audio is not captured.
+     * Each captured stream has its own ring buffer that is bound to the audio Rtp session's ring buffer
+     * @param id Id of the ring buffer of the audio Rtp session that will receive/send the captured audio.
+     */
+    void startCaptureStream(const std::string& id) override;
+
+    /**
+     * Stop capturing audio from PipeWire nodes in the 'Stream/Output/Audio' class.
+     * @param id Id of the ring buffer of the audio Rtp session that was receiving/sending the captured audio.
+     */
+    void stopCaptureStream(const std::string& id) override;
+
+    void stopStream(AudioDeviceType stream = AudioDeviceType::ALL) override;
+
+    // PipeWire specific methods
+    void updateDeviceList();
+    /*void readFromMic();
+    void writeToSpeaker();
+    void ringtoneToSpeaker();*/
+    void updatePreference(AudioPreference& preference, int index, AudioDeviceType type) override;
+    std::string getAudioDeviceName(int index, AudioDeviceType type) const override;
+
+private:
+    // PipeWire context and core
+    pw_context* context_;
+    pw_core* core_;
+    pw_thread_loop* loop_;
+    pw_registry* registry_;
+    pw_client *client;
+    spa_hook registry_listener;
+    pw_registry_events registry_events;
+    spa_hook client_listener;
+    std::atomic_uint pendingStreams {0};
+    spa_hook metadata_listener;
+
+    // Streams
+    std::unique_ptr<PipeWireStream> playback_;
+    std::unique_ptr<PipeWireStream> record_;
+    std::unique_ptr<PipeWireStream> ringtone_;
+
+    // Device lists
+    std::vector<PwDeviceInfo> sinkList_;
+    std::vector<PwDeviceInfo> sourceList_;
+    
+    // Synchronization for device enumeration
+    mutable std::mutex deviceListMutex_;
+    std::condition_variable deviceListCv_;
+    std::atomic_bool devicesEnumerated_ {false};
+
+    LoopbackCapture loopbackCapture_;
+
+    // Helper methods
+    void createStream(std::unique_ptr<PipeWireStream>& stream, AudioDeviceType type, const PwDeviceInfo& dev_info, std::function<void(pw_buffer* buf)>&& onData);
+    void disconnectAudioStream();
+    void onStreamReady();
+
+    // Callback methods
+    static void registryEventCallback(void* data, uint32_t id, uint32_t permissions, const char* type, uint32_t version, const struct spa_dict* props);
+    static void registryEventCallbackRemove(void* data, uint32_t id);
+
+    void handleSinkEvent(uint32_t id, const struct spa_dict* props);
+    void handleSourceEvent(uint32_t id, const struct spa_dict* props);
+    void queryDeviceProperties(PwDeviceInfo& deviceInfo, const struct spa_dict* props);
+    /**
+     * Write data from the ring buffer to the hardware and read data
+     * from the hardware.
+     */
+    void readFromMic(pw_buffer* buf);
+    void writeToSpeaker(pw_buffer* buf);
+    void ringtoneToSpeaker(pw_buffer* buf);
+
+    int getAudioDeviceIndex(const std::string& descr, AudioDeviceType type) const override;
+    int getAudioDeviceIndexByName(const std::string& name, AudioDeviceType type) const;
+
+    virtual int getIndexCapture() const override;
+    virtual int getIndexPlayback() const override;
+    virtual int getIndexRingtone() const override;
+
+    //AudioPreference& preference_;
+};
+
+} // namespace jami
