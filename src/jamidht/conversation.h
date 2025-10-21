fc47ced88ffe48208eaf762debd4d30aff549fd4
Ilyas Erdogan <ilyas.erdogan@savoirfairelinux.com>
2025-10-21 12:45:23 -0400

    Conversation: make announce() outside of PImpl
    Change-Id: Iea6ce9938f365bb0d84dca23fddc647cb9a0c312

--- src/jamidht/conversation.h
+++ src/jamidht/conversation.h
@@ -574,6 +574,8 @@ public:
      */
     std::shared_ptr<Typers> typers() const;
 
+    void announce(const std::vector<std::map<std::string, std::string>>& commits, bool commitFromSelf = false);
+
 private:
     std::shared_ptr<Conversation> shared() { return std::static_pointer_cast<Conversation>(shared_from_this()); }
     std::shared_ptr<Conversation const> shared() const
