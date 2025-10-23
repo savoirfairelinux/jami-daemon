ac56058f107f1356375946834688abd9cc8fa81c
Ilyas Erdogan <ilyas.erdogan@savoirfairelinux.com>
2025-10-23 16:54:28 -0400

    ConversationRepository: add merge commit validation
    A recent addition (https://review.jami.net/c/jami-daemon/+/31995) to the
    commit validation logic introduced improper validation for merge
    commits. This patch introduces a new function checkValidMergeCommit()
    which verifies that no unintended files were added in the merge commit
    itself.
    
    Change-Id: I8b69831966e997753c538f56e4e06e3f7707e1dc

--- src/jamidht/conversationrepository.h
+++ src/jamidht/conversationrepository.h
@@ -127,7 +127,11 @@ struct ConversationMember
     MSGPACK_DEFINE(uri, role)
 };
 
-enum class CallbackResult { Skip, Break, Ok };
+enum class CallbackResult {
+    Skip,
+    Break,
+    Ok
+};
 
 using PreConditionCb = std::function<CallbackResult(const std::string&, const GitAuthor&, const GitCommit&)>;
 using PostConditionCb = std::function<bool(const std::string&, const GitAuthor&, ConversationCommit&)>;
