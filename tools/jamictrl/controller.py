de3f5f68aa0eff1feb34fe2301385b7e8d2ea48c
ovari123 <ovari123@zoho.com>
2025-10-28 19:08:49 -0400

    controller.py: fix missing account in response signature
    https://github.com/jchdel/jami-cli/commit/185a3cdfc80141d00f4d56bed553c0b15332b21f
    
    Change-Id: I5508c8b44cc1e9c0f0b634a6471a0388f4ee1a38

--- tools/jamictrl/controller.py
+++ tools/jamictrl/controller.py
@@ -253,22 +253,24 @@ class libjamiCtrl(Thread):
         self.onCallOver_cb()
         del self.activeCalls[callid]
 
-    def onCallStateChanged_cb(self, callid, state, code):
+    def onCallStateChanged_cb(self, account, callid, state, code):
         pass
 
-    def onCallStateChanged(self, callid, state, code):
+    def onCallStateChanged(self, account, callid, state, code):
         """ On call state changed event, set the values for new calls,
         or delete the call from the list of active calls
         """
-        print(("On call state changed " + callid + " " + state))
+        print(("On call state changed " + str(account) + "; " + str(callid) + "; " + str(state) + "; " + str(code)))
 
         if callid not in self.activeCalls:
             print("This call didn't exist!: " + callid + ". Adding it to the list.")
             callDetails = self.getCallDetails(callid)
-            self.activeCalls[callid] = {'Account': callDetails['ACCOUNTID'],
-                                             'To': callDetails['PEER_NUMBER'],
-                                          'State': state,
-                                          'Code': code }
+            print(callDetails)
+            if callDetails is not None:
+                self.activeCalls[callid] = {'Account': callDetails['ACCOUNTID'],
+                                            'To': callDetails['PEER_NUMBER'],
+                                            'State': state,
+                                            'Code': code }
 
         self.currentCallId = callid
 
@@ -291,8 +293,8 @@ class libjamiCtrl(Thread):
         elif state == "INACTIVE":
             self.onCallInactive(callid,state)
         else:
-            print("unknown state:" + str(state))
-        self.onCallStateChanged_cb(callid, state, code)
+            print("Unknown state:" + str(state))
+        self.onCallStateChanged_cb(account, callid, state, code)
 
     def onConferenceCreated_cb(self):
         pass
