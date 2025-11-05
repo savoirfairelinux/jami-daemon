468e1cb2e1232cc097a361660734b19462a590cd
ovari123 <ovari123@zoho.com>
2025-11-04 22:58:47 -0400

    manager.h: update unhold to resume
    GitLab: https://git.jami.net/savoirfairelinux/jami-daemon/-/issues/1154
    Change-Id: I6e4bc166c53450f8f7738a30fa259352a3ae4547

--- src/manager.h
+++ src/manager.h
@@ -1,19 +1,20 @@
 /*
- *  Copyright (C) 2004-2025 Savoir-faire Linux Inc.
+ * Copyright (C) 2004-2025 Savoir-faire Linux Inc.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 #pragma once
 
 #ifdef HAVE_CONFIG_H
@@ -238,10 +239,10 @@ public:
     bool holdConference(const std::string& accountId, const std::string& confId);
 
     /**
-     * Unhold all conference participants
+     * Resume all conference participants
      * @param the conference id
      */
-    bool unHoldConference(const std::string& accountId, const std::string& confId);
+    bool resumeConference(const std::string& accountId, const std::string& confId);
 
     /**
      * Add a subcall to a conference
