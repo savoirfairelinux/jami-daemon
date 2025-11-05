01c74db4b130ce4641318d83bd2efedf7c90ad38
ovari123 <ovari123@zoho.com>
2025-11-04 23:01:13 -0400

    callmanager.cpp: update unhold to resume
    GitLab: https://git.jami.net/savoirfairelinux/jami-daemon/-/issues/1154
    Change-Id: I4677d4f8706d047ea8b31e95df550b6d77ca58b2

--- src/client/callmanager.cpp
+++ src/client/callmanager.cpp
@@ -1,19 +1,20 @@
 /*
- *  Copyright (C) 2004-2025 Savoir-faire Linux Inc.
+ * Copyright (C) 2004-2025 Savoir-faire Linux Inc.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 #include <vector>
 #include <cstring>
 
@@ -129,9 +130,9 @@ hold(const std::string& accountId, const std::string& callId)
 }
 
 bool
-unhold(const std::string& accountId, const std::string& callId)
+resume(const std::string& accountId, const std::string& callId)
 {
-    return jami::Manager::instance().offHoldCall(accountId, callId);
+    return jami::Manager::instance().resumeCall(accountId, callId);
 }
 
 bool
@@ -261,9 +262,9 @@ holdConference(const std::string& accountId, const std::string& confId)
 }
 
 bool
-unholdConference(const std::string& accountId, const std::string& confId)
+resumeConference(const std::string& accountId, const std::string& confId)
 {
-    return jami::Manager::instance().unHoldConference(accountId, confId);
+    return jami::Manager::instance().resumeConference(accountId, confId);
 }
 
 std::map<std::string, std::string>
