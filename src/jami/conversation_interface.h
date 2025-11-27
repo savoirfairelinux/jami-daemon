cb72dc6e2032ca7d323879c3ffc9028230850a52
Ilyas Erdogan <ilyas.erdogan@savoirfairelinux.com>
2025-11-27 16:59:14 -0500

    Conversation: fix announcement of commits
    GitLab: #1140
    Change-Id: I142337516f62fb90209b0690bf28e1b335f2efb5

--- src/jami/conversation_interface.h
+++ src/jami/conversation_interface.h
@@ -42,12 +42,15 @@ struct SwarmMessage
     // Id of the edition commit that last set the current body (empty = no editions yet).
     // Used to correctly tag superseded bodies when new editions arrive.
     std::string latestEditionId;
+    std::string reannounce {"0"};
 
     void fromMapStringString(const std::map<std::string, std::string>& commit)
     {
         id = commit.at("id");
         type = commit.at("type");
+        linearizedParent = commit.at("linearizedParent");
         body = commit; // TODO erase type/id?
+        reannounce = commit.at("reannounce");
     }
 };
 
