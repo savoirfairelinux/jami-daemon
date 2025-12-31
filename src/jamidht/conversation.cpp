7d2a0e22f21ced267fbbcb532a76ba0afdfe38b1
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2025-12-30 20:28:55 -0500

    conversation: listen for member presence
    Change-Id: I28d3f42a5ce0c4ea1979b69a060f9cae7fd180ec

--- src/jamidht/conversation.cpp
+++ src/jamidht/conversation.cpp
@@ -2351,6 +2351,13 @@ Conversation::bootstrap(std::function<void()> onBootstrapped, const std::vector<
     }
 }
 
+void
+Conversation::addKnownDevice(const DeviceId& deviceId)
+{
+    if (pimpl_->swarmManager_)
+        pimpl_->swarmManager_->setKnownNodes({deviceId});
+}
+
 std::vector<std::string>
 Conversation::commitsEndedCalls()
 {
--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -1262,6 +1262,7 @@ JamiAccount::loadAccount(const std::string& archive_password_scheme,
             } else {
                 convModule()->setAccountManager(accountManager_);
             }
+            convModule()->initPresence();
             if (not isEnabled()) {
                 setRegistrationState(RegistrationState::UNREGISTERED);
             }
