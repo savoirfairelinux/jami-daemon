6d0a4949c282a9809206a87fd7a6d2158ce9f047
ovari123 <ovari123@zoho.com>
2026-01-05 22:27:22 -0500

    media_const.h: update unhold to resume
    GitLab: https://git.jami.net/savoirfairelinux/jami-daemon/-/issues/1154
    Change-Id: I84ad57b8930e0ac135c9100e494c77f4282e1faa

--- src/jami/media_const.h
+++ src/jami/media_const.h
@@ -1,19 +1,20 @@
 /*
- *  Copyright (C) 2004-2026 Savoir-faire Linux Inc.
+ * Copyright (C) 2004-2026 Savoir-faire Linux Inc.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+
 #ifndef LIBJAMI_MEDIA_H
 #define LIBJAMI_MEDIA_H
 
@@ -52,7 +53,7 @@ constexpr static char ENABLED[] = "ENABLED";       // bool
 constexpr static char MUTED[] = "MUTED";           // bool
 constexpr static char SOURCE[] = "SOURCE";         // string
 constexpr static char LABEL[] = "LABEL";           // string
-constexpr static char ON_HOLD[] = "ON_HOLD";       // bool
+constexpr static char HOLD[] = "HOLD";             // bool
 } // namespace MediaAttributeKey
 
 namespace MediaAttributeValue {
