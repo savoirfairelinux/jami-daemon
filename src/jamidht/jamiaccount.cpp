9c121b14c9a6d6fffaed1a03c8e344179668e5ea
pknellore <pavankoushik.nellore@savoirfairelinux.com>
2026-02-11 15:48:00 -0500

    manager: add pending accounts
    Change-Id: I81a240aed103519632de0d0bf4c55cc403a16d8b

--- src/jamidht/jamiaccount.cpp
+++ src/jamidht/jamiaccount.cpp
@@ -1102,6 +1102,12 @@ JamiAccount::loadAccount(const std::string& archive_password_scheme,
         return;
 
     JAMI_DEBUG("[Account {:s}] Loading account", getAccountID());
+    const auto scheduleAccountReady = [accountId = getAccountID()] {
+        runOnMainThread([accountId] {
+            auto& manager = Manager::instance();
+            manager.markAccountReady(accountId);
+        });
+    };
     AccountManager::OnChangeCallback callbacks {
         [this](const std::string& uri, bool confirmed) {
             if (!id_.first)
@@ -1257,6 +1263,7 @@ JamiAccount::loadAccount(const std::string& archive_password_scheme,
             if (not isEnabled()) {
                 setRegistrationState(RegistrationState::UNREGISTERED);
             }
+            scheduleAccountReady();
         } else if (isEnabled()) {
             JAMI_WARNING("[Account {}] useIdentity failed!", getAccountID());
             if (not conf.managerUri.empty() and archive_password.empty()) {
@@ -1314,10 +1321,14 @@ JamiAccount::loadAccount(const std::string& archive_password_scheme,
                 fDeviceKey,
                 ip_utils::getDeviceName(),
                 std::move(creds),
-                [w = weak(), this, migrating, hasPassword](const AccountInfo& info,
-                                                           const std::map<std::string, std::string>& config,
-                                                           std::string&& receipt,
-                                                           std::vector<uint8_t>&& receipt_signature) {
+                [w = weak(),
+                 this,
+                 migrating,
+                 hasPassword,
+                 scheduleAccountReady](const AccountInfo& info,
+                                       const std::map<std::string, std::string>& config,
+                                       std::string&& receipt,
+                                       std::vector<uint8_t>&& receipt_signature) {
                     auto sthis = w.lock();
                     if (not sthis)
                         return;
@@ -1388,6 +1399,7 @@ JamiAccount::loadAccount(const std::string& archive_password_scheme,
                         }
                     }
                     doRegister();
+                    scheduleAccountReady();
                 },
                 [w = weak(), id, accountId = getAccountID(), migrating](AccountManager::AuthError error,
                                                                         const std::string& message) {
