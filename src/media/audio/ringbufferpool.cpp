3f9f3f3fe7b1705e36892609b6f33fafa7c5c801
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2026-02-12 14:17:59 -0500

    ringbufferpool: check for null buffer
    Change-Id: I1a24ba28ce0c4ff733f1906c20b77a541f0dab8b

--- src/media/audio/ringbufferpool.cpp
+++ src/media/audio/ringbufferpool.cpp
@@ -247,8 +247,12 @@ RingBufferPool::unBindAllHalfDuplexOut(const std::string& ringbufferId)
 void
 RingBufferPool::unBindAllHalfDuplexIn(const std::string& sourceBufferId)
 {
+    auto ringBuffer = getRingBuffer(sourceBufferId);
+    if (not ringBuffer) {
+        JAMI_ERROR("No ringbuffer associated to id '{}'", sourceBufferId);
+        return;
+    }
     std::lock_guard lk(stateLock_);
-    const std::shared_ptr<RingBuffer>& ringBuffer = getRingBuffer(sourceBufferId);
     const std::vector<std::string>& subscribers = ringBuffer->getSubscribers();
     for (const auto& subscriber : subscribers) {
         removeReaderFromRingBuffer(ringBuffer, subscriber);
