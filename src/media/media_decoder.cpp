a7cf9d71631921451903f04f8a8533fdeb39d39c
pknellore <pavankoushik.nellore@savoirfairelinux.com>
2026-02-18 12:14:01 -0500

    call: request keyframe on timeout
    Change-Id: I4a0c42f0ddd8c7f6d8538f508ca615cc181d2228

--- src/media/media_decoder.cpp
+++ src/media/media_decoder.cpp
@@ -213,15 +213,34 @@ MediaDemuxer::seekFrame(int, int64_t timestamp)
 }
 
 void
-MediaDemuxer::findStreamInfo()
+MediaDemuxer::findStreamInfo(bool videoStream)
 {
     if (not streamInfoFound_) {
         inputCtx_->max_analyze_duration = 30 * AV_TIME_BASE;
+        if (videoStream && keyFrameRequestCb_) {
+            if (!streamInfoTimerScheduled_.exchange(true)) {
+                auto timer = std::make_shared<asio::steady_timer>(*Manager::instance().ioContext());
+                timer->expires_after(std::chrono::milliseconds(1500));
+                timer->async_wait([this, timer](const std::error_code& ec) {
+                    streamInfoTimerScheduled_.store(false);
+                    if (ec)
+                        return;
+                    if (!streamInfoFound_) {
+                        JAMI_DBG("findStreamInfo: 1500ms elapsed, requesting keyframe to aid probing");
+                        if (keyFrameRequestCb_)
+                            keyFrameRequestCb_();
+                    }
+                });
+            }
+        }
+
         int err;
         if ((err = avformat_find_stream_info(inputCtx_, nullptr)) < 0) {
             JAMI_ERR() << "Unable to find stream info: " << libav_utils::getError(err);
         }
         streamInfoFound_ = true;
+        if (streamInfoTimerScheduled_.load())
+            streamInfoTimerScheduled_.store(false);
     }
 }
 
@@ -260,6 +279,12 @@ void
 MediaDemuxer::setFileFinishedCb(std::function<void(bool)> cb)
 {
     fileFinishedCb_ = std::move(cb);
+}
+
+void
+MediaDemuxer::setKeyFrameRequestCb(std::function<void()> cb)
+{
+    keyFrameRequestCb_ = std::move(cb);
 }
 
 void
@@ -497,10 +522,16 @@ MediaDecoder::setIOContext(MediaIOHandle* ioctx)
     demuxer_->setIOContext(ioctx);
 }
 
+void
+MediaDecoder::setKeyFrameRequestCb(std::function<void()> cb)
+{
+    demuxer_->setKeyFrameRequestCb(std::move(cb));
+}
+
 int
 MediaDecoder::setup(AVMediaType type)
 {
-    demuxer_->findStreamInfo();
+    demuxer_->findStreamInfo(type == AVMEDIA_TYPE_VIDEO);
     auto stream = demuxer_->selectStream(type);
     if (stream < 0) {
         JAMI_ERR("No stream found for type %i", static_cast<int>(type));
