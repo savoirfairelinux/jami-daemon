10a2ad2acc959cbc75f4fe08c7eba7c4086cb5d5
ovari123 <ovari123@zoho.com>
2026-02-25 01:05:12 -0500

    transfert (fr) → transfer (en)
    Change-Id: I01e0b8defeffc8c3ca1bb80871ad9d0f3c413874

--- src/conference.cpp
+++ src/conference.cpp
@@ -1,18 +1,18 @@
 /*
- *  Copyright (C) 2004-2026 Savoir-faire Linux Inc.
+ * Copyright (C) 2004-2026 Savoir-faire Linux Inc.
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <regex>
@@ -1495,7 +1495,7 @@ Conference::muteParticipant(const std::string& participant_id, const bool& state
     // the conference merge (we don't send back info to remoteHost,
     // cf. getConfInfoHostUri method)
 
-    // Transfert remote participant mute
+    // Transfer remote participant mute
     auto remoteHost = findHostforRemoteParticipant(participant_id);
     if (not remoteHost.empty()) {
         if (auto call = getCallFromPeerID(string_remove_suffix(remoteHost, '@'))) {
