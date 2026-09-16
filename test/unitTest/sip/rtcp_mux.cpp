ab36cd64b5f60c5f8caa7dae7623756d5670b686
Pavan Koushik Nellore <pavankoushik.nellore@savoirfairelinux.com>
2026-09-16 14:40:45 -0400

    sdp: negotiate generic nack
    Change-Id: I62c81089e390c5cec11cc7e12ecbe695a50cc611

--- test/unitTest/sip/rtcp_mux.cpp
+++ test/unitTest/sip/rtcp_mux.cpp
@@ -471,7 +471,9 @@ RtcpMuxSdpTest::offerAdvertisesPliAndFirForVideoOnly()
     CPPUNIT_ASSERT_EQUAL(2u, localSession->media_count);
 
     CPPUNIT_ASSERT(!hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* nack pli"));
+    CPPUNIT_ASSERT(!hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* nack"));
     CPPUNIT_ASSERT(!hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* ccm fir"));
+    CPPUNIT_ASSERT(hasMediaAttributeValue(localSession->media[1], "rtcp-fb", "* nack"));
     CPPUNIT_ASSERT(hasMediaAttributeValue(localSession->media[1], "rtcp-fb", "* nack pli"));
     CPPUNIT_ASSERT(hasMediaAttributeValue(localSession->media[1], "rtcp-fb", "* ccm fir"));
 
@@ -715,6 +717,7 @@ RtcpMuxSdpTest::answerNegotiatesPliAndFir()
     auto* localSession = sdp.getLocalSdpSession();
     CPPUNIT_ASSERT(localSession);
     CPPUNIT_ASSERT_EQUAL(1u, localSession->media_count);
+    CPPUNIT_ASSERT(hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* nack"));
     CPPUNIT_ASSERT(hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* nack pli"));
     CPPUNIT_ASSERT(hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* ccm fir"));
 
@@ -762,6 +765,7 @@ RtcpMuxSdpTest::answerSkipsPliAndFirWhenOfferDoesNot()
     auto* localSession = sdp.getLocalSdpSession();
     CPPUNIT_ASSERT(localSession);
     CPPUNIT_ASSERT_EQUAL(1u, localSession->media_count);
+    CPPUNIT_ASSERT(!hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* nack"));
     CPPUNIT_ASSERT(!hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* nack pli"));
     CPPUNIT_ASSERT(!hasMediaAttributeValue(localSession->media[0], "rtcp-fb", "* ccm fir"));
 
